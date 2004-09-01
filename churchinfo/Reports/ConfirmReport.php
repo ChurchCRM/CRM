61feab3dd9c56d047c22d1a19687cfc9038c682f
mikewiltwork <mikewiltwork>
2004-09-01 04:08:32 +0000

    Finished implementation

--- churchinfo/Reports/ConfirmReport.php
+++ churchinfo/Reports/ConfirmReport.php
@@ -23,9 +23,9 @@ class PDF_ConfirmReport extends ChurchInfoReport {
 	// Constructor
 	function PDF_ConfirmReport() {
 		parent::FPDF("P", "mm", $this->paperFormat);
-
+		$this->leftX = 10;
 		$this->SetFont("Times",'',10);
-		$this->SetMargins(20,20);
+		$this->SetMargins(10,20);
 		$this->Open();
 		$this->SetAutoPageBreak(false);
 	}
@@ -41,11 +41,10 @@ class PDF_ConfirmReport extends ChurchInfoReport {
 
 	function FinishPage ($curY) {
 		$curY += 2 * $this->incrementY;
-		$blurb = $this->sTaxReport2;
-		$this->WriteAt ($this->leftX, $curY, $blurb);
+		$this->WriteAt ($this->leftX, $curY, $this->sConfirm2);
+
 		$curY += 3 * $this->incrementY;
-		$blurb = $this->sTaxReport3;
-		$this->WriteAt ($this->leftX, $curY, $blurb);
+		$this->WriteAt ($this->leftX, $curY, $this->sConfirm3);
 
 		$curY += 4 * $this->incrementY;
 
@@ -59,10 +58,10 @@ class PDF_ConfirmReport extends ChurchInfoReport {
 $pdf = new PDF_ConfirmReport();
 
 // Get all the families
-$sSQL = "SELECT * FROM family_fam WHERE 1";
+$sSQL = "SELECT * FROM family_fam WHERE 1 ORDER BY fam_Name";
 $rsFamilies = RunQuery($sSQL);
 
-$dataCol = 50;
+$dataCol = 40;
 $dataWid = 50;
 
 // Loop through families
@@ -71,9 +70,106 @@ while ($aFam = mysql_fetch_array($rsFamilies)) {
 
 	$curY = $pdf->StartNewPage ($fam_ID, $fam_Name, $fam_Address1, $fam_Address2, $fam_City, 
                                $fam_State, $fam_Zip, $fam_Country, $iYear);
+	$curY += $pdf->incrementY;
 
+	$pdf->SetFont("Times",'B',10);
    $pdf->WriteAtCell ($pdf->leftX, $curY, $dataCol - $pdf->leftX, gettext ("Family name"));
-   $pdf->WriteAtCell ($dataCol, $curY, $dataWid, $fam_Name);
+	$pdf->SetFont("Times",'',10);
+   $pdf->WriteAtCell ($dataCol, $curY, $dataWid, $fam_Name); $curY += $pdf->incrementY;
+	$pdf->SetFont("Times",'B',10);
+   $pdf->WriteAtCell ($pdf->leftX, $curY, $dataCol - $pdf->leftX, gettext ("Address 1"));
+	$pdf->SetFont("Times",'',10);
+   $pdf->WriteAtCell ($dataCol, $curY, $dataWid, $fam_Address1); $curY += $pdf->incrementY;
+	$pdf->SetFont("Times",'B',10);
+   $pdf->WriteAtCell ($pdf->leftX, $curY, $dataCol - $pdf->leftX, gettext ("Address 2"));
+	$pdf->SetFont("Times",'',10);
+   $pdf->WriteAtCell ($dataCol, $curY, $dataWid, $fam_Address2); $curY += $pdf->incrementY;
+	$pdf->SetFont("Times",'B',10);
+   $pdf->WriteAtCell ($pdf->leftX, $curY, $dataCol - $pdf->leftX, gettext ("City, State, Zip"));
+	$pdf->SetFont("Times",'',10);
+   $pdf->WriteAtCell ($dataCol, $curY, $dataWid, ($fam_City . ", " . $fam_State . "  " . $fam_Zip)); $curY += $pdf->incrementY;
+	$pdf->SetFont("Times",'B',10);
+   $pdf->WriteAtCell ($pdf->leftX, $curY, $dataCol - $pdf->leftX, gettext ("Home phone"));
+	$pdf->SetFont("Times",'',10);
+   $pdf->WriteAtCell ($dataCol, $curY, $dataWid, $fam_HomePhone); $curY += $pdf->incrementY;
+	$pdf->SetFont("Times",'B',10);
+   $pdf->WriteAtCell ($pdf->leftX, $curY, $dataCol - $pdf->leftX, gettext ("Send Newsletter"));
+	$pdf->SetFont("Times",'',10);
+   $pdf->WriteAtCell ($dataCol, $curY, $dataWid, $fam_SendNewsLetter); $curY += $pdf->incrementY;
+	$curY += $pdf->incrementY;
+
+	$sSQL = "SELECT *, cls.lst_OptionName AS sClassName, fmr.lst_OptionName AS sFamRole FROM person_per 
+				LEFT JOIN list_lst cls ON per_cls_ID = cls.lst_OptionID AND cls.lst_ID = 1
+				LEFT JOIN list_lst fmr ON per_fmr_ID = fmr.lst_OptionID AND fmr.lst_ID = 2
+				WHERE per_fam_ID = " . $fam_ID . " ORDER BY per_fmr_ID";
+	$rsFamilyMembers = RunQuery ($sSQL);
+
+	$XName = 10;
+	$XGender = 50;
+	$XRole = 60;
+	$XEmail = 90;
+	$XBirthday = 135;
+	$XCellPhone = 155;
+	$XClassification = 180;
+	$XRight = 208;
+
+	$pdf->SetFont("Times",'B',10);
+   $pdf->WriteAtCell ($XName, $curY, $XGender - $XName, gettext ("Member Name"));
+   $pdf->WriteAtCell ($XGender, $curY, $XRole - $XGender, gettext ("M/F"));
+   $pdf->WriteAtCell ($XRole, $curY, $XEmail - $XRole, gettext ("Role"));
+   $pdf->WriteAtCell ($XEmail, $curY, $XBirthday - $XEmail, gettext ("Email"));
+   $pdf->WriteAtCell ($XBirthday, $curY, $XCellPhone - $XBirthday, gettext ("Birthday"));
+   $pdf->WriteAtCell ($XCellPhone, $curY, $XClassification - $XCellPhone, gettext ("Cell phone"));
+   $pdf->WriteAtCell ($XClassification, $curY, $XRight - $XClassification, gettext ("Class"));
+	$pdf->SetFont("Times",'',10);
+	$curY += $pdf->incrementY;
+
+	while ($aMember = mysql_fetch_array($rsFamilyMembers)) {
+		extract ($aMember);
+		
+		$pdf->WriteAtCell ($XName, $curY, $XGender - $XName, $per_FirstName . " " . $per_MiddleName . " " . $per_LastName);
+		$genderStr = ($per_Gender == 1 ? "M" : "F");
+		$pdf->WriteAtCell ($XGender, $curY, $XRole - $XGender, $genderStr);
+		$pdf->WriteAtCell ($XRole, $curY, $XEmail - $XRole, $sFamRole);
+		$pdf->WriteAtCell ($XEmail, $curY, $XBirthday - $XEmail, $per_Email);
+		if ($per_BirthYear)
+			$birthdayStr = $per_BirthYear . "-" . $per_BirthMonth . "-" . $per_BirthDay;
+		else
+			$birthdayStr = "";
+		$pdf->WriteAtCell ($XBirthday, $curY, $XCellPhone - $XBirthday, $birthdayStr);
+		$pdf->WriteAtCell ($XCellPhone, $curY, $XClassification - $XCellPhone, $per_CellPhone);
+		$pdf->WriteAtCell ($XClassification, $curY, $XRight - $XClassification, $sClassName);
+		$curY += $pdf->incrementY;
+	}
+
+	$curY += $pdf->incrementY;
+
+	$sSQL = "SELECT * FROM person_per WHERE per_fam_ID = " . $fam_ID . " ORDER BY per_fmr_ID";
+	$rsFamilyMembers = RunQuery ($sSQL);
+	while ($aMember = mysql_fetch_array($rsFamilyMembers)) {
+		extract ($aMember);
+
+		// Get the Groups this Person is assigned to
+		$sSQL = "SELECT grp_ID, grp_Name, grp_hasSpecialProps, role.lst_OptionName AS roleName
+				FROM group_grp
+				LEFT JOIN person2group2role_p2g2r ON p2g2r_grp_ID = grp_ID
+				LEFT JOIN list_lst role ON lst_OptionID = p2g2r_rle_ID AND lst_ID = grp_RoleListID
+				WHERE person2group2role_p2g2r.p2g2r_per_ID = " . $per_ID . "
+				ORDER BY grp_Name";
+		$rsAssignedGroups = RunQuery($sSQL);
+		if (mysql_num_rows ($rsAssignedGroups) > 0) {
+			$groupStr = "Assigned groups for " . $per_FirstName . " " . $per_LastName . ": ";
+
+			while ($aGroup = mysql_fetch_array($rsAssignedGroups)) {
+				extract ($aGroup);
+				$groupStr .= $grp_Name . " (" . $roleName . ") ";
+			}
+
+			$pdf->WriteAt ($pdf->leftX, $curY, $groupStr);
+			$curY += 2 * $pdf->incrementY;
+		}
+	}
+	$pdf->FinishPage ($curY);
 }
 
 if ($iPDFOutputType == 1)
