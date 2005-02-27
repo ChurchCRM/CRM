5d2d2dfbb397f2e3ed63cfc43bc8659fd9ea5ed6
mikewiltwork <mikewiltwork>
2005-02-27 03:11:57 +0000

    Need canvasser permission to use the canvass entry form.

--- churchinfo/FamilyView.php
+++ churchinfo/FamilyView.php
@@ -757,9 +757,13 @@ if ($_SESSION['sshowPledges'] || $_SESSION['sshowPayments'])
 
 <?php } ?>
 
+<?php if (!$_SESSION['bCanvasser']) { ?>
+
 <p>
 	<a class="SmallText" href="CanvassEditor.php?FamilyID=<?php echo $fam_ID;?>&FYID=<?php echo $_SESSION['idefaultFY'];?>&linkBack=FamilyView.php?FamilyID=<?php echo $iFamilyID;?>"><?php echo (($_SESSION['idefaultFY'] + 1995) . "/" . ($_SESSION['idefaultFY'] + 1996)) . gettext(" Canvass Entry"); ?></a></font>
 <p>
+
+<?php } ?>
 
 <br>
 
