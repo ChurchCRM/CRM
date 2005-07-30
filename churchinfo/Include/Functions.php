59bfc749d4bf95fd56831e1acfcabae8e95cf04f
timdearborn <timdearborn>
2005-07-30 01:45:25 +0000

    Modified FilterInput function to escape special charaterics, protecting against SQL injection.

--- churchinfo/Include/Functions.php
+++ churchinfo/Include/Functions.php
@@ -212,6 +212,7 @@ function RunQuery($sSQL, $bStopOnError = true)
 
 // Sanitizes user input as a security measure
 // Optionally, a filtering type and size may be specified.  By default, strip any tags from a string.
+// Note that a database connection must already be established for the mysql_real_escape_string function to work.
 function FilterInput($sInput,$type = 'string',$size = 1)
 {
 	if (strlen($sInput) > 0)
@@ -219,15 +220,27 @@ function FilterInput($sInput,$type = 'string',$size = 1)
 		switch($type) {
 			case 'string':
 				// or use htmlspecialchars( stripslashes( ))
-				return strip_tags(trim($sInput));
+				$sInput = strip_tags(trim($sInput));
+				if (get_magic_quotes_gpc())
+        			$sInput = stripslashes($sInput);
+				$sInput = mysql_real_escape_string($sInput);
+				return $sInput;
 			case 'htmltext':
-				return strip_tags(trim($sInput),'<a><b><i><u>');
+				$sInput = strip_tags(trim($sInput),'<a><b><i><u>');
+				if (get_magic_quotes_gpc())
+        			$sInput = stripslashes($sInput);
+				$sInput = mysql_real_escape_string($sInput);
+				return $sInput;
 			case 'char':
-				return substr(trim($sInput),0,$size);
+				$sInput = substr(trim($sInput),0,$size);
+				if (get_magic_quotes_gpc())
+        			$sInput = stripslashes($sInput);
+				$sInput = mysql_real_escape_string($sInput);
+				return $sInput;
 			case 'int':
-				return (int) trim($sInput);
+				return (int) intval(trim($sInput));
 			case 'float':
-				return (float) trim($sInput);
+				return (float) floatval(trim($sInput));
 			case 'date':
 				// Attempts to take a date in any format and convert it to YYYY-MM-DD format
 				return date("Y-m-d",strtotime($sInput));
