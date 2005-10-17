a7d29f2178c3dae3a663276ea4555fcccaa26325
mikewiltwork <mikewiltwork>
2005-10-17 00:57:50 +0000

    Show Lat/Lon values

--- churchinfo/FamilyView.php
+++ churchinfo/FamilyView.php
@@ -3,10 +3,10 @@
  *
  *  filename    : FamilyView.php
  *  last change : 2002-04-18
- *  website     : http://www.infocentral.org
- *  copyright   : Copyright 2001, 2002 Deane Barker, 2003 Chris Gebhardt
+ *  website     : http://www.churchdb.org
+ *  copyright   : Copyright 2001, 2002 Deane Barker, 2003 Chris Gebhardt, 2004-2005 Michael Wilt
  *
- *  InfoCentral is free software; you can redistribute it and/or modify
+ *  ChurchInfo is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -309,6 +309,10 @@ elseif ($next_link_text) {
 			<tr>
 				<td class="TinyLabelColumn"><?php echo gettext("Wedding Date:"); ?></td>
 				<td class="TinyTextColumn"><?php echo FormatDate($fam_WeddingDate,false) ?>				  </td>
+			</tr>
+			<tr>
+				<td class="TinyLabelColumn"><?php echo gettext("Latitude/Longitude"); ?></td>
+				<td class="TinyTextColumn"><?php echo $fam_Latitude . " / ", $fam_Longitude; ?>				  </td>
 			</tr>
 			</table>
 		</td>
