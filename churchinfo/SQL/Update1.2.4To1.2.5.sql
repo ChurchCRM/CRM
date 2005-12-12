0ec236e64394953ef6c9aa0214fb6a6c99666d7a
jsu9728 <jsu9728>
2005-12-12 05:06:34 +0000

    Initial checkin. -Ed Davis

---
+++ churchinfo/SQL/Update1.2.4To1.2.5.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO `config_cfg` VALUES (82, 'bHidePersonAddress', '1', 'boolean', '1', 'Set true to disable entering addresses in Person Editor.  Set false to enable entering addresses in Person Editor.', 'General');
+INSERT IGNORE INTO `config_cfg` VALUES (83, 'bHideFriendDate', '0', 'boolean', '0', 'Set true to disable entering Friend Date in Person Editor.  Set false to enable entering Friend Date in Person Editor.', 'General');
+INSERT IGNORE INTO `config_cfg` VALUES (84, 'bHideFamilyNewsletter', '0', 'boolean', '0', 'Set true to disable management of newsletter subscriptions in the Family Editor.', 'General');
