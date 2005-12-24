0d0547101800e667edb71dd347cf10f20837fbbe
mikewiltwork <mikewiltwork>
2005-12-24 13:23:19 +0000

    no message

---
+++ churchinfo/locale/en_AU/LC_MESSAGES/messages.php
@@ -0,0 +1,38 @@
+<?php
+
+$locale["Zip:"] =
+   "Postcode:";
+$locale["Mobile Phone:"] =
+   "Mobile:";
+$locale["Email:"] =
+   "Email:";
+$locale["Mobile Phone"] =
+   "Mobile";
+$locale["Email"] =
+   "Email";
+$locale["Family Cell Phone"] =
+   "Family Mobile";
+$locale["Personal Cell Phone"] =
+   "Personal Mobile";
+$locale["Check Number:"] =
+   "Cheque Number:";
+$locale["You must enter a valid check number."] =
+   "You must enter a valid cheque number.";
+$locale["Check No"] =
+   "Cheque No";
+$locale["Check"] =
+   "Cheque";
+$locale["ZIP"] =
+   "Postcode";
+$locale["By ZIP/Postal Code"] =
+   "By Postcode";
+$locale["Zip/Postal Code"] =
+   "Postcode";
+$locale["City, State Zip"] =
+   "City, State Postcode";
+$locale["Fall"] =
+   "Autumn";
+$locale["Cell"] =
+   "Mobile";
+
+?>
