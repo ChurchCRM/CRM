f14d7019825c1a8a7d05fd649176ada7cd7fba77
jsu9728 <jsu9728>
2006-02-24 14:40:49 +0000

    Initial commit.  Adding functionality to verify US family addresses. -Ed Davis

---
+++ churchinfo/SQL/Update1.2.5To1.2.6.sql
@@ -0,0 +1,3 @@
+DELETE FROM `config_cfg` WHERE cfg_id > 88 AND cfg_id < 91;
+INSERT IGNORE INTO `config_cfg` VALUES (89, 'sISTusername', 'username', 'text', 'username', 'Intelligent Search Technolgy, Ltd. CorrectAddress Username for https://www.name-searching.com/CaddressASP', 'General');
+INSERT IGNORE INTO `config_cfg` VALUES (90, 'sISTpassword', '', 'text', '', 'Intelligent Search Technolgy, Ltd. CorrectAddress Password for https://www.name-searching.com/CaddressASP', 'General');
