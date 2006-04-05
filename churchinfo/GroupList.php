5f27bc7d72548b33a0864e061a8408e0e0204591
jsu9728 <jsu9728>
2006-04-05 19:36:45 +0000

    New links to add group to cart or remove group from cart.  Modified Files:  	churchinfo/GroupList.php -Ed Davis

--- churchinfo/GroupList.php
+++ churchinfo/GroupList.php
@@ -1,18 +1,25 @@
 <?php
 /*******************************************************************************
- *
- *  filename    : GroupList.php
- *  last change : 2002-03-03
- *  website     : http://www.infocentral.org
- *  copyright   : Copyright 2001, 2002 Deane Barker
- *
- *  InfoCentral is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- ******************************************************************************/
-
+*
+*  filename    : GroupList.php
+*  website     : http://www.churchdb.org
+*  copyright   : Copyright 2001, 2002 Deane Barker
+*
+*
+*  Additional Contributors:
+*  2006 Ed Davis
+*
+*
+*  Copyright Contributors
+*
+*  ChurchInfo is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This file best viewed in a text editor with tabs stops set to 4 characters
+*
+******************************************************************************/
 //Include the function library
 require "Include/Config.php";
 require "Include/Functions.php";
@@ -22,80 +29,104 @@ $sPageTitle = gettext("Group Listing");
 
 require "Include/Header.php";
 
-?>
-
-<?php if ($_SESSION['bManageGroups']) { ?>
-<p align="center">
-	<a href="GroupEditor.php"><?php echo gettext("Add a New Group"); ?></a>
-</p>
-<?php } ?>
-
-<?php
+if ($_SESSION['bManageGroups']) 
+{
+	echo '<p align="center"><a href="GroupEditor.php">';
+	echo gettext("Add a New Group") . '</a></p>';
+}
 
 //Get all the User records
 $sSQL = "SELECT * FROM group_grp ORDER BY grp_Type,grp_Name";
 $rsGroups = RunQuery($sSQL);
 
-?>
+echo '	<center><table cellpadding="4" cellspacing="0" width="70%">
+		<tr class="TableHeader">
+		<td>' . gettext("Name") . '</td>
+		<td align="center">' . gettext("Members") . '</td>
+		<td align="center">' . gettext("Type") . '</td>
+		<td align="center">' . gettext("Add to Cart") . '</td>
+		<td align="center">' . gettext("Remove from Cart") . '</td></tr>';
 
-<table cellpadding="4" align="center" cellspacing="0" width="50%">
+//Set the initial row color
+$sRowClass = "RowColorA";
 
-	<tr class="TableHeader">
-		<td><?php echo gettext("Name"); ?></td>
-		<td align="center"><?php echo gettext("Members"); ?></td>
-		<td align="center"><?php echo gettext("Type"); ?></td>
-	</tr>
+//Loop through the person recordset
+while ($aRow = mysql_fetch_array($rsGroups))
+{
+	extract($aRow);
 
-<?php
+	//Alternate the row color
+	$sRowClass = AlternateRowStyle($sRowClass);
 
-	//Set the initial row color
-	$sRowClass = "RowColorA";
-
-	//Loop through the person recordset
-	while ($aRow = mysql_fetch_array($rsGroups))
-	{
-
-		extract($aRow);
-
-		//Alternate the row color
-		$sRowClass = AlternateRowStyle($sRowClass);
-
-		//Get the count for this group
-		$sSQL = "SELECT Count(*) AS iCount FROM person2group2role_p2g2r WHERE p2g2r_grp_ID = " . $grp_ID;
-		$rsMemberCount = mysql_fetch_array(RunQuery($sSQL));
-		extract($rsMemberCount);
+	//Get the count for this group
+	$sSQL = "SELECT Count(*) AS iCount FROM person2group2role_p2g2r " .
+			"WHERE p2g2r_grp_ID = " . $grp_ID;
+	$rsMemberCount = mysql_fetch_array(RunQuery($sSQL));
+	extract($rsMemberCount);
 		
-
-		//Get the group's type name
-		if ($grp_Type > 0)
-		{
-			$sSQL = "SELECT lst_OptionName FROM list_lst WHERE lst_ID=3 AND lst_OptionID = " . $grp_Type;
-			$rsGroupType = mysql_fetch_array(RunQuery($sSQL));
-			$sGroupType = $rsGroupType[0];
-		}
-		else
-			$sGroupType = gettext("Undefined");
+	//Get the group's type name
+	if ($grp_Type > 0)
+	{
+		$sSQL =	"SELECT lst_OptionName FROM list_lst WHERE " . 
+				"lst_ID=3 AND lst_OptionID = " . $grp_Type;
+		$rsGroupType = mysql_fetch_array(RunQuery($sSQL));
+		$sGroupType = $rsGroupType[0];
+	}
+	else
+		$sGroupType = gettext("Undefined");
 
 		//Display the row
-		?>
-		
-		<tr class="<?php echo $sRowClass ?>">
-			<td><a href="GroupView.php?GroupID=<?php echo $grp_ID ?>"><?php echo $grp_Name ?></a></td>
-			<td align="center"><?php echo $iCount ?></td>
-			<td align="center"><?php echo $sGroupType ?></td>
-		</tr>
-		
-		<?php
-		
+
+		echo '	<tr class="' .$sRowClass. '">
+				<td><a href="GroupView.php?GroupID=' .$grp_ID. '">' .$grp_Name. '</a></td>
+				<td align="center">' .$iCount. '</td>
+				<td align="center">' .$sGroupType. '</td>
+				<td align="center">'; // end echo
+
+		$sSQL =	"SELECT p2g2r_per_ID FROM person2group2role_p2g2r " .
+				"WHERE p2g2r_grp_ID = " . $grp_ID;
+		$rsGroupMembers = RunQuery($sSQL);
+
+		$bNoneInCart = TRUE;
+		$bAllInCart = TRUE;
+		//Loop through the recordset
+		while ($aPeople = mysql_fetch_array($rsGroupMembers))
+		{
+			extract($aPeople);
+
+			if (!isset($_SESSION['aPeopleCart']))
+				$bAllInCart = FALSE; // Cart does not exist.  This person is not in cart.
+			elseif (!in_array($p2g2r_per_ID, $_SESSION['aPeopleCart'], false))
+				$bAllInCart = FALSE; // This person is not in cart.
+			elseif (in_array($p2g2r_per_ID, $_SESSION['aPeopleCart'], false))
+				$bNoneInCart = FALSE; // This person is in the cart
+		}
+
+		if (!$bAllInCart)
+		{
+			// Add to cart ... screen should return to this location
+			// after this group is added to cart
+			echo '<a onclick=saveScrollCoordinates() 
+					href="GroupList.php?AddGroupToPeopleCart=' .$grp_ID. '">' .
+					gettext("Add all") . '</a>';
+		}
+
+		echo '</td><td align="center">';
+
+		if (!$bNoneInCart)
+		{
+			// Add to cart ... screen should return to this location
+			// after this group is removed from cart
+			echo '	<a onclick=saveScrollCoordinates()
+					href="GroupList.php?RemoveGroupFromPeopleCart=' .$grp_ID. '">' .
+					gettext("Remove all") . '</a>';
+		}
+
+		echo '</td>';
 	}
 
 	//Close the table
-	echo "</table>";
+	echo '</table></center>';
 
-//}
-
-?>
-
-<?php
 require "Include/Footer.php";
 ?>
