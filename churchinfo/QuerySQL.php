5380697fd34af96a8aabc9fb2f2f32d13bcc3262
jsu9728 <jsu9728>
2006-05-22 01:48:34 +0000

    Added option to export Free-Text Query results to a CSV file.  Modified Files:  	churchinfo/QuerySQL.php - Ed Davis

--- churchinfo/QuerySQL.php
+++ churchinfo/QuerySQL.php
@@ -38,10 +38,22 @@ else
 	$sSQL = "";
 }
 
+if (isset($_POST["CSV"]))
+{
+
+    ExportQueryResults();
+    exit;
+}
+
 require "Include/Header.php";
 ?>
 
 <form method="post">
+
+<center><table><tr>
+    <td class="LabelColumn"> <?php echo gettext("Export Results to CSV file") ?> </td>
+    <td class="TextColumn"><input name="CSV" type="checkbox" id="CSV" value="1"></td>
+</tr></table></center>
 
 <p align="center">
 	<textarea style="font-family:courier,fixed; font-size:9pt; padding:1;" cols="60" rows="10" name="SQL"><?php echo $sSQL; ?></textarea>
@@ -57,12 +69,67 @@ require "Include/Header.php";
 
 if (isset($_POST["SQL"]))
 {
-
 	if (strtolower(substr($sSQL,0,6)) == "select")
 	{
 		RunFreeQuery();
 	}
 }
+
+
+function ExportQueryResults()
+{
+
+	global $cnInfoCentral;
+	global $aRowClass;
+	global $rsQueryResults;
+	global $sSQL;
+	global $iQueryID;
+
+
+    $sCSVstring = "";
+	
+	//Run the SQL
+	$rsQueryResults = RunQuery($sSQL);
+
+	if (mysql_error() != "")
+	{
+		$sCSVstring = gettext("An error occured: ") . mysql_errno() . "--" . mysql_error();
+	}
+	else
+	{
+
+		//Loop through the fields and write the header row
+		for ($iCount = 0; $iCount < mysql_num_fields($rsQueryResults); $iCount++)
+		{
+            $sCSVstring .= mysql_field_name($rsQueryResults,$iCount) . ",";
+		}
+
+        $sCSVstring .= "\n";
+
+		//Loop through the recordsert
+		while($aRow =mysql_fetch_array($rsQueryResults))
+		{
+			//Loop through the fields and write each one
+			for ($iCount = 0; $iCount < mysql_num_fields($rsQueryResults); $iCount++)
+			{
+				$sCSVstring .= $aRow[$iCount] . ",";
+			}
+
+			$sCSVstring .= "\n";
+		}
+	}
+
+    header("Content-type: application/csv");
+	header("Content-Disposition: attachment; filename=Query-" . date("Ymd-Gis") . ".csv");
+	header("Content-Transfer-Encoding: binary");
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+	header('Pragma: public'); 
+	echo $sCSVstring;
+    exit;
+
+}
+
 
 function RunFreeQuery()
 {
@@ -86,17 +153,19 @@ function RunFreeQuery()
 
 		$sRowClass = "RowColorA";
 
-		echo "<table align=\"center\" cellpadding=\"5\" cellspacing=\"0\">";
+		echo '<table align="center" cellpadding="5" cellspacing="0">';
 
-		echo "<tr class=\"" . $sRowClass . "\">";
+		echo '<tr class="' . $sRowClass . '">';
 
 		//Loop through the fields and write the header row
 		for ($iCount = 0; $iCount < mysql_num_fields($rsQueryResults); $iCount++)
 		{
-			echo "<td align=\"center\"><b>" . mysql_field_name($rsQueryResults,$iCount) . "</b></td>";
+            echo '  <td align="center">
+                        <b>' . mysql_field_name($rsQueryResults,$iCount) . '</b>
+                    </td>';
 		}
 
-		echo "</tr>";
+		echo '</tr>';
 
 		//Loop through the recordsert
 		while($aRow =mysql_fetch_array($rsQueryResults))
@@ -104,21 +173,21 @@ function RunFreeQuery()
 
 			$sRowClass = AlternateRowStyle($sRowClass);
 
-			echo "<tr class=\"" . $sRowClass . "\">";
+			echo '<tr class="' . $sRowClass . '">';
 
 			//Loop through the fields and write each one
 			for ($iCount = 0; $iCount < mysql_num_fields($rsQueryResults); $iCount++)
 			{
-				echo "<td align=\"center\">" . $aRow[$iCount] . "</td>";
+				echo '<td align="center">' . $aRow[$iCount] . '</td>';
 			}
 
-			echo "</tr>";
+			echo '</tr>';
 
 		}
 
-		echo "</table>";
+		echo '</table>';
 
-		echo "<br><p class=\"ShadedBox\" style=\"border-style: solid; margin-left: 50px; margin-right: 50 px; border-width: 1px;\"><span class=\"SmallText\">" . str_replace(Chr(13),"<br>",htmlspecialchars($sSQL)) . "</span></p>";
+		echo '<br><p class="ShadedBox" style="border-style: solid; margin-left: 50px; margin-right: 50 px; border-width: 1px;"><span class="SmallText">' . str_replace(Chr(13),"<br>",htmlspecialchars($sSQL)) . '</span></p>';
 	}
 }
 
