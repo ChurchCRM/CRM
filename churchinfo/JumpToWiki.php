57e481c95b87bae4ddd92f1c3d59c9055263e824
mikewiltwork <mikewiltwork>
2006-06-11 11:51:35 +0000

    Small file to open a new window with the wiki documentation

---
+++ churchinfo/JumpToWiki.php
@@ -0,0 +1,4 @@
+<script language="javascript" type="text/javascript">
+	window.open('http://www.churchdb.org/dokuwiki-2006-03-09/doku.php','ChurchInfo Wiki Documentation','')
+	history.go (-1);
+</script>
