3ee607fec52bd97663ae82f2ab53e5af4dc328a0
mikewiltwork <mikewiltwork>
2008-04-24 11:19:44 +0000

    Added option to translate the menu settings, fixed back-up and restore to process tables affected by this upgrade

--- churchinfo/AutoUpdate/Update1_2_10To1_2_11.php
+++ churchinfo/AutoUpdate/Update1_2_10To1_2_11.php
@@ -19,6 +19,7 @@
 *  This file best viewed in a text editor with tabs stops set to 4 characters
 *
 ******************************************************************************/
+
 $sVersion = '1.2.11';
 
 for (; ; ) {    // This is not a loop but a section of code to be 
@@ -32,90 +33,27 @@ for (; ; ) {    // This is not a loop but a section of code to be
 // Make a backup copy of config_cfg before making changes to the table.  This
 // makes it possible to recover from an error.
 
-$sSQL = "DROP TABLE IF EXISTS `tempconfig_tcfg`";
-RunQuery($sSQL, TRUE);
-
-$sSQL = "CREATE TABLE `tempconfig_tcfg` (
-  `cfg_id` int(11) NOT NULL default '0',
-  `cfg_name` varchar(50) NOT NULL default '',
-  `cfg_value` text,
-  `cfg_type` enum('text','number','date','boolean','textarea') NOT NULL default 'text',
-  `cfg_default` text NOT NULL,
-  `cfg_tooltip` text NOT NULL,
-  `cfg_section` varchar(50) NOT NULL default '',
-  PRIMARY KEY  (`cfg_id`),
-  UNIQUE KEY `tcfg_name` (`cfg_name`),
-  KEY `cfg_id` (`cfg_id`)
-) ENGINE=MyISAM";
-RunQuery($sSQL, TRUE);
-
-$sSQL = "INSERT INTO `tempconfig_tcfg` 
-SELECT `cfg_id`,`cfg_name`,`cfg_value`,`cfg_type`,`cfg_default`,`cfg_tooltip`,`cfg_section`
-FROM `config_cfg` ORDER BY `cfg_id`";
-RunQuery($sSQL, TRUE);
-
-// Make a backup copy of list_lst before making changes to the table.  This
-// makes it possible to recover from an error.
-
-$sSQL = "DROP TABLE IF EXISTS `templist_tlst`";
-RunQuery($sSQL, TRUE);
-
-$sSQL = "CREATE TABLE IF NOT EXISTS `templist_tlst` (
-  `lst_ID` mediumint(8) unsigned NOT NULL default '0',
-  `lst_OptionID` mediumint(8) unsigned NOT NULL default '0',
-  `lst_OptionSequence` tinyint(3) unsigned NOT NULL default '0',
-  `lst_OptionName` varchar(50) NOT NULL default ''
-) ENGINE=MyISAM";
-RunQuery($sSQL, TRUE);
-
-$sSQL = "INSERT INTO `templist_tlst` 
-SELECT `lst_ID`,`lst_OptionID`,`lst_OptionSequence`,`lst_OptionName`
-FROM `list_lst` ORDER BY `lst_id`, `lst_OptionID`";
-RunQuery($sSQL, TRUE);
-
-// Make a backup copy of userconfig_ucfg before making changes to the table.  This
-// makes it possible to recover from an error.
-
-$sSQL = "DROP TABLE IF EXISTS `tempuserconfig_tucfg`";
-RunQuery($sSQL, TRUE);
-
-$sSQL = "CREATE TABLE IF NOT EXISTS `tempuserconfig_tucfg` (
-  `ucfg_per_id` mediumint(9) unsigned NOT NULL,
-  `ucfg_id` int(11) NOT NULL default '0',
-  `ucfg_name` varchar(50) NOT NULL default '',
-  `ucfg_value` text,
-  `ucfg_type` enum('text','number','date','boolean','textarea') NOT NULL default 'text',
-  `ucfg_tooltip` text NOT NULL,
-  `ucfg_permission` enum('FALSE','TRUE') NOT NULL default 'FALSE',
-  PRIMARY KEY  (`ucfg_per_ID`,`ucfg_id`)
-) ENGINE=MyISAM";
-RunQuery($sSQL, TRUE);
-
-$sSQL = "INSERT INTO `tempuserconfig_tucfg` 
-SELECT `ucfg_per_id`,`ucfg_id`,`ucfg_name`,`ucfg_value`, `ucfg_type`, `ucfg_tooltip`, `ucfg_permission`
-FROM `userconfig_ucfg` ORDER BY `ucfg_per_ID`, `ucfg_id`";
-RunQuery($sSQL, TRUE);
-
-// Make a backup copy of person_custom_master before making changes to the table.  This
-// makes it possible to recover from an error.
-
-$sSQL = "DROP TABLE IF EXISTS `temp_person_custom_master`";
-RunQuery($sSQL, TRUE);
-
-$sSQL = "CREATE TABLE IF NOT EXISTS `temp_person_custom_master` (
-  `custom_Order` smallint(6) NOT NULL default '0',
-  `custom_Field` varchar(5) NOT NULL default '',
-  `custom_Name` varchar(40) NOT NULL default '',
-  `custom_Special` mediumint(8) unsigned default NULL,
-  `custom_Side` enum('left','right') NOT NULL default 'left',
-  `type_ID` tinyint(4) NOT NULL default '0'
-) ENGINE=MyISAM";
-RunQuery($sSQL, TRUE);
-
-$sSQL = "INSERT INTO `temp_person_custom_master` 
-SELECT `custom_Order`, `custom_Field`, `custom_Name`, `custom_Special`, `custom_Side`, `type_ID` 
-FROM `person_custom_master` ";
-RunQuery($sSQL, TRUE);
+// Need to back up tables we will be modifying- query_qry, queryparameteroptions_qpo, and menuconfig_mcf
+$sSQL = "DROP TABLE IF EXISTS query_qry_backup"; 
+if (!RunQuery($sSQL, FALSE))
+	break;
+$sSQL = "CREATE TABLE query_qry_backup SELECT * FROM query_qry"; 
+if (!RunQuery($sSQL, FALSE))
+	break;
+	
+$sSQL = "DROP TABLE IF EXISTS queryparameteroptions_qpo_backup"; 
+if (!RunQuery($sSQL, FALSE))
+	break;
+$sSQL = "CREATE TABLE queryparameteroptions_qpo_backup SELECT * FROM queryparameteroptions_qpo"; 
+if (!RunQuery($sSQL, FALSE))
+	break;
+	
+$sSQL = "DROP TABLE IF EXISTS menuconfig_mcf_backup"; 
+if (!RunQuery($sSQL, FALSE))
+	break;
+$sSQL = "CREATE TABLE menuconfig_mcf_backup SELECT * FROM menuconfig_mcf"; 
+if (!RunQuery($sSQL, FALSE))
+	break;
 
 // ********************************************************
 // ********************************************************
@@ -153,6 +91,14 @@ $sSQL = "UPDATE `queryparameteroptions_qpo` SET `qpo_Value` = 'fam_City' WHERE `
 if (!RunQuery($sSQL, FALSE))
 	break;
 
+$sSQL = "ALTER TABLE menuconfig_mcf ADD `content_english` varchar(100) NOT NULL AFTER ismenu"; 
+if (!RunQuery($sSQL, FALSE))
+	break;
+
+$sSQL = "UPDATE menuconfig_mcf SET content_english=content"; 
+if (!RunQuery($sSQL, FALSE))
+	break;
+	
 // If we got this far it means all queries ran without error.  It is okay to update
 // the version information.
 
@@ -175,25 +121,33 @@ $sSQL_Last = $sSQL;
     if ($ver_version == $sVersion) {
         // We're good.  Clean up by dropping the
         // temporary tables
-        $sSQL  = "DROP TABLE IF EXISTS `tempconfig_tcfg`, `templist_tlst`, ";
-        $sSQL .= "`tempuserconfig_tucfg`, `temp_person_custom_master`";
+    	$sSQL = "DROP TABLE IF EXISTS query_qry_backup";
         RunQuery($sSQL, TRUE);
-
+    	
+    	$sSQL = "DROP TABLE IF EXISTS queryparameteroptions_qpo_backup";
+        RunQuery($sSQL, TRUE);
+    	
+        $sSQL = "DROP TABLE IF EXISTS menuconfig_mcf_backup";
+        RunQuery($sSQL, TRUE);
+    	
     } else {
         // An error occured.  Clean up by restoring
         // tables to their original condition by using
         // the temporary tables.
 
-        $sSQL  = "DROP TABLE IF EXISTS `config_cfg`, `list_lst`, ";
-        $sSQL .= "`userconfig_ucfg`, `person_custom_master`";
+    	$sSQL = "DROP TABLE IF EXISTS query_qry";
         RunQuery($sSQL, TRUE);
-
-        $sSQL  = "RENAME TABLE `tempconfig_tcfg`           TO `config_cfg`, ";
-        $sSQL .= "             `templist_tlst`             TO `list_lst`, ";
-        $sSQL .= "             `tempuserconfig_tucfg`      TO `userconfig_ucfg`, ";
-        $sSQL .= "             `temp_person_custom_master` TO `person_custom_master`";
+    	
+    	$sSQL = "DROP TABLE IF EXISTS queryparameteroptions_qpo";
         RunQuery($sSQL, TRUE);
-
+    	
+        $sSQL = "DROP TABLE IF EXISTS menuconfig_mcf";
+        RunQuery($sSQL, TRUE);
+    	
+        $sSQL  = "RENAME TABLE `query_qry_backup`                 TO `query_qry`, ";
+        $sSQL .= "             `queryparameteroptions_qpo_backup` TO `queryparameteroptions_qpo`, ";
+        $sSQL .= "             `menuconfig_mcf_backup`            TO `menuconfig_mcf`";
+        RunQuery($sSQL, TRUE);
     }
 
 $sSQL = $sSQL_Last;
