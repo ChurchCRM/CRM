e30d06a1b5c9aa911ce7820f6bdecee850a6fbb6
mikewiltwork <mikewiltwork>
2008-04-24 13:22:07 +0000

    Update the version for 1.2.11

--- churchinfo/SQL/Install.sql
+++ churchinfo/SQL/Install.sql
@@ -1276,7 +1276,7 @@ CREATE TABLE `version_ver` (
 -- 
 
 INSERT INTO `version_ver` (`ver_version`, `ver_date`) VALUES 
-('1.2.10', NOW() );
+('1.2.11', NOW() );
 
 -- --------------------------------------------------------
 
