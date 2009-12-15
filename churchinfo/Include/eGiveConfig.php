208ffc105ea5c937b1069d289d2e44c6b6873422
kirbyb <kirbyb>
2009-12-15 15:29:33 +0000

    *** empty log message ***

--- churchinfo/Include/eGiveConfig.php
+++ churchinfo/Include/eGiveConfig.php
@@ -25,9 +25,9 @@ $defaultFundId = 5; // Misc
 
 // 64 character 'alpha' key provided by eGive
 //              0123456789012345678901234567890123456789012345678901234567890123
-$eGiveApiKey = "0000000000000000000000000000000000000000000000000000000000000000";
+$eGiveApiKey = "6ebef5c8c0848a7091ba23f68d06770c1476ccc169f2ade3be2952fc03de4cd7";
 $eGiveURL = "https://www.egive-usa.com";
 
 // specific number for your church
-$eGiveOrgID = 000;
+$eGiveOrgID = 13821;
 ?>
