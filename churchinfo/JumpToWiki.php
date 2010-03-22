e860861cc70be1e627b541f67430f43cfefd2013
mikewiltwork <mikewiltwork>
2010-03-22 14:09:06 +0000

    Fixed the link to the wiki

--- churchinfo/JumpToWiki.php
+++ churchinfo/JumpToWiki.php
@@ -1,4 +1,4 @@
 <script language="javascript" type="text/javascript">
-	window.open('http://www.churchdb.org/dokuwiki-2006-03-09/doku.php','ChurchInfo Wiki Documentation','')
+	window.open('http://www.churchdb.org/dokuwiki-2007-06-26b/doku.php','ChurchInfo Wiki Documentation','')
 	history.go (-1);
 </script>
