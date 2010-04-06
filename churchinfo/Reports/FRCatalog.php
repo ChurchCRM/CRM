2fc59f7e7d9d2d4c146c7ec5ece49e8b7a23c029
mikewiltwork <mikewiltwork>
2010-04-06 01:34:38 +0000

    Updated and debugged.  This is the first working version.

--- churchinfo/Reports/FRCatalog.php
+++ churchinfo/Reports/FRCatalog.php
@@ -20,8 +20,25 @@ require "../Include/ReportConfig.php";
 
 $iCurrentFundraiser = $_GET["CurrentFundraiser"];
 
-class PDF_FRCatalogReport extends ChurchInfoReport {
+$wid_ID = 12;
+$wid_Name = 25;
+$wid_MinBid = 15;
+$wid_EstValue = 15;
+$wid_Description = 100;
 
+$topMargin = 25;
+$leftMargin = 20;
+
+$leftID = $leftMargin;
+$leftName = $leftID+$wid_ID;
+$leftMinBid = $leftName + $wid_Name;
+$leftEstValue = $leftMinBid + $wid_MinBid;
+$leftDescription = $leftEstValue + $wid_EstValue;
+
+$curY = $topMargin;
+$lineIncr = 5;
+
+class PDF_FRCatalogReport extends ChurchInfoReport {
 	// Constructor
 	function PDF_FRCatalogReport() {
 		parent::FPDF("P", "mm", $this->paperFormat);
@@ -29,19 +46,37 @@ class PDF_FRCatalogReport extends ChurchInfoReport {
 		$this->SetFont("Times",'',10);
 		$this->SetMargins(10,20);
 		$this->Open();
-		$this->SetAutoPageBreak(true);
+		$this->AddPage ();
+		$this->SetAutoPageBreak(true, 25);
+	}
+	
+	function AddPage () {
+		global $wid_ID,$wid_Name,$wid_MinBid,$wid_EstValue,$wid_Description;
+		global $topMargin,$leftMargin,$leftID,$leftName,$leftMinBid,$leftEstValue,$leftDescription;
+		global $curY, $lineIncr;
+		global $fr_title, $fr_description;
+		
+		parent::AddPage();
+    	$curY = $topMargin;
+		
+    	$this->SetFont("Times",'B',16);
+    	$this->WriteAt ($leftMargin, $curY, $fr_title);
+		$curY += 8;
+		$this->WriteAt ($leftMargin, $curY, $fr_description);
+		$curY += 8;
+    	
+    	$this->SetFont("Times",'B',10);
+		$this->WriteAtCell ($leftID, $curY, $wid_ID, gettext ("Item"),0,"L");
+		$this->WriteAtCell ($leftName, $curY, $wid_Name, gettext ("Name"),0,"L");
+		$this->WriteAtCell ($leftMinBid, $curY, $wid_MinBid, gettext ("Min bid"),0,"L");
+		$this->WriteAtCell ($leftEstValue, $curY, $wid_EstValue, gettext ("Est value"),0,"L");
+		$this->WriteAtCell ($leftDescription, $curY, $wid_Description, gettext ("Description"),0,"L");
+		$curY += $lineIncr;
+		$this->SetY ($curY);
+    	$this->SetFont("Times",'',10);
 	}
 }
 
-$pdf = new PDF_FRCatalogReport();
-
-// Read in report settings from database
-$rsConfig = mysql_query("SELECT cfg_name, IFNULL(cfg_value, cfg_default) AS value FROM config_cfg WHERE cfg_section='ChurchInfoReport'");
-if ($rsConfig) {
-	while (list($cfg_name, $cfg_value) = mysql_fetch_row($rsConfig)) {
-		$pdf->$cfg_name = $cfg_value;
-	}
-}
 
 // Get the information about this fundraiser
 $sSQL = "SELECT * FROM fundraiser_fr WHERE fr_ID=".$iCurrentFundraiser;
@@ -53,51 +88,52 @@ extract ($thisFR);
 $sSQL = "SELECT * FROM donateditem_di WHERE di_FR_ID=".$iCurrentFundraiser." ORDER BY di_item";
 $rsItems = RunQuery($sSQL);
 
-$wid_ID = 12;
-$wid_Name = 25;
-$wid_MinBid = 15;
-$wid_EstValue = 15;
-$wid_Description = 100;
+$pdf = new PDF_FRCatalogReport();
+$pdf->SetTitle ($fr_title);
 
-$topMargin = 25;
-$leftMargin = 20;
-$leftID = $leftMargin;
-$leftName = $leftID+$wid_ID;
-$leftMinBid = $leftName + $wid_Name;
-$leftEstValue = $leftMinBid + $wid_MinBid;
-$leftDescription = $leftExtValue + $wid_EstValue;
-
-$curY = $topMargin;
-$lineIncr = 5;
-
-$pdf->WriteAt ($leftMargin, $curY, $fr_title);
-$curY += $lineIncr;
-$pdf->WriteAt ($leftMargin, $topMargin, $fr_description);
-$curY += $lineIncr;
-
-$pdf->SetFont("Times",'B',10);
-$pdf->WriteAtCell ($leftID, $curY, $wid_ID, gettext ("Item"));
-$pdf->WriteAtCell ($leftName, $curY, $wid_ID, gettext ("Name"));
-$pdf->WriteAtCell ($leftMinBid, $curY, $wid_ID, gettext ("Min bid"));
-$pdf->WriteAtCell ($leftEstValue, $curY, $wid_ID, gettext ("Est value"));
-$pdf->WriteAtCell ($leftDescription, $curY, $wid_ID, gettext ("Description"));
-$curY += $lineIncr;
+// Read in report settings from database
+$rsConfig = mysql_query("SELECT cfg_name, IFNULL(cfg_value, cfg_default) AS value FROM config_cfg WHERE cfg_section='ChurchInfoReport'");
+if ($rsConfig) {
+	while (list($cfg_name, $cfg_value) = mysql_fetch_row($rsConfig)) {
+		$pdf->$cfg_name = $cfg_value;
+	}
+}
 
 // Loop through items
 while ($oneItem = mysql_fetch_array($rsItems)) {
 	extract ($oneItem);
 
 	$pdf->SetFont("Times",'',10);
-	$pdf->WriteAtCell ($leftID, $curY, $wid_ID, $di_item);
-	$pdf->WriteAtCell ($leftName, $curY, $wid_ID, $di_title);
-	$pdf->WriteAtCell ($leftMinBid, $curY, $wid_ID, $di_minimum);
-	$pdf->WriteAtCell ($leftEstValue, $curY, $wid_ID, $di_estprice);
-	$pdf->WriteAtCell ($leftDescription, $curY, $wid_ID, $di_description);
-	$curY += $lineIncr;
-
-    if ($curY > 183)	// This insures the trailer information fits continuously on the page (3 inches of "footer"
-    {
-    }
+	$pdf->SetXY ($leftID, $curY);
+	$pdf->MultiCell ($wid_ID, $lineIncr, $di_item,0,"L");
+	$maxY = $pdf->GetY();
+	$maxPage = $pdf->PageNo ();
+	$pdf->SetXY ($leftName, $curY);
+	$pdf->MultiCell ($wid_Name, $lineIncr, $di_title,0,"L");
+	if ($pdf->PageNo() > $maxPage)
+		$maxY = $pdf->GetY();
+	else
+		$maxY = max($maxY, $pdf->GetY());
+	$pdf->SetXY ($leftMinBid, $curY);
+	$pdf->MultiCell ($wid_MinBid, $lineIncr, $di_minimum,0,"L");
+	if ($pdf->PageNo() > $maxPage)
+		$maxY = $pdf->GetY();
+	else
+		$maxY = max($maxY, $pdf->GetY());
+	$pdf->SetXY ($leftEstValue, $curY);
+	$pdf->MultiCell ($wid_EstValue, $lineIncr, $di_estprice,0,"L");
+	if ($pdf->PageNo() > $maxPage)
+		$maxY = $pdf->GetY();
+	else
+		$maxY = max($maxY, $pdf->GetY());
+	$pdf->SetXY ($leftDescription, $curY);
+	$pdf->MultiCell ($wid_Description, $lineIncr, $di_description,0,"L");
+	if ($pdf->PageNo() > $maxPage)
+		$maxY = $pdf->GetY();
+	else
+		$maxY = max($maxY, $pdf->GetY());
+		
+	$curY = $maxY+$lineIncr;
 }
 
 header('Pragma: public');  // Needed for IE when using a shared SSL certificate
