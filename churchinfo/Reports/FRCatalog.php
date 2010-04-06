41798e37309415aa65252b1f8f619d6f14e58a37
mikewiltwork <mikewiltwork>
2010-04-06 16:21:34 +0000

    Use a paragraph for each item rather than a table

--- churchinfo/Reports/FRCatalog.php
+++ churchinfo/Reports/FRCatalog.php
@@ -20,24 +20,6 @@ require "../Include/ReportConfig.php";
 
 $iCurrentFundraiser = $_GET["CurrentFundraiser"];
 
-$wid_ID = 12;
-$wid_Name = 25;
-$wid_MinBid = 15;
-$wid_EstValue = 15;
-$wid_Description = 100;
-
-$topMargin = 25;
-$leftMargin = 20;
-
-$leftID = $leftMargin;
-$leftName = $leftID+$wid_ID;
-$leftMinBid = $leftName + $wid_Name;
-$leftEstValue = $leftMinBid + $wid_MinBid;
-$leftDescription = $leftEstValue + $wid_EstValue;
-
-$curY = $topMargin;
-$lineIncr = 5;
-
 class PDF_FRCatalogReport extends ChurchInfoReport {
 	// Constructor
 	function PDF_FRCatalogReport() {
@@ -51,30 +33,16 @@ class PDF_FRCatalogReport extends ChurchInfoReport {
 	}
 	
 	function AddPage () {
-		global $wid_ID,$wid_Name,$wid_MinBid,$wid_EstValue,$wid_Description;
-		global $topMargin,$leftMargin,$leftID,$leftName,$leftMinBid,$leftEstValue,$leftDescription;
-		global $curY, $lineIncr;
 		global $fr_title, $fr_description;
-		
+
 		parent::AddPage();
-    	$curY = $topMargin;
 		
     	$this->SetFont("Times",'B',16);
-    	$this->WriteAt ($leftMargin, $curY, $fr_title);
+    	$this->Write (8, $fr_title."\n");
 		$curY += 8;
-		$this->WriteAt ($leftMargin, $curY, $fr_description);
+		$this->Write (8, $fr_description."\n\n");
 		$curY += 8;
-    	
-    	$this->SetFont("Times",'B',10);
-		$this->WriteAtCell ($leftID, $curY, $wid_ID, gettext ("Item"),0,"L");
-		$this->WriteAtCell ($leftName, $curY, $wid_Name, gettext ("Name"),0,"L");
-		$this->WriteAtCell ($leftMinBid, $curY, $wid_MinBid, gettext ("Min bid"),0,"L");
-		$this->WriteAtCell ($leftEstValue, $curY, $wid_EstValue, gettext ("Est value"),0,"L");
-		$this->WriteAtCell ($leftDescription, $curY, $wid_Description, gettext ("Description"),0,"L");
-		$curY += $lineIncr;
-		$this->SetY ($curY);
-    	$this->SetFont("Times",'',10);
-	}
+   	}
 }
 
 
@@ -85,7 +53,7 @@ $thisFR = mysql_fetch_array($rsFR);
 extract ($thisFR);
 
 // Get all the donated items
-$sSQL = "SELECT * FROM donateditem_di WHERE di_FR_ID=".$iCurrentFundraiser." ORDER BY di_item";
+$sSQL = "SELECT * FROM donateditem_di LEFT JOIN person_per on per_ID=di_donor_ID WHERE di_FR_ID=".$iCurrentFundraiser." ORDER BY di_item";
 $rsItems = RunQuery($sSQL);
 
 $pdf = new PDF_FRCatalogReport();
@@ -103,37 +71,16 @@ if ($rsConfig) {
 while ($oneItem = mysql_fetch_array($rsItems)) {
 	extract ($oneItem);
 
+	$pdf->SetFont("Times",'B',10);
+	$pdf->Write (5, $di_item."\t");
+	$pdf->Write (5, $di_title."\n");
 	$pdf->SetFont("Times",'',10);
-	$pdf->SetXY ($leftID, $curY);
-	$pdf->MultiCell ($wid_ID, $lineIncr, $di_item,0,"L");
-	$maxY = $pdf->GetY();
-	$maxPage = $pdf->PageNo ();
-	$pdf->SetXY ($leftName, $curY);
-	$pdf->MultiCell ($wid_Name, $lineIncr, $di_title,0,"L");
-	if ($pdf->PageNo() > $maxPage)
-		$maxY = $pdf->GetY();
-	else
-		$maxY = max($maxY, $pdf->GetY());
-	$pdf->SetXY ($leftMinBid, $curY);
-	$pdf->MultiCell ($wid_MinBid, $lineIncr, $di_minimum,0,"L");
-	if ($pdf->PageNo() > $maxPage)
-		$maxY = $pdf->GetY();
-	else
-		$maxY = max($maxY, $pdf->GetY());
-	$pdf->SetXY ($leftEstValue, $curY);
-	$pdf->MultiCell ($wid_EstValue, $lineIncr, $di_estprice,0,"L");
-	if ($pdf->PageNo() > $maxPage)
-		$maxY = $pdf->GetY();
-	else
-		$maxY = max($maxY, $pdf->GetY());
-	$pdf->SetXY ($leftDescription, $curY);
-	$pdf->MultiCell ($wid_Description, $lineIncr, $di_description,0,"L");
-	if ($pdf->PageNo() > $maxPage)
-		$maxY = $pdf->GetY();
-	else
-		$maxY = max($maxY, $pdf->GetY());
-		
-	$curY = $maxY+$lineIncr;
+	$pdf->Write (5, $di_description."\n");
+	if ($di_minimum > 0)
+		$pdf->Write (5, gettext ("Minimum bid ")."\$".$di_minimum.".  ");
+	if ($di_estprice > 0)
+		$pdf->Write (5, gettext ("Estimated value ")."\$".$di_estprice.".  ");
+	$pdf->Write (5, gettext ("Donated by ") . $per_FirstName . " " .$per_LastName.".\n\n");
 }
 
 header('Pragma: public');  // Needed for IE when using a shared SSL certificate
