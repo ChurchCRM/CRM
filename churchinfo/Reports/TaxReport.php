ae43120e40ee0b0da6e311f6b32358dfbef1ea34
mikewiltwork <mikewiltwork>
2010-06-07 02:41:27 +0000

    Replace eregi with preg_match because eregi is being deprecated

--- churchinfo/Reports/TaxReport.php
+++ churchinfo/Reports/TaxReport.php
@@ -421,7 +421,7 @@ if ($output == "pdf") {
 	$eol = "\r\n";
 	
 	// Build headings row
-	eregi ("SELECT (.*) FROM ", $sSQL, $result);
+	preg_match ("/SELECT (.*) FROM /i", $sSQL, $result);
 	$headings = explode(",",$result[1]);
 	$buffer = "";
 	foreach ($headings as $heading) {
