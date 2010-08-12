c767b8f1faf5ef777ae4d7e8d90caa3de2e04de0
jsu9728 <jsu9728>
2010-08-12 15:30:53 +0000

    Major rewrite of file:  Added support for Shared SSL certificates  Added data integrity checks  Added warning and security checks to delete Volunteer Opportunities  Modified Files:  	churchinfo/VolunteerOpportunityEditor.php

--- churchinfo/VolunteerOpportunityEditor.php
+++ churchinfo/VolunteerOpportunityEditor.php
@@ -2,21 +2,37 @@
 /*******************************************************************************
  *
  *  filename    : VolunteerOpportunityEditor.php
- *  last change : 2003-03-29
- *  website     : http://www.infocentral.org
+ *  website     : http://www.churchdb.org
  *  copyright   : Copyright 2005 Michael Wilt
  *
- *  function    : Editor for donation funds
+ *  LICENSE:
+ *  (C) Free Software Foundation, Inc.
  *
  *  ChurchInfo is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  http://www.gnu.org/licenses
  *
  ******************************************************************************/
 
 require "Include/Config.php";
 require "Include/Functions.php";
+
+// Security: User must have proper permission
+// For now ... require $bAdmin
+// Future ... $bManageVol
+
+if (!$_SESSION['bAdmin']) {
+    Redirect("Menu.php");
+    exit;
+}
 
 // top down design....
 // title line
@@ -29,34 +45,125 @@ require "Include/Functions.php";
 // action is change of order number, up, down, delete, Name, Desc, or Active, or Add New
 
 
-$sAction = $_GET["Action"];
+$sAction = $_GET["act"];
 $sOpp = FilterInput($_GET["Opp"],'int');
 
 $sDeleteError = "";
 
-if ($sAction = 'delete' && strlen($sOpp) > 0) {
-   $sSQL = "DELETE FROM volunteeropportunity_vol WHERE vol_ID = '" . $sOpp . "'";
-   RunQuery($sSQL);
-   $sSQL = "DELETE FROM person2volunteeropp_p2vo WHERE p2vo_vol_ID = '" . $sOpp . "'";
-   RunQuery($sSQL);
+if (($sAction == 'delete') && (strlen($sOpp) > 0)) {
+    // Delete Confirmation Page
 
-   // we need to now re-order the vol_Order field, since its likely now to be missing a number
-   $sSQL = "SELECT * FROM volunteeropportunity_vol ORDER by vol_Order";
-   $rsOpps = RunQuery($sSQL);
-   $numRows = mysql_num_rows($rsOpps);
+    // Security: User must have Delete records permission
+    // Otherwise, redirect to the mail menu
+    if (!$_SESSION['bDeleteRecords']) {
+        Redirect("Menu.php");
+        exit;
+    }
 
-   $orderCounter = 1;
-   for ($row = 1; $row <= $numRows; $row++) {
-      $aRow = mysql_fetch_array($rsOpps, MYSQL_BOTH);
-      extract($aRow);
-      if ($orderCounter <> $vol_Order) { // found hole, update all records to the end
-        $sSQL = "UPDATE volunteeropportunity_vol
-                 SET `vol_Order` = '" . $orderCounter . "' " .
-	        "WHERE `vol_ID` = '" . $vol_ID . "';";
+    $sSQL = "SELECT * FROM `volunteeropportunity_vol` WHERE `vol_ID` = '" . $sOpp . "'";
+    $rsOpps = RunQuery($sSQL);
+    $aRow = mysql_fetch_array($rsOpps);
+    extract($aRow);
+
+    $sPageTitle = gettext("Volunteer Opportunity Delete Confirmation");
+    require "Include/Header.php";
+    echo "\n<br><p>" . gettext("Please confirm deletion of:") . "</p><br>";
+    echo "\n<table><tr><th>&nbsp;</th>";
+    echo "\n<th>" . gettext("Name") . "</th>";
+    echo "\n<th>" . gettext("Description") . "</th></tr>";
+    echo "\n<tr><td class=\"LabelColumn\"><b>" . $vol_Order . "</b></td>";
+    echo "\n<td class=\"TextColumn\">" . $vol_Name . "</td>";
+    echo "\n<td class=\"TextColumn\">" . $vol_Description . "</td></tr></table>";
+
+    // Do some error checking before deleting this Opportunity.
+    // Notify user if there are currently people assigned to this
+    // Volunteer Opportunity.
+
+
+    $sSQL = "SELECT `per_FirstName`, `per_LastName` FROM `person_per` ";
+    $sSQL .= "LEFT JOIN `person2volunteeropp_p2vo` ";
+    $sSQL .= "ON `p2vo_per_ID`=`per_ID` ";
+    $sSQL .= "WHERE `p2vo_vol_ID` = '" . $sOpp . "' ";
+    $sSQL .= "ORDER BY `per_LastName`, `per_FirstName` ";
+    $rsPeople = RunQuery($sSQL);
+    $numRows = mysql_num_rows($rsPeople);
+    if ($numRows > 0) {
+        echo "\n<br><h3>" . gettext("Warning!") . "</h3>";
+        echo "\n<h3>" . gettext("There are people assigned to this Volunteer Opportunity.") . "</h3>";
+        echo "\n<br>" . gettext("Volunteer Opportunity will be unassigned for the following people.");
+        echo "\n<br>";
+        for ( $i=0; $i<$numRows; $i++ ) {
+            $aRow = mysql_fetch_array($rsPeople);
+            extract($aRow);
+            echo "\n<br><b> $per_FirstName $per_LastName</b>";
+        }
+    }
+    echo "\n<br><h3><a href=\"" . $sURLPath . "/VolunteerOpportunityEditor.php?act=ConfDelete&amp;Opp=" . $sOpp . "\"> ";
+    echo gettext("Yes, delete this Volunteer Opportunity") . " </a></h3> ";
+    echo "\n<h2><a href=\"" . $sURLPath . "/VolunteerOpportunityEditor.php\"> ";
+    echo gettext("No, cancel this deletion") . " </a></h2> ";
+    require "Include/Footer.php";
+    exit;
+}
+
+
+if (($sAction == 'ConfDelete') && (strlen($sOpp) > 0)) {
+
+    // Security: User must have Delete records permission
+    // Otherwise, redirect to the mail menu
+    if (!$_SESSION['bDeleteRecords']) {
+        Redirect("Menu.php");
+        exit;
+    }
+    $sSQL = "DELETE FROM `volunteeropportunity_vol` WHERE `vol_ID` = '" . $sOpp . "'";
+    RunQuery($sSQL);
+    $sSQL = "DELETE FROM `person2volunteeropp_p2vo` WHERE `p2vo_vol_ID` = '" . $sOpp . "'";
+    RunQuery($sSQL);
+    }
+
+
+// Data integrity check #1
+// The default value of `vol_Order` is '0'.  But '0' is not assigned. 
+// If we find a '0' add it to the end of the list by changing it to
+// MAX(vol_Order)+1. 
+
+$sSQL = "SELECT `vol_ID` FROM `volunteeropportunity_vol` WHERE vol_Order = '0' ";
+$sSQL .= "ORDER BY `vol_ID`";
+$rsOrder = RunQuery($sSQL);
+$numRows = mysql_num_rows($rsOrder);
+if ($numRows) {
+    $sSQL = "SELECT MAX(`vol_Order`) AS `Max_vol_Order` FROM `volunteeropportunity_vol`";
+    $rsMax = RunQuery($sSQL);
+    $aRow = mysql_fetch_array($rsMax);
+    extract($aRow);
+    for ($row = 1; $row <= $numRows; $row++) {
+        $aRow = mysql_fetch_array($rsOrder);
+        extract($aRow);
+        $num_vol_Order = $Max_vol_Order + $row;
+        $sSQL = "UPDATE `volunteeropportunity_vol` " .
+                "SET `vol_Order` = '" . $num_vol_Order . "' " .
+                "WHERE `vol_ID` = '" . $vol_ID . "'";
         RunQuery($sSQL);
-      }
-      ++$orderCounter;
+    }
+}
+
+// Data integrity check #2
+// re-order the vol_Order field just in case there is a missing number(s)
+$sSQL = "SELECT * FROM `volunteeropportunity_vol` ORDER by `vol_Order`";
+$rsOpps = RunQuery($sSQL);
+$numRows = mysql_num_rows($rsOpps);
+
+$orderCounter = 1;
+for ($row = 1; $row <= $numRows; $row++) {
+     $aRow = mysql_fetch_array($rsOpps);
+     extract($aRow);
+     if ($orderCounter <> $vol_Order) { // found hole, update all records to the end
+     $sSQL = "UPDATE `volunteeropportunity_vol` " .
+             "SET `vol_Order` = '" . $orderCounter . "' " .
+             "WHERE `vol_ID` = '" . $vol_ID . "'";
+     RunQuery($sSQL);
    }
+   ++$orderCounter;
 }
 
 $sPageTitle = gettext("Volunteer Opportunity Editor");
@@ -65,85 +172,86 @@ require "Include/Header.php";
 
 // Does the user want to save changes to text fields?
 if (isset($_POST["SaveChanges"])) {
-   $sSQL = "SELECT * FROM volunteeropportunity_vol";
-   $rsOpps = RunQuery($sSQL);
-   $numRows = mysql_num_rows($rsOpps);
+    $sSQL = "SELECT * FROM `volunteeropportunity_vol`";
+    $rsOpps = RunQuery($sSQL);
+    $numRows = mysql_num_rows($rsOpps);
 
-   for ($iFieldID = 1; $iFieldID <= $numRows; $iFieldID++ ) {
-      $aNameFields[$iFieldID] = FilterInput($_POST[$iFieldID . "name"]);
+    for ($iFieldID = 1; $iFieldID <= $numRows; $iFieldID++ ) {
+        $aNameFields[$iFieldID] = FilterInput($_POST[$iFieldID . "name"]);
 
-      if ( strlen($aNameFields[$iFieldID]) == 0 ) {
-         $aNameErrors[$iFieldID] = true;
-	 $bErrorFlag = true;
-      } else {
-        $aNameErrors[$iFieldID] = false;
-      }
+        if ( strlen($aNameFields[$iFieldID]) == 0 ) {
+            $aNameErrors[$iFieldID] = true;
+        $bErrorFlag = true;
+        } else {
+            $aNameErrors[$iFieldID] = false;
+        }
 
-      $aDescFields[$iFieldID] = FilterInput($_POST[$iFieldID . "desc"]);
+        $aDescFields[$iFieldID] = FilterInput($_POST[$iFieldID . "desc"]);
 
-      $aRow = mysql_fetch_array($rsOpps);
-      $aIDFields[$iFieldID] = $aRow[0];
-   }
+        $aRow = mysql_fetch_array($rsOpps);
+        $aIDFields[$iFieldID] = $aRow[0];
+    }
 
-   // If no errors, then update.
-   if (!$bErrorFlag) {
-      for ( $iFieldID=1; $iFieldID <= $numRows; $iFieldID++ ) {
-         $sSQL = "UPDATE volunteeropportunity_vol
-                  SET `vol_Name` = '" . $aNameFields[$iFieldID] . "',
-	              `vol_Description` = '" . $aDescFields[$iFieldID] .
-	         "' WHERE `vol_ID` = '" . $aIDFields[$iFieldID] . "';";
-         RunQuery($sSQL);
-      }
-   }
+    // If no errors, then update.
+    if (!$bErrorFlag) {
+        for ( $iFieldID=1; $iFieldID <= $numRows; $iFieldID++ ) {
+            $sSQL = "UPDATE `volunteeropportunity_vol`
+                     SET `vol_Name` = '" . $aNameFields[$iFieldID] . "',
+                     `vol_Description` = '" . $aDescFields[$iFieldID] .
+                     "' WHERE `vol_ID` = '" . $aIDFields[$iFieldID] . "';";
+             RunQuery($sSQL);
+         }
+    }
 } else {
-   if (isset($_POST["AddField"])) { // Check if we're adding a VolOp
-      $newFieldName = FilterInput($_POST["newFieldName"]);
-      $newFieldDesc = FilterInput($_POST["newFieldDesc"]);
-      if (strlen($newFieldName) == 0) {
-         $bNewNameError = true;
-      } else { // Insert into table
-         //  there must be an easier way to get the number of rows in order to generate the last order number.
-         $sSQL = "SELECT * FROM volunteeropportunity_vol";
-         $rsOpps = RunQuery($sSQL);
-         $numRows = mysql_num_rows($rsOpps);
-	 $newOrder = $numRows + 1;
-	 $sSQL = "INSERT INTO `volunteeropportunity_vol` 
-		(`vol_ID` , `vol_Order` , `vol_Name` , `vol_Description`)
-        	VALUES ('', '". $newOrder . "', '" . $newFieldName . "', '" . $newFieldDesc . "');";
-	 RunQuery($sSQL);
-	 $bNewNameError = false;
-      }
-   }
-   // Get data for the form as it now exists..
-   $sSQL = "SELECT * FROM volunteeropportunity_vol";
+    if (isset($_POST["AddField"])) { // Check if we're adding a VolOp
+        $newFieldName = FilterInput($_POST["newFieldName"]);
+        $newFieldDesc = FilterInput($_POST["newFieldDesc"]);
+        if (strlen($newFieldName) == 0) {
+            $bNewNameError = true;
+        } else { // Insert into table
+        //  there must be an easier way to get the number of rows in order to generate the last order number.
+        $sSQL = "SELECT * FROM `volunteeropportunity_vol`";
+        $rsOpps = RunQuery($sSQL);
+        $numRows = mysql_num_rows($rsOpps);
+        $newOrder = $numRows + 1;
+        $sSQL = "INSERT INTO `volunteeropportunity_vol` 
+           (`vol_ID` , `vol_Order` , `vol_Name` , `vol_Description`)
+           VALUES ('', '". $newOrder . "', '" . $newFieldName . "', '" . $newFieldDesc . "');";
+        RunQuery($sSQL);
+        $bNewNameError = false;
+        }
+    }
+    // Get data for the form as it now exists..
+    $sSQL = "SELECT * FROM `volunteeropportunity_vol`";
 
-   $rsOpps = RunQuery($sSQL);
-   $numRows = mysql_num_rows($rsOpps);
+    $rsOpps = RunQuery($sSQL);
+    $numRows = mysql_num_rows($rsOpps);
 
-   // Create arrays of Vol Opps.
-   for ($row = 1; $row <= $numRows; $row++) {
-      $aRow = mysql_fetch_array($rsOpps, MYSQL_BOTH);
-      extract($aRow);
-      $rowIndex = $vol_Order; // is this dangerous?  the vol_Order field had better be correct.
-      $aIDFields[$rowIndex] = $vol_ID;
-      $aNameFields[$rowIndex] = $vol_Name;
-      $aDescFields[$rowIndex] = $vol_Description;
-   }
+    // Create arrays of Vol Opps.
+    for ($row = 1; $row <= $numRows; $row++) {
+        $aRow = mysql_fetch_array($rsOpps, MYSQL_BOTH);
+        extract($aRow);
+        $rowIndex = $vol_Order; // is this dangerous?  the vol_Order field had better be correct.
+        $aIDFields[$rowIndex] = $vol_ID;
+        $aNameFields[$rowIndex] = $vol_Name;
+        $aDescFields[$rowIndex] = $vol_Description;
+    }
 
 }
 
 // Construct the form
-?>
 
-<script language="javascript">
+/*
+<script language="javascript" type="text/javascript">
 
 function confirmDeleteOpp( Opp ) {
 var answer = confirm (<?php echo '"' . gettext("Are you sure you want to delete this Vol Opp?") . '"'; ?>)
 if ( answer )
-	window.location="VolunteerOpportunityEditor.php?Opp=" + Opp + "&Action=delete"
+    window.location="VolunteerOpportunityEditor.php?Opp=" + Opp + "&Action=delete"
 }
 </script>
-
+*/
+?>
 <form method="post" action="VolunteerOpportunityEditor.php" name="OppsEditor">
 
 <table cellpadding="3" width="75%" align="center">
@@ -151,9 +259,9 @@ if ( answer )
 <?php
 if ($numRows == 0) {
 ?>
-	<center><h2><?php echo gettext("No volunteer opportunities have been added yet"); ?></h2>
-	<input type="button" class="icButton" <?php echo 'value="' . gettext("Exit") . '"'; ?> Name="Exit" onclick="javascript:document.location='Menu.php';">
-	</center>
+    <center><h2><?php echo gettext("No volunteer opportunities have been added yet"); ?></h2>
+    <input type="button" class="icButton" <?php echo 'value="' . gettext("Exit") . '"'; ?> Name="Exit" onclick="javascript:document.location='Menu.php'">
+    </center>
 <?php
 } else { // if an 'action' (up/down arrow clicked, or order was input)
    if ($_GET['row_num'] and $_GET['act']) {
@@ -171,12 +279,12 @@ if ($numRows == 0) {
 
       $sSQL = "UPDATE volunteeropportunity_vol
                SET vol_Order = '" . $newRow . "' " .
-	      "WHERE vol_ID = '" . $aIDFields[$swapRow] . "';";
+          "WHERE vol_ID = '" . $aIDFields[$swapRow] . "';";
       RunQuery($sSQL);
 
       $sSQL = "UPDATE volunteeropportunity_vol
                SET vol_Order = '" . $swapRow . "' " .
-	      "WHERE vol_ID = '" . $aIDFields[$newRow] . "';";
+          "WHERE vol_ID = '" . $aIDFields[$newRow] . "';";
       RunQuery($sSQL);
 
       // now update internal data to match
@@ -192,7 +300,7 @@ if ($numRows == 0) {
       $aNameFields[$newRow] = $saveName;
       $aDescFields[$newRow] = $saveDesc;
    }
-} // end if GET	 
+} // end if GET  
 
 ?>
 <tr><td colspan="5">
@@ -204,13 +312,13 @@ if ($numRows == 0) {
 if ( $bErrorFlag ) echo gettext("Invalid fields or selections. Changes not saved! Please correct and try again!");
 if (strlen($sDeleteError) > 0) echo $sDeleteError;
 ?>
-</span></tr></td>
+</span></td></tr>
 
 <tr>
 <td colspan="5" align="center">
 <input type="submit" class="icButton" <?php echo 'value="' . gettext("Save Changes") . '"'; ?> Name="SaveChanges">
 &nbsp;
-<input type="button" class="icButton" <?php echo 'value="' . gettext("Exit") . '"'; ?> Name="Exit" onclick="javascript:document.location='Menu.php';">
+<input type="button" class="icButton" <?php echo 'value="' . gettext("Exit") . '"'; ?> Name="Exit" onclick="javascript:document.location='Menu.php'">
 </td>
 </tr>
 
@@ -224,27 +332,27 @@ if (strlen($sDeleteError) > 0) echo $sDeleteError;
 <?php
 
 for ($row=1; $row <= $numRows; $row++) {
-   echo "<tr>";
-   echo "<td class=\"LabelColumn\"><b>" . $row . "</b></td>";
-   echo "<td class=\"TextColumn\">";
-   if ($row == 1) {
-      echo "<a href=\"{$_SERVER['PHP_SELF']}?act=na&row_num=" . $row . "\"> <img src=\"Images/Spacer.gif\" border=\"0\" width=\"15\"></a> ";
-   } else {
-      echo "<a href=\"{$_SERVER['PHP_SELF']}?act=up&row_num=" . $row . "\"> <img src=\"Images/uparrow.gif\" border=\"0\" width=\"15\"></a> ";
-   }
-   if ($row <> $numRows) {
-      echo "<a href=\"{$_SERVER['PHP_SELF']}?act=down&row_num=" . $row . "\"> <img src=\"Images/downarrow.gif\" border=\"0\" width=\"15\"></a>";
-   } else {
-      echo "<a href=\"{$_SERVER['PHP_SELF']}?act=na&row_num=" . $row . "\"> <img src=\"Images/Spacer.gif\" border=\"0\" width=\"15\"></a>";
-   }
+    echo "<tr>";
+    echo "<td class=\"LabelColumn\"><b>" . $row . "</b></td>";
+    echo "<td class=\"TextColumn\">";
+    if ($row == 1) {
+      echo "<a href=\"" . $sURLPath . "/VolunteerOpportunityEditor.php?act=na&amp;row_num=" . $row . "\"> <img src=\"Images/Spacer.gif\" border=\"0\" width=\"15\" alt=''></a> ";
+    } else {
+      echo "<a href=\"" . $sURLPath . "/VolunteerOpportunityEditor.php?act=up&amp;row_num=" . $row . "\"> <img src=\"Images/uparrow.gif\" border=\"0\" width=\"15\" alt=''></a> ";
+    }
+    if ($row <> $numRows) {
+      echo "<a href=\"" . $sURLPath . "/VolunteerOpportunityEditor.php?act=down&amp;row_num=" . $row . "\"> <img src=\"Images/downarrow.gif\" border=\"0\" width=\"15\" alt=''></a> ";
+    } else {
+      echo "<a href=\"" . $sURLPath . "/VolunteerOpportunityEditor.php?act=na&amp;row_num=" . $row . "\"> <img src=\"Images/Spacer.gif\" border=\"0\" width=\"15\" alt=''></a> ";
+    }
     
-   echo "<a href=\"{$_SERVER['PHP_SELF']}?act=delete&row_num=" . $row . "\" onclick=confirmDeleteOpp(" . $aIDFields[$row] . ") <img src=\"Images/x.gif\" border=\"0\" width=\"15\"></a></td>";
+    echo "<a href=\"" . $sURLPath . "/VolunteerOpportunityEditor.php?act=delete&amp;Opp=" . $aIDFields[$row] . "\"> <img src=\"Images/x.gif\" border=\"0\" width=\"15\" alt=''></a></td>";
    ?>
 
    <td class="TextColumn" align="center">
    <input type="text" name="<?php echo $row . "name"; ?>" value="<?php echo htmlentities(stripslashes($aNameFields[$row]),ENT_NOQUOTES, "UTF-8"); ?>" size="20" maxlength="30">
    <?php
-	  
+      
    if ( $aNameErrors[$row] ) {
       echo "<span style=\"color: red;\"><BR>" . gettext("You must enter a name.") . " </span>";
    }
@@ -268,7 +376,7 @@ for ($row=1; $row <= $numRows; $row++) {
 <td width="40%" align="center" valign="bottom">
 <input type="submit" class="icButton" <?php echo 'value="' . gettext("Save Changes") . '"'; ?> Name="SaveChanges">
 &nbsp;
-<input type="button" class="icButton" <?php echo 'value="' . gettext("Exit") . '"'; ?> Name="Exit" onclick="javascript:document.location='Menu.php';">
+<input type="button" class="icButton" <?php echo 'value="' . gettext("Exit") . '"'; ?> Name="Exit" onclick="javascript:document.location='Menu.php'">
 </td>
 <td width="30%"></td>
 </tr>
