d0a806410258833248e1e1548e6ec98aee6b0361
mikewiltwork <mikewiltwork>
2012-11-06 00:20:50 +0000

    Added help for Fundraiser feature

--- churchinfo/Help.php
+++ churchinfo/Help.php
@@ -2,13 +2,14 @@
 /*******************************************************************************
  *
  *  filename    : Help.php
- *  last change : 2003-01-17
+ *  last change : 2012-11/05
  *  description : Online help system (eventually should be XML based)
  *
- *  http://www.infocentral.org/
+ *  http://www.churchdb.org/
  *  Copyright 2001-2002 Phillip Hullquist, Deane Barker
+ *  Copyright 2012 Michael Wilt
  *
- *  InfoCentral is free software; you can redistribute it and/or modify
+ *  ChurchInfo is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -20,7 +21,7 @@ require "Include/Config.php";
 require "Include/Initialize.php";
 
 // Valid pages to request via the 'page' GET variable. (prevent XSS)
-$aValidPages = array('About', 'Admin', 'Cart', 'Class', 'Custom', 'Family', 'Geographic', 'Finances', 'Groups', 'Notes', 'People', 'Properties', 'Reports', 'Types', 'Canvass', 'Events','Wiki');
+$aValidPages = array('About', 'Admin', 'Cart', 'Class', 'Custom', 'Family', 'Geographic', 'Finances', 'Fundraiser', 'Groups', 'Notes', 'People', 'Properties', 'Reports', 'Types', 'Canvass', 'Events','Wiki');
 
 if (in_array($_GET['page'], $aValidPages))
 {
