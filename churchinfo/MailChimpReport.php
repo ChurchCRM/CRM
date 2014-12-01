00d6c3067893229d00c3cca4db9e1370db2c9334
George Dawoud <george@dawouds.com>
2014-11-30 22:48:57 -0800

    mailchimp support
    Added support to review emails to see if they are in mail chimp

---
+++ churchinfo/MailChimpReport.php
@@ -0,0 +1,82 @@
+<?php
+/*******************************************************************************
+ *
+ *  filename    : MailChimpReprot.php
+ *  last change : 2014-11-29
+ *  website     : http://www.churchdb.org
+ *  copyright   : Copyright 2014 
+ *
+ *  ChurchInfo is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ ******************************************************************************/
+
+require 'Include/Config.php';
+require 'Include/Functions.php';
+require 'Include/Mailchimp.php';
+
+//Set the page title
+$sPageTitle = gettext('MailChimp Menu');
+
+
+
+function isEmailInMailChimp($email) {
+	try {
+		$MailChimp = new MailChimp("api-key");
+		$lists = $MailChimp->helper->listsForEmail(array("email" => $email));
+		//return "found";
+		return $lists[0]["name"];
+	 } catch (Exception $e) {
+		return "";
+	 }
+}
+
+require 'Include/Header.php';
+
+
+// Get all the groups
+$sSQL = "SELECT per_FirstName, per_LastName, per_Email, per_id FROM `stgeorge_churchinfo`.`person_per` where per_Email != '' order by per_DateLastEdited desc";
+$rsPeopleWithEmail = RunQuery($sSQL);
+
+?>
+<script src="http://mottie.github.io/tablesorter/js/jquery.tablesorter.js"></script>
+<script src="http://mottie.github.io/tablesorter/js/jquery.tablesorter.widgets.js"></script>
+<table id="people_with_email" width="100%" data-toggle="table" class="table table-striped table-bordered tablesorter">
+<thead>
+<tr>
+	<td>Name</td>
+	<td>Email</td>
+	<td>MailChimp Status</td>
+</tr>
+</thead>
+<tbody>
+<?php
+while ($aRow = mysql_fetch_array($rsPeopleWithEmail)) {
+	extract($aRow);
+	echo "<tr>";
+		echo "<td><a href='PersonView.php?PersonID=".$per_id."'>".$per_FirstName." ".$per_LastName."</a></td>";
+		echo "<td>".$per_Email."</td>";
+		echo "<td>".isEmailInMailChimp($per_Email)."</td>";	
+	echo "</tr>";
+}
+
+?>
+</tbody>
+</table>
+
+<script>
+$(function(){
+  $("#people_with_email").tablesorter();
+});
+</script>
+
+<?php
+
+
+
+
+
+require "Include/Footer.php";
+?>
