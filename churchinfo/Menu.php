dc6506f03b846c7ceb7c37e4030d1f4cbcbf02f4
George Dawoud <george@dawouds.com>
2014-12-26 16:38:11 -0800

    Now fully working Dashboard

--- churchinfo/Menu.php
+++ churchinfo/Menu.php
@@ -45,6 +45,14 @@ $rsAdultsGender = RunQuery($sSQL);
 $sSQL = "select count(*) as numb, per_Gender from person_per where per_Gender in (1,2) and per_fmr_ID not in (1,2) group by per_Gender ;";
 $rsKidsGender = RunQuery($sSQL);
 
+$sSQL = "select
+        (select count(*) from family_fam ) as familyCount,
+        (select count(*) from person_per ) as PersonCount,
+        (select count(*) from group_grp where grp_Type = 4 ) as SundaySchoolClasses,
+        (select count(*) from person_per,`group_grp` grp, `person2group2role_p2g2r` person_grp   where person_grp.p2g2r_rle_ID = 2 and grp_Type = 4 and grp.grp_ID = person_grp.p2g2r_grp_ID  and person_grp.p2g2r_per_ID = per_ID) as SundaySchoolKidsCount
+        from dual ;";
+$rsQuickStat = RunQuery($sSQL);
+
 
 // Set the page title
 $sPageTitle = gettext('Welcome to ChurchInfo');
@@ -53,14 +61,15 @@ require 'Include/Header.php';
 ?>
 <!-- this page specific styles -->
 <script src="http://cdn.oesmith.co.uk/morris-0.4.1.min.js"></script>
-<script src="js/raphael-min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js"></script>
 
 
 <div class="row">
     <div class="col-lg-12 col-md-6 col-sm-4">
-        <div class="main-box clearfix">
-            <header class="main-box-header clearfix">
-            <div class="filter-block">
+        <div class="box box-solid">
+            <div class="box-body clearfix">
+                <i class="fa fa-search"></i><input type="text" class="search searchPerson" placeholder="Search..." onfocus="ClearFieldOnce(this);"/>
+
                 <a href="PersonEditor.php" class="btn btn-primary">
                     <i class="fa fa-plus-circle fa-md"></i> Add Person
                 </a>
@@ -73,14 +82,95 @@ require 'Include/Header.php';
         </div>
     </div>
 </div>
+<?php while ($row = mysql_fetch_array($rsQuickStat)) { ?>
+<!-- Small boxes (Stat box) -->
+<div class="row">
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+            <div class="inner">
+                <h3>
+                    <?php echo $row['familyCount'];?>
+                </h3>
+                <p>
+                    Families
+                </p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-person-stalker"></i>
+            </div>
+            <a href="/SelectList.php?mode=family" class="small-box-footer">
+                See all Families <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div><!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3>
+                    <?php echo $row['PersonCount'];?>
+                </h3>
+                <p>
+                    Members
+                </p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-person-add"></i>
+            </div>
+            <a href="/SelectList.php?mode=person" class="small-box-footer">
+                See All Member <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div><!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-yellow">
+            <div class="inner">
+                <h3>
+                    <?php echo $row['SundaySchoolClasses'];?>
+                </h3>
+                <p>
+                    Sunday School Classes
+                </p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-university"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+                More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div><!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-red">
+            <div class="inner">
+                <h3>
+                    <?php echo $row['SundaySchoolKidsCount'];?>
+                </h3>
+                <p>
+                    Sunday School Kids
+                </p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-happy"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+                More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div><!-- ./col -->
+</div><!-- /.row -->
+<?php } ?>
 <div class="row">
     <div class="col-lg-6 col-md-5 col-sm-4">
-        <div class="main-box clearfix">
-            <header class="main-box-header clearfix">
-                <h2 class="pull-left">New Families</h2>
-            </header>
-
-            <div class="main-box-body clearfix">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="ion ion-person-add"></i>
+                <h3 class="box-title">New Families</h3>
+            </div><!-- /.box-header -->
+            <div class="box-body clearfix">
                 <div class="table-responsive">
                     <table class="table table-striped table-condensed">
                         <thead>
@@ -105,12 +195,12 @@ require 'Include/Header.php';
         </div>
     </div>
     <div class="col-lg-6 col-md-5 col-sm-4">
-        <div class="main-box clearfix">
-            <header class="main-box-header clearfix">
-                <h2 class="pull-left">Updated Families</h2>
-            </header>
-
-            <div class="main-box-body clearfix">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="fa fa-check"></i>
+                <h3 class="box-title">Updated Families</h3>
+            </div><!-- /.box-header -->
+            <div class="box-body clearfix">
                 <div class="table-responsive">
                     <table class="table table-striped table-condensed">
                         <thead>
@@ -137,12 +227,12 @@ require 'Include/Header.php';
 </div>
 <div class="row">
     <div class="col-lg-3 col-md-3 col-sm-3">
-        <div class="main-box clearfix">
-            <header class="main-box-header clearfix">
-                <h2 class="pull-left">New People</h2>
-            </header>
-
-            <div class="main-box-body clearfix">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="fa fa-plus"></i>
+                <h3 class="box-title">New People</h3>
+            </div><!-- /.box-header -->
+            <div class="box-body clearfix">
                 <div class="table-responsive">
                     <table class="table table-striped table-condensed">
                         <thead>
@@ -165,39 +255,37 @@ require 'Include/Header.php';
         </div>
     </div>
     <div class="col-lg-3 col-md-3 col-sm-3">
-        <div class="main-box clearfix">
-            <header class="main-box-header clearfix">
-                <h2 class="pull-left">Modified People</h2>
-            </header>
-
-            <div class="main-box-body clearfix">
-                <div class="table-responsive">
-                    <table class="table table-striped table-condensed">
-                        <thead>
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="fa fa-check"></i>
+                <h3 class="box-title">Modified People</h3>
+            </div><!-- /.box-header -->
+            <div class="box-body clearfix">
+                <table class="table table-striped table-condensed">
+                    <thead>
+                    <tr>
+                        <th data-field="name">Name</th>
+                        <th data-field="name">Updated</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php while ($row = mysql_fetch_array($rsLastPeople)) { ?>
                         <tr>
-                            <th data-field="name">Name</th>
-                            <th data-field="name">Updated</th>
+                            <td><a href="PersonView.php?PersonID=<?php echo $row['per_ID'];?>"><?php echo $row['per_FirstName']." ".$row['per_LastName'];?></a></td>
+                            <td><?php echo FormatDate($row['per_DateLastEdited'], false);?></td>
                         </tr>
-                        </thead>
-                        <tbody>
-                        <?php while ($row = mysql_fetch_array($rsLastPeople)) { ?>
-                            <tr>
-                                <td><a href="PersonView.php?PersonID=<?php echo $row['per_ID'];?>"><?php echo $row['per_FirstName']." ".$row['per_LastName'];?></a></td>
-                                <td><?php echo FormatDate($row['per_DateLastEdited'], false);?></td>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
+                    <?php } ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-    <div class="col-lg-6 col-md-6 col-sm-3">
-        <div class="main-box">
-            <header class="main-box-header clearfix">
-                <h2>Gender Demographics</h2>
-            </header>
-
+    <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="box box-solid">
+            <div class="box-header">
+                <i class="ion ion-android-contacts"></i>
+                <h3 class="box-title">Gender Demographics</h3>
+            </div><!-- /.box-header -->
             <div class="main-box-body clearfix">
                 <div id="gender-donut"></div>
             </div>
@@ -206,6 +294,7 @@ require 'Include/Header.php';
 </div>
 
 <!-- this page specific inline scripts -->
+<script src="http://cdn.oesmith.co.uk/morris-0.4.1.min.js"></script>
 <script>
     Morris.Donut({
         element: 'gender-donut',
