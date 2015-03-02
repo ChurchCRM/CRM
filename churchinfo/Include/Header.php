76c9fdd31bf9856c575ac674ca42bf42306317a1
abramovkg <abramovkg@gmail.com>
2015-03-01 23:29:33 -0800

    removed viewport meta; will add to header function

--- churchinfo/Include/Header.php
+++ churchinfo/Include/Header.php
@@ -42,7 +42,7 @@ $MenuFirst = 1;
 <head>
 <?php Header_head_metatag(); ?>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimal-ui"/>
+
 </head>
 <body onload="javascript:scrollToCoordinates()">
 <?php 
