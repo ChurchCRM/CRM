907c189de751def30e5f6c523d5e9e87f32bfdd9
abramovkg <abramovkg@gmail.com>
2015-03-01 21:38:47 -0800

    modified email links and added text cart link

--- churchinfo/CartView.php
+++ churchinfo/CartView.php
@@ -276,17 +276,70 @@ if (count($_SESSION['aPeopleCart']) != 0)
             echo "<br>";
             echo "<a href=\"CSVExport.php?Source=cart\">" . gettext("CSV Export") . "</a>";
         }
+// Email Cart links	
+// Note: This will email entire group, even if a specific role is currently selected.
+$sSQL = "SELECT per_Email, fam_Email
+            FROM person_per
+            LEFT JOIN person2group2role_p2g2r ON per_ID = p2g2r_per_ID
+            LEFT JOIN group_grp ON grp_ID = p2g2r_grp_ID
+            LEFT JOIN family_fam ON per_fam_ID = family_fam.fam_ID
+        WHERE per_ID NOT IN (SELECT per_ID FROM person_per INNER JOIN record2property_r2p ON r2p_record_ID = per_ID INNER JOIN property_pro ON r2p_pro_ID = pro_ID AND pro_Name = 'Do Not Email') AND per_ID IN (" . ConvertCartToString($_SESSION['aPeopleCart']) . ")";
+$rsEmailList = RunQuery($sSQL);
+$sEmailLink = '';
+while (list ($per_Email, $fam_Email) = mysql_fetch_row($rsEmailList))
+{
+    $sEmail = SelectWhichInfo($per_Email, $fam_Email, False);
+    if ($sEmail)
+    {
+        /* if ($sEmailLink) // Don't put delimiter before first email
+            $sEmailLink .= $sMailtoDelimiter; */
+        // Add email only if email address is not already in string
+        if (!stristr($sEmailLink, $sEmail))
+            $sEmailLink .= $sEmail .= $sMailtoDelimiter;
+    }
+}
+if ($sEmailLink)
+{
+    // Add default email if default email has been set and is not already in string
+    if ($sToEmailAddress != '' && $sToEmailAddress != 'myReceiveEmailAddress' 
+                               && !stristr($sEmailLink, $sToEmailAddress))
+        $sEmailLink .= $sMailtoDelimiter . $sToEmailAddress;
+    $sEmailLink = urlencode($sEmailLink);  // Mailto should comply with RFC 2368
 
-        if ($iEmailNum > 0) {
-                // Add default email if default email has been set and is not already in string
-                if ($sToEmailAddress != "" && $sToEmailAddress != "myReceiveEmailAddress" && !stristr($sEmailLink, $sToEmailAddress))
-                        $sEmailLink .= $sMailtoDelimiter . $sToEmailAddress;
-                $sEmailLink = urlencode($sEmailLink);  // Mailto should comply with RFC 2368
-                if ($bEmailMailto) { // Does user have permission to email groups with mailto
-                echo "<br><a href=\"mailto:" . $sEmailLink ."\">". gettext("Email Cart") . "</a>";
-                echo "<br><a href=\"mailto:?bcc=".$sEmailLink."\">".gettext("Email (BCC)")."</a>";
-                }
-        }
+    if ($bEmailMailto) { // Does user have permission to email groups
+    // Display link
+    echo "<br><a href=\"mailto:" . mb_substr($sEmailLink,0,-3) . "\">". gettext("Email Cart") . "</a>";
+    echo "<br><a href=\"mailto:?bcc=". mb_substr($sEmailLink,0,-3) ."\">".gettext("Email (BCC)")."</a>";
+    }
+}
+//Text Cart Link
+$sSQL = "SELECT per_CellPhone, fam_CellPhone FROM person_per LEFT JOIN family_fam ON person_per.per_fam_ID = family_fam.fam_ID WHERE per_ID NOT IN (SELECT per_ID FROM person_per INNER JOIN record2property_r2p ON r2p_record_ID = per_ID INNER JOIN property_pro ON r2p_pro_ID = pro_ID AND pro_Name = 'Do Not SMS') AND per_ID IN (" . ConvertCartToString($_SESSION['aPeopleCart']) . ")";
+        $rsPhoneList = RunQuery($sSQL);       
+        $sPhoneLink = "";
+        $sCommaDelimiter = ', ';
+
+	while (list ($per_CellPhone, $fam_CellPhone) = mysql_fetch_row($rsPhoneList))
+	{
+	    $sPhone = SelectWhichInfo($per_CellPhone, $fam_CellPhone, False);
+	    if ($sPhone)
+	    {
+	        /* if ($sPhoneLink) // Don't put delimiter before first phone
+	            $sPhoneLink .= $sCommaDelimiter;  */
+	        // Add phone only if phone is not already in string
+	        if (!stristr($sPhoneLink, $sPhone))
+	            $sPhoneLink .= $sPhone .= $sCommaDelimiter;
+	    }
+	}
+	if ($sPhoneLink)
+	{
+	    if ($bEmailMailto) { // Does user have permission to email groups
+	
+	    // Display link
+	    echo '<br><a href="javascript:void(0)" onclick="allPhonesCommaD()">Text Cart</a>';
+	    echo '<script>function allPhonesCommaD() {prompt("Press CTRL + C to copy all group members\' phone numbers", "'. mb_substr($sPhoneLink,0,-2) .'")};</script>';
+	    }
+	}
+//Other Links	
         echo "<br><a href=\"MapUsingGoogle.php?GroupID=0\">" . gettext("Map Cart") . "</a>";
         echo "<br><a href=\"Reports/NameTags.php?labeltype=74536&labelfont=times&labelfontsize=36\">" . gettext("Name Tags") . "</a>";
         echo "</p></td>";
