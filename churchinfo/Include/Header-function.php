15c78e27e94c070e95c60f695c1f6042a845e615
abramovkg <abramovkg@gmail.com>
2015-03-01 23:30:11 -0800

    added viewport meta tag

--- churchinfo/Include/Header-function.php
+++ churchinfo/Include/Header-function.php
@@ -33,6 +33,7 @@ global $sPageTitle, $sURLPath;
 
 	$sURLPath = $_SESSION['sURLPath'];
 ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimal-ui"/>
     <meta http-equiv="pragma" content="no-cache">
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
     <link rel="stylesheet" type="text/css" href="<?php echo $sURLPath."/"; ?>Include/jquery/jquery-ui-1.8.18.custom.css">
