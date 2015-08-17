2697aaf00e0d72beaf01b7bc625d6f667bb85431
focuz07 <focuz07@gmail.com>
2015-08-17 01:40:59 -0400

    no official wiki, linked to docs at http://docs.churchcrm.io/en/latest/

--- churchinfo/Help/en/Wiki.php
+++ churchinfo/Help/en/Wiki.php
@@ -1,12 +1,12 @@
 <?php
-	$sPageTitle = "Wiki Documentation";
+	$sPageTitle = "Documentation";
 	require "Include/Header.php";
 ?>
 
 <div class="Help_Section">
-	<div class="Help_Header">ChurchInfo Wiki</div>
+	<div class="Help_Header">ChurchInfo Docs</div>
 
-	<table width="100%" class="LightShadedBox"><tr><td>Up-to-date documentation is maintained by the ChurchInfo community at the <a href="http://www.freechurchmanagement.org/dokuwiki-2013-05-10a/doku.php">ChurchInfo Documentation Wiki</a>. If you would like to contribute to the ducumentation effort, please contact the developers on the <a href="https://sourceforge.net/projects/churchinfo/forums">Churchinfo support forums</a></td></tr></table>
+	<table width="100%" class="LightShadedBox"><tr><td>Up-to-date documentation is maintained at <a href="http://docs.churchcrm.io/en/latest/">Church Web CRM Docs/Help</a>. If you would like to contribute to the ducumentation effort, please contact the developers via <a href="https://github.com/ChurchCRM/CRM">Git hub</a></td></tr></table>
 
 <?php
 	require "Include/Footer.php";
