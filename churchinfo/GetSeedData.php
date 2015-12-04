01266b653ee5a84f5b00648d2ba679c8de175290
crossan007 <crossan007@gmail.com>
2015-12-03 23:56:10 -0500

    Seed Members
    Successfully seed members from random data.

--- churchinfo/GetSeedData.php
+++ churchinfo/GetSeedData.php
@@ -2,7 +2,7 @@
 require "Include/Config.php";
 require "Include/Functions.php";
 
-$count = 10;
+$count = 100;
 $response = file_get_contents("http://api.randomuser.me/?results=".$count);
 $data=json_decode($response);
 
@@ -18,14 +18,14 @@ foreach($rs as $index=>$u)
 	$location = $user->location->street." ". $user->location->city." ". $user->location->state." ". $user->location->zip;
 	print $gender."\r\n";
 	print $name."\r\n";
+	print $user->dob;
+	if ($bHideAge) {
+			$per_Flags = 1;
+	} else {
+		$per_Flags = 0;
+	}
 	
-	
-	$sSQL = "INSERT INTO person_per (per_Title, per_FirstName, per_MiddleName, per_LastName, per_Suffix, per_Gender, per_Address1, per_Address2, per_City, per_State, per_Zip, per_Country, per_HomePhone, per_WorkPhone, per_CellPhone, per_Email, per_WorkEmail, per_BirthMonth, per_BirthDay, per_BirthYear, per_Envelope, per_fam_ID, per_fmr_ID, per_MembershipDate, per_cls_ID, per_DateEntered, per_EnteredBy, per_FriendDate, per_Flags ) VALUES ('" . $user->name->title . "','" . $user->name->first . "',NULL,'" . $user->name->last . "','" . $sSuffix . "'," . $iGender . ",'" . $sAddress1 . "','" . $sAddress2 . "','" . $sCity . "','" . $sState . "','" . $sZip . "','" . $sCountry . "','" . $sHomePhone . "','" . $sWorkPhone . "','" . $sCellPhone . "','" . $sEmail . "','" . $sWorkEmail . "'," . $iBirthMonth . "," . $iBirthDay . "," . $iBirthYear . "," . $iEnvelope . "," . $iFamily . "," . $iFamilyRole . ",";
-	if ( strlen($dMembershipDate) > 0 )
-	$sSQL .= "\"" . $dMembershipDate . "\"";
-	else
-	$sSQL .= "NULL";
-	$sSQL .= "," . $iClassification . ",'" . date("YmdHis") . "'," . $_SESSION['iUserID'] . ",";
+	$sSQL = "INSERT INTO person_per (per_Title, per_FirstName, per_MiddleName, per_LastName, per_Suffix, per_Gender, per_Address1, per_Address2, per_City, per_State, per_Zip, per_Country, per_HomePhone, per_WorkPhone, per_CellPhone, per_Email, per_WorkEmail, per_BirthMonth, per_BirthDay, per_BirthYear, per_Envelope, per_fam_ID, per_fmr_ID, per_MembershipDate, per_cls_ID, per_DateEntered, per_EnteredBy, per_FriendDate, per_Flags ) VALUES ('" . $user->name->title . "','" . $user->name->first . "',NULL,'" . $user->name->last . "',NULL,'" . $user->gender . "','" . $user->location->street . "',NULL,'" . $user->location->city . "','" . $user->location->state . "','" . $user->location->zip . "','USA','" . $user->phone . "',NULL,'" . $user->cell . "','" . $user->email . "',NULL," . date('m', $user->dob) . "," . date('d', $user->dob) . "," . date('Y', $user->dob) . ",NULL,0,0,"."\"" . date('Y-m-d', $user->registered) . "\"". ",1,'" . date("YmdHis") . "'," . $_SESSION['iUserID'] . ",";
 
 	if ( strlen($dFriendDate) > 0 )
 	$sSQL .= "\"" . $dFriendDate . "\"";
@@ -36,7 +36,7 @@ foreach($rs as $index=>$u)
 	$sSQL .= ")";
 
 	$bGetKeyBack = True;
-
+	
 	RunQuery($sSQL);
 
 		// If this is a new person, get the key back and insert a blank row into the person_custom table
