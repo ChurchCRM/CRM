903caaa18b800058ed2d5ca8d9113378cc4eac54
crossan007 <crossan007@gmail.com>
2015-12-16 22:01:34 -0500

    Move and Clean
    Move GetPledgeDetailsByFamily.php into the API

--- churchinfo/api/index.php
+++ churchinfo/api/index.php
@@ -104,6 +104,38 @@ $app->group('/deposits',function () use ($app) {
 $app->group('/payments',function () use ($app) {
 	$app->get('/','listPayments');
 	$app->get('/:id','listPayments')->conditions(array('id' => '[0-9]+'));
+	$app->get('/byFamily/:familyId', function ($familyId) use ($app) {
+		sSQL = "SELECT plg_fundID, plg_amount from pledge_plg where plg_famID=\"" . $familyId . "\" AND plg_PledgeOrPayment=\"Pledge\" AND plg_FYID=\"" . $iFYID . "\";";
+		$rsPledge = RunQuery($sSQL);
+		$totalPledgeAmount = 0;
+		while ($row = mysql_fetch_array($rsPledge)) {
+			$fundID = $row["plg_fundID"];
+			$plgAmount = $row["plg_amount"];
+			$fundID2Pledge[$fundID] = $plgAmount;
+			$totalPledgeAmount = $totalPledgeAmount + $plgAmount;
+		} 
+		if ($fundID2Pledge) {
+			// division rounding can cause total of calculations to not equal total.  Keep track of running total, and asssign any rounding error to 'default' fund
+			$calcTotal = 0;
+			$calcOtherFunds = 0;
+			foreach ($fundID2Pledge as $fundID => $plgAmount) {
+				$calcAmount = round($iTotalAmount * ($plgAmount / $totalPledgeAmount), 2);
+
+				$nAmount[$fundID] = number_format($calcAmount, 2, ".", "");
+				if ($fundID <> $defaultFundID) {
+					$calcOtherFunds = $calcOtherFunds + $calcAmount;
+				}
+
+				$calcTotal += $calcAmount;
+			}
+			if ($calcTotal <> $iTotalAmount) {
+				$nAmount[$defaultFundID] = number_format($iTotalAmount - $calcOtherFunds, 2, ".", "");
+			}
+		} else {
+			$nAmount[$defaultFundID] = number_format($iTotalAmount, 2, ".", "");
+		}
+		
+	});
 });
 
 $app->run();
