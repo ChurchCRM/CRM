e0d0c18e67981ee02c343063ac0f6326e2051c66
crossan007 <crossan007@gmail.com>
2015-12-30 11:35:04 -0500

    Use global property for URL path in getViewURI
    $sURLPath

--- churchinfo/service/FamilyService.php
+++ churchinfo/service/FamilyService.php
@@ -4,7 +4,7 @@ class FamilyService {
     
     function getViewURI($Id)
     {
-        return  "FamilyView.php?FamilyID=".$Id;
+        return  $sURLPath."/FamilyView.php?FamilyID=".$Id;
     }
     
     function search($searchTerm) {
