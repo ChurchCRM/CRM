1b006d32f5048b282affe063dcec907ca1200386
George Dawoud <george@dawouds.com>
2016-01-01 18:35:27 -0800

    moved to common NotloggedIn header/footer

--- churchinfo/Default.php
+++ churchinfo/Default.php
@@ -338,37 +338,9 @@ if ($iUserID > 0)
 ob_start();
 
 // Set the page title and include HTML header
+require ("Include/HeaderNotLoggedIn.php");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--<meta http-equiv="pragma" content="no-cache">-->
-    <meta http-equiv="Content-Type" content="text/html">
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="vendor/AdminLTE/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="vendor/AdminLTE/dist/css/AdminLTE.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="vendor/AdminLTE/plugins/iCheck/square/blue.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <title>ChurchCRM</title>
-</head>
-<body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
         <b>Church</b>CRM</a>
@@ -489,25 +461,11 @@ ob_start();
 </div>
 <!-- /.login-box -->
 
-<!-- jQuery 2.1.4 -->
-<script src="vendor/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-<!-- Bootstrap 3.3.5 -->
-<script src="vendor/AdminLTE/bootstrap/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="vendor/AdminLTE/plugins/iCheck/icheck.min.js"></script>
-<script>
-    $(function () {
-        $('input').iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square-blue',
-            increaseArea: '20%' // optional
-        });
-    });
-</script>
-</body>
-</html>
-
 <?php
+// Set the page title and include HTML header
+require ("Include/FooterNotLoggedIn.php");
+
+
 // Turn OFF output buffering
 ob_end_flush();
 ?>
