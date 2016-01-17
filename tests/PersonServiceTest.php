dd963c613efd29b05ecffd4cceea30a5e711af4e
George Dawoud <george@dawouds.com>
2016-01-16 19:29:27 -0800

    api-cleanup - added test for get person

--- tests/PersonServiceTest.php
+++ tests/PersonServiceTest.php
@@ -10,6 +10,14 @@ class PersonServiceTest extends PHPUnit_Framework_TestCase
         $results = $personService->search("admin");
         $this->assertNotEmpty($results);   
     }
+
+    public function testgetPerson()
+    {
+
+        $personService = new PersonService();
+        $results = $personService->get(1);
+        $this->assertNotEmpty($results);
+    }
     
     public function testGetPhoto()
     {
