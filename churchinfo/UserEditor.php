6310fef62f91cbd391914765d02acbed9dcb9506
George Dawoud <george@dawouds.com>
2016-02-08 22:02:03 -0800

    - removed the need for Mod5 passwords - removed the need for Sha40 passwords

--- churchinfo/UserEditor.php
+++ churchinfo/UserEditor.php
@@ -131,16 +131,16 @@ if (isset($_POST['save']) && $iPersonID > 0) {
 
         // Write the SQL depending on whether we're adding or editing
         if ($sAction == 'add'){
-                                    
-                            if ($undupCount == 0) {
-                                $sSQL = "INSERT INTO user_usr (usr_per_ID, usr_Password, usr_NeedPasswordChange, usr_LastLogin, usr_AddRecords, usr_EditRecords, usr_DeleteRecords, usr_MenuOptions, usr_ManageGroups, usr_Finance, usr_Notes, usr_Communication, usr_Admin, usr_Style, usr_SearchLimit, usr_defaultFY, usr_UserName, usr_EditSelf, usr_Canvasser) VALUES (" . $iPersonID . ",'" . md5($sDefault_Pass) . "',1,'" . date("Y-m-d H:i:s") . "', " . $AddRecords . ", " . $EditRecords . ", " . $DeleteRecords . ", " . $MenuOptions . ", " . $ManageGroups . ", " . $Finance . ", " . $Notes . ", " . $Communication . ", " . $Admin . ", '" . $Style . "', 10," . $defaultFY . ",\"" . $sUserName . "\"," . $EditSelf . "," . $Canvasser . ")";
-                            // Execute the SQL
-                            RunQuery($sSQL);
 
-                            }else{
-                            // Set the error text for duplicate when new user
-                            Redirect('UserEditor.php?NewPersonID='.$PersonID.'&ErrorText=Login already in use, please select a different login!');
-                            }
+            if ($undupCount == 0) {
+                $sPasswordHashSha256 = hash ("sha256", $sDefault_Pass.$iPersonID);
+                $sSQL = "INSERT INTO user_usr (usr_per_ID, usr_Password, usr_NeedPasswordChange, usr_LastLogin, usr_AddRecords, usr_EditRecords, usr_DeleteRecords, usr_MenuOptions, usr_ManageGroups, usr_Finance, usr_Notes, usr_Communication, usr_Admin, usr_Style, usr_SearchLimit, usr_defaultFY, usr_UserName, usr_EditSelf, usr_Canvasser) VALUES (" . $iPersonID . ",'" . $sPasswordHashSha256 . "',1,'" . date("Y-m-d H:i:s") . "', " . $AddRecords . ", " . $EditRecords . ", " . $DeleteRecords . ", " . $MenuOptions . ", " . $ManageGroups . ", " . $Finance . ", " . $Notes . ", " . $Communication . ", " . $Admin . ", '" . $Style . "', 10," . $defaultFY . ",\"" . $sUserName . "\"," . $EditSelf . "," . $Canvasser . ")";
+                // Execute the SQL
+                RunQuery($sSQL);
+            }else{
+                // Set the error text for duplicate when new user
+                Redirect('UserEditor.php?NewPersonID='.$PersonID.'&ErrorText=Login already in use, please select a different login!');
+            }
         } else{
         
         if ($undupCount == 0) {
