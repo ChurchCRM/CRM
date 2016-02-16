321c7b11ddb8026796fb2164867fc0ef78805974
George Dawoud <george@dawouds.com>
2016-02-15 17:18:15 -0800

    fixed copy script, also now cleans up old copies before it moves things

--- vagrant/build-skin.sh
+++ vagrant/build-skin.sh
@@ -7,20 +7,29 @@ composer install
 
 echo "Build: Composer install"
 
-cp -rf /dev/churchcrm/vendor/almasaeed2010/adminlte/dist/ /var/www/public/skin/adminlte/
+rm -rf /var/www/public/skin/adminlte
+rm -rf /var/www/public/skin/font-awesome/
+rm -rf /var/www/public/skin/ionicons/
+
+echo "Cleaning old copies"
+
+mkdir /var/www/public/skin/adminlte
+cp -rf /dev/churchcrm/vendor/almasaeed2010/adminlte/dist/ /var/www/public/skin/adminlte/dist
 cp -rf /dev/churchcrm/vendor/almasaeed2010/adminlte/bootstrap/ /var/www/public/skin/adminlte/
 cp -rf /dev/churchcrm/vendor/almasaeed2010/adminlte/plugins/ /var/www/public/skin/adminlte/
 
 echo "Build: AdminLTE copied"
 
-cp -rf /dev/churchcrm/vendor/components/font-awesome/css/ /var/www/public/skin/font-awesome/
+mkdir /var/www/public/skin/font-awesome/
+cp -rf /dev/churchcrm/vendor/components/font-awesome/css/ /var/www/public/skin/font-awesome/css/
 cp -rf /dev/churchcrm/vendor/components/font-awesome/fonts/ /var/www/public/skin/font-awesome/
 cp -rf /dev/churchcrm/vendor/components/font-awesome/less/ /var/www/public/skin/font-awesome/
 cp -rf /dev/churchcrm/vendor/components/font-awesome/scss/ /var/www/public/skin/font-awesome/
 
 echo "Build: font-awesome copied"
 
-cp -rf /dev/churchcrm/vendor/driftyco/ionicons/css/ /var/www/public/skin/ionicons/
+mkdir /var/www/public/skin/ionicons/
+cp -rf /dev/churchcrm/vendor/driftyco/ionicons/css/ /var/www/public/skin/ionicons/css/
 cp -rf /dev/churchcrm/vendor/driftyco/ionicons/fonts/ /var/www/public/skin/ionicons/
 cp -rf /dev/churchcrm/vendor/driftyco/ionicons/less/ /var/www/public/skin/ionicons/
 cp -rf /dev/churchcrm/vendor/driftyco/ionicons/png/ /var/www/public/skin/ionicons/
