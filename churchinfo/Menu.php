45f36e3945b417e7cbb84aacce67f4a59adb0f18
George Dawoud <george@dawouds.com>
2016-02-20 14:40:19 -0800

    matching new path for adminLte

--- churchinfo/Menu.php
+++ churchinfo/Menu.php
@@ -56,7 +56,7 @@ $sPageTitle = "Welcome to <b>Church</b>CRM";
 
 require 'Include/Header.php';
 ?>
-<script src="<?= $sRootPath ?>/vendor/almasaeed2010/adminlte/plugins/chartjs/Chart.min.js"></script>
+<script src="<?= $sRootPath ?>/skin/adminlte/plugins/chartjs/Chart.min.js"></script>
 
 <!-- Small boxes (Stat box) -->
 <div class="row">
