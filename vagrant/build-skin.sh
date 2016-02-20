5262b3f8b6ab3a8772886ca0bf4eb99a2735ac5b
George Dawoud <george@dawouds.com>
2016-02-20 09:14:29 -0800

    - updated build path to /vagrant

--- vagrant/build-skin.sh
+++ vagrant/build-skin.sh
@@ -2,7 +2,7 @@
 
 echo "============================= Build ==================================="
 
-cd /dev/churchcrm
+cd /vagrant
 composer install
 
 echo "Build: Composer install"
@@ -14,26 +14,26 @@ rm -rf /var/www/public/skin/ionicons/
 echo "Cleaning old copies"
 
 mkdir /var/www/public/skin/adminlte
-cp -rf /dev/churchcrm/vendor/almasaeed2010/adminlte/dist/ /var/www/public/skin/adminlte/dist
-cp -rf /dev/churchcrm/vendor/almasaeed2010/adminlte/bootstrap/ /var/www/public/skin/adminlte/
-cp -rf /dev/churchcrm/vendor/almasaeed2010/adminlte/plugins/ /var/www/public/skin/adminlte/
+cp -rf /vagrant/vendor/almasaeed2010/adminlte/dist/ /var/www/public/skin/adminlte/dist
+cp -rf /vagrant/vendor/almasaeed2010/adminlte/bootstrap/ /var/www/public/skin/adminlte/
+cp -rf /vagrant/vendor/almasaeed2010/adminlte/plugins/ /var/www/public/skin/adminlte/
 
 echo "Build: AdminLTE copied"
 
 mkdir /var/www/public/skin/font-awesome/
-cp -rf /dev/churchcrm/vendor/components/font-awesome/css/ /var/www/public/skin/font-awesome/css/
-cp -rf /dev/churchcrm/vendor/components/font-awesome/fonts/ /var/www/public/skin/font-awesome/
-cp -rf /dev/churchcrm/vendor/components/font-awesome/less/ /var/www/public/skin/font-awesome/
-cp -rf /dev/churchcrm/vendor/components/font-awesome/scss/ /var/www/public/skin/font-awesome/
+cp -rf /vagrant/vendor/components/font-awesome/css/ /var/www/public/skin/font-awesome/css/
+cp -rf /vagrant/vendor/components/font-awesome/fonts/ /var/www/public/skin/font-awesome/
+cp -rf /vagrant/vendor/components/font-awesome/less/ /var/www/public/skin/font-awesome/
+cp -rf /vagrant/vendor/components/font-awesome/scss/ /var/www/public/skin/font-awesome/
 
 echo "Build: font-awesome copied"
 
 mkdir /var/www/public/skin/ionicons/
-cp -rf /dev/churchcrm/vendor/driftyco/ionicons/css/ /var/www/public/skin/ionicons/css/
-cp -rf /dev/churchcrm/vendor/driftyco/ionicons/fonts/ /var/www/public/skin/ionicons/
-cp -rf /dev/churchcrm/vendor/driftyco/ionicons/less/ /var/www/public/skin/ionicons/
-cp -rf /dev/churchcrm/vendor/driftyco/ionicons/png/ /var/www/public/skin/ionicons/
+cp -rf /vagrant/vendor/driftyco/ionicons/css/ /var/www/public/skin/ionicons/css/
+cp -rf /vagrant/vendor/driftyco/ionicons/fonts/ /var/www/public/skin/ionicons/
+cp -rf /vagrant/vendor/driftyco/ionicons/less/ /var/www/public/skin/ionicons/
+cp -rf /vagrant/vendor/driftyco/ionicons/png/ /var/www/public/skin/ionicons/
 
 echo "Build: ionicons copied"
 
-rm -rf /dev/churchcrm/vendor
+rm -rf /vagrant/vendor
