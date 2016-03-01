aaccf87d83e9d6a4b54b94d757abeb4fdb30aa9d
crossan007 <crossan007@gmail.com>
2016-02-29 20:25:12 -0500

    move to js

--- churchinfo/FindDepositSlip.php
+++ churchinfo/FindDepositSlip.php
@@ -26,7 +26,7 @@ $sPageTitle = gettext("Deposit Listing");
 // Security: User must have finance permission to use this form
 if (!$_SESSION['bFinance'])
 {
-    Redirect("Menu.php");
+    Redirect("index.php");
     exit;
 }
 
@@ -110,152 +110,17 @@ require "Include/Header.php";
         <table class="table" id="depositsTable">
         </table>
 
-        <button type="button" id="deleteSelectedRows" class="btn btn-danger" disabled> Delete Selected Rows</button>
-        <button type="button" id="exportSelectedRows" class="btn btn-success" disabled><i class="fa fa-download"></i> Export Selected Rows (OFX)</button>
-        <button type="button" id="exportSelectedRowsCSV" class="btn btn-success" disabled><i class="fa fa-download"></i> Export Selected Rows (CSV)</button>
-        <button type="button" id="generateDepositSlip" class="btn btn-success" disabled> Generate Deposit Split for Selected Rows (PDF)</button>
+        <button type="button" id="deleteSelectedRows" class="btn btn-danger" disabled> <?= gettext("Delete Selected Rows") ?> </button>
+        <button type="button" id="exportSelectedRows" class="btn btn-success" disabled><i class="fa fa-download"></i> <?= gettext("Export Selected Rows (OFX)") ?></button>
+        <button type="button" id="exportSelectedRowsCSV" class="btn btn-success" disabled><i class="fa fa-download"></i> <?= gettext("Export Selected Rows (CSV)") ?></button>
+        <button type="button" id="generateDepositSlip" class="btn btn-success" disabled> <?= gettext("Generate Deposit Split for Selected Rows (PDF)") ?></button>
     </div>
 </div>
 
 <script>
 var depositData = <?php $json = $financialService->getDepositJSON($financialService->getDeposits()); if ($json) { echo $json; } else { echo 0; } ?>;
-
-if (!$.isArray(depositData.deposits))
-{
-    depositData.deposits=[depositData.deposits];
-}
-var dataT = 0;
-$(document).ready(function() {
-    $("#depositDate").datepicker({format:'yyyy-mm-dd'}).datepicker("setDate", new Date());
-    $("#addNewDeposit").click(function (e){
-        var newDeposit = {
-            'depositType':$("#depositType option:selected").val(),
-            'depositComment':$("#depositComment").val(),
-            'depositDate':$("#depositDate").val()
-        };
-        $.ajax({
-            method: "POST",
-            url:   "/api/deposits",
-            data:  JSON.stringify(newDeposit)
-        }).done(function(data){
-            dataT.row.add(data[0]);
-            dataT.rows().invalidate().draw(true);
-        });
-    });
-   
-    dataT = $("#depositsTable").DataTable({
-    data:depositData.deposits,
-    columns: [
-    {
-        width: 'auto',
-        title:'Deposit ID',
-        data:'dep_ID',
-        render: function  (data, type, full, meta ) {
-            if (type === 'display')
-            {
-                return '<a href=\'DepositSlipEditor.php?DepositSlipID='+full.dep_ID+'\'><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-search-plus fa-stack-1x fa-inverse"></i></span></a>'+full.dep_ID; 
-            }
-            else
-            {
-                return parseInt(full.dep_ID);
-            }
-        },
-        type:'num'
-    },
-    {
-        width: 'auto',
-        title:'Deposit Date',
-        data:'dep_Date',
-        searchable: true
-    },
-    {
-        width: 'auto',
-        title:'Deposit Total',
-        data:'dep_Total',
-        searchable: false,
-    },
-    {
-        width: 'auto',
-        title:'Deposit Comment',
-        data:'dep_Comment',
-        searchable: true
-    },
-    {
-        width: 'auto',
-        title:'Closed',
-        data:'dep_Closed',
-        searchable: true,
-        render: function (data,type,full,meta) {
-            return data == 1 ? 'Yes' : 'No';
-        }
-    },
-    {
-        width: 'auto',
-        title:'Deposit Type',
-        data:'dep_Type',
-        searchable: true
-    }
-    ],
-    order:[0,'desc']
-    });
-    
-    $("#depositsTable tbody").on('click', 'tr', function() {
-         $(this).toggleClass('selected');
-         var selectedRows = dataT.rows('.selected').data().length;
-          $("#deleteSelectedRows").prop('disabled', !(selectedRows));
-          $("#deleteSelectedRows").text("Delete ("+selectedRows+") Selected Rows");
-          $("#exportSelectedRows").prop('disabled', !(selectedRows));
-          $("#exportSelectedRows").html("<i class=\"fa fa-download\"></i> Export ("+selectedRows+") Selected Rows (OFX)");
-          $("#exportSelectedRowsCSV").prop('disabled', !(selectedRows));
-          $("#exportSelectedRowsCSV").html("<i class=\"fa fa-download\"></i> Export ("+selectedRows+") Selected Rows (CSV)");
-          $("#generateDepositSlip").prop('disabled', !(selectedRows));
-          $("#generateDepositSlip").html("<i class=\"fa fa-download\"></i> Generate Deposit Split for Selected ("+selectedRows+") Rows (PDF)");
-    });
-     
-    $('#deleteSelectedRows').click(function() {
-        var deletedRows = dataT.rows('.selected').data()
-        $("#deleteNumber").text(deletedRows.length);
-        $("#confirmDelete").modal('show');
-    });
-    
-    
-    $('#exportSelectedRows').click(function() {
-        var selectedRows = dataT.rows('.selected').data()
-        $.each(selectedRows, function(index, value){
-            window.open('/api/deposits/'+value.dep_ID+'/ofx');
-        });
-    });
-    
-    $('#exportSelectedRowsCSV').click(function() {
-        var selectedRows = dataT.rows('.selected').data()
-        $.each(selectedRows, function(index, value){
-            window.open('/api/deposits/'+value.dep_ID+'/csv');
-        });
-    });
-    
-    $('#generateDepositSlip').click(function() {
-        var selectedRows = dataT.rows('.selected').data()
-        $.each(selectedRows, function(index, value){
-            window.open('/api/deposits/'+value.dep_ID+'/pdf');
-        });
-    });
-
-    $("#deleteConfirmed").click(function() {
-        var deletedRows = dataT.rows('.selected').data()
-        $.each(deletedRows, function(index, value){
-            $.ajax({
-                type        : 'DELETE', // define the type of HTTP verb we want to use (POST for our form)
-                url         : '/api/deposits/'+value.dep_ID, // the url where we want to POST
-                dataType    : 'json', // what type of data do we expect back from the server
-                encode      : true
-            })
-            .done(function(data) {
-                $('#confirmDelete').modal('hide');
-                dataT.rows('.selected').remove().draw(false);
-            });
-        });
-    });
-});
 </script>
+
+<script src="<?= $sRootPath; ?>/js/FindDepositSlip.js"></script>
 
 <?php require "Include/Footer.php" ?>
