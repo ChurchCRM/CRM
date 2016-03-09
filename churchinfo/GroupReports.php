8c5ad70b3cd3bc1e3b1bc1ce1d14c7e58cf607de
crossan007 <crossan007@gmail.com>
2016-03-08 21:51:58 -0500

    group reports 1st pass

--- churchinfo/GroupReports.php
+++ churchinfo/GroupReports.php
@@ -88,106 +88,117 @@ function updateGroupRoles(generated_html)
 </script>
 
 <?php if (!isset($_POST["GroupID"])) { ?>
-
-<p align="center"><?= gettext("Select the group you would like to report:") ?></p>
-<form method="POST" action="GroupReports.php">
-<table align="center">
-	<tr>
-		<td class="LabelColumn"><?= gettext("Select Group:") ?></td>
-		<td class="TextColumn">
-			<?php
-			// Create the group select drop-down
-			echo "<select id=\"GroupID\" name=\"GroupID\" onChange=\"UpdateRoles();\"><option value=\"0\">". gettext('None') . "</option>";
-			while ($aRow = mysql_fetch_array($rsGroups)) {
-				extract($aRow);
-				echo "<option value=\"" . $grp_ID . "\">" . $grp_Name . "</option>";
-			}
-			echo "</select>";
-			?>
-		</td>
-	</tr>
-	<tr>
-		<td class="LabelColumn"><?= gettext("Select Role:") ?></td>
-		<td class="TextColumn"><span id="GroupRole"><?= gettext("No Group Selected") ?></span></td>
-	</tr>
-	<tr>
-		<td class="LabelColumn"><?= gettext("Only cart persons?:") ?></td>
-		<td class="TextColumn"><input type="checkbox" Name="OnlyCart" value="1"></td>
-	</tr>
-	<tr>
-		<td class="LabelColumn"><?= gettext("Report Model:") ?></td>
-		<td class="TextColumn">
-			<input type="radio" Name="ReportModel" value="1" checked><?= gettext("Report for group and role selected") ?><br>
-			<input type="radio" Name="ReportModel" value="2"><?= gettext("Report for any role in group selected") ?><br>
-<?php //			<input type="radio" Name="ReportModel" value="3"><?= gettext("Report any group and role") ?>
-		</td>
-	</tr>
-</table>
-<p align="center">
-<BR>
-<input type="submit" class="btn" name="Submit" value="<?= gettext("Next") ?>">
-<input type="button" class="btn" name="Cancel" value="<?= gettext("Cancel") ?>" onclick="javascript:document.location='ReportList.php';">
-</p>
-</form>
+<div class="row">
+    <div class="box col-lg-4 col-xs-4">
+        <div class="box-header with-border">
+            <h3 class="box-title"><?= gettext("Select the group you would like to report:") ?></h3>
+        </div>
+        <div class="box-body">
+            <form method="POST" action="GroupReports.php">
+            <table align="center">
+                <tr>
+                    <td class="LabelColumn"><?= gettext("Select Group:") ?></td>
+                    <td class="TextColumn">
+                        <?php
+                        // Create the group select drop-down
+                        echo "<select id=\"GroupID\" name=\"GroupID\" onChange=\"UpdateRoles();\"><option value=\"0\">". gettext('None') . "</option>";
+                        while ($aRow = mysql_fetch_array($rsGroups)) {
+                            extract($aRow);
+                            echo "<option value=\"" . $grp_ID . "\">" . $grp_Name . "</option>";
+                        }
+                        echo "</select>";
+                        ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="LabelColumn"><?= gettext("Select Role:") ?></td>
+                    <td class="TextColumn"><span id="GroupRole"><?= gettext("No Group Selected") ?></span></td>
+                </tr>
+                <tr>
+                    <td class="LabelColumn"><?= gettext("Only cart persons?:") ?></td>
+                    <td class="TextColumn"><input type="checkbox" Name="OnlyCart" value="1"></td>
+                </tr>
+                <tr>
+                    <td class="LabelColumn"><?= gettext("Report Model:") ?></td>
+                    <td class="TextColumn">
+                        <input type="radio" Name="ReportModel" value="1" checked><?= gettext("Report for group and role selected") ?><br>
+                        <input type="radio" Name="ReportModel" value="2"><?= gettext("Report for any role in group selected") ?><br>
+            <?php //			<input type="radio" Name="ReportModel" value="3"><?= gettext("Report any group and role") ?>
+                    </td>
+                </tr>
+            </table>
+            <p align="center">
+            <BR>
+            <input type="submit" class="btn" name="Submit" value="<?= gettext("Next") ?>">
+            <input type="button" class="btn" name="Cancel" value="<?= gettext("Cancel") ?>" onclick="javascript:document.location='ReportList.php';">
+            </p>
+            </form>
+        </div>
+   </div>
+</div>
 
 <?php } else {
 
 $iGroupID = FilterInput($_POST['GroupID'],'int');
 
 ?>
+<div class="row">
+    <div class="box col-lg-4 col-xs-4">
+        <div class="box-header with-border">
+            <h3 class="box-title"><?= gettext("Select which information you want to include") ?></h3>
+        </div>
+        <div class="box-body">
 
-<form method="POST" action="Reports/GroupReport.php">
-<input type="hidden" Name="GroupID" <?= "value=\"" . $iGroupID . "\"" ?>>
-<input type="hidden" Name="GroupRole" <?php if (array_key_exists ('GroupRole', $_POST)) { echo "value=\"" . $_POST['GroupRole'] . "\""; } ?>>
-<input type="hidden" Name="OnlyCart" <?php if (array_key_exists ('OnlyCart', $_POST)) { echo "value=\"" . $_POST['OnlyCart'] . "\""; } ?>>
-<input type="hidden" Name="ReportModel" <?= "value=\"" . $_POST['ReportModel'] . "\"" ?>>
+            <form method="POST" action="Reports/GroupReport.php">
+            <input type="hidden" Name="GroupID" <?= "value=\"" . $iGroupID . "\"" ?>>
+            <input type="hidden" Name="GroupRole" <?php if (array_key_exists ('GroupRole', $_POST)) { echo "value=\"" . $_POST['GroupRole'] . "\""; } ?>>
+            <input type="hidden" Name="OnlyCart" <?php if (array_key_exists ('OnlyCart', $_POST)) { echo "value=\"" . $_POST['OnlyCart'] . "\""; } ?>>
+            <input type="hidden" Name="ReportModel" <?= "value=\"" . $_POST['ReportModel'] . "\"" ?>>
 
-<?php
+            <?php
 
-$sSQL = "SELECT prop_Field, prop_Name FROM groupprop_master WHERE grp_ID = " . $iGroupID . " ORDER BY prop_ID";
-$rsPropFields = RunQuery($sSQL);
+            $sSQL = "SELECT prop_Field, prop_Name FROM groupprop_master WHERE grp_ID = " . $iGroupID . " ORDER BY prop_ID";
+            $rsPropFields = RunQuery($sSQL);
 
-?>
+            ?>
+            
+            <table align="center">
+                <tr>
+                    <td class="LabelColumn"><?= gettext("Standard Info:") ?></td>
+                    <td class="TextColumn">
+                        <input type="checkbox" Name="AddressEnable" value="1"> <?= gettext("Address") ?> <br>
+                        <input type="checkbox" Name="HomePhoneEnable" value="1"> <?= gettext("Home Phone") ?> <br>
+                        <input type="checkbox" Name="WorkPhoneEnable" value="1"> <?= gettext("Work Phone") ?> <br>
+                        <input type="checkbox" Name="CellPhoneEnable" value="1"> <?= gettext("Cell Phone") ?> <br>
+                        <input type="checkbox" Name="EmailEnable" value="1"> <?= gettext("Email") ?> <br>
+                        <input type="checkbox" Name="OtherEmailEnable" value="1"> <?= gettext("Other Email") ?> <br>
+                        <input type="checkbox" Name="GroupRoleEnable" value="1"> <?= gettext("GroupRole") ?> <br>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="LabelColumn"><?= gettext("Group-Specific Property Fields:") ?></td>
+                    <td class="TextColumn">
+                        <?php
+                            if (mysql_num_rows($rsPropFields) > 0)
+                            {
+                                while ($aRow = mysql_fetch_array($rsPropFields)) {
+                                    extract($aRow);
+                                    echo "<input type=\"checkbox\" Name=\"" . $prop_Field . "enable\" value=\"1\">" . $prop_Name . "<br>";
+                                }
+                            }
+                            else
+                                echo gettext("None");
+                        ?>
+                    </td>
+                </tr>
+            </table>
 
-<p align="center" class="MediumText"><?= gettext("Select which information you want to include") ?></p>
-
-<table align="center">
-	<tr>
-		<td class="LabelColumn"><?= gettext("Standard Info:") ?></td>
-		<td class="TextColumn">
-			<input type="checkbox" Name="AddressEnable" value="1"> <?= gettext("Address") ?> <br>
-			<input type="checkbox" Name="HomePhoneEnable" value="1"> <?= gettext("Home Phone") ?> <br>
-			<input type="checkbox" Name="WorkPhoneEnable" value="1"> <?= gettext("Work Phone") ?> <br>
-			<input type="checkbox" Name="CellPhoneEnable" value="1"> <?= gettext("Cell Phone") ?> <br>
-			<input type="checkbox" Name="EmailEnable" value="1"> <?= gettext("Email") ?> <br>
-			<input type="checkbox" Name="OtherEmailEnable" value="1"> <?= gettext("Other Email") ?> <br>
-			<input type="checkbox" Name="GroupRoleEnable" value="1"> <?= gettext("GroupRole") ?> <br>
-		</td>
-	</tr>
-	<tr>
-		<td class="LabelColumn"><?= gettext("Group-Specific Property Fields:") ?></td>
-		<td class="TextColumn">
-			<?php
-				if (mysql_num_rows($rsPropFields) > 0)
-				{
-					while ($aRow = mysql_fetch_array($rsPropFields)) {
-						extract($aRow);
-						echo "<input type=\"checkbox\" Name=\"" . $prop_Field . "enable\" value=\"1\">" . $prop_Name . "<br>";
-					}
-				}
-				else
-					echo gettext("None");
-			?>
-		</td>
-	</tr>
-</table>
-
-<p align="center">
-<BR>
-<input type="submit" class="btn" name="Submit" value="<?= gettext("Create Report") ?>">
-<input type="button" class="btn" name="Cancel" value="<?= gettext("Cancel") ?>" onclick="javascript:document.location='Menu.php';">
-</p>
-</form>
+            <p align="center">
+            <BR>
+            <input type="submit" class="btn" name="Submit" value="<?= gettext("Create Report") ?>">
+            <input type="button" class="btn" name="Cancel" value="<?= gettext("Cancel") ?>" onclick="javascript:document.location='Menu.php';">
+            </p>
+            </form>
 
 <?php } ?>
 
