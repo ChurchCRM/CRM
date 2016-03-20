7a3fd0c823b29fcca52d3e4ace557242f92c7ea9
George Dawoud <george@dawouds.com>
2016-03-20 01:27:30 -0700

    cleaner JS

--- churchinfo/calendar.php
+++ churchinfo/calendar.php
@@ -11,8 +11,8 @@ foreach ($birthDays as $birthDay) {
               start: new Date(". $year. ", ". $birthDay["birthMonth"]. ", ". $birthDay["birthDay"]. "),
               url: '".$birthDay["uri"]."',
               backgroundColor: '#f56954', //red
-              borderColor: '#f56954' //red
-
+              borderColor: '#f56954', //red
+              allDay: true
             }";
 
   array_push($events, $event);
@@ -55,40 +55,8 @@ require "Include/Header.php"; ?>
 <script src="<?= $sRootPath ?>/skin/adminlte/plugins/fullcalendar/fullcalendar.min.js"></script>
 <script>
   $(function () {
-
-    /* initialize the external events
-     -----------------------------------------------------------------*/
-    function ini_events(ele) {
-      ele.each(function () {
-
-        // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
-        // it doesn't need to have a start or end
-        var eventObject = {
-          title: $.trim($(this).text()) // use the element's text as the event title
-        };
-
-        // store the Event Object in the DOM element so we can get to it later
-        $(this).data('eventObject', eventObject);
-
-        // make the event draggable using jQuery UI
-        $(this).draggable({
-          zIndex: 1070,
-          revert: true, // will cause the event to go back to its
-          revertDuration: 0  //  original position after the drag
-        });
-
-      });
-    }
-
-    ini_events($('#external-events div.external-event'));
-
     /* initialize the calendar
      -----------------------------------------------------------------*/
-    //Date for the calendar events (dummy data)
-    var date = new Date();
-    var d = date.getDate(),
-      m = date.getMonth(),
-      y = date.getFullYear();
     $('#calendar').fullCalendar({
       header: {
         left: 'prev,next today',
@@ -104,28 +72,7 @@ require "Include/Header.php"; ?>
       //Random default events
       events: [
         <?= implode(",", $events) ?>
-      ],
-      editable: true,
-      droppable: true, // this allows things to be dropped onto the calendar !!!
-      drop: function (date, allDay) { // this function is called when something is dropped
-
-        // retrieve the dropped element's stored Event Object
-        var originalEventObject = $(this).data('eventObject');
-
-        // we need to copy it, so that multiple events don't have a reference to the same object
-        var copiedEventObject = $.extend({}, originalEventObject);
-
-        // assign it the date that was reported
-        copiedEventObject.start = date;
-        copiedEventObject.allDay = allDay;
-        copiedEventObject.backgroundColor = $(this).css("background-color");
-        copiedEventObject.borderColor = $(this).css("border-color");
-
-        // render the event on the calendar
-        // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-        $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
-
-      }
+      ]
     });
  });
 </script>
