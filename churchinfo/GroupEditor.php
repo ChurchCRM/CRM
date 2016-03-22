5ab43869ff345a192e283cacd7fae43f7bad5e70
crossan007 <crossan007@gmail.com>
2016-03-21 21:50:18 -0400

    Fix #471

--- churchinfo/GroupEditor.php
+++ churchinfo/GroupEditor.php
@@ -162,7 +162,7 @@ require "Include/Header.php";
         <div class="alert alert-info alert-dismissable">
             <i class="fa fa-info"></i>
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <strong></strong>Group name changes are saved as soon as the box loses focus
+            <strong></strong>Group role name changes are saved as soon as the box loses focus
         </div>
         <table class="table" id="groupRoleTable">
         </table>
