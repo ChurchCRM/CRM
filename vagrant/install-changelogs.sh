f9320b6f8a7d2503c53c8c7bf687de37aa7e585c
George Dawoud <george@dawouds.com>
2016-03-21 21:57:32 -0700

    beta 5 released

--- vagrant/install-changelogs.sh
+++ vagrant/install-changelogs.sh
@@ -8,7 +8,7 @@ cd ..
 
 github_changelog_generator -t 64f5ebabc85c0533ed7e69f0c8ecf8c5981a1c50
 
-echo =====================================================================
+echo ======================================================================
 echo === Generat changelog by command
 echo === github_changelog_generator -t 64f5ebabc85c0533ed7e69f0c8ecf8c5981a1c50
 echo =====================================================================
