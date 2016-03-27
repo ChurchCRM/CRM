5ca0dc923d38c37227b55fdbed46ac9164622b61
George Dawoud <george@dawouds.com>
2016-03-27 01:01:20 -0700

    updated sunday school dashboard

--- churchinfo/sundayschool/SundaySchoolDashboard.php
+++ churchinfo/sundayschool/SundaySchoolDashboard.php
@@ -100,7 +100,6 @@ require "../Include/Header.php";
   <div class="col-md-3 col-sm-6 col-xs-12">
     <div class="info-box">
       <span class="info-box-icon bg-fuchsia"><i class="fa fa-female"></i></span>
-
       <div class="info-box-content">
         <span class="info-box-text">Girls</span>
         <span class="info-box-number"> <?= $femaleKids ?></span>
