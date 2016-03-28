70f19b74f422e8d4e2d582fc58c498c45884c658
George Dawoud <george@dawouds.com>
2016-03-28 00:32:05 -0700

    CSV Now include groups
    - Shows the role if the person is a member
    - Shows parent if the person's kid is a member of a sunday school group

--- churchinfo/mailchimp/MailChimpCsvExport.php
+++ churchinfo/mailchimp/MailChimpCsvExport.php
@@ -1,6 +1,39 @@
 <?php
 require "../Include/Config.php";
 require "../Include/Functions.php";
+require "../Service/GroupService.php";
+require "../Service/SundaySchoolService.php";
+
+$groupService = new GroupService();
+$sundaySchoolService = new SundaySchoolService();
+$groups = $groupService->getGroups();
+
+$colNames = array();
+array_push($colNames, "CRM ID");
+array_push($colNames, "FirstName");
+array_push($colNames, "LastName");
+array_push($colNames, "Email");
+foreach ($groups as $group) {
+  array_push($colNames, $group["groupName"]);
+}
+
+$sundaySchoolsParents = array();
+foreach ($groups as $group) {
+  if ($group["grp_Type"] == 4) {
+    $sundaySchoolParents = array();
+    $kids = $sundaySchoolService->getKidsFullDetails($group["id"]);
+    $parentIds = array();
+    foreach ($kids as $kid) {
+      if ($kid["dadId"] != "") {
+        array_push($parentIds, $kid["dadId"]);
+      }
+      if ($kid["momId"] != "") {
+        array_push($parentIds, $kid["momId"]);
+      }
+    }
+    $sundaySchoolsParents[$group["id"]] = $parentIds;
+  }
+}
 
 header("Content-type: text/csv");
 header("Content-Disposition: attachment; filename=EmailExport-" . date("Ymd") . ".csv");
@@ -8,13 +41,23 @@ header("Pragma: no-cache");
 header("Expires: 0");
 
 $out = fopen('php://output', 'w');
-
-$sSQL = "select per_FirstName, per_LastName, per_Email, per_ID from person_per where per_Email != '' order by per_id";
-$rsPeopleWithEmails = RunQuery($sSQL);
-
-fputcsv($out, array('FirstName', 'LastName', 'Email', "CRM ID"));
-while ($row = mysql_fetch_array($rsPeopleWithEmails)) {
-  fputcsv($out, array($row["per_FirstName"], $row["per_LastName"], $row["per_Email"], $row["per_ID"]));
+fputcsv($out, $colNames);
+foreach ($personService->getPeopleEmailsAndGroups() as $person) {
+  $row = array();
+  array_push($row, $person["id"]);
+  array_push($row, $person["firstName"]);
+  array_push($row, $person["lastName"]);
+  array_push($row, $person["email"]);
+  foreach ($groups as $group) {
+    $groupRole = $person[$group["groupName"]];
+    if ($groupRole == "" && $group["grp_Type"] == 4) {
+      if (in_array($person["id"], $sundaySchoolsParents[$group["id"]])) {
+        $groupRole = "Parent";
+      }
+    }
+    array_push($row, $groupRole);
+  }
+  fputcsv($out, $row);
 }
 fclose($out);
 ?>
