ChurchCRM/CRM

commit 0eb85555fe48195b021d129d5eb103444e8ff612

API security Closes #516 (#515)
* initial idea

* Financial and System Service protected by access level

* Underp comments

* prepare for merge

* API Security

* Try to pass PHPUnit test with Session

* Formatting And update Index.php try/catch.
Begin Documenting Security.

* Secure more services.  Fix Secured search (Trim results appropriately)

* use sRootPath in PersonService

* Formatting.  Fix tests

* bAdmin = SuperUser. Always.

* Fix invalid JSON.
Fix Double-Check in GroupService.
Fix PersonService sRootPath.
Fix OmniSearch Javascript because of API change.

* Address @DawoudIO Comments

churchinfo/Menu.php
@@ -49,9 +49,10 @@ $dashboardService = new DashboardService();
 $personCount = $dashboardService->getPersonCount();
 $familyCount = $dashboardService->getFamilyCount();
 $groupStats = $dashboardService->getGroupStats();
-$depositData = $financialService->getDeposits();  //Get the deposit data from the financialService
 $shouldDisplayDeposits = $_SESSION['bFinance'] && count($depositData) > 1;  //Determine whether or not we should display the deposit line graph
-
+if ($shouldDisplayDeposits) {
+  $depositData = $financialService->getDeposits();  //Get the deposit data from the financialService
+}
 
 // Set the page title
 $sPageTitle = "Welcome to <b>Church</b>CRM";

churchinfo/BackupDatabase.php
@@ -23,8 +23,8 @@ require "Include/Functions.php";
 // Otherwise, re-direct them to the main menu.
 if (!$_SESSION['bAdmin'])
 {
-	Redirect("Menu.php");
-	exit;
+  Redirect("Menu.php");
+  exit;
 }
 
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {

churchinfo/Include/Functions.php
@@ -2011,4 +2011,30 @@ function getMailingAddress($Address1, $Address2, $City, $State, $Zip, $Country)
     return $mailingAddress;
 }
 
+function requireUserGroupMembership($allowedRoles=null)
+{
+  if(!$allowedRoles)
+  {
+    throw new Exception("Role(s) must be defined for the function which you are trying to access.  End users should never see this error unless something went horribly wrong.");
+  }
+  if ($_SESSION[$allowedRoles] || $_SESSION['bAdmin'])  //most of the time the API endpoint will specify a single permitted role, or the user is an admin
+  {
+    return true;
+  }
+  elseif (is_array($allowedRoles))  //sometimes we might have an array of allowed roles.
+  {
+    foreach($allowedRoles as $role)
+    {
+      if($_SESSION[$role])
+      {
+        // The current allowed role is in the user's session variable
+        return true;
+      }
+    }
+  }
+ 
+  //if we get to this point in the code, then the user is not authorized.
+  throw new Exception("User is not authorized to access " . debug_backtrace()[1]['function'], 401);
+}
+
 ?>

churchinfo/Include/Header.php
@@ -55,11 +55,16 @@ $MenuFirst = 1;
     }
 
     $(document).ajaxError(function (evt, xhr, settings) {
+      var CRMResponse = JSON.parse(xhr.responseText).error;
+      
       if (xhr.status == 400) {
         displayMessage("[" + settings.type + "] " + settings.url, "Invalid Request.");
       }
-      else if (evt.severity > 0) {
-        displayMessage("[" + settings.type + "] " + settings.url, xhr.responseText);
+      else if (CRMResponse && CRMResponse.severity > 0) {
+        displayMessage("[" + settings.type + "] " + settings.url, CRMResponse.text);
+      }
+      else {
+        displayMessage("[" + settings.type + "] " + settings.url, "Something terrible happened that should have never happened.  Please make a bug report and tell us what you did to receive this error.");
       }
     });
   </script>

churchinfo/Service/DataSeedService.php
@@ -2,14 +2,14 @@
 
 class DataSeedService
 {
-  function getPerson($rs, &$personPointer)
+  private function getPerson($rs, &$personPointer)
   {
     $user = $rs[$personPointer]->user;
     $personPointer += 1;
     return $user;
   }
 
-  function savePersonImage($person, $id)
+  private function savePersonImage($person, $id)
   {
     $dest = "../Images/Person/thumbnails/" . $id . ".jpg";
     #echo "Saving image for: ".$person->name->first." from: ".$person->picture->thumbnail. " to: ". $dest;
@@ -18,6 +18,7 @@ class DataSeedService
 
   function generateFamilies($families)
   {
+    requireUserGroupMembership("bAdmin");
     $PersonService = new PersonService();
     $FamilyService = new FamilyService();
     $kidsPerFamily = 3;
@@ -72,25 +73,25 @@ class DataSeedService
 
   function generateSundaySchoolClasses($classes, $childrenPerTeacher)
   {
-
+    requireUserGroupMembership("bAdmin");
     echo '{"status":"Sunday School Seed Data Not Implemented"}';
-
   }
 
   function generateEvents($events, $averageAttendance)
   {
-
+    requireUserGroupMembership("bAdmin");
     echo '{"status":"Events Seed Data Not Implemented"}';
-
   }
 
   function generateDeposits($deposits, $averagedepositvalue)
   {
+    requireUserGroupMembership("bAdmin");
     echo '{"status":"Deposits Seed Data Not Implemented"}';
   }
 
   function generateFundRaisers($fundraisers, $averageItems, $averageItemPrice)
   {
+    requireUserGroupMembership("bAdmin");
     echo '{"status":"Fundraisers Seed Data Not Implemented"}';
   }
 

churchinfo/Service/FamilyService.php
@@ -130,6 +130,7 @@ class FamilyService
 
   function setFamilyCheckingAccountDetails($tScanString, $iFamily)
   {
+    requireUserGroupMembership("bFinance");
     //Set the Routing and Account Number for a family
     $routeAndAccount = $micrObj->FindRouteAndAccount($tScanString); // use routing and account number for matching
     $sSQL = "UPDATE family_fam SET fam_scanCheck=\"" . $routeAndAccount . "\" WHERE fam_ID = " . $iFamily;
@@ -138,6 +139,7 @@ class FamilyService
 
   function insertFamily($user)
   {
+    requireUserGroupMembership("bAddRecords");
     $dWeddingDate = "NULL";
     $iCanvasser = 0;
     $nLatitude = 0;

churchinfo/Service/FinancialService.php
@@ -22,6 +22,7 @@ class FinancialService
 
   function processAuthorizeNet()
   {
+    requireUserGroupMembership("bFinance");
     $donation = new AuthorizeNetAIM;
     $donation->amount = "$plg_amount";
     $donation->first_name = $firstName;
@@ -169,6 +170,7 @@ class FinancialService
 
   function processVanco()
   {
+    requireUserGroupMembership("bFinance");
     $customerid = "$aut_ID";  // This is an optional value that can be used to indicate a unique customer ID that is used in your system
     // put aut_ID into the $customerid field
     // Create object to preform API calls
@@ -267,6 +269,7 @@ class FinancialService
 
   function runTransactions($depID)
   {
+    requireUserGroupMembership("bFinance");
     // Process all the transactions
 
     //Get the payments for this deposit slip
@@ -330,6 +333,7 @@ class FinancialService
 
   function loadAuthorized($depID)
   {
+    requireUserGroupMembership("bFinance");
 
     // Create all the payment records that have been authorized
 
@@ -412,6 +416,7 @@ class FinancialService
 
   function deletePayment($groupKey)
   {
+    requireUserGroupMembership("bFinance");
     $sSQL = "SELECT deposit_dep.dep_Closed FROM pledge_plg
         INNER JOIN deposit_dep ON
         pledge_plg.plg_depID = deposit_dep.dep_ID
@@ -431,6 +436,7 @@ class FinancialService
 
   function getMemberByScanString($sstrnig)
   {
+    requireUserGroupMembership("bFinance");
     global $bUseScannedChecks;
     if ($bUseScannedChecks) {
       require "../Include/MICRFunctions.php";
@@ -452,6 +458,7 @@ class FinancialService
 
   function getDepositsByFamilyID($fid)
   {
+    requireUserGroupMembership("bFinance");
     //This might not be finished....
     $sSQL = "SELECT plg_fundID, plg_amount from pledge_plg where plg_famID=\"" . $familyId . "\" AND plg_PledgeOrPayment=\"Pledge\"";
     if ($fyid != -1) {
@@ -497,6 +504,7 @@ class FinancialService
 
   function deleteDeposit($id)
   {
+    requireUserGroupMembership("bFinance");
     $DeleteDeposits = "DELETE FROM deposit_dep
             WHERE dep_ID = " . $id;
     RunQuery($DeleteDeposits);
@@ -507,7 +515,7 @@ class FinancialService
 
   function getDeposits($id = null)
   {
-
+    requireUserGroupMembership("bFinance");
     $sSQL = "SELECT * FROM deposit_dep";
     if ($id) {
       $sSQL .= " WHERE dep_ID = " . $id;
@@ -559,6 +567,7 @@ class FinancialService
 
   function getDepositTotal($id, $type = null)
   {
+    requireUserGroupMembership("bFinance");
     $sqlClause = '';
     if ($type) {
       $sqlClause = "AND plg_method = '" . $type . "'";
@@ -572,6 +581,7 @@ class FinancialService
 
   function getDepositCount($id, $type = null)
   {
+    requireUserGroupMembership("bFinance");
     $sqlClause = '';
     if ($type) {
       $sqlClause = "AND plg_method = '" . $type . "'";
@@ -605,6 +615,7 @@ class FinancialService
 
   function getPayments($depID)
   {
+    requireUserGroupMembership("bFinance");
     $sSQL = "SELECT * from pledge_plg
             INNER JOIN 
             donationfund_fun 
@@ -654,6 +665,7 @@ class FinancialService
 
   function searchDeposits($searchTerm)
   {
+    requireUserGroupMembership("bFinance");
     $fetch = 'SELECT dep_ID, dep_Comment, dep_Date, dep_EnteredBy, dep_Type
             FROM deposit_dep
             LEFT JOIN pledge_plg ON
@@ -681,6 +693,7 @@ class FinancialService
 
   function searchPayments($searchTerm)
   {
+    requireUserGroupMembership("bFinance");
     $fetch = 'SELECT dep_ID, dep_Comment, dep_Date, dep_EnteredBy, dep_Type, plg_FamID, plg_amount, plg_CheckNo, plg_plgID, plg_GroupKey
             FROM deposit_dep
             LEFT JOIN pledge_plg ON
@@ -712,6 +725,7 @@ class FinancialService
 
   function searchMembers($query)
   {
+    requireUserGroupMembership("bFinance");
     $sSearchTerm = $query;
     $sSearchType = "person";
     $fetch = 'SELECT per_ID, per_FirstName, per_LastName, CONCAT_WS(" ",per_FirstName,per_LastName) AS fullname, per_fam_ID  FROM `person_per` WHERE per_FirstName LIKE \'%' . $sSearchTerm . '%\' OR per_LastName LIKE \'%' . $sSearchTerm . '%\' OR per_Email LIKE \'%' . $sSearchTerm . '%\' OR CONCAT_WS(" ",per_FirstName,per_LastName) LIKE \'%' . $sSearchTerm . '%\' LIMIT 15';
@@ -761,6 +775,7 @@ class FinancialService
 
   function locateFamilyCheck($checkNumber, $fam_ID)
   {
+    requireUserGroupMembership("bFinance");
     $sSQL = "SELECT count(plg_FamID) from pledge_plg
                  WHERE plg_CheckNo = " . $checkNumber . " AND
                  plg_FamID = " . $fam_ID;
@@ -770,6 +785,7 @@ class FinancialService
 
   function validateChecks($payment)
   {
+    requireUserGroupMembership("bFinance");
     //validate that the payment options are valid
     //If the payment method is a check, then the check nubmer must be present, and it must not already have been used for this family
     //if the payment method is cash, there must not be a check number
@@ -790,6 +806,7 @@ class FinancialService
 
   function insertPledgeorPayment($payment)
   {
+    requireUserGroupMembership("bFinance");
     // Only set PledgeOrPayment when the record is first created
     // loop through all funds and create non-zero amount pledge records
     unset($sGroupKey);
@@ -862,6 +879,7 @@ class FinancialService
 
   function submitPledgeOrPayment($payment)
   {
+    requireUserGroupMembership("bFinance");
     $this->validateFund($payment);
     $this->validateChecks($payment);
     $this->validateDate($payment);
@@ -871,6 +889,7 @@ class FinancialService
 
   function getPledgeorPayment($GroupKey)
   {
+    requireUserGroupMembership("bFinance");
     require_once "FamilyService.php";
     $total = 0;
     $FamilyService = New FamilyService();
@@ -902,6 +921,7 @@ class FinancialService
 
   function getDepositOFX($depID)
   {
+    requireUserGroupMembership("bFinance");
     $fundTotal = array();
     $iDepositComment = "";
     $iDepositSlipID = 0;
@@ -1229,6 +1249,7 @@ class FinancialService
 
   function getDepositPDF($depID)
   {
+    requireUserGroupMembership("bFinance");
     $thisReport = new StdClass();
     $thisReport->payments = $this->getPayments($depID);
     if (count($thisReport->payments) == 0) {
@@ -1307,6 +1328,7 @@ class FinancialService
 
   function getDepositCSV($depID)
   {
+    requireUserGroupMembership("bFinance");
     $retstring = "";
     $line = array();
     $firstLine = true;
@@ -1337,6 +1359,7 @@ class FinancialService
 
   function getFund()
   {
+    requireUserGroupMembership("bFinance");
     $funds = array();
     $sSQL = "SELECT fun_ID,fun_Name,fun_Description,fun_Active FROM donationfund_fun";
     $sSQL .= " WHERE fun_Active = 'true'"; // New donations should show only active funds.

churchinfo/Service/GroupService.php
@@ -21,6 +21,7 @@ class GroupService
    */
   function setGroupMemberRole($groupID, $personID, $roleID)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQL = "UPDATE person2group2role_p2g2r
             SET p2g2r_rle_ID = " . $roleID . "
             WHERE
@@ -39,6 +40,7 @@ class GroupService
    */
   function removeUserFromGroup($groupID, $personID)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQL = "DELETE FROM person2group2role_p2g2r WHERE p2g2r_per_ID = " . $personID . " AND p2g2r_grp_ID = " . $groupID;
     RunQuery($sSQL);
 
@@ -78,6 +80,7 @@ class GroupService
    */
   function addUserToGroup($iGroupID, $iPersonID, $iRoleID)
   {
+    requireUserGroupMembership("bManageGroups");
     //
     // Adds a person to a group with specified role.
     // Returns false if the operation fails. (such as person already in group)
@@ -118,6 +121,7 @@ class GroupService
    */
   function search($searchTerm)
   {
+   requireUserGroupMembership("bManageGroups"); 
     $sSQL = 'SELECT grp_ID FROM group_grp LEFT JOIN list_lst on lst_ID = 3 AND lst_OptionID = grp_Type WHERE grp_Name LIKE \'%' . $searchTerm . '%\' OR  grp_Description LIKE \'%' . $searchTerm . '%\' OR lst_OptionName LIKE \'%' . $searchTerm . '%\'  order by grp_Name LIMIT 15';
     $result = mysql_query($sSQL);
     $return = array();
@@ -158,6 +162,7 @@ class GroupService
    */
   function getGroupRoles($groupID)
   {
+    requireUserGroupMembership("bManageGroups");
     $groupRoles = array ();
     $sSQL = "SELECT grp_ID, lst_OptionName, lst_OptionID, lst_OptionSequence
               FROM group_grp
@@ -188,6 +193,7 @@ class GroupService
    */
   function setGroupRoleName($groupID, $groupRoleID, $groupRoleName)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQL = 'UPDATE list_lst
                  INNER JOIN group_grp
                     ON group_grp.grp_RoleListID = list_lst.lst_ID
@@ -199,6 +205,7 @@ class GroupService
 
   function setGroupRoleOrder($groupID, $groupRoleID, $groupRoleOrder)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQL = 'UPDATE list_lst
                  INNER JOIN group_grp
                     ON group_grp.grp_RoleListID = list_lst.lst_ID
@@ -210,6 +217,7 @@ class GroupService
 
   function getGroupDefaultRole($groupID)
   {
+    requireUserGroupMembership("bManageGroups");
     //Look up the default role name
     $sSQL = "SELECT lst_OptionName from list_lst INNER JOIN group_grp on (group_grp.grp_RoleListID = list_lst.lst_ID AND group_grp.grp_DefaultRole = list_lst.lst_OptionID) WHERE group_grp.grp_ID = " . $groupID;
     $aDefaultRole = mysql_fetch_array(RunQuery($sSQL));
@@ -218,6 +226,7 @@ class GroupService
 
   function getGroupRoleOrder($groupID, $groupRoleID)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQL = 'SELECT list_lst.lst_OptionSequence FROM list_lst
                 INNER JOIN group_grp
                     ON group_grp.grp_RoleListID = list_lst.lst_ID
@@ -231,6 +240,7 @@ class GroupService
 
   function deleteGroupRole($groupID, $groupRoleID)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQL = 'SELECT * FROM list_lst
                 INNER JOIN group_grp
                     ON group_grp.grp_RoleListID = list_lst.lst_ID
@@ -296,6 +306,7 @@ class GroupService
 
   function addGroupRole($groupID, $groupRoleName)
   {
+    requireUserGroupMembership("bManageGroups");
     if (strlen($groupRoleName) == 0) {
       throw new Exception ("New field name cannot be blank");
     } else {
@@ -338,12 +349,14 @@ class GroupService
 
   function setGroupRoleAsDefault($groupID, $roleID)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQL = "UPDATE group_grp SET grp_DefaultRole = " . $roleID . " WHERE grp_ID = " . $groupID;
     RunQuery($sSQL);
   }
 
   function getGroupTotalMembers($groupID)
   {
+    requireUserGroupMembership("bManageGroups");
     //Get the count of members
     $sSQL = 'SELECT COUNT(*) AS iTotalMembers FROM person2group2role_p2g2r WHERE p2g2r_grp_ID = ' . $groupID;
     $rsTotalMembers = mysql_fetch_array(RunQuery($sSQL));
@@ -352,6 +365,7 @@ class GroupService
 
   function getGroupTypes()
   {
+    requireUserGroupMembership("bManageGroups");
     $groupTypes = array();
     // Get Group Types for the drop-down
     $sSQL = "SELECT * FROM list_lst WHERE lst_ID = 3 ORDER BY lst_OptionSequence";
@@ -364,6 +378,7 @@ class GroupService
 
   function getGroupRoleTemplateGroups()
   {
+    requireUserGroupMembership("bManageGroups");
     $templateGroups = array();
     $sSQL = "SELECT * FROM group_grp WHERE grp_RoleListID > 0 ORDER BY grp_Name";
     $rsGroupRoleSeed = RunQuery($sSQL);
@@ -375,11 +390,12 @@ class GroupService
 
   function setGroupName($groupID, $groupName)
   {
-
+    requireUserGroupMembership("bManageGroups");
   }
 
   function enableGroupSpecificProperties($groupID)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQLp = "CREATE TABLE groupprop_" . $groupID . " (
                         per_ID mediumint(8) unsigned NOT NULL default '0',
                         PRIMARY KEY  (per_ID),
@@ -397,6 +413,7 @@ class GroupService
 
   function disableGroupSpecificProperties($groupID)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQLp = "DROP TABLE groupprop_" . $groupID;
     RunQuery($sSQLp);
 
@@ -407,6 +424,7 @@ class GroupService
 
   function createGroup($groupName)
   {
+    requireUserGroupMembership("bManageGroups");
     if (!$groupName) {   //If there's no group name, throw an exception
       throw new Exception ("Unable to create a group without a name");
     }
@@ -448,6 +466,7 @@ class GroupService
 
   function deleteGroup($iGroupID)
   {
+    requireUserGroupMembership("bManageGroups");
     $sSQL = "SELECT grp_hasSpecialProps, grp_RoleListID FROM group_grp WHERE grp_ID =" . $iGroupID;
     $rsTemp = RunQuery($sSQL);
     $aTemp = mysql_fetch_array($rsTemp);
@@ -487,7 +506,7 @@ class GroupService
 
   function updateGroup($groupID, $groupData)
   {
-
+    requireUserGroupMembership("bManageGroups");
     //Assign everything locally
     $thisGroup['grp_Name'] = $groupData->groupName;
     $thisGroup['grp_type'] = $groupData->groupType;
@@ -508,6 +527,7 @@ class GroupService
 
   function getGroups($groupIDs = NULL)
   {
+    requireUserGroupMembership("bManageGroups");
     $whereClause = "";
     if (is_numeric($groupIDs)) {
       $whereClause = "WHERE grp_ID = " . $groupIDs;
@@ -546,6 +566,7 @@ class GroupService
 
   function getGroupMembers($groupID, $personID = null)
   {
+    requireUserGroupMembership("bManageGroups");
     $whereClause = "";
     if (is_numeric($personID)) {
       $whereClause = " AND p2g2r_per_ID = " . $personID;
@@ -575,6 +596,7 @@ class GroupService
 
   function getGroupMembersIds($groupID)
   {
+    requireUserGroupMembership("bManageGroups");
     $members = array();
     // Main select query
     $sSQL = "SELECT p2g2r_per_ID, p2g2r_grp_ID, p2g2r_rle_ID, lst_OptionName FROM person2group2role_p2g2r
@@ -597,6 +619,7 @@ class GroupService
 
   function checkGroupAgainstCart($groupID)
   {
+    requireUserGroupMembership("bManageGroups");
     $members = $this->getGroupMembersIds($groupID);
     //echo "Members: ".count($members);
     $bNoneInCart = TRUE;
@@ -624,6 +647,7 @@ class GroupService
 
   function copyCartToGroup()
   {
+    requireUserGroupMembership("bManageGroups");
     if (array_key_exists("EmptyCart", $_POST) && $_POST["EmptyCart"] && count($_SESSION['aPeopleCart']) > 0) {
       $iCount = 0;
       while ($element = each($_SESSION['aPeopleCart'])) {

commit 2d009b92173515598f1e3e98d80addd515a87afc

Fix deposit graph on menu.php (Broken by #515)

churchinfo/Menu.php
@@ -49,8 +49,8 @@ $dashboardService = new DashboardService();
 $personCount = $dashboardService->getPersonCount();
 $familyCount = $dashboardService->getFamilyCount();
 $groupStats = $dashboardService->getGroupStats();
-$shouldDisplayDeposits = $_SESSION['bFinance'] && count($depositData) > 1;  //Determine whether or not we should display the deposit line graph
-if ($shouldDisplayDeposits) {
+$depositData = false;  //Determine whether or not we should display the deposit line graph
+if ($_SESSION['bFinance']) {
   $depositData = $financialService->getDeposits();  //Get the deposit data from the financialService
 }
 
@@ -142,7 +142,7 @@ require 'Include/Header.php';
 </div><!-- /.row -->
 
 <?php 
-if ($shouldDisplayDeposits) // If the user has Finance permissions, then let's display the deposit line chart
+if ($depositData) // If the user has Finance permissions, then let's display the deposit line chart
 { 
 ?>
 <div class="row">
@@ -291,7 +291,7 @@ if ($shouldDisplayDeposits) // If the user has Finance permissions, then let's d
 <!-- this page specific inline scripts -->
 <script>
 <?php 
-if ($shouldDisplayDeposits) // If the user has Finance permissions, then let's display the deposit line chart
+if ($depositData) // If the user has Finance permissions, then let's display the deposit line chart
 { 
 ?>
     //---------------