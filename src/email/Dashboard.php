ea64ae792eff2bc6171de4fab11ee72671ac67ae
George Dawoud <george@dawouds.com>
2016-05-01 11:40:32 -0700

    refactor the mailchimp dashboard to be more about emails

--- src/email/Dashboard.php
+++ src/email/Dashboard.php
@@ -1,7 +1,7 @@
 <?php
 /*******************************************************************************
  *
- *  filename    : MailChimpDashboard.php
+ *  filename    : Dashboard.php
  *  last change : 2014-11-29
  *  website     : http://www.churchcrm.io
  *  copyright   : Copyright 2014
@@ -21,13 +21,32 @@ require '../Service/MailchimpService.php';
 $mailchimp = new MailChimpService();
 
 //Set the page title
-$sPageTitle = gettext('MailChimp Dashboard');
+$sPageTitle = gettext('eMail Dashboard');
 
 require '../Include/Header.php';
+?>
+<div class="row">
+  <div class="col-lg-4 col-md-2 col-sm-2">
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">Email Export</h3>
+      </div>
+      <div class="box-body">
+        You can import the generated CSV file to external email system.
+        For MailChimp see: <a href="http://kb.mailchimp.com/lists/growth/import-subscribers-to-a-list" target="_blank">import subscribers to a list.</a>
+        <br/><br/>
+        <p class="text-center">
+          <a class="btn btn-app" href="MemberEmailExport.php">
+            <i class="fa fa-file-o"></i> Generate
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
 
-$isActive = $mailchimp->isActive();
 
-if ($isActive) {
+<?php  if ($mailchimp->isActive()) {
   $mcLists = $mailchimp->getLists();
   ?>
   <div class="row">
@@ -55,23 +74,6 @@ if ($isActive) {
   </div>
   <div class="row">
     <div class="col-lg-4 col-md-2 col-sm-2">
-      <div class="box">
-        <div class="box-header">
-          <h3 class="box-title">Generate Email Export</h3>
-        </div>
-        <div class="box-body">
-          MailChimp offers several ways to add subscribers to your list.
-          This will generate a subscribers CSV file to <a href="http://kb.mailchimp.com/lists/growth/import-subscribers-to-a-list" target="_blank">import.</a>
-
-          <p class="text-center">
-            <a class="btn btn-app" href="MailChimpCsvExport.php">
-              <i class="fa fa-save"></i> Generate
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-md-2 col-sm-2">
       <h3>Reports</h3>
       <ul>
         <li><a href="MailChimpMissingReport.php">Missing emails report </a> (slow)</li>
@@ -84,7 +86,7 @@ if ($isActive) {
       <div class="box box-body">
         <div class="alert alert-danger alert-dismissible">
           <h4><i class="icon fa fa-ban"></i> MailChimp is not configured</h4>
-          Please update the MailChimp API key in Setting-><a href="../SettingsGeneral.php">Edit General Settings</a>, then update mailChimpApiKey. For more info see our <a href="http://docs.churchcrm.io">MailChimp support docs.</a>
+          Please update the MailChimp API key in Setting-><a href="../SystemSettings.php">Edit General Settings</a>, then update mailChimpApiKey. For more info see our <a href="http://docs.churchcrm.io">MailChimp support docs.</a>
         </div>
       </div>
     </div>
---
+++ src/mysql/upgrade/2.1.0.sql
@@ -0,0 +1,9 @@
+
+update menuconfig_mcf
+  set name = "email" AND
+      content = "eMail" AND
+      content_english = "eMail" AND
+      uri = "email/dashboard.php";
+
+
+update version_ver set ver_date = "5/1/16" and ver_version = "2.1.0";
