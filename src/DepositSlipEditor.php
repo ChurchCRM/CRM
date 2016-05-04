455f76344ea8d96f1874e6429da58303950e5d10
crossan007 <crossan007@gmail.com>
2016-05-04 18:10:52 -0400

    Fix deposit slip bug

--- src/DepositSlipEditor.php
+++ src/DepositSlipEditor.php
@@ -574,7 +574,7 @@ require "Include/Header.php";
 		<td align="center">
 			<input type="submit" class="btn" value="<?= gettext("Save") ?>" name="DepositSlipSubmit">
 			<input type="button" class="btn" value="<?= gettext("Cancel") ?>" name="DepositSlipCancel" onclick="javascript:document.location='<?php if (strlen($linkBack) > 0) { echo $linkBack; } else {echo "Menu.php"; } ?>';">
-			<input type="button" class="btn" value="<?= gettext("Deposit Slip Report") ?>" name="DepositSlipGeneratePDF" onclick="javascript:document.location='Reports/PrintDeposit.php?BankSlip=<?php echo ($dep_Type == 'Bank')?>';">
+			<input type="button" class="btn" value="<?= gettext("Deposit Slip Report") ?>" name="DepositSlipGeneratePDF" onclick="javascript:window.open(window.CRM.root+'/api/deposits/<?= $iDepositSlipID ?>/pdf');">
 			<input type="button" class="btn" value="<?= gettext("More Reports") ?>" name="DepositSlipGeneratePDF" onclick="javascript:document.location='FinancialReports.php';">
 			<?php
 			if ($iDepositSlipID && $sDepositType && !$dep_Closed) {
