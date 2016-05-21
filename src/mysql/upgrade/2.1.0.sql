2541ef3fe44f5c2caa2a643b828b366ed689d959
George Dawoud <george@dawouds.com>
2016-05-21 16:09:27 -0700

    added note table changes to 2.1.0

--- src/mysql/upgrade/2.1.0.sql
+++ src/mysql/upgrade/2.1.0.sql
@@ -24,6 +24,22 @@ CREATE TABLE `version_ver` (
 INSERT INTO version_ver (ver_version, ver_update_start)
 VALUES ('2.1.0', now());
 
+-- ------ Notes #608 - start
 
+ALTER TABLE note_nte
+  ADD COLUMN nte_Type VARCHAR(45) NOT NULL DEFAULT 'note' AFTER nte_EditedBy;
+
+INSERT INTO note_nte
+	(nte_per_ID, nte_fam_ID, nte_Private, nte_Text, nte_EnteredBy, nte_DateEntered, nte_Type)
+select per_id, 0, 0, "", per_EnteredBy, per_DateEntered, "create"
+from person_per;
+
+INSERT INTO note_nte
+	(nte_per_ID, nte_fam_ID, nte_Private, nte_Text, nte_EnteredBy, nte_DateEntered, nte_Type)
+select per_id, 0, 0, "", per_EditedBy, per_DateLastEdited, "edit"
+from person_per
+where per_DateLastEdited is not null;
+
+-- ------ Notes #608 - end
 
 update version_ver set ver_update_end = now();
