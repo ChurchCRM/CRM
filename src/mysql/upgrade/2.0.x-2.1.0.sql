319da60c1d4de6d36f181de9277e1931ef98cf80
Charles <crossan007@gmail.com>
2016-06-01 21:57:26 -0400

    Fix MySQL Upgrade Function (#647)
    * always add a new line for concatenated sql statements
    
    * cleanup old workarounds

--- src/mysql/upgrade/2.0.x-2.1.0.sql
+++ src/mysql/upgrade/2.0.x-2.1.0.sql
@@ -1,9 +1,9 @@
 SET @upgradeStartTime = NOW();
 
 ALTER TABLE `version_ver`
- CHANGE COLUMN `ver_date` `ver_update_start` datetime default NULL;
+CHANGE COLUMN `ver_date` `ver_update_start` datetime default NULL;
 
 ALTER TABLE `version_ver`
- ADD COLUMN `ver_update_end` datetime default NULL AFTER `ver_update_start`;
+ADD COLUMN `ver_update_end` datetime default NULL AFTER `ver_update_start`;
 
 INSERT IGNORE INTO version_ver (ver_version, ver_update_start, ver_update_end) VALUES ('2.1.0',@upgradeStartTime,NOW());
