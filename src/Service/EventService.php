4d6f7f9f4389648d20195d2080f0203e8dd1db5e
George Dawoud <george@dawouds.com>
2016-06-15 07:51:46 -0700

    added events to calendar view (#674)

--- src/Service/EventService.php
+++ src/Service/EventService.php
@@ -6,14 +6,30 @@
 
 class EventService
 {
+
+  function getEvents()
+  {
+    $sSQL = "SELECT event.* FROM events_event AS event ORDER BY event_start";
+    $result = RunQuery($sSQL);
+
+    $return = array();
+    while ($row = mysql_fetch_array($result)) {
+      $values['title'] = $row['event_title'];
+      $values['desc'] = $row['event_desc'];
+      $values['start'] = $row['event_start'];
+      $values['end'] = $row['event_end'];
+      array_push($return, $values);
+    }
+    return $return;
+  }
+
   function getEventsByPerson($id)
   {
- $sSQL = "SELECT t1.event_title, t1.event_desc, t1.event_start, t1.event_end
-            FROM events_event AS t1, event_attend AS t2
-              WHERE t1.event_id = t2.event_id
-              AND t2.person_id =" . $id . "
+    $sSQL = "SELECT event.event_title, event.event_desc, event.event_start, event.event_end
+            FROM events_event AS event, event_attend
+              WHERE event.event_id = event_attend.event_id
+              AND event_attend.person_id =" . $id . "
             ORDER BY event_start DESC LIMIT 10";
-
     $result = RunQuery($sSQL);
 
     $return = array();
