517eff8723910e453f19322595a5b501673d71a1
George Dawoud <george@dawouds.com>
2016-06-21 21:47:53 -0700

    added regex for count of families validation

--- src/api/routes/data.php
+++ src/api/routes/data.php
@@ -4,13 +4,13 @@
 $app->group('/data/seed', function () {
 
   // generate x families and people in each family
-  $this->get('/families/{count}', function ($request, $response, $args) {
+  $this->get('/families/{count:[0-9]+}', function ($request, $response, $args) {
     $query = $args['count'];
     $data = $this->DataSeedService->generateFamilies($query);
     return $response->write($data);
   });
 
-  // Not fully working yet but genrate sunday school classes
+  // Not fully working yet but generate sunday school classes
   $this->post('/sundaySchoolClasses/', function ($request, $response, $args) {
     return $response->withJson($this->DataSeedService->generateSundaySchoolClasses());
   });
