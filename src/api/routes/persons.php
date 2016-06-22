ed906e56a2b3ad5e5ff86ae3b00d463cc5f3020f
George Dawoud <george@dawouds.com>
2016-06-21 21:06:30 -0700

    comments on working apis

--- src/api/routes/persons.php
+++ src/api/routes/persons.php
@@ -1,7 +1,9 @@
 <?php
-// Routes
+// Person APIs
 
 $app->group('/persons', function () {
+
+  // search person by Name
   $this->get('/search/{query}', function ($request, $response, $args) {
     $query = $args['query'];
     $data = "[" . $this->PersonService->getPersonsJSON($this->PersonService->search($query)) . "]";
