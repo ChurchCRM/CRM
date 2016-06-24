2a71b62e080435a7583055a71b2693ee502dc496
crossan007 <crossan007@gmail.com>
2016-06-24 18:32:19 -0400

    2.1.2

--- src/mysql/upgrade/2.1.0-2.1.x.sql
+++ src/mysql/upgrade/2.1.0-2.1.x.sql
@@ -4,4 +4,4 @@ SET @upgradeStartTime = NOW();
 INSERT INTO `config_cfg` (`cfg_id`, `cfg_name`, `cfg_value`, `cfg_type`, `cfg_default`, `cfg_tooltip`, `cfg_section`, `cfg_category`) VALUES
 (1035, 'sEnableGravatarPhotos', '0', 'boolean', '0', 'lookup user images on Gravatar when no local image is present', 'General', NULL);
 
-INSERT IGNORE INTO version_ver (ver_version, ver_update_start, ver_update_end) VALUES ('2.1.3',@upgradeStartTime,NOW());
+INSERT IGNORE INTO version_ver (ver_version, ver_update_start, ver_update_end) VALUES ('2.1.2',@upgradeStartTime,NOW());
