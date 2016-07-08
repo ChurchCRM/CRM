92fc17e7d641b2485f7999945a2595c7a01048e3
crossan007 <crossan007@gmail.com>
2016-07-08 12:15:47 -0400

    fix versioning

--- src/Service/SystemService.php
+++ src/Service/SystemService.php
@@ -280,13 +280,13 @@ class SystemService {
       return true;
     }
     
-    if (strncmp($db_version, "2.1.3", 5) == 0 ) {
-      $this->rebuildWithSQL("/mysql/upgrade/2.1.3-2.2.0.sql");
-      return true;
-    }
-
     if (in_array($db_version, array("2.1.3", "2.1.4", "2.1.5", "2.1.6"))) {
       $this->rebuildWithSQL("/mysql/upgrade/2.1.3-2.1.7.sql");
+      return true;
+    }
+    
+    if (strncmp($db_version, "2.1.7", 5) == 0 ) {
+      $this->rebuildWithSQL("/mysql/upgrade/2.1.7-2.2.0.sql");
       return true;
     }
 
