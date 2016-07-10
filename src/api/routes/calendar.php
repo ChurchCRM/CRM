9d00a07761a1034871b63b34e5dc500d2d3b3b98
George Dawoud <george@dawouds.com>
2016-07-10 10:41:34 -0700

    now returns json

--- src/api/routes/calendar.php
+++ src/api/routes/calendar.php
@@ -5,7 +5,7 @@
 $app->group('/calendar', function () {
 
   $this->get('/events', function ($request, $response, $args) {
-    return $this->CalendarService->getEvents();
+    return $response->withJson($this->CalendarService->getEvents());
   });
 
 });
