da5b28fda7a4ebb94bd14fa05bd9ef039c1b374c
George Dawoud <george@dawouds.com>
2016-07-16 10:43:15 -0700

    composer autoloader cleanup

--- src/Service/PersonService.php
+++ src/Service/PersonService.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once dirname(__FILE__) . '/../vendor/autoload.php';
-require_once dirname(__FILE__) . '/../orm/conf/config.php';
-
 use ChurchCRM\ListOptionQuery;
 
 class PersonService
