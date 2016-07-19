65e75c9907963fecc53bc44549bba870ecb2d192
crossan007 <crossan007@gmail.com>
2016-07-18 21:14:35 -0400

    don't remove the files composer puts in the vendor directory

--- vagrant/build-skin.sh
+++ vagrant/build-skin.sh
@@ -5,34 +5,28 @@ echo "============================= Building Skin ==============================
 rm -rf /var/www/public/skin/adminlte
 mkdir /var/www/public/skin/adminlte
 
-mv -f /vagrant/src/vendor/almasaeed2010/adminlte/dist/ /var/www/public/skin/adminlte/dist
-mv -f /vagrant/src/vendor/almasaeed2010/adminlte/bootstrap/ /var/www/public/skin/adminlte/
-mv -f /vagrant/src/vendor/almasaeed2010/adminlte/plugins/ /var/www/public/skin/adminlte/
-
-rm -rf /vagrant/src/vendor/almasaeed2010
+cp -rf /vagrant/src/vendor/almasaeed2010/adminlte/dist/ /var/www/public/skin/adminlte/dist
+cp -rf /vagrant/src/vendor/almasaeed2010/adminlte/bootstrap/ /var/www/public/skin/adminlte/
+cp -rf /vagrant/src/vendor/almasaeed2010/adminlte/plugins/ /var/www/public/skin/adminlte/
 
 echo "Build: AdminLTE copied"
 
 rm -rf /var/www/public/skin/font-awesome/
 mkdir /var/www/public/skin/font-awesome/
 
-mv -f /vagrant/src/vendor/components/font-awesome/css/ /var/www/public/skin/font-awesome/css/
-mv -f /vagrant/src/vendor/components/font-awesome/fonts/ /var/www/public/skin/font-awesome/
-mv -f /vagrant/src/vendor/components/font-awesome/less/ /var/www/public/skin/font-awesome/
-mv -f /vagrant/src/vendor/components/font-awesome/scss/ /var/www/public/skin/font-awesome/
-
-rm -rf  /vagrant/src/vendor/components
+cp -rf /vagrant/src/vendor/components/font-awesome/css/ /var/www/public/skin/font-awesome/css/
+cp -rf /vagrant/src/vendor/components/font-awesome/fonts/ /var/www/public/skin/font-awesome/
+cp -rf /vagrant/src/vendor/components/font-awesome/less/ /var/www/public/skin/font-awesome/
+cp -rf /vagrant/src/vendor/components/font-awesome/scss/ /var/www/public/skin/font-awesome/
 
 echo "Build: font-awesome copied"
 
 rm -rf /var/www/public/skin/ionicons/
 mkdir /var/www/public/skin/ionicons/
 
-mv -f /vagrant/src/vendor/driftyco/ionicons/css/ /var/www/public/skin/ionicons/css/
-mv -f /vagrant/src/vendor/driftyco/ionicons/fonts/ /var/www/public/skin/ionicons/
-mv -f /vagrant/src/vendor/driftyco/ionicons/less/ /var/www/public/skin/ionicons/
-mv -f /vagrant/src/vendor/driftyco/ionicons/png/ /var/www/public/skin/ionicons/
-
-rm -rf /vagrant/src/vendor/driftyco
+cp -rf /vagrant/src/vendor/driftyco/ionicons/css/ /var/www/public/skin/ionicons/css/
+cp -rf /vagrant/src/vendor/driftyco/ionicons/fonts/ /var/www/public/skin/ionicons/
+cp -rf /vagrant/src/vendor/driftyco/ionicons/less/ /var/www/public/skin/ionicons/
+cp -rf /vagrant/src/vendor/driftyco/ionicons/png/ /var/www/public/skin/ionicons/
 
 echo "Build: ionicons copied"
