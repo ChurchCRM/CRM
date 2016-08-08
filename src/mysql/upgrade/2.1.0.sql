4eed727bbb591aa764576523dd1e738295d83a70
George Dawoud <george@dawouds.com>
2016-08-08 08:02:52 -0700

    cleanup

--- src/mysql/upgrade/2.1.0.sql
+++ src/mysql/upgrade/2.1.0.sql
@@ -1,10 +1,10 @@
 SET @upgradeStartTime = NOW();
 
-ALTER TABLE `version_ver`
-CHANGE COLUMN `ver_date` `ver_update_start` datetime default NULL;
+ALTER TABLE version_ver
+CHANGE COLUMN ver_date ver_update_start datetime default NULL;
 
-ALTER TABLE `version_ver`
-ADD COLUMN `ver_update_end` datetime default NULL AFTER `ver_update_start`;
+ALTER TABLE version_ver
+ADD COLUMN ver_update_end datetime default NULL AFTER ver_update_start;
 
 -- ------ Notes - start
 
