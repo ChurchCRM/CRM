09f0ff89fcb453d3c5ed9b3b4b55cede0d25abd4
crossan007 <crossan007@gmail.com>
2016-08-27 23:33:47 -0400

    first working transaction post.

--- src/orm/model/ChurchCRM/Pledge.php
+++ src/orm/model/ChurchCRM/Pledge.php
@@ -4,6 +4,8 @@ namespace ChurchCRM;
 
 use ChurchCRM\Base\Pledge as BasePledge;
 use Propel\Runtime\Exception\PropelException;
+use net\authorize\api\contract\v1 as AnetAPI;
+use net\authorize\api\controller as AnetController;
 
 /**
  * Skeleton subclass for representing a row from the 'pledge_plg' table.
@@ -27,4 +29,82 @@ class Pledge extends BasePledge
      throw new PropelException("Cannot delete a payment from a closed deposit",500);
     }
   }
+  
+  function processAuthorizeNet()
+  {
+    global $sAUTHORIZENET_API_LOGIN_ID, $sAUTHORIZENET_TRANSACTION_KEY;
+    $merchantAuthentication = new AnetAPI\MerchantAuthenticationType();
+    $merchantAuthentication->setName($sAUTHORIZENET_API_LOGIN_ID);
+    $merchantAuthentication->setTransactionKey($sAUTHORIZENET_TRANSACTION_KEY);
+    $refId = 'ref' . time();
+    $paymentOne = new AnetAPI\PaymentType();
+    echo "Determining Payment Type: ".$this->getDeposit()->getType();
+    if($this->getDeposit()->getType() == "CreditCard")
+    {
+      echo "Setting Credit Card \r\n";
+     // Create the payment data for a credit card
+      $creditCard = new AnetAPI\CreditCardType();
+      $creditCard->setCardNumber("4111111111111111");
+      $creditCard->setExpirationDate("1226");
+      $creditCard->setCardCode("123");
+      $paymentOne->setCreditCard($creditCard);
+    }
+    elseif($this->getDeposit()->getType() == "eGive")
+    {
+      echo "Setting Checking \r\n";
+        // Create the payment data for a Bank Account
+      $bankAccount = new AnetAPI\BankAccountType();
+      //$bankAccount->setAccountType('CHECKING');
+      $bankAccount->setEcheckType('WEB');
+      $bankAccount->setRoutingNumber('121042882');
+      $bankAccount->setAccountNumber('123456789123');
+      $bankAccount->setNameOnAccount('Jane Doe');
+      $bankAccount->setBankName('Bank of the Earth'); 
+      $paymentOne->setBankAccount($bankAccount);
+    }
+    
+    $transactionRequestType = new AnetAPI\TransactionRequestType();
+    $transactionRequestType->setTransactionType("authCaptureTransaction");
+    $transactionRequestType->setAmount($this->getAmount());
+    $transactionRequestType->setPayment($paymentOne);
+    
+    $request = new AnetAPI\CreateTransactionRequest();
+    $request->setMerchantAuthentication($merchantAuthentication);
+    $request->setRefId( $refId);
+    $request->setTransactionRequest( $transactionRequestType);
+    $controller = new AnetController\CreateTransactionController($request);
+    $response = $controller->executeWithApiResponse( \net\authorize\api\constants\ANetEnvironment::SANDBOX);
+    if ($response != null)
+    {
+      $tresponse = $response->getTransactionResponse();
+      print_r($response);
+      print_r($tresponse);
+      if (($tresponse != null) && ($tresponse->getResponseCode()== \SampleCode\Constants::RESPONSE_OK) )   
+      {
+        echo  "Debit Bank Account APPROVED  :" . "\n";
+        echo " Debit Bank Account AUTH CODE : " . $tresponse->getAuthCode() . "\n";
+        echo " Debit Banlk Account TRANS ID  : " . $tresponse->getTransId() . "\n";
+      }
+      elseif (($tresponse != null) && ($tresponse->getResponseCode()=="2") )
+      {
+        echo  "Debit Bank Account ERROR : DECLINED" . "\n";
+        $errorMessages = $tresponse->getErrors();
+        echo  "Error : " . $errorMessages[0]->getErrorText() . "\n";
+      }
+      elseif (($tresponse != null) && ($tresponse->getResponseCode()=="4") )
+      {
+          echo  "Debit Bank Account ERROR: HELD FOR REVIEW:"  . "\n";
+      }
+      else
+      {
+          echo  "Debit Bank Account 3 response returned";
+      }
+    }
+    else
+    {
+      echo  "Debit Bank Account Null response returned";
+    }
+    return $response;
+  }
+
 }
