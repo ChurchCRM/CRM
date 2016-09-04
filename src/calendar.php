5906fd5335cd5786c9d96ca60d88d681f9099d5b
George Dawoud <george@dawouds.com>
2016-09-04 09:32:04 -0700

    match spacing

--- src/calendar.php
+++ src/calendar.php
@@ -25,7 +25,7 @@ require "Include/Header.php"; ?>
       <div class="fc-event-container fc-day-grid-event" style="background-color:<?= $type["backgroundColor"]?>;border-color:<?= $type["backgroundColor"]?>;color: white; width: 100px">
           <div class="fc-title"><?= gettext($type["Name"])?></div>
       </div>
-      <?php } ?>
+      <?php }?>
     </div>
   </div>
 </div>
