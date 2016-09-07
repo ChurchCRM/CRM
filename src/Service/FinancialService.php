dcd42d8f4d54c217dffd6185d3f26005bcf202a1
George Dawoud <george@dawouds.com>
2016-09-07 08:01:43 -0700

    person Service import Cleanup

--- src/Service/FinancialService.php
+++ src/Service/FinancialService.php
@@ -11,7 +11,6 @@ require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
 require_once dirname(dirname(__FILE__)) . '/orm/conf/config.php';
 
 use ChurchCRM\PledgeQuery;
-use ChurchCRM\Service\PersonService;
 
 class FinancialService
 {
