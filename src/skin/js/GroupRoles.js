1bd566401ae8a0d38c4626bc80e680c97b8e83c4
George Dawoud <george@dawouds.com>
2016-09-11 08:45:33 -0700

    using roles api for values

--- src/skin/js/GroupRoles.js
+++ src/skin/js/GroupRoles.js
@@ -3,16 +3,14 @@ function UpdateRoles()
   var group_ID = $('#GroupID option:selected').val();  // get the selected group ID
   $.ajax({
     method: "GET",
-    url: window.CRM.root + "/api/groups/" + group_ID,
+    url: window.CRM.root + "/api/groups/" + group_ID+ "/roles",
+    dataType: "json"
   }).done(function (data) {
-    var defaultRole = data.groups.grp_DefaultRole;
     var html = "";
-    $.each(data.groups.roles, function (index, value) {
-      html += "<option value=\"" + value.lst_OptionID + "\"";
-      if (value.lst_OptionID === defaultRole) {
-        html += " selected";
-      }
-      html += ">" + value.lst_OptionName + "</option>";
+    alert(data);
+    $.each(data.ListOptions, function (index, value) {
+      html += "<option value=\"" + value.OptionId + "\"";
+      html += ">" + value.OptionName + "</option>";
     });
     $("#GroupRole").html(html);
   });
