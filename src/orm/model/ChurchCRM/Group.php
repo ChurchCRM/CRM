8fd41f22901bfa71b1fe2d981ee50489f9669492
George Dawoud <george@dawouds.com>
2016-09-19 22:43:02 -0700

    added sunday school roles

--- src/orm/model/ChurchCRM/Group.php
+++ src/orm/model/ChurchCRM/Group.php
@@ -5,6 +5,7 @@ namespace ChurchCRM;
 use ChurchCRM\Base\Group as BaseGroup;
 use ChurchCRM\ListOptionQuery;
 use ChurchCRM\ListOption;
+
 /**
  * Skeleton subclass for representing a row from the 'group_grp' table.
  *
@@ -17,48 +18,71 @@ use ChurchCRM\ListOption;
  */
 class Group extends BaseGroup
 {
+
+  protected $typeSundaySchool = 4;
+
+  public function isSundaySchool()
+  {
+    return $this->getType() == $this->typeSundaySchool;
+  }
+
   public function preSave(\Propel\Runtime\Connection\ConnectionInterface $con = null)
   {
     requireUserGroupMembership("bManageGroups");
     parent::preSave($con);
     return true;
   }
-  
+
   public function preUpdate(\Propel\Runtime\Connection\ConnectionInterface $con = null)
   {
     requireUserGroupMembership("bManageGroups");
     parent::preUpdate($con);
     return true;
   }
+
   public function preDelete(\Propel\Runtime\Connection\ConnectionInterface $con = null)
   {
     requireUserGroupMembership("bManageGroups");
     parent::preDelete($con);
     return true;
   }
+
   public function preInsert(\Propel\Runtime\Connection\ConnectionInterface $con = null)
   {
     requireUserGroupMembership("bManageGroups");
-    $newListID = ListOptionQuery::create()->withColumn("MAX(ListOption.Id)","newListId")->find()->getColumnValues('newListId')[0] + 1;
+    $defaultRole = 1;
+    if ($this->isSundaySchool()) {
+      $defaultRole = 2;
+    }
+    $newListID = ListOptionQuery::create()->withColumn("MAX(ListOption.Id)", "newListId")->find()->getColumnValues('newListId')[0] + 1;
     $this->setRoleListId($newListID);
+    $this->setDefaultRole($defaultRole);
     parent::preInsert($con);
     return true;
   }
-  
+
   public function postInsert(\Propel\Runtime\Connection\ConnectionInterface $con = null)
   {
-    $listOption = new ListOption();
-    $listOption->setId($this->getRoleListId());
-    $listOption->setOptionId(0);
-    $listOption->setOptionSequence(1);
-    $listOption->setOptionName("Member");
-    $listOption->save();
+    $optionList = array("Member");
+    if ($this->isSundaySchool()) {
+      $optionList = array("Teacher", "Student");
+    }
+    $i = 1;
+    foreach ($optionList as $option) {
+      $listOption = new ListOption();
+      $listOption->setId($this->getRoleListId());
+      $listOption->setOptionId($i);
+      $listOption->setOptionSequence($i);
+      $listOption->setOptionName($option);
+      $listOption->save();
+      $i++;
+    }
     parent::postInsert($con);
     return true;
   }
-  
-  
-  public function checkAgainstCart() 
+
+
+  public function checkAgainstCart()
   {
     $groupMemberships = $this->getPerson2group2roleP2g2rsJoinPerson();
     $bNoneInCart = TRUE;
