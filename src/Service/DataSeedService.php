3a568c81ea71cec41a2ebb200a5ee5c4d40c2fc5
George Dawoud <george@dawouds.com>
2016-09-29 07:18:38 -0700

    Fixed capitalization and country/state selection
    Made sure Country/State are matching dropdown not random stuff.

--- src/Service/DataSeedService.php
+++ src/Service/DataSeedService.php
@@ -40,11 +40,11 @@ class DataSeedService
       
       $hoh = $this->getPerson($rs, $personPointer);
       $family = new Family();
-      $family->setName($hoh->name->last);
-      $family->setAddress1($hoh->location->street);
-      $family->setCity($hoh->location->city);
-      $family->setState($hoh->location->state);
-      $family->setCountry("USA");
+      $family->setName(ucwords ($hoh->name->last));
+      $family->setAddress1(ucwords($hoh->location->street));
+      $family->setCity(ucwords($hoh->location->city));
+      $family->setState($this->getStateAbb(ucwords($hoh->location->state)));
+      $family->setCountry("United States");
       $family->setZip($hoh->location->zip);
       $family->setHomephone($hoh->phone);
       $family->setDateEntered(date('Y-m-d h:i:s', $hoh->registered));
@@ -52,7 +52,7 @@ class DataSeedService
       $family->save();
       $FamilyID = $family->getId();
 
-      $familyName = $hoh->name->last;
+      $familyName = ucwords ($hoh->name->last);
       $hoh->famID = $FamilyID;
       $hoh->per_fmr_id = 1;
 
@@ -119,18 +119,18 @@ class DataSeedService
   function insertPerson($user)
   {
     $person = new Person();
-    $person->setTitle($user->name->title);
-    $person->setFirstName($user->name->first);
-    $person->setLastName($user->name->last);
+    $person->setTitle(ucwords($user->name->title));
+    $person->setFirstName(ucwords ($user->name->first));
+    $person->setLastName(ucwords ($user->name->last));
     if (FilterInput($user->gender) == "male") {
       $person->setGender(1);
     } else {
       $person->setGender(2);
     }
 
-    $person->setAddress1($user->location->street);
-    $person->setCity($user->location->city);
-    $person->setState($user->location->state);
+    $person->setAddress1(ucwords ($user->location->street));
+    $person->setCity(ucwords ($user->location->city));
+    $person->setState($this->getStateAbb(ucwords ($user->location->state)));
     $person->setZip($user->location->zip);
     $person->setCountry("USA");
     $person->setHomePhone($user->phone);
@@ -148,5 +148,59 @@ class DataSeedService
 
   }
 
-
+  function getStateAbb($state) {
+    $states = array(
+      'Alabama'=>'AL',
+      'Alaska'=>'AK',
+      'Arizona'=>'AZ',
+      'Arkansas'=>'AR',
+      'California'=>'CA',
+      'Colorado'=>'CO',
+      'Connecticut'=>'CT',
+      'Delaware'=>'DE',
+      'Florida'=>'FL',
+      'Georgia'=>'GA',
+      'Hawaii'=>'HI',
+      'Idaho'=>'ID',
+      'Illinois'=>'IL',
+      'Indiana'=>'IN',
+      'Iowa'=>'IA',
+      'Kansas'=>'KS',
+      'Kentucky'=>'KY',
+      'Louisiana'=>'LA',
+      'Maine'=>'ME',
+      'Maryland'=>'MD',
+      'Massachusetts'=>'MA',
+      'Michigan'=>'MI',
+      'Minnesota'=>'MN',
+      'Mississippi'=>'MS',
+      'Missouri'=>'MO',
+      'Montana'=>'MT',
+      'Nebraska'=>'NE',
+      'Nevada'=>'NV',
+      'New Hampshire'=>'NH',
+      'New Jersey'=>'NJ',
+      'New Mexico'=>'NM',
+      'New York'=>'NY',
+      'North Carolina'=>'NC',
+      'North Dakota'=>'ND',
+      'Ohio'=>'OH',
+      'Oklahoma'=>'OK',
+      'Oregon'=>'OR',
+      'Pennsylvania'=>'PA',
+      'Rhode Island'=>'RI',
+      'South Carolina'=>'SC',
+      'South Dakota'=>'SD',
+      'Tennessee'=>'TN',
+      'Texas'=>'TX',
+      'Utah'=>'UT',
+      'Vermont'=>'VT',
+      'Virginia'=>'VA',
+      'Washington'=>'WA',
+      'West Virginia'=>'WV',
+      'Wisconsin'=>'WI',
+      'Wyoming'=>'WY'
+    );
+    return $states[$state];
+  }
 }
