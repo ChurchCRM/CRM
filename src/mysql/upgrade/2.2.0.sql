f1ed079c9ed6a57a72d8f9cb884ce2b537fd7cfd
George Dawoud <george@dawouds.com>
2016-10-01 22:18:21 -0700

    \

--- src/mysql/upgrade/2.2.0.sql
+++ src/mysql/upgrade/2.2.0.sql
@@ -3,7 +3,7 @@ ALTER TABLE config_cfg CHANGE cfg_type cfg_type ENUM('text','number','date','boo
 
 SET @JSONV = '{"date1":{"x":"12","y":"42"},"date2X":"185","leftX":"64","topY":"7","perforationY":"97","amountOffsetX":"35","lineItemInterval":{"x":"49","y":"7"},"max":{"x":"200","y":"140"},"numberOfItems":{"x":"136","y":"68"},"subTotal":{"x":"197","y":"42"},"topTotal":{"x":"197","y":"68"},"titleX":"85"}';
 INSERT IGNORE INTO `config_cfg` (`cfg_id`, `cfg_name`, `cfg_value`, `cfg_type`, `cfg_default`, `cfg_tooltip`, `cfg_section`, `cfg_category`) VALUES
-  (1043, 'sQBDTSettings', @JSONV , 'json', @JSONV , 'QuickBooks Deposit Ticket Settings', 'ChurchInfoReport', 'Step7');
+(1043, 'sQBDTSettings', @JSONV , 'json', @JSONV , 'QuickBooks Deposit Ticket Settings', 'ChurchInfoReport', 'Step7');
 
 DELETE FROM `config_cfg` WHERE cfg_id = 3;
 
