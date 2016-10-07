a0399fad11da6cdd6ee35a502f77f3d06e43cd47
Charles <crossan007@gmail.com>
2016-10-06 22:41:13 -0400

    api cleanup

--- src/api/routes/timerjobs.php
+++ src/api/routes/timerjobs.php
@@ -4,10 +4,6 @@ $app->group('/timerjobs', function () {
   $this->post('/run', function () {
       $this->SystemService->runTimerJobs();
     });
-    
-    $this->get('/run', function () {
-      //$this->SystemService->runTimerJobs();
-    });
 });
 
 ?>
