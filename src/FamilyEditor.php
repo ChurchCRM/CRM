2d80b3446d17eafce0b49391ae8ca2d29591e3d6
George Dawoud <george@dawouds.com>
2016-10-06 22:50:35 -0700

    /plugins/input-mask/ is in all footers

--- src/FamilyEditor.php
+++ src/FamilyEditor.php
@@ -1068,12 +1068,6 @@ require "Include/Header.php";
 		echo " onclick=\"javascript:document.location='FamilyList.php';\">";
 	echo "</td></tr></form></table>";
 ?>
-	<!-- InputMask -->
-	<script src="<?= $sRootPath ?>/skin/adminlte/plugins/input-mask/jquery.inputmask.js" type="text/javascript"></script>
-	<script src="<?= $sRootPath ?>/skin/adminlte/plugins/input-mask/jquery.inputmask.date.extensions.js" type="text/javascript"></script>
-	<script src="<?= $sRootPath ?>/skin/adminlte/plugins/input-mask/jquery.inputmask.extensions.js" type="text/javascript"></script>
-
-	<script src="<?= $sRootPath ?>/skin/adminlte/plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
 
 	<script type="text/javascript">
 		$(function() {
