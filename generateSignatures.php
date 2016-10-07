a65324f9733e9fb4b5df9313d34ff45f5ce7d6cb
Charles <crossan007@gmail.com>
2016-10-06 22:58:31 -0400

    update build script to generate signature file

--- generateSignatures.php
+++ generateSignatures.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$CRMRoot = __DIR__."/src";
+$CRMRoot = $argv[1];
 $CRMRootLen = strlen($CRMRoot);
 $signatureFile = $CRMRoot."/signatures.json";
 $composerFile = file_get_contents(__DIR__. "/src/composer.json");
