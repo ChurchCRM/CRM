51f320caf546e2edd31ca7c660b2a279aa0c7dbb
George Dawoud <george@dawouds.com>
2016-10-06 22:49:15 -0700

    cleanup
    - added lastname by default
    - swtiched bithday to input mask

--- src/external/templates/registration/family-register-members.php
+++ src/external/templates/registration/family-register-members.php
@@ -52,7 +52,7 @@ require(__DIR__ ."/../../../Include/HeaderNotLoggedIn.php");
                                placeholder="<?= gettext("First Name") ?>" required>
                       </div>
                       <div class="col-lg-6">
-                        <input name="memberLastName-<?= $x ?>" class="form-control"
+                        <input name="memberLastName-<?= $x ?>" class="form-control" value="<?= $family->getName()?>"
                                placeholder="<?= gettext("Last Name") ?>" required>
                       </div>
                     </div>
@@ -67,25 +67,15 @@ require(__DIR__ ."/../../../Include/HeaderNotLoggedIn.php");
                   </div>
                   <div class="form-group has-feedback">
                     <div class="row">
-                      <div class="col-lg-4">
-                        <select name="memberBirthDay-<?= $x ?>" class="form-control">
-                          <?php for ($y = 1; $y <= 31; $y++) { ?>
-                            <option><?= $y ?></option>
-                          <?php } ?>
-                        </select>
-                      </div>
-                      <div class="col-lg-4">
-                        <select name="memberBirthMonth-<?= $x ?>" class="form-control">
-                          <?php for ($z = 1; $z < 13; $z++) {
-                            $month = gettext(date('F', mktime(0, 0, 0, $z, 1, 2016))) ?>
-                            <option value="<?= $z ?>"><?= $month ?></option>
-                          <?php } ?>
-                        </select>
-                      </div>
-                      <div class="col-lg-4">
-                        <input name="memberBirthYear-<?= $x ?>" class="form-control"
-                               placeholder="<?= gettext("Birth Year") ?>" required>
-                      </div>
+                      <div class="col-lg-6">
+                          <div class="input-group">
+                            <div class="input-group-addon">
+                              <i class="fa fa-birthday-cake"></i>
+                            </div>
+                            <input type="text" class="form-control inputDatePicker" id="birthday-<?= $x ?>" class="form-control" data-inputmask="'alias': 'mm/dd/yyyy'" data-mask="">
+                          </div>
+                          <!-- /.input group -->
+                        </div>
                     </div>
                   </div>
                 </div>
@@ -100,7 +90,11 @@ require(__DIR__ ."/../../../Include/HeaderNotLoggedIn.php");
     </div>
 
   </form>
-
+  <script type="text/javascript">
+    $(function() {
+      $("[data-mask]").inputmask();
+    });
+  </script>
 <?php
 // Add the page footer
 require(__DIR__ ."/../../../Include/FooterNotLoggedIn.php");
