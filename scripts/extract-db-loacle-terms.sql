f59d798337896f3a08fec87d45e4a9dfc2fb60ca
George Dawoud <george@dawouds.com>
2016-10-27 08:02:53 -0700

    new tables

--- scripts/extract-db-loacle-terms.sql
+++ scripts/extract-db-loacle-terms.sql
@@ -4,4 +4,10 @@ select DISTINCT qry_Name as term, "" as translation, "query_qry" as cntx   from 
 union all
 select DISTINCT qry_Description as term, "" as translation, "query_qry" as cntx    from query_qry
 union all
+select DISTINCT qpo_Display as term, "" as translation, "queryparameteroptions_qpo" as cntx from queryparameteroptions_qpo
+union all
+select DISTINCT qrp_Name as term, "" as translation, "queryparameters_qrp" as cntx from queryparameters_qrp
+union all
+select DISTINCT qrp_Description term, "" as translation, "queryparameters_qrp" as cntx from queryparameters_qrp
+union all
 select DISTINCT content_english as term, "" as translation, "menuconfig_mcf" as cntx    from menuconfig_mcf;
