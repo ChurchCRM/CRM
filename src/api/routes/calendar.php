52937283ae8053730247082d63abe0e168d1eca1
Brad Gearon <bradgearon@gmail.com>
2016-11-07 21:39:06 -0600

    fixed range calculation and query in CalendarService added start and end query parameters to get events resolves ChurchCRM/CRM#1289

--- src/api/routes/calendar.php
+++ src/api/routes/calendar.php
@@ -5,7 +5,8 @@
 $app->group('/calendar', function () {
 
   $this->get('/events', function ($request, $response, $args) {
-    return $response->withJson($this->CalendarService->getEvents());
+    $params = $request->getQueryParams();
+    return $response->withJson($this->CalendarService->getEvents($params['start'], $params['end']));
   });
 
 });
