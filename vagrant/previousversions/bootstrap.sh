425890c095adeb5ec5a806767189d457b6e2cef1
Charles <crossan007@gmail.com>
2016-11-11 19:58:11 -0500

    tweak the previousversions script

--- vagrant/previousversions/bootstrap.sh
+++ vagrant/previousversions/bootstrap.sh
@@ -3,9 +3,11 @@
 #=============================================================================
 # DB Setup
 rm -rf /var/www/public/*
-launchversion="$(grep "^[^#;]" VersionToLaunch)"
+launchversion=`grep -i '^[^#;]' /vagrant/VersionToLaunch`
 
-if [ $launchVersion == "1.2.14" ] ; then
+
+if [ $launchversion == "1.2.14" ] ; then
+  echo "bootstrapping 1.2.14"
   wget -nv -O /var/www/1.2.14.zip http://downloads.sourceforge.net/project/churchinfo/churchinfo/1.2.14/churchinfo-1.2.14.zip
   unzip -d /var/www/public /var/www/1.2.14.zip
   mv  /var/www/public/churchinfo/* /var/www/public/
@@ -14,15 +16,20 @@ if [ $launchVersion == "1.2.14" ] ; then
   CRM_DB_PASS="churchinfo"
   CRM_DB_NAME="churchinfo"
 
-elif [[ $launchVersion =~ [2\.] ]] ; then
-  filename=ChurchCRM-$launchVersion.zip
-  wget -nv -O /var/www/$filename https://github.com/ChurchCRM/CRM/releases/download/$launchVersion/$filename
+elif [[ $launchversion =~ [2\.] ]] ; then
+  echo "bootstrapping $launchversion"
+  filename=ChurchCRM-$launchversion.zip
+  wget -nv -O /var/www/$filename https://github.com/ChurchCRM/CRM/releases/download/$launchversion/$filename
   unzip -d /var/www/public /var/www/$filename
   mv  /var/www/public/churchcrm/* /var/www/public/
   CRM_DB_INSTALL_SCRIPT="/vagrant/src/mysql/install/Install.sql"
   CRM_DB_USER="churchcrm"
   CRM_DB_PASS="churchcrm"
   CRM_DB_NAME="churchcrm"
+
+else
+  echo "version string not valid"
+  exit 1
 fi
 
 DB_USER="root"
@@ -59,7 +66,7 @@ echo "Database: tables and metadata deployed"
 # Help info
 
 echo "============================================================================="
-echo "======== ChurchCRM is now hosted @ http://192.168.33.11/       =============="
+echo "======== ChurchCRM is now hosted @ http://192.168.33.12/       =============="
 echo "======== Version is: $launchversion                            =============="
 echo "======== CRM User Name: admin                                  =============="
 echo "======== 1st time login password for admin: changeme           =============="
