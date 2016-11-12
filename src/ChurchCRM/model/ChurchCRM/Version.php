6def9bf3dbb96f31394835431a9996e19c5b46b2
Charles <crossan007@gmail.com>
2016-11-11 20:53:36 -0500

    move logic for verifying version table schema onto the version ORM object (#1322)

--- src/ChurchCRM/model/ChurchCRM/Version.php
+++ src/ChurchCRM/model/ChurchCRM/Version.php
@@ -16,5 +16,30 @@ use ChurchCRM\Base\Version as BaseVersion;
  */
 class Version extends BaseVersion
 {
-
+  public function preSave(\Propel\Runtime\Connection\ConnectionInterface $con = null) 
+  {
+    //before we try to save this version object to the database, ensure that 
+    //the database has the correct columns to accomedate the version data
+    $connection = \Propel\Runtime\Propel::getConnection();
+    $query = "DESCRIBE version_ver";
+    $statement = $connection->prepare($query);
+    $resultset = $statement->execute();
+    $results = $statement->fetchAll(\PDO::FETCH_ASSOC);
+    
+    if ( !in_array_recursive( "ver_update_start",$results)) //the versions table does not contain the ver_update_start column.
+    {
+      $query = "ALTER TABLE version_ver CHANGE COLUMN ver_date ver_update_start datetime default NULL;";
+      $statement = $connection->prepare($query);
+      $resultset = $statement->execute();
+    }
+    
+    if ( !in_array_recursive("ver_update_start",$results)) //the versions table does not contain the ver_update_end column.
+    {
+      $query = "ALTER TABLE version_ver ADD COLUMN ver_update_end datetime default NULL AFTER ver_update_start;";
+      $statement = $connection->prepare($query);
+      $resultset = $statement->execute();
+    }
+    //then save this version
+    return true;
+  }
 }
