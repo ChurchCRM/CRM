2f617e3604efb8eb4481dd7c0d17b920506ca0c3
George Dawoud <george@dawouds.com>
2016-11-12 09:05:14 -0800

    using new generated locale files for full calendar

--- src/Include/Footer.php
+++ src/Include/Footer.php
@@ -53,7 +53,10 @@
   <script src="<?= $sRootPath ?>/skin/js/DataTables.js"></script>
   <script src="<?= $sRootPath ?>/skin/js/Tooltips.js"></script>
   <script src="<?= $sRootPath ?>/skin/js/Events.js"></script>
-  <script src="<?= $sRootPath ?>/skin/js/Footer.js"></script>
+  <script src="<?= $sRootPath ?>/skin/js/Events.js"></script>
+
+  <script src="<?= $sRootPath ?>/skin/locale/<?= $localeInfo->getLocale() ?>.js"></script>
+
   <?php if ($_SESSION['bAdmin']) { ?>
   <script>
     ((window.gitter = {}).chat = {}).options = {
