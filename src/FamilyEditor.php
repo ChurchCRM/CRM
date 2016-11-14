09258a65db1a9fa390f5cc875681545750e357f5
George Dawoud <george@dawouds.com>
2016-11-13 23:46:49 -0800

    added missing save note with family update

--- src/FamilyEditor.php
+++ src/FamilyEditor.php
@@ -442,7 +442,7 @@ if (isset($_POST["FamilySubmit"]) || isset($_POST["FamilySubmitAndAdd"]))
           $dbPersonId = mysql_insert_id();
           $note = new Note();
           $note->setPerId($dbPersonId);
-          $note->setText("Created");
+          $note->setText("Created via Family");
           $note->setType("create");
           $note->setEntered($_SESSION['iUserID']);
           $note->save();
@@ -482,6 +482,12 @@ if (isset($_POST["FamilySubmit"]) || isset($_POST["FamilySubmitAndAdd"]))
 					$sSQL = "UPDATE person_per SET per_FirstName='" . $aFirstNames[$iCount] . "', per_MiddleName='" . $aMiddleNames[$iCount] . "',per_LastName='" . $aLastNames[$iCount] . "',per_Suffix='" . $aSuffix[$iCount] . "',per_Gender='" . $aGenders[$iCount] . "',per_fmr_ID='" . $aRoles[$iCount] . "',per_BirthMonth='" . $aBirthMonths[$iCount] . "',per_BirthDay='" . $aBirthDays[$iCount] . "', " . $sBirthYearScript . "per_cls_ID='" . $aClassification[$iCount] . "' WHERE per_ID=" . $aPersonIDs[$iCount];
 					RunQuery($sSQL);
 					//RunQuery("UNLOCK TABLES");
+          $note = new Note();
+          $note->setPerId($aPersonIDs[$iCount]);
+          $note->setText("Updated via Family");
+          $note->setType("edit");
+          $note->setEntered($_SESSION['iUserID']);
+          $note->save();
 				}
 			}
 			$note = new Note();
