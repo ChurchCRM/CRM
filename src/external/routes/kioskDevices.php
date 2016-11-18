7721a9fe1c942220b8fac7456750df3a5d89ddd9
Charles <crossan007@gmail.com>
2016-11-17 21:58:02 -0500

    buttons and pictures!

--- src/external/routes/kioskDevices.php
+++ src/external/routes/kioskDevices.php
@@ -7,6 +7,9 @@ use ChurchCRM\Person;
 use Slim\Views\PhpRenderer;
 use ChurchCRM\GroupQuery;
 use ChurchCRM\Person2group2roleP2g2rQuery;
+use ChurchCRM\PersonQuery;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 
 $app->group('/kioskdevices', function () {
@@ -14,18 +17,29 @@ $app->group('/kioskdevices', function () {
   $this->get('/{guid}', function ($request, $response, $args) {
 
       $renderer = new PhpRenderer("templates/kioskDevices/");
-    
-      return $renderer->render($response, "sunday-school-class-view.php", array("sRootPath" => $_SESSION['sRootPath'], ""=>$ssClass));
+      $pageObjects = array("sRootPath" => $_SESSION['sRootPath'], "thisDeviceGuid" => $args['guid']);
+      return $renderer->render($response, "sunday-school-class-view.php", $pageObjects);
 
     });
     
     $this->get('/{guid}/activeClassMembers', function ($request, $response, $args) {
      $ssClass = ChurchCRM\Person2group2roleP2g2rQuery::create()
+            ->orderByRoleId()
+            ->addJoin(ChurchCRM\Map\ListOptionTableMap::COL_LST_ID, ChurchCRM\Map\GroupTableMap::COL_GRP_ID,  Propel\Runtime\ActiveQuery\Criteria::INNER_JOIN)
             ->joinWithPerson()
-            ->findByGroupId(2);
+            ->findByGroupId(5);
       return $ssClass->toJSON();
-     
-
+    });
+    
+    $this->get('/{guid}/activeClassMember/{PersonId}/photo', function (ServerRequestInterface  $request, ResponseInterface  $response, $args) {
+     $photo = ChurchCRM\PersonQuery::create()
+              ->findOneById($args['PersonId'])
+              ->getPhoto();
+    
+     $image = file_get_contents(dirname(dirname(__DIR__))."/".$photo);
+     $finfo = new finfo(FILEINFO_MIME_TYPE);
+     $response->getBody()->write($image);      
+      return $response->withHeader('Content-Type', 'content-type: ' . $finfo->buffer($image));
     });
   
 });
--- src/external/templates/kioskDevices/sunday-school-class-view.php
+++ src/external/templates/kioskDevices/sunday-school-class-view.php
@@ -3,29 +3,42 @@
 $sPageTitle = "ChurchCRM - Sunday School Device Kiosk";
 require(__DIR__ ."/../../../Include/HeaderNotLoggedIn.php");
 ?>
+<style>
+  .widget-user-2 .widget-user-header {
+    padding: 5px;
+  }
+  
+</style>
 
 <div class="container" id="classMemberContainer">
     
 </div>
 
 <script>
+  window.CRM.thisDeviceGuid = "<?= $thisDeviceGuid ?>";
   //first, define the function that will render the active members
+  
+  window.CRM.displayPersonInfo = function (personId)
+  {
+    console.log(personId);
+  }
+  
   window.CRM.renderClassMember = function (classMember) {
-      var outerDiv = $("<div>").addClass("col-md-4");
+      var outerDiv = $("<div>",{id:"personId-"+classMember.personId}).addClass("col-md-4");
       var innerDiv = $("<div>").addClass("box box-widget widget-user-2");
-      var userHeaderDiv = $("<div>",{class :"widget-user-header bg-yellow"});
+      var userHeaderDiv = $("<div>",{class :"widget-user-header bg-yellow"}).attr("data-personid",classMember.personId);
       var imageDiv = $("<div>", {class:"widget-user-image"})
               .append($("<img>",{
-                src: window.CRM.root+"/Images/Person/kid_boy-128.png"
+                src: window.CRM.root+"/external/kioskdevices/"+window.CRM.thisDeviceGuid+"/activeClassMember/"+classMember.personId+"/photo"
               }));
       userHeaderDiv.append(imageDiv);
       userHeaderDiv.append($("<h3>",{class:"widget-user-username", text:classMember.displayName})).append($("<h3>",{class:"widget-user-desc", text:classMember.classRole}));
       innerDiv.append(userHeaderDiv);
       innerDiv.append($("<div>", { class : "box-footer no-padding"})
-              .append($("<ul>", {class:"nav nav-stacked"})
-                .append($("<li>",{class: "btn btn-block btn-danger", text : "Trigger Parent Alert"}).prepend($("<i>",{class:"fa fa-exclamation-triangle",'aria-hidden':"true"}) ))
-                .append($("<li>",{class: "btn btn-block btn-primary", text : "Checkout Child"}))
-                .append($("<li>",{class: "btn btn-block btn-info", text : "View Student Info"}))
+              .append($("<ul>", {class:"nav navbar-nav", style:"width:100%"})
+                .append($("<li>", {style:"width:50%"})
+                  .append($("<button>",{class: "btn btn-danger parentAlertButton", style:"width:100%", text : "Trigger Parent Alert", "data-personid": classMember.personId}).prepend($("<i>",{class:"fa fa-exclamation-triangle",'aria-hidden':"true"}) )))
+                .append($("<li>",{class: "btn btn-primary checkinButton", style:"width:50%", text : "Checkin", "data-personid": classMember.personId}))
               ));
       outerDiv.append(innerDiv);
       $("#classMemberContainer").append(outerDiv);   
@@ -35,12 +48,55 @@ require(__DIR__ ."/../../../Include/HeaderNotLoggedIn.php");
     window.CRM.updateActiveClassMembers = function()
     {
       $.ajax( {
-        url: window.CRM.root+"/external/"+window.CRM.thisDeviceGuid+"/activeClassMembers"
+        method: "GET",
+        url: window.CRM.root+"/external/kioskdevices/"+window.CRM.thisDeviceGuid+"/activeClassMembers",
+        dataType: "json"
       }).
         done(function(data){
-            
+          $(data.Person2group2roleP2g2rs).each(function(i,d){
+            //console.log(d);
+            window.CRM.renderClassMember({displayName:d.Person.FirstName+" "+d.Person.LastName, classRole:d.RoleId,personId:d.PersonId})
+          });
       })
     };
+    $(document).ready(function() {
+      window.CRM.updateActiveClassMembers();
+    });
+    
+    $(document).on('click','.widget-user-header', function(event)
+    {
+      var personId  = $(event.currentTarget).data('personid')
+      window.CRM.displayPersonInfo(personId);
+    });
+    
+    $(document).on('click','.parentAlertButton', function(event)
+    {
+      var personId  = $(event.currentTarget).data('personid')
+      console.log("Parent Alert for: "+personId);
+    });
+    
+    $(document).on('click','.checkinButton', function(event)
+    {
+      var personId  = $(event.currentTarget).data('personid');
+      $(event.currentTarget).removeClass("checkinButton");
+      $(event.currentTarget).addClass("checkoutButton");
+      $(event.currentTarget).text("Checkout");
+      $("#personId-"+personId).find(".widget-user-header").removeClass("bg-yellow");
+      $("#personId-"+personId).find(".widget-user-header").addClass("bg-green");
+      
+      console.log("Checkin for: "+personId);
+    });
+    
+     $(document).on('click','.checkoutButton', function(event)
+    {
+      var personId  = $(event.currentTarget).data('personid');
+      $(event.currentTarget).removeClass("checkoutButton");
+      $(event.currentTarget).addClass("checkinButton");
+      $(event.currentTarget).text("CheckIn");
+      $("#personId-"+personId).find(".widget-user-header").removeClass("bg-green");
+      $("#personId-"+personId).find(".widget-user-header").addClass("bg-yellow");
+      console.log("Checkout for: "+personId);
+    });
 
 </script>
 
