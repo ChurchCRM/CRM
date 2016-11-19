48f0c8a14fb82715280644908418d3175db3e76c
Charles <crossan007@gmail.com>
2016-11-18 23:57:32 -0500

    boostrap.sh missing php7 dependency

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -28,7 +28,7 @@ if [ $1 == 'php7' ]; then
   sudo apt-get update
   sudo apt-get install -y php7.0
   sudo apt-get install -y php7.0-mysql
-  sudo apt-get install -y php7.0-xml php7.0-curl
+  sudo apt-get install -y php7.0-xml php7.0-curl php7.0-zip
   sudo a2dismod php5
   sudo a2enmod php7.0
   sudo service apache2 start
