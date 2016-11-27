f4ee5bce7b4e8c639839fb49f3c3560d7e62dcbc
George Dawoud <george@dawouds.com>
2016-11-27 10:16:11 -0800

    created
    - new dto
    - fixes bad URL for sRootPath
    - throws error if invalid URL

---
+++ src/ChurchCRM/dto/SystemURLs.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: georg
+ * Date: 11/27/2016
+ * Time: 9:33 AM
+ */
+
+namespace ChurchCRM\dto;
+
+
+class SystemURLs
+{
+    private static $rootPath;
+    private static $urls;
+    private static $documentRoot;
+
+    public static function init($rootPath, $urls, $documentRoot)
+    {
+        // Avoid consecutive slashes when $sRootPath = '/'
+        if ($rootPath == "/") {
+            $rootPath = "";
+        }
+        self::$rootPath = $rootPath;
+        self::$urls = $urls;
+        self::$documentRoot = $documentRoot;
+    }
+
+    public static function getRootPath()
+    {
+        if (self::isValidRootPath()) {
+            return self::$rootPath;
+        }
+        throw new \Exception("Please check the value for '\$sRootPath' in <b>`Include\\Config.php`</b>, the following is not valid [". self::$rootPath . "]");
+    }
+
+    public static function getDocumentRoot()
+    {
+        return self::$documentRoot;
+    }
+
+    public static function getURLs()
+    {
+        return self::$urls;
+    }
+
+    public static function getURL($index)
+    {
+        return self::$urls[$index];
+    }
+
+    private static function isValidRootPath()
+    {
+        if (stripos(self::$rootPath, "http") !== true ) {
+            return false;
+        }
+        return true;
+
+    }
+}
