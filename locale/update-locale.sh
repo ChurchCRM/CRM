055e52d034de328d31af3c7a81f37510cfc3f586
Charles <crossan007@gmail.com>
2016-11-27 20:49:55 -0500

    underp locale-gen (#1473)

--- locale/update-locale.sh
+++ locale/update-locale.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /vagrant/src
-find . -iname '*.php' | grep -v ./vendor | xargs xgettext --from-code=UTF-8 -o cd /vagrant/locale/messages.po -L PHP
+find . -iname '*.php' | grep -v ./vendor | xargs xgettext --from-code=UTF-8 -o /vagrant/locale/messages.po -L PHP
 cd /vagrant/locale/
 php extract-db-locale-terms.php
 cd db-strings
