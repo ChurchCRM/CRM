09e9916a8efdcbfce25083c7d2420ca1b5f37901
Charles <crossan007@gmail.com>
2016-12-03 14:33:39 -0500

    fix upgrade logic and syntax

--- src/mysql/upgrade/2.4.0.sql
+++ src/mysql/upgrade/2.4.0.sql
@@ -2,7 +2,7 @@ ALTER TABLE `config_cfg`
 MODIFY `cfg_type` ENUM('text','number','date','boolean','textarea','json','choice', 'country') NOT NULL default 'text';
 
 INSERT INTO `config_cfg` (`cfg_id`, `cfg_name`, `cfg_value`, `cfg_type`, `cfg_default`, `cfg_tooltip`, `cfg_section`) VALUES
-(1047, 'sChurchCountry', 'United States', 'country', '', 'Church Country', 'ChurchInfoReport';
+(1047, 'sChurchCountry', 'United States', 'country', '', 'Church Country', 'ChurchInfoReport');
 
 ALTER TABLE `user_usr`
 DROP COLUMN `usr_BaseFontSize`,
