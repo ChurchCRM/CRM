79e2006160474e2feaac1fbaf3b989c46a043bbe
George Dawoud <george@dawouds.com>
2016-12-17 07:51:16 -0800

    final tokens table sql

--- src/mysql/install/Install.sql
+++ src/mysql/install/Install.sql
@@ -1377,4 +1377,13 @@ CREATE TABLE `egive_egv` (
 ) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 
+CREATE TABLE `tokens` (
+  `token` VARCHAR(99) NOT NULL,
+  `type` ENUM('verify') NOT NULL,
+  `reference_id` INT(9) NOT NULL,
+  `valid_until_date` datetime NULL,
+  `use_count` INT(2) NULL,
+  PRIMARY KEY (`token`)
+) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
 update version_ver set ver_update_end = now();
--- src/mysql/upgrade/2.5.0/2.5.0-tokens.sql
+++ src/mysql/upgrade/2.5.0/2.5.0-tokens.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `tokens` (
-  `token` VARCHAR(99) NOT NULL COMMENT '',
-  `type` ENUM('verify') NOT NULL COMMENT '',
-  `reference_id` INT(9) NOT NULL COMMENT '',
-  `valid_until_date` TIMESTAMP NULL COMMENT '',
-  `use_count` INT(2) NULL COMMENT '',
-  PRIMARY KEY (`token`)  COMMENT '');
+  `token` VARCHAR(99) NOT NULL,
+  `type` ENUM('verify') NOT NULL,
+  `reference_id` INT(9) NOT NULL,
+  `valid_until_date` datetime NULL,
+  `use_count` INT(2) NULL,
+  PRIMARY KEY (`token`)
+) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci;
