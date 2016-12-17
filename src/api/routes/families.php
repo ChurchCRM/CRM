49a50b4d4fa1beb4682fdf8c984a67a2041cdc39
George Dawoud <george@dawouds.com>
2016-12-17 07:52:22 -0800

    added verify api to create the token for now

--- src/api/routes/families.php
+++ src/api/routes/families.php
@@ -1,5 +1,7 @@
 <?php
-// Routes
+
+use ChurchCRM\FamilyQuery;
+use ChurchCRM\Token;
 
 
 $app->group('/families', function () {
@@ -21,5 +23,19 @@ $app->group('/families', function () {
   $this->get('/byEnvelopeNumber/{envelopeNumber:[0-9]+}', function ($request, $response, $args) {
     $envelopeNumber = $args['envelopeNumber'];
     echo $this->FamilyService->getFamilyStringByEnvelope($envelopeNumber);
+  });
+
+  $this->post('/verify/{familyId}', function ($request, $response, $args) {
+    $familyId = $args["familyId"];
+    $family = FamilyQuery::create()->findPk($familyId);
+    if ($family != null) {
+      $token = new Token();
+      $token->build("verify", $family->getId());
+      $token->save();
+      $response->withStatus(200);
+    } else {
+      $response->withStatus(404)->getBody()->write("familyId: " . $familyId . " not found");
+    }
+    return $response;
   });
 });
