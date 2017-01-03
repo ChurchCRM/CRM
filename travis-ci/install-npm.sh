b8ae0343e3485415ce54a0608c78ae2b9acd4bf9
George Dawoud <george@dawouds.com>
2017-01-03 00:52:08 -0800

    with no prams

--- travis-ci/install-npm.sh
+++ travis-ci/install-npm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "===============   npm  install -g       ================="
-sudo npm install -g npm@latest --unsafe-perm
+sudo npm install -g
 echo "===============   npm  install          ================="
-sudo npm install --unsafe-perm
+sudo npm install
