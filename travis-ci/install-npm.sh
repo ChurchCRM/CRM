99cec92ea1a0bfd15b177dae4207b7f105b5c20c
George Dawoud <george@dawouds.com>
2017-01-03 01:05:08 -0800

    manual grunt install

--- travis-ci/install-npm.sh
+++ travis-ci/install-npm.sh
@@ -3,5 +3,5 @@
 echo "===============   npm  install -g       ================="
 sudo npm install -g npm@latest --unsafe-perm
 echo "===============   npm  install          ================="
-pwd
+sudo npm install grunt --unsafe-perm
 sudo npm install --unsafe-perm
