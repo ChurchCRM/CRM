00bedea1d6e5adb1eeffa2d73ff63f1477f94bb4
George Dawoud <george@dawouds.com>
2017-01-03 00:32:57 -0800

    better logs

--- travis-ci/setup-db.sh
+++ travis-ci/setup-db.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+echo "=========================================================="
+echo "===============   db script import       ================="
+echo "=========================================================="
 mysql -e "create database IF NOT EXISTS churchcrm_test;" -uroot;
 mysql churchcrm_test < src/mysql/install/Install.sql -uroot;
 mysql churchcrm_test < src/mysql/upgrade/update_config.sql -uroot;
+echo "===============   db script import  Done  ================"
