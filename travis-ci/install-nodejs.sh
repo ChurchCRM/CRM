7f22bb1f664b112c04791c7a142ca479012c3891
George Dawoud <george@dawouds.com>
2017-01-03 01:02:37 -0800

    build-essential and node 7

--- travis-ci/install-nodejs.sh
+++ travis-ci/install-nodejs.sh
@@ -7,5 +7,6 @@ echo "===============   nodejs install         ================="
 echo "=========================================================="
 # setup for node 6/7 (v6.9 is an LTS release, so we'll support it for the next time)
 sudo rm -rf ~/.nvm
-curl -sL "https://deb.nodesource.com/setup_5.x" | sudo -E bash -
+curl -sL "https://deb.nodesource.com/setup_7.x" | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
