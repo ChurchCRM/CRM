dc12722880e11e9d09ac13119aac95149f08b734
George Dawoud <george@dawouds.com>
2017-01-02 23:52:17 -0800

    node install hardcoded version

--- travis-ci/install.sh
+++ travis-ci/install.sh
@@ -4,7 +4,7 @@ set -e
 
 # setup for node 6/7 (v6.9 is an LTS release, so we'll support it for the next time)
 sudo rm -rf ~/.nvm
-curl -sL "https://deb.nodesource.com/setup_${NODE_VERSION}" | sudo -E bash -
+curl -sL "https://deb.nodesource.com/setup_5.x" | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # install global npm dependencies
