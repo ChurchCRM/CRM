4b891ac6cdbb8319b9f9855df133c52ff16c8881
George Dawoud <george@dawouds.com>
2017-01-03 00:43:12 -0800

    split node js install

--- travis-ci/install-npm.sh
+++ travis-ci/install-npm.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
-echo "=========================================================="
-echo "===============   nodejs install         ================="
-echo "=========================================================="
-# setup for node 6/7 (v6.9 is an LTS release, so we'll support it for the next time)
-sudo rm -rf ~/.nvm
-curl -sL "https://deb.nodesource.com/setup_7.x" | sudo -E bash -
-sudo apt-get install -y nodejs
 echo "===============   npm  install -g       ================="
 
 # install global npm dependencies
