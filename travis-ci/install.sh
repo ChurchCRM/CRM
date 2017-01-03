98614a2e69c23140f7fef336a07ded5bb25be1cf
George Dawoud <george@dawouds.com>
2017-01-03 00:19:35 -0800

    added echo and running composer 1st

--- travis-ci/install.sh
+++ travis-ci/install.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-# setup for node 6/7 (v6.9 is an LTS release, so we'll support it for the next time)
-sudo rm -rf ~/.nvm
-curl -sL "https://deb.nodesource.com/setup_6.x" | sudo -E bash -
-sudo apt-get install -y nodejs
 
+echo "=========================================================="
+echo "===============   Composer Install/Setup ================="
+echo "=========================================================="
 # composer install
 cd src
 composer install
@@ -15,7 +14,16 @@ src/vendor/bin/propel model:build --config-dir=propel
 cd src
 composer dump-autoload
 
+echo "=========================================================="
+echo "===============   nodejs install         ================="
+echo "=========================================================="
+# setup for node 6/7 (v6.9 is an LTS release, so we'll support it for the next time)
+sudo rm -rf ~/.nvm
+curl -sL "https://deb.nodesource.com/setup_6.x" | sudo -E bash -
+sudo apt-get install -y nodejs
+echo "===============   npm  install -g       ================="
 cd ../
 # install global npm dependencies
 sudo npm install -g npm@latest --unsafe-perm --no-bin-links
+echo "===============   npm  install          ================="
 npm install
