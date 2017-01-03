eb30e9a162ec26e199d481b39b94373ff0dddb44
George Dawoud <george@dawouds.com>
2017-01-03 00:33:57 -0800

    node 7

--- travis-ci/install-npm.sh
+++ travis-ci/install-npm.sh
@@ -7,7 +7,7 @@ echo "===============   nodejs install         ================="
 echo "=========================================================="
 # setup for node 6/7 (v6.9 is an LTS release, so we'll support it for the next time)
 sudo rm -rf ~/.nvm
-curl -sL "https://deb.nodesource.com/setup_6.x" | sudo -E bash -
+curl -sL "https://deb.nodesource.com/setup_7.x" | sudo -E bash -
 sudo apt-get install -y nodejs
 echo "===============   npm  install -g       ================="
 cd ../
