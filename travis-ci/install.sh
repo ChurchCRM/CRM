dc890e2a0784c3ea9629cb2a63f93e038daa6a80
George Dawoud <george@dawouds.com>
2017-01-02 23:55:22 -0800

    npm 6 and fixed composer dir

--- travis-ci/install.sh
+++ travis-ci/install.sh
@@ -4,11 +4,12 @@ set -e
 
 # setup for node 6/7 (v6.9 is an LTS release, so we'll support it for the next time)
 sudo rm -rf ~/.nvm
-curl -sL "https://deb.nodesource.com/setup_5.x" | sudo -E bash -
+curl -sL "https://deb.nodesource.com/setup_6.x" | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # install global npm dependencies
 sudo npm install
 
 # composer install
+cd src
 composer install
