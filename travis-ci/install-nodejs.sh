862103ccf9644194caab154446958239b0dcba6e
George Dawoud <george@dawouds.com>
2017-01-07 12:47:54 -0800

    Update install-nodejs.sh

--- travis-ci/install-nodejs.sh
+++ travis-ci/install-nodejs.sh
@@ -6,9 +6,5 @@ echo "=========================================================="
 echo "===============   nodejs install         ================="
 echo "=========================================================="
 # setup for node 6/7 (v6.9 is an LTS release, so we'll support it for the next time)
-rm -rf ~/.nvm 
-git clone https://github.com/creationix/nvm.git ~/.nvm 
-cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
-source ~/.nvm/nvm.sh 
-nvm install $TRAVIS_NODE_VERSION
-npm install
+nvm install node
+
