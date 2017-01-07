02815babdba78cd01da3777e7aad6e3437af5949
George Dawoud <george@dawouds.com>
2017-01-07 13:22:28 -0800

    Delete install-npm.sh

--- travis-ci/install-npm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "===============   Node & NPM       ================="
-nvm install node 6.9
-nvm alias default 6.9
-npm install npm -g
-echo "===============   npm  install          ================="
-npm install -g
-npm install
