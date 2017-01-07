255408b762281f35c0c3b1d8e299a9567cc06f30
George Dawoud <george@dawouds.com>
2017-01-07 13:44:01 -0800

    Update setup-db.sh

--- travis-ci/setup-db.sh
+++ travis-ci/setup-db.sh
@@ -3,5 +3,8 @@ echo "=========================================================="
 echo "===============   db script import       ================="
 echo "=========================================================="
 mysql -e "create database IF NOT EXISTS churchcrm_test;" -uroot;
-mysql churchcrm_test < demo/ChurchCRM-Database.sql;
+mysql churchcrm_test < src/mysql/install/Install.sql -uroot;
+mysql churchcrm_test < src/mysql/upgrade/update_config.sql -uroot;
+echo "===============   db demo script import  ================"
+mysql churchcrm_test < demo/ChurchCRM-Database.sql -uroot;
 echo "===============   db script import  Done  ================"
