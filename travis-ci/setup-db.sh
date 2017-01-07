bd8fbd622e7b7a8ac7363242b166da615a80a76e
George Dawoud <george@dawouds.com>
2017-01-07 13:38:38 -0800

    Update setup-db.sh
    using demo data for tests

--- travis-ci/setup-db.sh
+++ travis-ci/setup-db.sh
@@ -3,6 +3,5 @@ echo "=========================================================="
 echo "===============   db script import       ================="
 echo "=========================================================="
 mysql -e "create database IF NOT EXISTS churchcrm_test;" -uroot;
-mysql churchcrm_test < src/mysql/install/Install.sql -uroot;
-mysql churchcrm_test < src/mysql/upgrade/update_config.sql -uroot;
+mysql churchcrm_test < demo/ChurchCRM-Database.sql;
 echo "===============   db script import  Done  ================"
