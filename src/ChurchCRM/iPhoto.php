6ac54fb7a4bec5a1a3a55e8a4c2c340ccc9c581c
George Dawoud <george@dawouds.com>
2017-01-10 01:44:36 +0000

    Apply fixes from StyleCI

--- src/ChurchCRM/iPhoto.php
+++ src/ChurchCRM/iPhoto.php
@@ -3,15 +3,13 @@ namespace ChurchCRM;
 
 interface iPhoto
 {
-  public function getPhotoBytes();
-  public function getThumbnailBytes();
-  public function getThumbnailURI();
-  public function getPhotoURI();
-  public function deletePhoto();
-  public function setImageFromBase64($base64);
-  public function isPhotoLocal();
-  public function isPhotoRemote();
-  public function getPhotoContentType();
+    public function getPhotoBytes();
+    public function getThumbnailBytes();
+    public function getThumbnailURI();
+    public function getPhotoURI();
+    public function deletePhoto();
+    public function setImageFromBase64($base64);
+    public function isPhotoLocal();
+    public function isPhotoRemote();
+    public function getPhotoContentType();
 }
-
-?>
