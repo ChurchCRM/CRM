361b5396ce439af9c6ad2358874d9ea53762f753
George Dawoud <george@dawouds.com>
2017-01-14 10:51:23 -0800

    clenaup of sLanguage to use key:value for choise to display country/lang

---
+++ src/mysql/upgrade/2.5.0/2.5.0-locale.sql
@@ -0,0 +1,3 @@
+update config_cfg set cfg_data = '{"Choices":["English - United States:en_US", "English - Australia:en_AU", "English - Great Britain:en_GB", "German - Germany:de_DE", "Spanish - Spain:es_ES", "French - France:fr_FR", "Hungarian:hu_HU", "Italian - Italy:it_IT", "Norwegian:nb_NO", "Dutch - Netherlands:nl_NL", "Polish:pl_PL", "Portuguese - Brazil:pt_BR", "Romanian - Romania:ro_RO", "Russian:ru_RU", "Sami (Northern) (Sweden):se_SE", "Albanian:sq_AL", "Swedish - Sweden:sv_SE", "Vietnamese:vi_VN", "Chinese - China:zh_CN", "Chinese - Taiwan:zh_TW"]}' where cfg_id = 39;
+
+update config_cfg set cfg_tooltip = 'Internationalization (I18n) support' where cfg_id = 39;
