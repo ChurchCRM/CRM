e69665f58622e5dbcbb69439c82fc7df8997b211
George Dawoud <george@dawouds.com>
2017-01-15 15:59:11 -0800

    merge all db scripts into 2.5.0 RC1 file

--- src/mysql/upgrade/2.5.0/2.5.0-tokens.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `tokens`;
-CREATE TABLE `tokens` (
-  `token` VARCHAR(99) NOT NULL,
-  `type` ENUM('verifyFamily', 'verifyPerson') NOT NULL,
-  `reference_id` INT(9) NOT NULL,
-  `valid_until_date` datetime NULL,
-  `remainingUses` INT(2) NULL,
-  PRIMARY KEY (`token`)
-) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci;
--- src/mysql/upgrade/2.5.0/2.5.0-zip.sql
+++
@@ -1,1 +0,0 @@
-update config_cfg set cfg_tooltip = 'Make user-entered zip/postcodes UPPERCASE when saving to the database.' where cfg_id = 67;
