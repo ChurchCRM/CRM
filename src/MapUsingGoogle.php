1e7c0740d7403aaabb1061a82eff42bec2c5e851
Simon Philip <simonkph007@gmail.com>
2017-02-10 01:56:16 +0800

    Complete rewrite of mapusingGoogle.
    Complete rewrite of mapusingGoogle.
    * added legends on map screen for desktop.
    * legend at the bootom for mobile.

--- src/MapUsingGoogle.php
+++ src/MapUsingGoogle.php
@@ -4,6 +4,8 @@ require 'Include/Functions.php';
 require 'Include/ReportFunctions.php';
 
 use ChurchCRM\dto\SystemConfig;
+use ChurchCRM\Base\FamilyQuery;
+use ChurchCRM\Base\ListOptionQuery;
 
 //Set the page title
 $sPageTitle = gettext('Family View');
@@ -12,6 +14,7 @@ require 'Include/Header.php';
 
 $iGroupID = FilterInput($_GET['GroupID'], 'int');
 
+//update SystemConfig with coordinates if nChurchLatitude or nChurchLongitude is not set by using address lookup
 if (SystemConfig::getValue('nChurchLatitude') == '' || SystemConfig::getValue('nChurchLongitude') == '') {
     require 'Include/GeoCoder.php';
     $myAddressLatLon = new AddressLatLon();
@@ -26,157 +29,291 @@ if (SystemConfig::getValue('nChurchLatitude') == '' || SystemConfig::getValue('n
         SystemConfig::setValue('nChurchLatitude', $nChurchLatitude);
         SystemConfig::setValue('nChurchLongitude', $nChurchLongitude);
     }
-}
+} //end update systemConfig
 
 if (SystemConfig::getValue('nChurchLatitude') == '') {
     ?>
-  <div class="callout callout-danger">
-    <?= gettext('Unable to display map due to missing Church Latitude or Longitude. Please update the church Address in the settings menu.') ?>
-  </div>
-<?php
+    <div class="callout callout-danger">
+        <?= gettext('Unable to display map due to missing Church Latitude or Longitude. Please update the church Address in the settings menu.') ?>
+    </div>
+    <?php
 } else {
     if (SystemConfig::getValue('sGoogleMapKey') == '') {
         ?>
-    <div class="callout callout-warning">
-      <?= gettext('Google Map API key is not set. The Map will work for smaller set of locations. Please create a Key in the maps sections of the setting menu.') ?>
-    </div>
-<?php
-    } ?>
-
-<script type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=<?= SystemConfig::getValue('sGoogleMapKey') ?>&sensor=false"></script>
-
-<div class="box box-body">
-    <div class="col-lg-12">
-        <div id="map" class="col-lg-12" style="height: 600px;"></div>
-        <script type="text/javascript">
-            var mapOptions = {
-               center: new google.maps.LatLng(<?= SystemConfig::getValue('nChurchLatitude').', '.SystemConfig::getValue('nChurchLongitude') ?>),
-               zoom: 10, //move this to systemconfig
-               mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-
-            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-            var shadow = new google.maps.MarkerImage('http://google-maps-icons.googlecode.com/files/shadow.png',
-                new google.maps.Size(51, 37),
-                null,
-                new google.maps.Point(18, 37));
-
-            var churchMark = new google.maps.Marker({
-                icon: window.CRM.root + "/skin/icons/church.png",
-                shadow: shadow,
-                position: new google.maps.LatLng(<?= SystemConfig::getValue('nChurchLatitude').', '.SystemConfig::getValue('nChurchLongitude') ?>),
-                map: map});
-
-
-            var churchInfoWin = new google.maps.InfoWindow({content: "<?= SystemConfig::getValue('sChurchName').'<p>'.SystemConfig::getValue('sChurchAddress').'<p>'.SystemConfig::getValue('sChurchCity').', '.SystemConfig::getValue('sChurchState').'  '.SystemConfig::getValue('sChurchZip'); ?>"});
-
-            google.maps.event.addListener(churchMark, "click", function() {
-                churchInfoWin.open(map,churchMark);
-            });
-
-            <?php
-                $appendToQuery = '';
-    if ($iGroupID > 0) {
-        // If mapping only members of  a group build a condition to add to the query used below
-
-                    //Get all the members of this group
-                    $sSQL = 'SELECT per_fam_ID FROM person_per, person2group2role_p2g2r WHERE per_ID = p2g2r_per_ID AND p2g2r_grp_ID = '.$iGroupID;
-        $rsGroupMembers = RunQuery($sSQL);
-        $appendToQuery = ' WHERE fam_ID IN (';
-        while ($aPerFam = mysqli_fetch_array($rsGroupMembers)) {
-            extract($aPerFam);
-            $appendToQuery .= $per_fam_ID.',';
-        }
-        $appendToQuery = mb_substr($appendToQuery, 0, strlen($appendToQuery) - 1);
-        $appendToQuery .= ')';
-    } elseif ($iGroupID > -1) {
-        // group zero means map the cart
-                    $sSQL = 'SELECT per_fam_ID FROM person_per WHERE per_ID IN ('.ConvertCartToString($_SESSION['aPeopleCart']).')';
-        $rsGroupMembers = RunQuery($sSQL);
-        $appendToQuery = ' WHERE fam_ID IN (';
-        while ($aPerFam = mysqli_fetch_array($rsGroupMembers)) {
-            extract($aPerFam);
-            $appendToQuery .= $per_fam_ID.',';
-        }
-        $appendToQuery = mb_substr($appendToQuery, 0, strlen($appendToQuery) - 1);
-        $appendToQuery .= ')';
-    }
-
-    $sSQL = 'SELECT fam_ID, per_cls_ID, fam_Name, fam_latitude, fam_longitude, fam_Address1, fam_Address2, fam_City, fam_State, fam_Zip FROM family_fam LEFT JOIN person_per on family_fam.fam_ID = person_per.per_fam_ID AND per_fmr_ID IN ( '.SystemConfig::getValue('sDirRoleHead').')';
-    $sSQL .= $appendToQuery;
-    $rsFams = RunQuery($sSQL);
-    $markerIcons = explode(',', SystemConfig::getValue('sGMapIcons'));
-    array_unshift($markerIcons, 'red-pushpin');
-
-    while ($aFam = mysqli_fetch_array($rsFams)) {
-        extract($aFam);
-        if ($fam_longitude != 0 && $fam_latitude != 0) {
-            ?>
-
-                                 var image = new google.maps.MarkerImage('http://www.google.com/intl/en_us/mapfiles/ms/micons/<?= (array_key_exists($per_cls_ID, $markerIcons) ? $markerIcons[$per_cls_ID] : 0) ?>.png',
-                                             new google.maps.Size(32, 32),
-                                             new google.maps.Point(0,0),
-                                             new google.maps.Point(0, 32));
-                                 var shadow = new google.maps.MarkerImage('http://maps.google.com/mapfiles/shadow50.png',
-                                             new google.maps.Size(37, 34),
-                                             new google.maps.Point(0,0),
-                                             new google.maps.Point(-4, 34));
-
-                        var famMark<?= $fam_ID ?> = new google.maps.Marker({
-                                                                                position: new google.maps.LatLng(<?= $fam_latitude.', '.$fam_longitude ?>),
-                                                                                shadow:shadow,
-                                                                                icon: image,
-                                                                                map: map
-                                                                                               });
-                        <?php
-                        $photoFileThumb = "Images/Family/thumbnails/" . $fam_ID . ".jpg";
-            if (!file_exists($photoFileThumb)) {
-                $photoFileThumb = "Images/Family/family-128.png";
-            }
-
-            $famDescription = "<b><a href='FamilyView.php?FamilyID=" . $fam_ID . "'>" . MakeSalutationUtility($fam_ID) . "</a></b>";
-            $famDescription .= '<p>'.$fam_Address1.'<br/>'. $fam_Address2 .'<br/>'. $fam_City.
-                            (!empty($fam_City) && (!empty($fam_State)) ? ',': ''). $fam_State.
-                            (!empty($fam_State) ? '  ': '') .$fam_Zip . '</p>';
-            $famDescription .= "<p style='text-align: center'><a href='FamilyView.php?FamilyID=" . $fam_ID . "'>";
-            $famDescription .= "<img class='img-circle img-responsive profile-user-img' border='1' src='" . $photoFileThumb . "'></a></p>"; ?>
-                                    var fam<?= $fam_ID ?>InfoWin = new google.maps.InfoWindow({content: "<?= $famDescription ?>"});
-                        google.maps.event.addListener(famMark<?= $fam_ID ?>, "click", function() {
-                                                                  fam<?= $fam_ID ?>InfoWin.open(map,famMark<?php echo $fam_ID; ?>);
-                                                                 });
-            <?php
-
-        }
-
-    } ?>
-
-    //]]>
-    </script>
-
-    </div>
-    <div id="mapkey" class="col-lg-2 col-md-2 col-sm-2">
-        <table>
-            <tr>
-                <th colspan='2'><?= gettext('Key') ?>:</th>
-            </tr>
-            <?php
-                $sSQL = 'SELECT lst_OptionID, lst_OptionName from list_lst WHERE lst_ID = 1 ORDER BY lst_OptionSequence';
-    $rsIcons = RunQuery($sSQL);
-    while ($aIcons = mysqli_fetch_array($rsIcons)) {
-        extract($aIcons); ?>
-                  <tr>
-                       <td><img style="vertical-align:middle;" src='http://www.google.com/intl/en_us/mapfiles/ms/micons/<?= $markerIcons[$lst_OptionID] ?>.png'/></td>
-                       <td><?= $lst_OptionName ?></td>
-                 </tr>
+        <div class="callout callout-warning">
+            <?= gettext('Google Map API key is not set. The Map will work for smaller set of locations. Please create a Key in the maps sections of the setting menu.') ?>
+        </div>
         <?php
 
-    } ?>
-        </table>
-    </div>
-</div>
+    }
 
-<?php
+    //Get the details from DB
+    if ($iGroupID > 0) {
+        //Get all the members of this group
+        if (!empty(SystemConfig::getValue('sDirRoleHead'))) {
+            $families = FamilyQuery::create()
+                ->filterByDateDeactivated(null)
+                ->usePersonQuery('per')
+                ->usePerson2group2roleP2g2rQuery()
+                ->filterByGroupId($iGroupID)
+                ->endUse()
+                ->filterByFmrId(SystemConfig::getValue('sDirRoleHead'))
+                ->endUse()
+                ->find();
+
+        }
+    } elseif ($iGroupID == 0) {
+        // group zero means map the cart
+        if (!empty($_SESSION['aPeopleCart']) && !empty(SystemConfig::getValue('sDirRoleHead'))) {
+            $families = FamilyQuery::create()
+                ->filterByDateDeactivated(null)
+                ->usePersonQuery('per')
+                ->filterById($_SESSION['aPeopleCart'])
+                ->filterByFmrId(SystemConfig::getValue('sDirRoleHead'))
+                ->endUse()
+                ->find();
+
+        }
+    } else {
+        //Map all the families
+        if (!empty(SystemConfig::getValue('sDirRoleHead'))) {
+            $families = FamilyQuery::create()
+                ->filterByDateDeactivated(null)
+                ->usePersonQuery('per')
+                ->filterByFmrId(SystemConfig::getValue('sDirRoleHead'))
+                ->endUse()
+                ->find();
+
+        }
+    }
+
+    //Markericons list
+    $icons = ListOptionQuery::create()
+        ->filterById(1)
+        ->orderByOptionSequence()
+        ->find();
+
+    ?>
+    <style>
+        #maplegend {
+            font-family: Arial, sans-serif;
+            background: #fff;
+            padding: 5px;
+            margin: 5px;
+            border: 1px solid #000;
+            opacity: 0.7;
+        }
+
+        #maplegend h4 {
+            margin-top: 0;
+        }
+
+        #maplegend img {
+            vertical-align: middle;
+        }
+
+        @media screen
+        and (max-device-width: 600px) {
+            #maplegend {
+                visibility: hidden;
+            }
+        }
+
+        @media screen
+        and (min-device-width: 599px) {
+            #maplegend {
+                display: block;
+            }
+        }
+
+        @media screen
+        and (max-device-width: 600px) {
+            #maplegend-mobile {
+                display: block;
+            }
+        }
+
+        @media screen
+        and (min-device-width: 599px) {
+            #maplegend-mobile {
+                visibility: hidden;
+            }
+        }
+
+        }
+    </style>
+
+    <div class="box">
+        <div class="col-lg-12">
+            <!-- Google map div -->
+            <div id="map" style="height: 700px;"></div>
+            <div id="maplegend"><h4>Legend</h4></div>
+
+            <!--Google Map Scripts -->
+            <script
+                src="https://maps.googleapis.com/maps/api/js?key=<?= SystemConfig::getValue('sGoogleMapKey') ?>">
+            </script>
+
+            <script type="text/javascript">
+                var churchloc = {
+                    lat: <?= SystemConfig::getValue('nChurchLatitude') ?>,
+                    lng: <?= SystemConfig::getValue('nChurchLongitude') ?>};
+
+                <?php
+                $markerIcons = explode(',', SystemConfig::getValue('sGMapIcons'));
+                array_unshift($markerIcons, 'red-pushpin');
+                ?>
+
+                var markerIcons = <?= json_encode($markerIcons) ?>;
+                var iconsJSON = <?= $icons->toJSON() ?>;
+                var icons = iconsJSON.ListOptions;
+
+
+                var iconBase = 'https://www.google.com/intl/en_us/mapfiles/ms/micons/';
+                var map = null;
+                var infowindow = new google.maps.InfoWindow({
+                    maxWidth: 200
+                });
+
+                function addMarkerWithInfowindow(map, marker_position, image, title, infowindow_content) {
+                    //Create marker
+                    var marker = new google.maps.Marker({
+                        position: marker_position,
+                        map: map,
+                        icon: image,
+                        title: title
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function () {
+                        infowindow.setContent(infowindow_content);
+                        infowindow.open(map, marker);
+                    });
+                }
+
+                function initialize() {
+                    // init map
+                    map = new google.maps.Map(document.getElementById('map'), {
+                        zoom: 10,
+                        center: churchloc
+
+                    });
+
+                    //Churchmark
+                    var churchMark = new google.maps.Marker({
+                        icon: window.CRM.root + "/skin/icons/church.png",
+                        position: new google.maps.LatLng(<?= SystemConfig::getValue('nChurchLatitude') . ', ' . SystemConfig::getValue('nChurchLongitude') ?>),
+                        map: map
+                    });
+
+
+                    google.maps.event.addListener(map, 'click', function () {
+                        infowindow.close();
+                    });
+
+                    <?php
+
+                    $arr = array();
+                    $arrFamily = array();
+                    foreach ($families as $family) {
+                        //this helps to add head people persons details: otherwise doesn't seems to populate
+                        $family->getHeadPeople()[0];
+
+                        $photoFileThumb = "Images/Family/thumbnails/" . $family->getId() . ".jpg";
+                        if (!file_exists($photoFileThumb)) {
+                            $photoFileThumb = "Images/Family/family-128.png";
+                        }
+                        $arr['ID'] = $family->getId();
+                        $arr['Salutation'] = MakeSalutationUtility($family->getId());
+                        $arr['Address'] = $family->getAddress();
+                        $arr['Thumbnail'] = $photoFileThumb;
+                        array_push($arrFamily, $arr);
+                    }
+                    ?>
+
+                    var familyArray = <?= json_encode($arrFamily) ?>;
+                    //convert Propel object to JS array
+                    var familiesJSON = <?= $families->toJSON() ?>;
+                    var families = familiesJSON.Families;
+
+                    //loop through the families and add markers
+                    for (var i = 0; i < families.length; i++) {
+                        if (families[i].Latitude + families[i].Latitude == 0)
+                            continue;
+
+                        //icon image
+                        var clsid = families[i].People["0"].ClsId;
+                        var markerIcon = markerIcons[clsid];
+                        var image = {
+                            url: iconBase + markerIcon + '.png',
+                            // This marker is 37 pixels wide by 34 pixels high.
+                            size: new google.maps.Size(37, 34),
+                            // The origin for this image is (0, 0).
+                            origin: new google.maps.Point(0, 0),
+                            // The anchor for this image is the base of the flagpole at (0, 32).
+                            anchor: new google.maps.Point(0, 32)
+                        };
+
+                        //Latlng object
+                        var latlng = new google.maps.LatLng(families[i].Latitude, families[i].Longitude);
+
+                        //Infowindow Content
+                        var contentString = "<b><a href='FamilyView.php?FamilyID=" + families[i].Id + "'>" + familyArray[i].Salutation + "</a></b>";
+                        contentString += "<p>" + familyArray[i].Address + "</p>";
+                        contentString += "<p style='text-align: center'><a href='FamilyView.php?FamilyID=" + familyArray[i].ID + "'>";
+                        contentString += "<img class='img-circle img-responsive profile-user-img' border='1' src='" + familyArray[i].Thumbnail + "'></a></p>";
+
+                        //contentString="Hello";
+
+                        //Add marker and infowindow
+                        addMarkerWithInfowindow(map, latlng, image, families[i].Name, contentString);
+                    }
+                    //Add Legend
+                    var legend = document.getElementById('maplegend');
+                    var i = 0;
+                    for (var i = 0; i < icons.length; i++) {
+                        var type = icons[i];
+                        var name = type['OptionName'];
+                        var icon = iconBase + markerIcons[i + 1] + '.png';
+                        var div = document.createElement('div');
+                        div.innerHTML = '<img src="' + icon + '"> ' + name;
+                        legend.appendChild(div);
+                    }
+                    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
+                }
+
+                initialize();
+
+            </script>
+
+        </div>
+
+
+        <br>
+        <div id="maplegend-mobile">
+            <label><b>Key:</b></label>
+            <div class="row">
+                <?php
+                $i = 0;
+                foreach ($icons as $icon) {
+
+                    ?>
+                    <div class="col-xs-6 col-sm-4 col-md-3">
+                        <img style="vertical-align:middle;"
+                             src='http://www.google.com/intl/en_us/mapfiles/ms/micons/<?= $markerIcons[$icon->getOptionId()] ?>.png'/>
+                        <?= $icon->getOptionName() ?>
+                    </div>
+                    <?php
+                    $i += 1;
+
+                }
+                ?>
+
+            </div>
+        </div>
+    </div>
+
+    <?php
+
 }
 
 require 'Include/Footer.php' ?>
