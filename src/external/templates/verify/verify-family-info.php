0c1e5933222243ed37f7f16de5f19fd08fbd215a
Charles <crossan007@gmail.com>
2017-02-11 21:54:46 -0500

    working verify images.

--- src/external/templates/verify/verify-family-info.php
+++ src/external/templates/verify/verify-family-info.php
@@ -16,8 +16,12 @@ require(SystemURLs::getDocumentRoot(). "/Include/HeaderNotLoggedIn.php");
   </div>
   <div class="box box-info" id="verifyBox">
     <div class="panel-body">
-      <img class="img-circle center-block pull-right img-responsive" width="200" height="200"
+      <img class="img-circle center-block pull-right img-responsive initials-image" width="200" height="200"
+           data-name="<?= $family->getName() ?>"
+           <?php if ( $family->getThumbnailBytes() != FALSE ) 
+           { ?>
            src="data:image/png;base64,<?= base64_encode($family->getThumbnailBytes()) ?>">
+           <?php } ?>
       <h2><?= $family->getName() ?></h2>
       <div class="text-muted font-bold m-b-xs">
         <i class="fa fa-fw fa-map-marker" title="<?= gettext("Home Address")?>"></i><?= $family->getAddress() ?><br/>
@@ -49,8 +53,12 @@ require(SystemURLs::getDocumentRoot(). "/Include/HeaderNotLoggedIn.php");
           <div class="col-md-3 col-sm-4">
             <div class="box box-primary">
               <div class="box-body box-profile">  
-                 <img class="profile-user-img img-responsive img-circle" 
+                 <img class="profile-user-img img-responsive img-circle initials-image"
+                      data-name="<?= $person->getFullName() ?>"
+                       <?php if ( $person->getThumbnailBytes() != FALSE)
+                        {?>
                       src="data:image/png;base64,<?= base64_encode($person->getThumbnailBytes()) ?>">
+                        <?php } ?>
                 
 
                 <h3 class="profile-username text-center"><?= $person->getTitle() ?> <?= $person->getFullName() ?></h3>
@@ -135,8 +143,8 @@ require(SystemURLs::getDocumentRoot(). "/Include/HeaderNotLoggedIn.php");
     </div>
   </div>
 
-
-
+  
+  
 <style>
 
   #verifyBox {
@@ -173,7 +181,9 @@ require(SystemURLs::getDocumentRoot(). "/Include/HeaderNotLoggedIn.php");
 
 </style>
 
-  <script src="<?= SystemURLs::getRootPath() ?>/skin/js/FamilyVerify.js"></script>
+<script src="<?= SystemURLs::getRootPath(); ?>/skin/randomcolor/randomColor.js"></script>
+<script src="<?= SystemURLs::getRootPath(); ?>/skin/js/initial.js"></script>
+<script src="<?= SystemURLs::getRootPath() ?>/skin/js/FamilyVerify.js"></script>
 
 <?php
 // Add the page footer
