aa63d51f508977a0e9ee4c94126ba78c1faf1743
Charles <crossan007@gmail.com>
2017-03-02 23:33:05 -0500

    fix path

--- travis-ci/install-tests.sh
+++ travis-ci/install-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get install apache2
 sudo a2enmod rewrite
-sudo cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
+sudo cp /home/travis/build/ChurchCRM/CRM/travis-ci/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 sudo chmod -R a+rw /home/travis/build/ChurchCRM/CRM/src
 sudo service apache2 restart
 
