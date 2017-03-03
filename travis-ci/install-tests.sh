835aca42700f1c9a315a20b773664cb730b05727
Charles <crossan007@gmail.com>
2017-03-02 23:21:13 -0500

    debugging

--- travis-ci/install-tests.sh
+++ travis-ci/install-tests.sh
@@ -2,10 +2,13 @@
 sudo apt-get install apache2
 sudo a2enmod rewrite
 sudo sed -i 's/^\s*DocumentRoot.*$/DocumentRoot \/home\/travis\/build\/ChurchCRM\/CRM\/src/g' /etc/apache2/sites-enabled/000-default.conf
+sudo chmod -R a+rw /home/travis/build/ChurchCRM/CRM/src
 sudo service apache2 restart
 
 cat /etc/apache2/sites-enabled/000-default.conf
 
 curl http://localhost
 
+
+ls -la /home/travis/build/ChurchCRM/CRM/src
 exit 186
