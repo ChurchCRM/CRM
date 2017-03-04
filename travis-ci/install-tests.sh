9bfd1207dd1c524034cc61c92dd344110e713dcc
Charles <crossan007@gmail.com>
2017-03-04 08:43:46 -0500

    tweaks

--- travis-ci/install-tests.sh
+++ travis-ci/install-tests.sh
@@ -7,11 +7,10 @@ sudo apt-get update
 sudo apt-get install -y apache2  libapache2-mod-fastcgi
 
 # enable php-fpm
-sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+#sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 sudo a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
-sudo a2enmod rewrite
+phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
 # configure apache virtual hosts
 sudo cp /home/travis/build/ChurchCRM/CRM/travis-ci/000-default.conf /etc/apache2/sites-enabled/000-default.conf
