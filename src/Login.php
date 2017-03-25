bb56b7c6a6f97530054f7672beb11000ec2ab4ca
George Dawoud <george@dawouds.com>
2017-03-25 13:51:41 -0700

    no longer needed

--- src/Login.php
+++ src/Login.php
@@ -40,61 +40,6 @@ use ChurchCRM\UserQuery;
 
 $systemService = new SystemService();
 
-// Is the user requesting to logoff or timed out?
-if (isset($_GET['Logoff']) || isset($_GET['Timeout'])) {
-    if (!isset($_SESSION['sshowPledges']) || ($_SESSION['sshowPledges'] == '')) {
-        $_SESSION['sshowPledges'] = 0;
-    }
-    if (!isset($_SESSION['sshowPayments']) || ($_SESSION['sshowPayments'] == '')) {
-        $_SESSION['sshowPayments'] = 0;
-    }
-    if (!isset($_SESSION['bSearchFamily']) || ($_SESSION['bSearchFamily'] == '')) {
-        $_SESSION['bSearchFamily'] = 0;
-    }
-
-    if (!empty($_SESSION['iUserID'])) {
-        $currentUser = UserQuery::create()->findOneByPersonId($_SESSION['iUserID']);
-        $currentUser->setShowPledges($_SESSION['sshowPledges']);
-        $currentUser->setShowPayments($_SESSION['sshowPayments']);
-        $currentUser->setShowSince($_SESSION['sshowSince']);
-        $currentUser->setDefaultFY($_SESSION['idefaultFY']);
-        $currentUser->setCurrentDeposit($_SESSION['iCurrentDeposit']);
-
-        if ($_SESSION['dCalStart'] != '') {
-            $currentUser->setCalStart($_SESSION['dCalStart']);
-        }
-        if ($_SESSION['dCalEnd'] != '') {
-            $currentUser->setCalEnd($_SESSION['dCalEnd']);
-        }
-        if ($_SESSION['dCalNoSchool1'] != '') {
-            $currentUser->setCalNoSchool1($_SESSION['dCalNoSchool1']);
-        }
-        if ($_SESSION['dCalNoSchool2'] != '') {
-            $currentUser->dCalNoSchool2($_SESSION['dCalNoSchool2']);
-        }
-        if ($_SESSION['dCalNoSchool3'] != '') {
-            $currentUser->dCalNoSchool3($_SESSION['dCalNoSchool3']);
-        }
-        if ($_SESSION['dCalNoSchool4'] != '') {
-            $currentUser->dCalNoSchool4($_SESSION['dCalNoSchool4']);
-        }
-        if ($_SESSION['dCalNoSchool5'] != '') {
-            $currentUser->dCalNoSchool5($_SESSION['dCalNoSchool5']);
-        }
-        if ($_SESSION['dCalNoSchool6'] != '') {
-            $currentUser->dCalNoSchool6($_SESSION['dCalNoSchool6']);
-        }
-        if ($_SESSION['dCalNoSchool7'] != '') {
-            $currentUser->dCalNoSchool7($_SESSION['dCalNoSchool7']);
-        }
-        if ($_SESSION['dCalNoSchool8'] != '') {
-            $currentUser->dCalNoSchool8($_SESSION['dCalNoSchool8']);
-        }
-        $currentUser->setSearchfamily($_SESSION['bSearchFamily']);
-        $currentUser->save();
-    }
-}
-
 $currentUser = 0;
 // Get the UserID out of user name submitted in form results
 if (isset($_POST['User']) && !isset($sErrorText)) {
@@ -278,7 +223,7 @@ if (isset($loginPageMsg)) {
         <?php if ($enableSelfReg) {
     ?>
         <a href="external/register/" class="text-center btn bg-olive"><i class="fa fa-user-plus"></i> <?= gettext('Register a new Family'); ?></a><br>
-        <?php 
+        <?php
 } ?>
       <!--<a href="external/family/verify" class="text-center">Verify Family Info</a> -->
     </div>
