08c63e9fd589ec5a4b9bffcd559ecfb4e7268196
George Dawoud <george@dawouds.com>
2017-04-02 21:57:32 -0700

    moved delete person to bootbox and AJAX Call

--- src/skin/js/PersonView.js
+++ src/skin/js/PersonView.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     $("#input-person-properties").on("select2:select", function (event) {
         promptBox = $("#prompt-box");
         promptBox.removeClass('form-group').html('');
@@ -16,15 +16,15 @@ $(document).ready(function () {
                     $('<textarea rows="3" class="form-control" name="PropertyValue"></textarea>').val(pro_value)
                 );
         }
-        
+
     });
-    
+
     $('#assign-property-form').submit(function (event) {
         event.preventDefault();
         var thisForm = $(this);
         var url = thisForm.attr('action');
         var dataToSend = thisForm.serialize();
-       
+
         $.ajax({
             type: 'POST',
             url: url,
@@ -36,9 +36,9 @@ $(document).ready(function () {
                 }
             }
         });
-       
+
     });
-    
+
     $('.remove-property-btn').click(function (event) {
         event.preventDefault();
         var thisLink = $(this);
@@ -47,7 +47,7 @@ $(document).ready(function () {
             PropertyId: thisLink.data('property_id')
         };
         var url = window.CRM.root + '/api/properties/persons/unassign';
-        
+
         bootbox.confirm('Are you sure you want to unassign this property?', function (result) {
             if (result) {
                 $.ajax({
@@ -63,8 +63,35 @@ $(document).ready(function () {
                 });
             }
         });
-        
+
     });
-    
-    
+
+    $('#delete-person').click(function (event) {
+        event.preventDefault();
+        var thisLink = $(this);
+        bootbox.confirm({
+            title: "Delete this person?",
+            message: "Do you want to delete <b>" + thisLink.data('person_name')  + "</b>? This cannot be undone.",
+            buttons: {
+                cancel: {
+                    label: '<i class="fa fa-times"></i> Cancel'
+                },
+                confirm: {
+                    label: '<i class="fa fa-trash-o"></i> Delete'
+                }
+            },
+            callback: function (result) {
+                if(result) {
+                    $.ajax({
+                        type: 'DELETE',
+                        url: window.CRM.root + '/api/persons/' + thisLink.data('person_id'),
+                        dataType: 'json',
+                        success: function (data, status, xmlHttpReq) {
+                            location.replace( window.CRM.root + "/");
+                        }
+                    });
+                }
+            }
+        });
+    });
 });
