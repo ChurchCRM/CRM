191a9b6bfa208eb12d7a46bc79aabebe4a600ba6
George Dawoud <george@dawouds.com>
2017-04-11 23:40:17 -0700

    moving gren config into grenrc.json

--- scripts/gen-changelogs.sh
+++ scripts/gen-changelogs.sh
@@ -8,5 +8,5 @@ fi
 
 sudo npm install github-release-notes@0.8.0 -g
 
-gren --action=changelog --generate --tags=all --username=ChurchCRM --repo=CRM --time-wrap=history --data-source=issues --ignore-issues-with=wontfix,duplicate,norepro,question --token=${githubToken}
+gren --action=changelog --generate --tags=all --username=ChurchCRM --repo=CRM --token=${githubToken}
 
