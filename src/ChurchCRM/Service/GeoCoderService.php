80a0df66ac96c8eaed3000a6a194fdd4b729991a
George Dawoud <george@dawouds.com>
2017-04-16 22:12:41 -0700

    new GeoCoder Service to select the provider based on system config

---
+++ src/ChurchCRM/Service/GeoCoderService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ChurchCRM\Service;
+
+use ChurchCRM\dto\SystemConfig;
+use Geocoder\Exception\NoResult;
+use Geocoder\Geocoder;
+use Geocoder\Provider\BingMaps;
+use Geocoder\Provider\GoogleMaps;
+use Ivory\HttpAdapter\CurlHttpAdapter;
+
+class GeoCoderService
+{
+    /**
+     * @var Geocoder
+     */
+    private $geocoder;
+
+    public function __construct()
+    {
+        $curl = new CurlHttpAdapter();
+        $provider = SystemConfig::getValue("sGeoCoderProvider");
+        switch ($provider) {
+            case "GoogleMaps":
+                $this->geocoder = new GoogleMaps($curl, null, null, false, SystemConfig::getValue("sGoogleMapKey"));
+                break;
+            case "BingMaps":
+                $this->geocoder = new BingMaps($curl);
+                break;
+        }
+    }
+
+    public function getLatLong($address)
+    {
+        $lat = 0;
+        $long = 0;
+
+        try {
+            $addressCollection = $this->geocoder->geocode($address);
+            $geoAddress = $addressCollection->first();
+            if (!empty($geoAddress)) {
+                $lat = $geoAddress->getLongitude();
+                $long = $geoAddress->getLongitude();
+            }
+        } catch (NoResult $exception) {
+            // no result for the address
+        }
+
+        return array(
+            'Latitude' => $lat,
+            'Longitude' => $long
+        );
+
+    }
+
+}
