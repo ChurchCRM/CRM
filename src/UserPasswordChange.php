a61868e046e223212df6e174a7f408bbfc614b87
Charles <crossan007@gmail.com>
2017-04-23 20:37:00 -0400

    update behavior for admin change, and docs

--- src/UserPasswordChange.php
+++ src/UserPasswordChange.php
@@ -91,7 +91,7 @@ if (isset($_POST['Submit'])) {
             // Update the user record with the password hash
             $curUser = UserQuery::create()->findPk($iPersonID);
             $curUser->updatePassword($sNewPassword1);
-            $curUser->setNeedPasswordChange(true);
+            $curUser->setNeedPasswordChange(false);
             $curUser->save();
 
             // Set the session variable so they don't get sent back here
