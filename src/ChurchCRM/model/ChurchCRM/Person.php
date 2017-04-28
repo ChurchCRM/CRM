a36e30ebd013475a7863c73eb27b247a20183c9a
Charles <crossan007@gmail.com>
2017-04-27 23:13:32 -0400

    spacing

--- src/ChurchCRM/model/ChurchCRM/Person.php
+++ src/ChurchCRM/model/ChurchCRM/Person.php
@@ -68,7 +68,7 @@ class Person extends BasePerson implements iPhoto
         if (isset($roleId) && $roleId !== 0) {
             $familyRole = ListOptionQuery::create()->filterById(2)->filterByOptionId($roleId)->findOne();
         }
-
+        
         return $familyRole;
     }
 
