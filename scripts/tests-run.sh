9a8cd6f1962e71b6872e40f3c6d193256e38f24c
Charles <crossan007@gmail.com>
2017-05-13 10:33:18 -0400

    tweaks

--- scripts/tests-run.sh
+++ scripts/tests-run.sh
@@ -2,19 +2,30 @@
 
 #Hints here: http://codegist.net/code/sauce%20connect%20setup/
 
-SC_READYFILE=/tmp/sc/ready
+SC_READYFILE=/tmp/sceady
+SC_LOG=/tmp/sauce.log
 
-if [[ -z "${TRAVIS}" ]]; then
-  echo "Not TravisCI - Manually starting Sauce Connect"
-  /tmp/sc/sc-4.4.6-linux/bin/sc --readyfile ${SC_READYFILE} 1>&
-  while [ ! -f ${SC_READYFILE} ]; do
-    echo "Waiting for Sauce Connect readyfile"
-    sleep .5
-  done
-fi
 
 if [[ "${SAUCE_USERNAME}" && "${SAUCE_ACCESS_KEY}" ]]; then
   echo "SAUCE"
+
+  if [[ -z "${TRAVIS}" ]]; then
+
+    echo "Not TravisCI - Manually starting Sauce Connect"
+    /tmp/sc/sc-4.4.6-linux/bin/sc --readyfile ${SC_READYFILE}  &>${SC_LOG} &
+
+    while [ ! -f ${SC_READYFILE} ]; do
+      echo "Waiting for Sauce Connect readyfile"
+
+      if grep -q Goodbye "${SC_LOG}"; then
+        echo "Sauce Connect unexpectedly terminated.  Please check ${SC_LOG}"
+        exit
+      fi
+
+      sleep .5
+    done
+  fi
+
   export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"selenium2" : { "wd_host":"'${SAUCE_USERNAME}':'${SAUCE_ACCESS_KEY}'@ondemand.saucelabs.com/wd/hub"}}}}'
 else
   echo "NO SAUCE"
