e9e223aa0fc9e4252a8117a504aea9e5242f7555
Charles <crossan007@gmail.com>
2017-05-17 22:59:45 -0400

    cleanup

--- src/mysql/upgrade/2.8.0/devices.sql
+++ src/mysql/upgrade/2.8.0/devices.sql
@@ -7,7 +7,7 @@ CREATE TABLE `kioskdevice_kdev` (
   `kdev_deviceConfiguration` text,
   `kdev_lastHeartbeat` TIMESTAMP,
   `kdev_Accepted` BOOLEAN,
-  `kdev_PendingCommands` varchar(50)
+  `kdev_PendingCommands` varchar(50),
 
   PRIMARY KEY  (`kdev_ID`),
   UNIQUE KEY `kdev_ID` (`kdev_ID`)
@@ -23,9 +23,6 @@ CREATE TABLE `kioskassginment_kasm` (
   PRIMARY KEY  (`kasm_ID`),
   UNIQUE KEY `kasm_ID` (`kasm_ID`)
 ) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-
-
   
 INSERT INTO `menuconfig_mcf` (`mid`, `name`, `parent`, `ismenu`, `content_english`, `content`, `uri`, `statustext`, `security_grp`, `session_var`, `session_var_in_text`, `session_var_in_uri`, `url_parm_name`, `active`, `sortorder`, `icon`) VALUES
 (83, 'kioskmanager', 'root', 0, 'Kiosk Manager', 'Kiosk Manager', 'KioskManager.php', '', 'bAll', NULL, 0, 0, NULL, 1, 5, 'fa-laptop');
-
--- src/mysql/upgrade/2.8.0/eventGroups.sql
+++ src/mysql/upgrade/2.8.0/eventGroups.sql
@@ -1,7 +1,5 @@
 ALTER TABLE events_event
-  ADD COLUMN event_grpid mediumint(9) AFTER event_typename,
-  ADD COLUMN event_kioskid mediumint(9) AFTER event_grpid;   
+  ADD COLUMN event_grpid mediumint(9) AFTER event_typename;
 
 ALTER TABLE event_types
-  ADD COLUMN type_grpid mediumint(9) AFTER type_active,
-  ADD COLUMN type_kioskid mediumint(9) AFTER type_grpid;
+  ADD COLUMN type_grpid mediumint(9) AFTER type_active;
