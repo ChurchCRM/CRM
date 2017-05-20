4e4bd8f398df0d7423a8f3f24d7fc54b7e3a3fbc
George Dawoud <george@dawouds.com>
2017-05-20 12:34:16 -0700

    Apply fixes from StyleCI (#2520)

--- src/kiosk/index.php
+++ src/kiosk/index.php
@@ -23,33 +23,27 @@ require __DIR__ . '/routes/kiosk.php';
 
 $windowOpen = new DateTime(SystemConfig::getValue("sKioskVisibilityTimestamp")) > new DateTime();
 
-if (isset($_COOKIE['kioskCookie']))
-{
-  $g = hash('sha256',$_COOKIE['kioskCookie']);
-  $Kiosk =  \ChurchCRM\Base\KioskDeviceQuery::create()
+if (isset($_COOKIE['kioskCookie'])) {
+    $g = hash('sha256', $_COOKIE['kioskCookie']);
+    $Kiosk =  \ChurchCRM\Base\KioskDeviceQuery::create()
           ->findOneByGUIDHash($g);
-  if (is_null($Kiosk))
-  {
-    setcookie(kioskCookie,'',time() - 3600);
-    header('Location: '.$_SERVER['REQUEST_URI']);
-  }
+    if (is_null($Kiosk)) {
+        setcookie(kioskCookie, '', time() - 3600);
+        header('Location: '.$_SERVER['REQUEST_URI']);
+    }
 }
 
-if (!isset($_COOKIE['kioskCookie']))
-{
-  if ($windowOpen)
-  {
-    $guid = MiscUtils::getGUID();
-    setcookie("kioskCookie",$guid, 2147483647);
-    $Kiosk = new \ChurchCRM\KioskDevice();
-    $Kiosk->setGUIDHash(hash('sha256',$guid));
-    $Kiosk->setAccepted($false);
-    $Kiosk->save();
-  }
-  else
-  {
-    exit;
-  }
+if (!isset($_COOKIE['kioskCookie'])) {
+    if ($windowOpen) {
+        $guid = MiscUtils::getGUID();
+        setcookie("kioskCookie", $guid, 2147483647);
+        $Kiosk = new \ChurchCRM\KioskDevice();
+        $Kiosk->setGUIDHash(hash('sha256', $guid));
+        $Kiosk->setAccepted($false);
+        $Kiosk->save();
+    } else {
+        exit;
+    }
 }
 $app->kiosk = $Kiosk;
 
