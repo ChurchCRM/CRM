b4c10f18389f7375f93b88d2ec9ef119ef7ba847
Charles <crossan007@gmail.com>
2017-06-04 22:18:09 -0400

    fix depositslipeditor

--- src/skin/js/DepositSlipEditor.js
+++ src/skin/js/DepositSlipEditor.js
@@ -6,7 +6,7 @@ function initPaymentTable()
       title:'Family',
       data:'FamilyString',
       render: function(data, type, full, meta) {
-        var familyName = data ? data : "<?= gettext('Anonymous')?>";
+        var familyName = data ? data : i18next.t('Anonymous');
         return '<a href=\'PledgeEditor.php?linkBack=DepositSlipEditor.php?DepositSlipID=' + depositSlipID +
             '&GroupKey=' + full.Groupkey + '\'><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa '+  (isDepositClosed ? "fa-search-plus": "fa-pencil" ) +' fa-stack-1x fa-inverse"></i></span></a>' + familyName;
       }
@@ -46,7 +46,8 @@ function initPaymentTable()
 
   dataT = $("#paymentsTable").DataTable({
     ajax:{
-      url :window.CRM.root+"/api/deposits/"+depositSlipID+"/pledges"
+      url :window.CRM.root+"/api/deposits/"+depositSlipID+"/pledges",
+      dataSrc:''
     },
     columns: colDef,
     responsive: true,
