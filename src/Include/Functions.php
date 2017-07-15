1d571dd48da696c4e3b9d861700494ca252b042b
phili67 <philippelogel@free.fr>
2017-07-15 12:37:05 +0200

    Update Functions.php
    To transform the date like : d/m/Y

--- src/Include/Functions.php
+++ src/Include/Functions.php
@@ -574,8 +574,14 @@ function FormatDate($dDate, $bWithTime = false)
   }
 
     if ($bWithTime) {
+      if (SystemConfig::getValue("sDateFormatLong") == "d/m/Y")
+        return "$dm $month, $y $h$m $sAMPM";
+      else
         return "$month $dm, $y $h$m $sAMPM";
     } else {
+      if (SystemConfig::getValue("sDateFormatLong") == "d/m/Y")
+        return "$dm $month, $y";
+      else
         return "$month $dm, $y";
     }
 }
@@ -993,7 +999,7 @@ function formCustomField($type, $fieldname, $data, $special, $bFirstPassFlag)
       break;
     // Handler for date fields
     case 2:
-        // code rajouté par Philippe Logel
+    	// code rajouté par Philippe Logel
       echo '<div class="input-group">'.
         '<div class="input-group-addon">'.
         '<i class="fa fa-calendar"></i>'.
@@ -1330,11 +1336,11 @@ function validateCustomField($type, &$data, $col_Name, &$aErrors)
 
     switch ($type) {
     // Validate a date field
-    case 2:
-        // this part will work with each date format
-        // Philippe logel
-        $data = InputUtils::FilterDate($data);
-        
+    case 2:    	
+    	// this part will work with each date format
+    	// Philippe logel
+    	$data = InputUtils::FilterDate($data);
+    	
       if (strlen($data) > 0) {
           $dateString = parseAndValidateDate($data);
           if ($dateString === false) {
