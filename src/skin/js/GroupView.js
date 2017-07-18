4934213fa3f528def375e4a98fc85a4f15efc122
Charles <crossan007@gmail.com>
2017-07-18 06:51:28 -0400

    fix the fix

--- src/skin/js/GroupView.js
+++ src/skin/js/GroupView.js
@@ -259,7 +259,7 @@ function initDataTable() {
           title: '',
           data: 'PersonId',
           render: function (data, type, full, meta) {
-            	return '<img data-name="'+full.Person.FirstName + ' ' + full.Person.LastName + '" data-src="' + window.CRM.root + '/api/persons/' + full.PersonId + '/thumbnail" class="direct-chat-img initials-image"> &nbsp <a href="PersonView.php?PersonID="' + full.PersonId + '">' + full.Person.FirstName + " " + full.Person.LastName + '</a><a target="_top" href="PersonView.php?PersonID=' + full.PersonId + '"></a>';
+            	return '<img data-name="'+full.Person.FirstName + ' ' + full.Person.LastName + '" data-src="' + window.CRM.root + '/api/persons/' + full.PersonId + '/thumbnail" class="direct-chat-img initials-image">';
           }
       },
       {
@@ -267,7 +267,7 @@ function initDataTable() {
         title:i18next.t( 'Name'),
         data: 'PersonId',
         render: function (data, type, full, meta) {
-          return full.Person.FirstName + " " + full.Person.LastName;
+          return '<a target="_top" href="PersonView.php?PersonID=' + full.PersonId + '">' + full.Person.FirstName + " " + full.Person.LastName + '</a>';
         }
       },
       {
