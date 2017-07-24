34e9e294c5939830bdd00c337fae7b18d8c2888f
George Dawoud <george@dawouds.com>
2017-07-24 21:46:01 +0000

    Apply fixes from StyleCI

--- src/Include/Functions.php
+++ src/Include/Functions.php
@@ -525,17 +525,17 @@ function RemoveGroupFromPeopleCart($iGroupID)
 
 function FormatDateOutput()
 {
-	$fmt = SystemConfig::getValue("sDateFormatLong");
-	
-	$fmt = str_replace ("/"," ",$fmt);
-	
-	$fmt = str_replace ("-"," ",$fmt);
-	
-	$fmt = str_replace ("d","%d",$fmt);
-	$fmt = str_replace ("m","%B",$fmt);
-	$fmt = str_replace ("Y","%Y",$fmt);
-	
-	return $fmt;
+    $fmt = SystemConfig::getValue("sDateFormatLong");
+    
+    $fmt = str_replace("/", " ", $fmt);
+    
+    $fmt = str_replace("-", " ", $fmt);
+    
+    $fmt = str_replace("d", "%d", $fmt);
+    $fmt = str_replace("m", "%B", $fmt);
+    $fmt = str_replace("Y", "%Y", $fmt);
+    
+    return $fmt;
 }
 
 
@@ -576,27 +576,27 @@ function FormatDate($dDate, $bWithTime = false)
     extract(mysqli_fetch_array(RunQuery($sSQL)));
     
 
-		if ($h > 11) {
-				$sAMPM = gettext('pm');
-				if ($h > 12) {
-						$h = $h - 12;
-				}
-		} else {
-				$sAMPM = gettext('am');
-				if ($h == 0) {
-						$h = 12;
-				}
-		}
-		
-		$fmt = FormatDateOutput();
-	 	
-  	setlocale(LC_ALL, SystemConfig::getValue("sLanguage"));
-  	
-  	if ($bWithTime) {
-			return utf8_encode(strftime("$fmt %H:%M $sAMPM", strtotime( $dDate)));
+    if ($h > 11) {
+        $sAMPM = gettext('pm');
+        if ($h > 12) {
+            $h = $h - 12;
+        }
     } else {
-			return utf8_encode(strftime("$fmt", strtotime( $dDate)));    
-		}
+        $sAMPM = gettext('am');
+        if ($h == 0) {
+            $h = 12;
+        }
+    }
+        
+    $fmt = FormatDateOutput();
+        
+    setlocale(LC_ALL, SystemConfig::getValue("sLanguage"));
+    
+    if ($bWithTime) {
+        return utf8_encode(strftime("$fmt %H:%M $sAMPM", strtotime($dDate)));
+    } else {
+        return utf8_encode(strftime("$fmt", strtotime($dDate)));
+    }
 }
 
 function AlternateRowStyle($sCurrentStyle)
@@ -1012,7 +1012,7 @@ function formCustomField($type, $fieldname, $data, $special, $bFirstPassFlag)
       break;
     // Handler for date fields
     case 2:
-    	// code rajouté par Philippe Logel
+        // code rajouté par Philippe Logel
       echo '<div class="input-group">'.
         '<div class="input-group-addon">'.
         '<i class="fa fa-calendar"></i>'.
@@ -1351,10 +1351,10 @@ function validateCustomField($type, &$data, $col_Name, &$aErrors)
     switch ($type) {
     // Validate a date field
     case 2:
-    	// this part will work with each date format
-    	// Philippe logel
-    	$data = InputUtils::FilterDate($data);
-    	
+        // this part will work with each date format
+        // Philippe logel
+        $data = InputUtils::FilterDate($data);
+        
       if (strlen($data) > 0) {
           $dateString = parseAndValidateDate($data);
           if ($dateString === false) {
--- src/sundayschool/SundaySchoolClassListExport.php
+++ src/sundayschool/SundaySchoolClassListExport.php
@@ -6,7 +6,6 @@ require '../Include/Functions.php';
 use ChurchCRM\dto\SystemConfig;
 use ChurchCRM\Utils\InputUtils;
 
-
 header('Pragma: no-cache');
 header('Expires: 0');
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
@@ -20,9 +19,8 @@ $delimiter = SystemConfig::getValue("sCSVExportDelemiter");
 $out = fopen('php://output', 'w');
 
 //add BOM to fix UTF-8 in Excel 2016 but not under, so the problem is solved with the sCSVExportCharset variable
-if (SystemConfig::getValue("sCSVExportCharset") == "UTF-8")
-{
-	fputs($out, $bom =( chr(0xEF) . chr(0xBB) . chr(0xBF) ));
+if (SystemConfig::getValue("sCSVExportCharset") == "UTF-8") {
+    fputs($out, $bom =(chr(0xEF) . chr(0xBB) . chr(0xBF)));
 }
 
 // Get all the groups
@@ -76,5 +74,3 @@ while ($aRow = mysqli_fetch_array($rsKids)) {
 
 
 fclose($out);
-                                                                                                                            
-?>
