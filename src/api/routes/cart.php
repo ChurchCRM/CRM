e440d33d9ceb202edec53bd1b770186d39da079c
Charles <crossan007@gmail.com>
2017-07-28 09:17:33 -0400

    add cart code - no operational changes yet

--- src/api/routes/cart.php
+++ src/api/routes/cart.php
@@ -1,28 +1,61 @@
 <?php
+use ChurchCRM\GroupQuery;
+use ChurchCRM\Person2group2roleP2g2r;
+use ChurchCRM\dto\Cart;
 
 $app->group('/cart', function () {
+  
+    $this->get('/',function($request,$response,$args) {
+      return $response->withJSON(['PeopleCart' =>  $_SESSION['aPeopleCart']]);
+    });
 
-  $this->post('/', function ($request, $response, $args) {
+    $this->post('/', function ($request, $response, $args) {
+          $cartPayload = (object)$request->getParsedBody();
+          if ( isset ($cartPayload->Persons) && count($cartPayload->Persons) > 0 )
+          {
+            Cart::AddPersonArray($cartPayload->Persons);
+          }
+          elseif ( isset ($cartPayload->Family) )
+          {
+            Cart::AddFamily($cartPayload->Family);
+          }
+          elseif ( isset ($cartPayload->Group) )
+          {
+            Cart::AddGroup($cartPayload->Group);
+          }
+          else
+          {
+            throw new \Exception(gettext("POST to cart requires a Persons array, FamilyID, or GroupID"),500);
+          }
+          return $response->withJson(['status' => "success"]);
+      });
+      
+    $this->post('/emptyToGroup', function($request, $response, $args) {
         $cartPayload = (object)$request->getParsedBody();
-        if ( isset ($cartPayload->Persons) && count($cartPayload->Persons) > 0 )
-        {
-         AddArrayToPeopleCart($cartPayload->Persons);
-        }
-        else
-        {
-          throw new \Exception(gettext("POST to cart requires a Persons array"),500);
-        }
-        return $response->withJson(['status' => "success"]);
+        Cart::EmptyToGroup($cartPayload->groupID, $cartPayload->groupRoleID);
+        return $response->withJson([
+            'status' => "success",
+            'message' => $iCount.' records(s) successfully added to selected Group.'
+        ]);
     });
 
     /**
      * delete. This will empty the cart
      */
     $this->delete('/', function ($request, $response, $args) {
-        $sMessage = gettext('Your cart is empty');
-        if(sizeof($_SESSION['aPeopleCart'])>0) {
-            $_SESSION['aPeopleCart'] = [];
-            $sMessage = gettext('Your cart has been successfully emptied');
+      
+        $cartPayload = (object)$request->getParsedBody();
+        if ( isset ($cartPayload->Persons) && count($cartPayload->Persons) > 0 )
+        {
+          Cart::RemovePersonArray($cartPayload->Persons);
+        }
+        else
+        {
+          $sMessage = gettext('Your cart is empty');
+          if(sizeof($_SESSION['aPeopleCart'])>0) {
+              $_SESSION['aPeopleCart'] = [];
+              $sMessage = gettext('Your cart has been successfully emptied');
+          }
         }
         return $response->withJson([
             'status' => "success",
