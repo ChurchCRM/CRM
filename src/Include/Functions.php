6920dde0f72084eaf48cab2d571cc37ca45babb9
phili67 <philippelogel@free.fr>
2017-07-31 08:09:55 +0200

    Update Functions.php
    I've just made the last part .... hopefully.

--- src/Include/Functions.php
+++ src/Include/Functions.php
@@ -524,24 +524,24 @@ function RemoveGroupFromPeopleCart($iGroupID)
 }
 
 
-function change_date_for_place_holder($string)
+function change_date_for_place_holder ($string)
 {
-    return ((strtotime($string) != "")?date(SystemConfig::getValue("sDatePickerFormat"), strtotime($string)):strtotime($string));
+	return ((strtotime($string) != "")?date(SystemConfig::getValue("sDatePickerFormat"), strtotime($string)):strtotime($string));
 }
 
 function FormatDateOutput()
 {
-    $fmt = SystemConfig::getValue("sDateFormatLong");
-    
-    $fmt = str_replace("/", " ", $fmt);
-    
-    $fmt = str_replace("-", " ", $fmt);
-    
-    $fmt = str_replace("d", "%d", $fmt);
-    $fmt = str_replace("m", "%B", $fmt);
-    $fmt = str_replace("Y", "%Y", $fmt);
-    
-    return $fmt;
+	$fmt = SystemConfig::getValue("sDateFormatLong");
+	
+	$fmt = str_replace ("/"," ",$fmt);
+	
+	$fmt = str_replace ("-"," ",$fmt);
+	
+	$fmt = str_replace ("d","%d",$fmt);
+	$fmt = str_replace ("m","%B",$fmt);
+	$fmt = str_replace ("Y","%Y",$fmt);
+	
+	return $fmt;
 }
 
 
@@ -582,27 +582,27 @@ function FormatDate($dDate, $bWithTime = false)
     extract(mysqli_fetch_array(RunQuery($sSQL)));
     
 
-    if ($h > 11) {
-        $sAMPM = gettext('pm');
-        if ($h > 12) {
-            $h = $h - 12;
-        }
+		if ($h > 11) {
+				$sAMPM = gettext('pm');
+				if ($h > 12) {
+						$h = $h - 12;
+				}
+		} else {
+				$sAMPM = gettext('am');
+				if ($h == 0) {
+						$h = 12;
+				}
+		}
+		
+		$fmt = FormatDateOutput();
+	 	
+  	setlocale(LC_ALL, SystemConfig::getValue("sLanguage"));
+  	
+  	if ($bWithTime) {
+			return utf8_encode(strftime("$fmt %H:%M $sAMPM", strtotime( $dDate)));
     } else {
-        $sAMPM = gettext('am');
-        if ($h == 0) {
-            $h = 12;
-        }
-    }
-        
-    $fmt = FormatDateOutput();
-        
-    setlocale(LC_ALL, SystemConfig::getValue("sLanguage"));
-    
-    if ($bWithTime) {
-        return utf8_encode(strftime("$fmt %H:%M $sAMPM", strtotime($dDate)));
-    } else {
-        return utf8_encode(strftime("$fmt", strtotime($dDate)));
-    }
+			return utf8_encode(strftime("$fmt", strtotime( $dDate)));    
+		}
 }
 
 function AlternateRowStyle($sCurrentStyle)
@@ -1018,12 +1018,12 @@ function formCustomField($type, $fieldname, $data, $special, $bFirstPassFlag)
       break;
     // Handler for date fields
     case 2:
-        // code rajouté par Philippe Logel
+    	// code rajouté par Philippe Logel
       echo '<div class="input-group">'.
         '<div class="input-group-addon">'.
         '<i class="fa fa-calendar"></i>'.
         '</div>'.
-        '<input class="form-control date-picker" type="text" id="'.$fieldname.'" Name="'.$fieldname.'" value="'.((strtotime($data) != "")?date(SystemConfig::getValue("sDatePickerFormat"), strtotime($data)):strtotime($data)).'" placeholder="'.SystemConfig::getValue("sDatePickerPlaceHolder").'"> '.
+        '<input class="form-control date-picker" type="text" id="'.$fieldname.'" Name="'.$fieldname.'" value="'.change_date_for_place_holder($data).'" placeholder="'.SystemConfig::getValue("sDatePickerPlaceHolder").'"> '.
         '</div>';
       break;
 
@@ -1357,10 +1357,10 @@ function validateCustomField($type, &$data, $col_Name, &$aErrors)
     switch ($type) {
     // Validate a date field
     case 2:
-        // this part will work with each date format
-        // Philippe logel
-        $data = InputUtils::FilterDate($data);
-        
+    	// this part will work with each date format
+    	// Philippe logel
+    	$data = InputUtils::FilterDate($data);
+    	
       if (strlen($data) > 0) {
           $dateString = parseAndValidateDate($data);
           if ($dateString === false) {
