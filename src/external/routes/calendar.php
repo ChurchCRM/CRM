9b1c8cbcd658f1769f4e2b21ce13de9c78f48e82
Charles <crossan007@gmail.com>
2017-09-08 21:05:10 -0400

    add query parameters for events api

--- src/external/routes/calendar.php
+++ src/external/routes/calendar.php
@@ -2,13 +2,31 @@
 
 // Routes
 
+use ChurchCRM\Utils\InputUtils;
+use Propel\Runtime\ActiveQuery\Criteria;
+
 $app->group('/calendar', function () {
     $this->get('/events', function ($request, $response, $args) {
         if (!ChurchCRM\dto\SystemConfig::getBooleanValue("bEnableExternalCalendarAPI"))
         {
           throw new \Exception(gettext("External Calendar API is disabled")  , 400);
         }
-        $events = ChurchCRM\EventQuery::create()->find();
-        return $response->withJson($events->toArray());
+        
+        $params = $request->getQueryParams();
+        $start_date = InputUtils::FilterDate($params['start']);
+        $max_events = InputUtils::FilterInt($params['max']);
+
+        $events = ChurchCRM\EventQuery::create()
+                ->orderByStart(Criteria::ASC);
+
+        if($start_date) {
+          $events->filterByStart($start_date, Criteria::GREATER_EQUAL);
+        }
+
+        if ($max_events) {
+          $events->limit($max_events);
+        }
+        
+        return $response->withJson($events->find()->toArray());
     });
 });
