623e3a766528100edec2a912004e9c5f8602935b
George Dawoud <george@dawouds.com>
2017-10-17 21:43:52 -0700

    Update gen-changelogs.sh

--- scripts/gen-changelogs.sh
+++ scripts/gen-changelogs.sh
@@ -6,7 +6,7 @@ if [ -z ${githubToken} ]; then
     read githubToken
 fi
 
-sudo npm install github-release-notes@0.11.0 -g
+sudo npm install github-release-notes@0.12.1 -g
 
 gren --action=changelog --generate --token=${githubToken}
 
