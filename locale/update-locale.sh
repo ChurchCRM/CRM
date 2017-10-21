8e3ea44c09a2064ac5cd58956c6f3816b899d385
George Dawoud <george@dawouds.com>
2017-10-21 14:39:16 -0700

    Update update-locale.sh

--- locale/update-locale.sh
+++ locale/update-locale.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-cd /vagrant/src
-find . -iname '*.php' | sort | grep -v ./vendor | xargs xgettext --from-code=UTF-8 -o /vagrant/locale/messages.po -L PHP
-cd /vagrant/locale/
+cd src
+# Extract PHP Terms
+find . -iname '*.php' | sort | grep -v ./vendor | xargs xgettext --from-code=UTF-8 -o ../locale/messages.po -L PHP
+
+# Extract JS Terms
+i18next-extract-gettext --files=skin/js/*.js --output=../locale/js-strings.po
+
+cd ../locale
+# merge PHP & JS Terms
+msgcat messages.po js-strings.po -o messages.po
+
+# Extract DB Terms
 php extract-db-locale-terms.php
 cd db-strings
-find . -iname "*.php" | sort | xargs xgettext --join-existing --from-code=UTF-8 -o /vagrant/locale/messages.po
+find . -iname "*.php" | sort | xargs xgettext --join-existing --from-code=UTF-8 -o ../messages.po
+
+# Cleanup
 cd ..
+rm js-strings.po
 rm db-strings/*
-i18next-extract-gettext --files='/vagrant/src/skin/js/*.js' --output=/vagrant/locale/js-strings.po
-msgcat /vagrant/locale/messages.po /vagrant/locale/js-strings.po -o /vagrant/locale/messages.po
-rm /vagrant/locale/js-strings.po
