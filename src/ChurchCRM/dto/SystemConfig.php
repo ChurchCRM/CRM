6c4faa1df591c3b5e4eb49bcb7f8d2133950b7e4
phili67 <philippelogel@free.fr>
2017-10-27 14:00:47 +0200

    Update SystemConfig.php
    I've just added the value to improve the possibility to manage correctly the numbers of items in the search field.

--- src/ChurchCRM/dto/SystemConfig.php
+++ src/ChurchCRM/dto/SystemConfig.php
@@ -200,7 +200,16 @@ class SystemConfig
         "bSearchIncludeFamilyHOH" => new ConfigItem(2022, "bSearchIncludeFamilyHOH", "boolean", "1", gettext("Show Family Head of House Names")),
         "bSearchIncludeGroups" => new ConfigItem(2023, "bSearchIncludeGroups", "boolean", "1", gettext("Search Groups")),
         "bSearchIncludeDeposits" => new ConfigItem(2024, "bSearchIncludeDeposits", "boolean", "1", gettext("Search Deposits")),
-        "bSearchIncludePayments" => new ConfigItem(2025, "bSearchIncludePayments", "boolean", "1", gettext("Search Payments"))
+        "bSearchIncludePayments" => new ConfigItem(2025, "bSearchIncludePayments", "boolean", "1", gettext("Search Payments")),
+        "bSearchIncludeAdresses" => new ConfigItem(2026, "bSearchIncludeAdresses", "boolean", "1", gettext("Search Payments")),
+        
+        "bSearchIncludePersonsNumbers" => new ConfigItem(2027, "bSearchIncludePersonsNumbers", "text", "15", gettext("Search Members numbers")),
+        "bSearchIncludeFamiliesNumbers" => new ConfigItem(2028, "bSearchIncludeFamiliesNumbers", "text", "15", gettext("Search Family ")),
+        "bSearchIncludeFamilyHOHNumbers" => new ConfigItem(2029, "bSearchIncludeFamilyHOHNumbers", "text", "15", gettext("Show Family Head of House Names numbers")),
+        "bSearchIncludeGroupsNumbers" => new ConfigItem(2030, "bSearchIncludeGroupsNumbers", "text", "15", gettext("Search Groups numbers")),
+        "bSearchIncludeDepositsNumbers" => new ConfigItem(2031, "bSearchIncludeDepositsNumbers", "text", "5", gettext("Search Deposits numbers")),
+        "bSearchIncludePaymentsNumbers" => new ConfigItem(2032, "bSearchIncludePaymentsNumbers", "text", "5", gettext("Search Payments numbers")),
+        "bSearchIncludeAdressesNumbers" => new ConfigItem(20233, "bSearchIncludeAdressesNumbers", "text", "15", gettext("Search Payments"))
 
     );
   }
@@ -216,7 +225,7 @@ class SystemConfig
       gettext('Report Settings')  => ["sQBDTSettings","leftX","incrementY","sTaxReport1","sTaxReport2","sTaxReport3","sTaxSigner","sReminder1","sReminderSigner","sReminderNoPledge","sReminderNoPayments","sConfirm1","sConfirm2","sConfirm3","sConfirm4","sConfirm5","sConfirm6","sDear","sConfirmSincerely","sConfirmSigner","sPledgeSummary1","sPledgeSummary2","sDirectoryDisclaimer1","sDirectoryDisclaimer2","bDirLetterHead","sZeroGivers","sZeroGivers2","sZeroGivers3", "iPDFOutputType"],
       gettext('Financial Settings') => ["sDepositSlipType","iChecksPerDepositForm","bDisplayBillCounts","bUseScannedChecks","sElectronicTransactionProcessor","bEnableNonDeductible","iFYMonth","bUseDonationEnvelopes","aFinanceQueries"],
       gettext('System Settings')  => ["sLogLevel", "bRegistered","sGZIPname","sZIPname","sPGPname","bCSVAdminOnly","sHeader","bEnableIntegrityCheck","iIntegrityCheckInterval","sLastIntegrityCheckTimeStamp"],
-      gettext('Quick Search') => ["bSearchIncludePersons","bSearchIncludeFamilies","bSearchIncludeFamilyHOH","bSearchIncludeGroups","bSearchIncludeDeposits", "bSearchIncludePayments"],
+      gettext('Quick Search') => ["bSearchIncludePersons","bSearchIncludePersonsNumbers","bSearchIncludeAdresses", "bSearchIncludeAdressesNumbers", "bSearchIncludeFamilies","bSearchIncludeFamiliesNumbers","bSearchIncludeFamilyHOH","bSearchIncludeFamilyHOHNumbers","bSearchIncludeGroups","bSearchIncludeGroupsNumbers","bSearchIncludeDeposits", "bSearchIncludeDepositsNumbers", "bSearchIncludePayments", "bSearchIncludePaymentsNumbers"],
       gettext('Backup')  => ["sLastBackupTimeStamp","bEnableExternalBackupTarget","sExternalBackupType","sExternalBackupAutoInterval","sExternalBackupEndpoint","sExternalBackupUsername","sExternalBackupPassword"],
       gettext('Localization')  => ["sLanguage","sDistanceUnit","sPhoneFormat","sPhoneFormatWithExt","sDateFormatLong","sDateFormatNoYear","sDateFormatShort","sDateTimeFormat","sDateFilenameFormat","sCSVExportDelemiter","sCSVExportCharset","sDatePickerFormat","sDatePickerPlaceHolder"],
       gettext('Integration')  => ["sMailChimpApiKey","bEnableGravatarPhotos","sGoogleTrackingID","bEnableGooglePhotos","sNexmoAPIKey","sNexmoAPISecret","sNexmoFromNumber","sOLPURL","sOLPUserName","sOLPPassword","bEnableExternalCalendarAPI"]
