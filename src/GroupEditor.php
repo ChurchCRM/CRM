3cf5abd67c7c03adc2245578a80444a2e9b0a991
phili67 <philippelogel@free.fr>
2017-10-29 22:53:05 +0100

    Update GroupEditor.php
    Big update and bug resolution

--- src/GroupEditor.php
+++ src/GroupEditor.php
@@ -82,9 +82,17 @@ require 'Include/Header.php';
         <div class="row">
           <div class="col-sm-3">
             <label for="GroupType"><?= gettext('Type of Group') ?>:</label>
-            <?php if (!$thisGroup->isSundaySchool()) {
-    ?>
-            <select class="form-control input-small" name="GroupType">
+            <?php 
+            			if ($thisGroup->isSundaySchool()) 
+            			{
+            				$hide = "style=\"display:none;\"";
+            			}
+            			else
+            			{
+            				$hide = "";
+            			}
+    				?>
+            <select class="form-control input-small" name="GroupType" <?php echo $hide; ?>>
               <option value="0"><?= gettext('Unassigned') ?></option>
               <option value="0">-----------------------</option>
               <?php
@@ -97,7 +105,8 @@ require 'Include/Header.php';
               } ?>              
             </select>
             <?php
-} else {
+								if ($thisGroup->isSundaySchool()) 
+								{
                   ?>
             	<b><?= gettext("Sunday School") ?></b>
             	<p><?= gettext("Sunday School group can't be modified, only in this two cases :")?></p>
