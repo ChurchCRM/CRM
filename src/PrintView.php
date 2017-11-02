00767b91223fbec32abdcba784c9a92635bea43c
phili67 <philippelogel@free.fr>
2017-11-02 10:11:52 +0100

    Update PrintView.php
    Update this part to hide the age in function of the hidden flag.
    I've added the photo too to the report.

--- src/PrintView.php
+++ src/PrintView.php
@@ -5,8 +5,9 @@
  *  last change : 2003-01-29
  *
  *  http://www.churchcrm.io/
- *  Copyright 2001-2003 Phillip Hullquist, Deane Barker, Chris Gebhardt
-  *
+ *  Copyright 2001-2003 Phillip Hullquist, Deane Barker, Chris Gebhardt, 
+ * 												Philippe LOgel
+ *
  ******************************************************************************/
 
 // Include the function library
@@ -15,6 +16,9 @@ require 'Include/Functions.php';
 
 use ChurchCRM\dto\SystemURLs;
 use ChurchCRM\Utils\InputUtils;
+use ChurchCRM\Reports\ChurchInfoReport;
+use ChurchCRM\dto\SystemConfig;
+use ChurchCRM\PersonQuery;
 
 // Get the person ID from the querystring
 $iPersonID = InputUtils::LegacyFilterInput($_GET['PersonID'], 'int');
@@ -127,8 +131,27 @@ require 'Include/Header-Short.php';
 <p class="ShadedBox">
 
 <?php
+
+$personSheet = PersonQuery::create()->findPk($per_ID);
+
+if ($personSheet)
+{
+	echo "<table>";
+	echo "	<tr>";
+	echo "	<td  style=\"padding:5px;\">";
+	$imgName = str_replace(SystemURLs::getDocumentRoot(),"",$personSheet->getPhotoURI());
+	
+	echo "<img src=\"".$imgName."\"/>";
+	echo "</td><td>";
+	echo '<b><font size="4">'.$personSheet->getFullName().'</font></b><br>';
+	echo "</td></tr></table>";
+}
+else
+{
+	echo '<b><font size="4">'.$personSheet->getFullName().'</font></b><br>';
+}
+
 // Print the name and address header
-echo '<b><font size="4">'.$per_FirstName.' '.$per_LastName.'</font></b><br>';
 echo '<font size="3">';
 if ($sAddress1 != '') {
     echo $sAddress1.'<br>';
@@ -326,8 +349,10 @@ if ($fam_ID) {
             while ($aRow = mysqli_fetch_array($rsFamilyMembers)) {
                 $per_BirthYear = '';
                 $agr_Description = '';
-
+                
                 extract($aRow);
+                
+                $person = PersonQuery::create()->findPk($per_ID);
 
                 // Alternate the row style
                 $sRowClass = AlternateRowStyle($sRowClass)
@@ -346,7 +371,9 @@ if ($fam_ID) {
 				<?= $sFamRole ?>&nbsp;
 			</td>
 			<td data-birth-date="<?= $per_Flags == 1 ? '' : date_create($per_BirthYear.'-'.$per_BirthMonth.'-'.$per_BirthDay)->format('Y-m-d') ?>">
-
+				<?php if ($per_Flags == 0)
+					echo FormatAge($per_BirthMonth,$per_BirthDay,$per_BirthYear);
+				?>
 			</td>
 		</tr>
 	<?php
@@ -384,7 +411,7 @@ if (mysqli_num_rows($rsAssignedGroups) == 0) {
         // DISPLAY THE ROW
         echo '<tr class="'.$sRowClass.'">';
         echo ' <td>'.$grp_Name.'</td>';
-        echo ' <td>'.$roleName.'</td>';
+        echo ' <td>'.gettext($roleName).'</td>';
         echo '</tr>';
 
         // If this group has associated special properties, display those with values and prop_PersonDisplay flag set.
