28250922bb165e86da3d1a3c307f00f5c455a17f
phili67 <philippelogel@free.fr>
2017-11-11 09:14:53 +0100

    Update GroupRoles.js
    I now include the js code to : GroupRoles.js

--- src/skin/js/GroupRoles.js
+++ src/skin/js/GroupRoles.js
@@ -14,4 +14,42 @@ function UpdateRoles()
     $("#GroupRole").html(html);
   });
 }
+
+$(document).ready(function (e, confirmed) {
+  $("#addToGroup").click(function () {
+    bootbox.prompt({
+      title: i18next.t("Add A Group Name"),
+      value: i18next.t("Default Name Group"),
+      onEscape: true,
+      closeButton: true,
+      buttons: {
+        confirm: {
+          label:  i18next.t('Yes'),
+            className: 'btn-success'
+        },
+        cancel: {
+          label:  i18next.t('No'),
+          className: 'btn-danger'
+        }
+      },
+      callback: function (result)
+      {
+      	if (result)
+      	{
+	      	var newGroup = {'groupName': result};
+	      	
+					$.ajax({
+						method: "POST",
+						url: window.CRM.root + "/api/groups/",               //call the groups api handler located at window.CRM.root
+						data: JSON.stringify(newGroup),                      // stringify the object we created earlier, and add it to the data payload
+						contentType: "application/json; charset=utf-8",
+						dataType: "json"
+					}).done(function (data) {                               //yippie, we got something good back from the server
+						location.href = 'CartToGroup.php?groupeCreationID='+data.Id;
+					});
+				}
+       }
+    });
+  });
+});
 	
