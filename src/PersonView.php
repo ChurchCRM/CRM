679c395746cb5055276a719e29486e4792d5b96d
Charles <crossan007@gmail.com>
2017-11-16 21:59:48 -0500

    use correct icons

--- src/PersonView.php
+++ src/PersonView.php
@@ -322,19 +322,19 @@ if ($per_ID == $iPersonID) {
     
     if ($per_FacebookID > 0) {
         ?>
-              <li><i class="fa-li fa fa-phone"></i><?= gettext('Facebook') ?>: <span><a href="https://www.facebook.com/<?= InputUtils::FilterInt($per_FacebookID) ?>"><?= gettext('Facebook') ?></a></span></li>
+              <li><i class="fa-li fa fa-facebook-official"></i><?= gettext('Facebook') ?>: <span><a href="https://www.facebook.com/<?= InputUtils::FilterInt($per_FacebookID) ?>"><?= gettext('Facebook') ?></a></span></li>
           <?php
     }
     
     if (strlen($per_Twitter) > 0) {
         ?>
-              <li><i class="fa-li fa fa-phone"></i><?= gettext('Twitter') ?>: <span><a href="https://www.twitter.com/<?= InputUtils::FilterString($per_Twitter) ?>"><?= gettext('Twitter') ?></a></span></li>
+              <li><i class="fa-li fa fa-twitter"></i><?= gettext('Twitter') ?>: <span><a href="https://www.twitter.com/<?= InputUtils::FilterString($per_Twitter) ?>"><?= gettext('Twitter') ?></a></span></li>
           <?php
     }
     
     if (strlen($per_LinkedIn) > 0) {
         ?>
-              <li><i class="fa-li fa fa-phone"></i><?= gettext('LinkedIn') ?>: <span><a href="https://www.linkedin.com/in/<?= InputUtils::FiltersTring($per_LinkedIn) ?>"><?= gettext('LinkedIn') ?></a></span></li>
+              <li><i class="fa-li fa fa-linkedin"></i><?= gettext('LinkedIn') ?>: <span><a href="https://www.linkedin.com/in/<?= InputUtils::FiltersTring($per_LinkedIn) ?>"><?= gettext('LinkedIn') ?></a></span></li>
           <?php
     }
 
