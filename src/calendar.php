fbb5e8c149099189f55e579ac4d6b049ebe79956
Charles <crossan007@gmail.com>
2017-11-16 22:19:28 -0500

    fix spelling error

--- src/calendar.php
+++ src/calendar.php
@@ -67,7 +67,7 @@ require 'Include/Header.php'; ?>
   <div class="box-body">
       <form>
           <div class="col-sm-3"> <b><?= gettext("Birthdate") ?>:</b> <input data-size="small" id="isBirthdateActive" type="checkbox" data-toggle="toggle" data-on="<?= gettext("Include") ?>" data-off="<?= gettext("Exclude") ?>"> </div>
-          <div class="col-sm-3"> <b><?= gettext("anniversary") ?>:</b> <input data-size="small" id="isAnniversaryActive" type="checkbox" data-toggle="toggle" data-on="<?= gettext("Include") ?>" data-off="<?= gettext("Exclude") ?>"></div>
+          <div class="col-sm-3"> <b><?= gettext("Anniversary") ?>:</b> <input data-size="small" id="isAnniversaryActive" type="checkbox" data-toggle="toggle" data-on="<?= gettext("Include") ?>" data-off="<?= gettext("Exclude") ?>"></div>
           <div class="col-sm-3"> <b><?= gettext("With Limit") ?>:</b> <input data-size="small" id="isWithLimit" type="checkbox" data-toggle="toggle" data-on="<?= gettext("Include") ?>" data-off="<?= gettext("Exclude") ?>"></div>
       </form>
   </div>
