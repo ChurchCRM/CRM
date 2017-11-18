38229cca850df988a527593cceaefbbeae42d29c
George Dawoud <george@dawouds.com>
2017-11-18 09:16:25 -0800

    upgrade existing tables to InnoDB

--- src/mysql/upgrade/2.9.0-InnoDB.php
+++ src/mysql/upgrade/2.9.0-InnoDB.php
@@ -1,8 +1,15 @@
 <?php
 require '../../Include/Config.php';
 
+use Propel\Runtime\Propel;
+
+$connection = Propel::getConnection();
+
+//this is needed as we create DB tables on the fly and we don't know what they are at upgrade time.
 $sqlUpgrade = "SELECT CONCAT('ALTER TABLE ',TABLE_NAME,' ENGINE=InnoDB;')  
     FROM INFORMATION_SCHEMA.TABLES
     WHERE ENGINE='MyISAM'
     AND table_schema = '". $sDATABASE ."';";
 
+$statement = $connection->prepare($query);
+$statement->execute();
