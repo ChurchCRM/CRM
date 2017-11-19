c86a04e93e14f6a92dd6e31a9b3eb117310c4860
George Dawoud <george@dawouds.com>
2017-11-19 18:20:27 +0000

    Apply fixes from StyleCI

--- src/calendar.php
+++ src/calendar.php
@@ -21,7 +21,6 @@ use ChurchCRM\EventTypesQuery;
 $calenderService = new CalendarService();
 use ChurchCRM\dto\SystemURLs;
 
-
 $groups = GroupQuery::Create()
       ->orderByName()
       ->find();
@@ -84,8 +83,8 @@ require 'Include/Header.php'; ?>
             <select type="text" id="EventTypeFilter" value="0">
               <option value='0' ><?= gettext("None") ?></option>
             <?php
-                  foreach ($eventTypes as $eventType){
-                    echo "+\"<option value='".$eventType->getID()."'>".$eventType->getName()."</option>\"";
+                  foreach ($eventTypes as $eventType) {
+                      echo "+\"<option value='".$eventType->getID()."'>".$eventType->getName()."</option>\"";
                   }
             ?>
             </select>
@@ -94,8 +93,8 @@ require 'Include/Header.php'; ?>
             <select type="text" id="EventGroupFilter" value="0">
               <option value='0' ><?= gettext("None") ?></option>
             <?php
-                  foreach ($groups as $group){
-                    echo "+\"<option value='".$group->getID()."'>".$group->getName()."</option>\"";
+                  foreach ($groups as $group) {
+                      echo "+\"<option value='".$group->getID()."'>".$group->getName()."</option>\"";
                   }
                 ?>  
             </select>
@@ -125,8 +124,11 @@ require 'Include/Header.php'; ?>
   var birthday    = true;
   var withlimit   = false;
   var isModifiable  = <?php 
-    if($_SESSION['bAddEvent'])echo "true"; 
-    else echo "false"; 
+    if ($_SESSION['bAddEvent']) {
+        echo "true";
+    } else {
+        echo "false";
+    }
   ?>;
   
  
@@ -257,8 +259,8 @@ require 'Include/Header.php'; ?>
               +'<select type="text" id="eventType" value="39">'
                    //+"<option value='0' ><?= gettext("Personal") ?></option>"
                    <?php
-                      foreach ($eventTypes as $eventType){
-                        echo "+\"<option value='".$eventType->getID()."'>".$eventType->getName()."</option>\"";
+                      foreach ($eventTypes as $eventType) {
+                          echo "+\"<option value='".$eventType->getID()."'>".$eventType->getName()."</option>\"";
                       }
                     ?>
                 +'</select>'
@@ -282,8 +284,8 @@ require 'Include/Header.php'; ?>
                 +'<select type="text" id="EventGroup" value="39">'
                    +"<option value='0' Selected><?= gettext("None") ?></option>"
                 <?php
-                  foreach ($groups as $group){
-                    echo "+\"<option value='".$group->getID()."'>".$group->getName()."</option>\"";
+                  foreach ($groups as $group) {
+                      echo "+\"<option value='".$group->getID()."'>".$group->getName()."</option>\"";
                   }
                 ?>              
                 +'</select>'
