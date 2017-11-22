6ebe251faf6432c01afb629dacd4d7dbec27c3ea
George Dawoud <george@dawouds.com>
2017-11-22 12:52:33 -0800

    Update Header.php

--- src/Include/Header.php
+++ src/Include/Header.php
@@ -156,7 +156,7 @@ $MenuFirst = 1;
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="<?= SystemURLs::getRootPath()?>PersonView.php?PersonID=<?= $_SESSION['user']->getPersonId() ?>"
+                  <a href="<?= SystemURLs::getRootPath()?>/PersonView.php?PersonID=<?= $_SESSION['user']->getPersonId() ?>"
                      class="btn btn-default btn-flat"><?= gettext('Profile') ?></a>
                 </div>
                 <div class="pull-right">
