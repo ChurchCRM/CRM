a4547bdeeb7a3a50741e2ac7190e2abed92a0a93
Philippe Logel <philippelogel@free.fr>
2017-11-25 17:56:24 +0100

    All the new folder

---
+++ src/skin/js/ckeditor/calendar_event_editor_config.js
@@ -0,0 +1,27 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/terms-of-use/#open-source-licences
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+  config.height = '250px';
+  
+  config.plugins =
+    'undo,' +
+    'colorbutton,' +
+    'colordialog,' +
+    'font,' +
+    'format,' +
+    'horizontalrule,' +
+    'image,' +
+    'justify,' +
+    'link,' +
+    'list,' +
+    'pastefromword,' +
+    'pastetext,' +
+    'resize,' +
+    'tableselection,' +
+    'smiley,' +
+    'maximize,' +
+    'wysiwygarea';
+};
