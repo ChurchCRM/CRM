17e19c3e1ac132f957ac0c38e3506020b90c50ea
phili67 <philippelogel@free.fr>
2017-11-25 18:39:14 +0100

    Update EventEditor.php security + ckeditor in EventEditor.php and in skin/js/Calendar.js  plus .... (#3424)
    * Update EventEditor.php
    
    Bug resolution of #3417
    
    * Update EventEditor.php
    
    I've added a ckeditor for the the event-text textarea
    
    * Create ckeditor_event_editor_config.js
    
    I've added the toolbar config for ckeditor
    
    * Update ckeditor_event_editor_config.js
    
    a little modication
    
    * Update EventEditor.php
    
    I've change the area size like this.
    
    * Update Calendar.js
    
    Now there's a CKeditor in the calendar creation event
    
    * Update calendar.php
    
    For ckeditor
    
    * Create ckeditor_calendar_event_editor_config.js
    
    The config file for create event in the calendar
    
    * Update EventEditor.php
    
    Some changes.
    
    * Update events.php
    
    To filter the hmtl code for xss attack
    
    * Update GetText.php
    
    The GetText is now full translated and can render html text.
    
    * Update ListEvents.php
    
    problem of localisation
    
    * I've added the smilley button
    
    * The change is done
    
    * Same too
    
    * the js file
    
    * All the new folder
    
    * Delete ckeditor_note_editor_config.js
    
    Ok it's unusefull
    
    * Delete ckeditor_calendar_event_editor_config.js
    
    Unusefull too.
    
    * Delete ckeditor_event_editor_config.js
    
    Same too.

---
+++ src/skin/js/ckeditor/event_editor_config.js
@@ -0,0 +1,50 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/terms-of-use/#open-source-licences
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+  config.height = '250px';
+  
+  config.plugins =
+    'about,' +
+    'blockquote,' +
+    'clipboard,' +
+    'colorbutton,' +
+    'colordialog,' +
+    'copyformatting,' +
+    'dialogadvtab,' +
+    'elementspath,' +
+    'enterkey,' +
+    'entities,' +
+    'find,' +
+    'floatingspace,' +
+    'font,' +
+    'format,' +
+    'horizontalrule,' +
+    'image,' +
+    'indentlist,' +
+    'indentblock,' +
+    'justify,' +
+    'link,' +
+    'list,' +
+    'magicline,' +
+    'maximize,' +
+    'pastefromword,' +
+    'pastetext,' +
+    'preview,' +
+    'print,' +
+    'removeformat,' +
+    'resize,' +
+    'selectall,' +
+    'smiley,' +
+    'specialchar,' +
+    'stylescombo,' +
+    'tab,' +
+    'table,' +
+    'tableselection,' +
+    'tabletools,' +
+    'templates,' +
+    'undo,' +
+    'wysiwygarea';
+};
