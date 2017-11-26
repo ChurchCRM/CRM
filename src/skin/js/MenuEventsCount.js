af4fcc68b2e0dd00ed4c760875e04012bf85571f
phili67 <philippelogel@free.fr>
2017-11-26 09:40:58 +0100

    Create MenuEventsCount.js
    Same too

---
+++ src/skin/js/MenuEventsCount.js
@@ -0,0 +1,32 @@
+  /* Philippe Logel 2017 */
+  
+  time=setInterval(function(){
+      //your code
+      window.CRM.APIRequest({
+          method: 'GET',
+          path: 'persons/numbers',
+    }).done(function(data) {     
+      var BirthdateNumber = document.getElementById('BirthdateNumber');
+      BirthdateNumber.innerText=data;
+    });  
+  
+    window.CRM.APIRequest({
+          method: 'GET',
+          path: 'families/numbers',
+    }).done(function(data) {     
+      var AnniversaryNumber = document.getElementById('AnniversaryNumber');
+      AnniversaryNumber.innerText=data;
+    });
+  
+  
+    window.CRM.APIRequest({
+          method: 'GET',
+          path: 'events/numbers',
+    }).done(function(data) {     
+      var EventsNumber = document.getElementById('EventsNumber');
+      EventsNumber.innerText=data;
+    });
+
+  },window.CRM.eventsInMenuBarIntervalTime);
+  
+
