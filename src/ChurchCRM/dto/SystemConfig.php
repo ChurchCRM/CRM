8b1c68b4819f58b10b87c47f3ee650cb2f5f1a69
Philippe Logel <philippelogel@free.fr>
2017-11-27 02:34:48 +0100

    I've just see a conflit

--- src/ChurchCRM/dto/SystemConfig.php
+++ src/ChurchCRM/dto/SystemConfig.php
@@ -215,9 +215,11 @@ class SystemConfig
         "iInitialsPointSize" => new ConfigItem(2037, "iInitialsPointSize", "number", "150", gettext("Point size to use for initials thumbnails")),
         "iPhotoClientCacheDuration" => new ConfigItem(2038, "iPhotoClientCacheDuration", "number", "3600", gettext("Client cache seconds for images")),
         "iRemotePhotoCacheDuration" => new ConfigItem(2039, "iRemotePhotoCacheDuration", "number", "72 hours", gettext("Server cache time for remote images")),
-        "bEventsOnDashboardPresence" => new ConfigItem(2040, "bEventsOnDashboardPresence", "boolean", "1", gettext("Show Birthdates Anniversaries on start up of the CRM")),        
-        "iEventsOnDashboardPresenceTimeOut" => new ConfigItem(2041, "iEventsOnDashboardPresenceTimeOut", "number", "30", gettext("Number of seconds after page load until the banner disappears, default 30 seconds")),
-        "iEventsInMenuBarIntervalTime" => new ConfigItem(2042, "iEventsInMenuBarIntervalTime", "number", "1", gettext("Menu bar event counter asynchronous refresh interval, default 1 second")),
+        "iPersonConfessionFatherCustomField" => new ConfigItem(2040, "iPersonConfessionFatherCustomField", "ajax", "", gettext("Field where Father Of Confession is listed, must be a people of group type"), "", "/api/system/custom-fields/person/?typeId=9"),
+        "iPersonConfessionDateCustomField" => new ConfigItem(2041, "iPersonConfessionDateCustomField", "ajax", "", gettext("Field where last Confession is stored, must be a date type"), "", "/api/system/custom-fields/person/?typeId=2"),
+        "bEventsOnDashboardPresence" => new ConfigItem(2042, "bEventsOnDashboardPresence", "boolean", "1", gettext("Show Birthdates Anniversaries on start up of the CRM")),        
+        "iEventsOnDashboardPresenceTimeOut" => new ConfigItem(2043, "iEventsOnDashboardPresenceTimeOut", "number", "30", gettext("Number of seconds after page load until the banner disappears, default 30 seconds")),
+        "iEventsInMenuBarIntervalTime" => new ConfigItem(2044, "iEventsInMenuBarIntervalTime", "number", "1", gettext("Menu bar event counter asynchronous refresh interval, default 1 second"))
     );
   }
 
@@ -236,6 +238,7 @@ class SystemConfig
       gettext('Backup')  => ["sLastBackupTimeStamp","bEnableExternalBackupTarget","sExternalBackupType","sExternalBackupAutoInterval","sExternalBackupEndpoint","sExternalBackupUsername","sExternalBackupPassword"],
       gettext('Localization')  => ["sLanguage","sDistanceUnit","sPhoneFormat","sPhoneFormatWithExt","sDateFormatLong","sDateFormatNoYear","sDateFormatShort","sDateTimeFormat","sDateFilenameFormat","sCSVExportDelemiter","sCSVExportCharset","sDatePickerFormat","sDatePickerPlaceHolder"],
       gettext('Integration')  => ["sMailChimpApiKey","sGoogleTrackingID","bEnableGravatarPhotos","bEnableGooglePhotos","iRemotePhotoCacheDuration","sNexmoAPIKey","sNexmoAPISecret","sNexmoFromNumber","sOLPURL","sOLPUserName","sOLPPassword","bEnableExternalCalendarAPI"],
+      gettext('Church Services')  => ["iPersonConfessionFatherCustomField","iPersonConfessionDateCustomField"],
       gettext('Events')  => ["bEventsOnDashboardPresence","iEventsOnDashboardPresenceTimeOut","iEventsInMenuBarIntervalTime"]
     );
   }
