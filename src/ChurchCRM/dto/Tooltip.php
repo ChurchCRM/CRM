511ef3f67a25169e810a3232a31ce86050d3e374
Philippe Logel <philippelogel@free.fr>
2017-12-02 13:12:13 +0100

    A new Tooltip class

---
+++ src/ChurchCRM/dto/Tooltip.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Philippe Logel 2017 */
+
+namespace ChurchCRM\dto;
+
+class Tooltip
+{
+  public function gettext($text)
+  {
+    if ($_SESSION['bShowTooltip']) {// $bShowTooltip is initialized in Header-function.php but not useable here ??
+      return gettext($text);
+    } else {
+      return "";
+    }
+  }
+}
