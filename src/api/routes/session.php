e82352bf7b982842d389dede0bcf940d1e801547
Philippe Logel <philippelogel@free.fr>
2017-12-02 12:20:23 +0100

    I simplify the code

--- src/api/routes/session.php
+++ src/api/routes/session.php
@@ -4,9 +4,6 @@
 // Person APIs
 use ChurchCRM\PersonQuery;
 use Propel\Runtime\ActiveQuery\Criteria;
-use ChurchCRM\dto\Photo;
-use ChurchCRM\Utils\MiscUtils;
-use ChurchCRM\dto\MenuEventsCount;
 
 $app->group('/session', function () {
     $this->get('/lock', function ($request, $response, $args) {
