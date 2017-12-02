6dbb2eca9cf5aaf3ae9a4a61c6a5cf2138ff225c
Philippe Logel <philippelogel@free.fr>
2017-12-03 00:13:07 +0100

    I suppress the coucou

--- src/Login.php
+++ src/Login.php
@@ -137,7 +137,7 @@ if (isset($_SESSION['iUserID'])) {
     $id = $_SESSION['iUserID'];
 }
 
-$type = "login".$_POST['iLoginType']."coucou";
+$type = $_POST['iLoginType'];
 if (isset($_SESSION['iLoginType'])) {
     $type = $_SESSION['iLoginType'];
 }
@@ -172,6 +172,70 @@ if ($_SESSION['iLoginType'] == "Lock") {
 
 
 ?>
+<div class="login-box" id="Login">
+    <div class="login-logo">
+        Church<b>CRM</b>
+    </div>
+    
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+        <p class="login-box-msg">
+            <b><?= ChurchMetaData::getChurchName() ?></b><br/>
+            <?= gettext('Please Login') ?>
+        </p>
+
+        <?php
+        if (isset($_GET['Timeout'])) {
+            $loginPageMsg = gettext('Your previous session timed out.  Please login again.');
+        }
+
+        // output warning and error messages
+        if (isset($sErrorText)) {
+            echo '<div class="alert alert-error">' . $sErrorText . '</div>';
+        }
+        if (isset($loginPageMsg)) {
+            echo '<div class="alert alert-warning">' . $loginPageMsg . '</div>';
+        }
+        ?>
+
+        <form class="form-signin" role="form" method="post" name="LoginForm" action="Login.php">
+            <div class="form-group has-feedback">
+                <input type="text" id="UserBox" name="User" class="form-control" value=""
+                   placeholder="<?= gettext('Email/Username') ?>" required autofocus>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" id="PasswordBox" name="Password" class="form-control" data-toggle="password"
+                   placeholder="<?= gettext('Password') ?>" required autofocus>
+                <br/>
+                <?php if (SystemConfig::getBooleanValue('bEnableLostPassword')) {
+            ?>
+                    <span class="text-right"><a
+                                href="external/password/"><?= gettext("I forgot my password") ?></a></span>
+                    <?php
+        } ?>
+            </div>
+            <div class="row">
+                <!-- /.col -->
+                <div class="col-xs-5">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat"><i
+                                class="fa fa-sign-in"></i> <?= gettext('Login') ?></button>
+                </div>
+            </div>
+        </form>
+
+        <?php if (SystemConfig::getBooleanValue('bEnableSelfRegistration')) {
+            ?>
+            <a href="external/register/" class="text-center btn bg-olive"><i
+                        class="fa fa-user-plus"></i> <?= gettext('Register a new Family'); ?></a><br>
+            <?php
+        } ?>
+        <!--<a href="external/family/verify" class="text-center">Verify Family Info</a> -->
+
+    </div>
+
+<!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
 <div class="lockscreen-wrapper" id="Lock">
     <div class="login-logo">
         Church<b>CRM</b>        
@@ -238,70 +302,6 @@ if ($_SESSION['iLoginType'] == "Lock") {
 <!-- /.login-box-body -->
 </div>
 <!-- /.lockscreen-wrapper -->
-<div class="login-box" id="Login">
-    <div class="login-logo">
-        Church<b>CRM</b>
-    </div>
-    
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-        <p class="login-box-msg">
-            <b><?= ChurchMetaData::getChurchName() ?></b><br/>
-            <?= gettext('Please Login') ?>
-        </p>
-
-        <?php
-        if (isset($_GET['Timeout'])) {
-            $loginPageMsg = gettext('Your previous session timed out.  Please login again.');
-        }
-
-        // output warning and error messages
-        if (isset($sErrorText)) {
-            echo '<div class="alert alert-error">' . $sErrorText . '</div>';
-        }
-        if (isset($loginPageMsg)) {
-            echo '<div class="alert alert-warning">' . $loginPageMsg . '</div>';
-        }
-        ?>
-
-        <form class="form-signin" role="form" method="post" name="LoginForm" action="Login.php">
-            <div class="form-group has-feedback">
-                <input type="text" id="UserBox" name="User" class="form-control" value=""
-                   placeholder="<?= gettext('Email/Username') ?>" required autofocus>
-            </div>
-            <div class="form-group has-feedback">
-                <input type="password" id="PasswordBox" name="Password" class="form-control" data-toggle="password"
-                   placeholder="<?= gettext('Password') ?>" required autofocus>
-                <br/>
-                <?php if (SystemConfig::getBooleanValue('bEnableLostPassword')) {
-            ?>
-                    <span class="text-right"><a
-                                href="external/password/"><?= gettext("I forgot my password") ?></a></span>
-                    <?php
-        } ?>
-            </div>
-            <div class="row">
-                <!-- /.col -->
-                <div class="col-xs-5">
-                    <button type="submit" class="btn btn-primary btn-block btn-flat"><i
-                                class="fa fa-sign-in"></i> <?= gettext('Login') ?></button>
-                </div>
-            </div>
-        </form>
-
-        <?php if (SystemConfig::getBooleanValue('bEnableSelfRegistration')) {
-            ?>
-            <a href="external/register/" class="text-center btn bg-olive"><i
-                        class="fa fa-user-plus"></i> <?= gettext('Register a new Family'); ?></a><br>
-            <?php
-        } ?>
-        <!--<a href="external/family/verify" class="text-center">Verify Family Info</a> -->
-
-    </div>
-
-<!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
 <script type="text/javascript" src="<?= SystemURLs::getRootPath() ?>/skin/external/bootstrap-show-password/bootstrap-show-password.min.js"></script>
 <script>
   <?php if ($_SESSION['iLoginType'] == "Lock") {
