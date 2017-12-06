2d2bfab484c0127d6c5e19b9bc6c83b8dca51c17
Philippe Logel <philippelogel@free.fr>
2017-12-06 22:15:41 +0100

    Hopefully solve the bug with events/numbers

--- src/skin/js/MenuEventsCount.js
+++ src/skin/js/MenuEventsCount.js
@@ -1,30 +1,16 @@
   /* Philippe Logel 2017 */
   
   time=setInterval(function(){
-      //your code
-      window.CRM.APIRequest({
-          method: 'GET',
-          path: 'persons/numbers',
-    }).done(function(data) {     
-      var BirthdateNumber = document.getElementById('BirthdateNumber');
-      BirthdateNumber.innerText=data;
-    });  
-  
-    window.CRM.APIRequest({
-          method: 'GET',
-          path: 'families/numbers',
-    }).done(function(data) {     
-      var AnniversaryNumber = document.getElementById('AnniversaryNumber');
-      AnniversaryNumber.innerText=data;
-    });
-  
-  
     window.CRM.APIRequest({
           method: 'GET',
           path: 'events/numbers',
     }).done(function(data) {     
       var EventsNumber = document.getElementById('EventsNumber');
-      EventsNumber.innerText=data;
+      EventsNumber.innerText=data.events;
+      var BirthdateNumber = document.getElementById('BirthdateNumber');
+      BirthdateNumber.innerText=data.birthdates;
+      var AnniversaryNumber = document.getElementById('AnniversaryNumber');
+      AnniversaryNumber.innerText=data.anniversaries;
     });
 
   },window.CRM.eventsInMenuBarIntervalTime*1000);
