ec7cd0418d802bf917101e38bc13cfc5faef31f8
Philippe Logel <philippelogel@free.fr>
2017-12-15 18:54:58 +0100

    Last changes too.

--- src/PersonView.php
+++ src/PersonView.php
@@ -439,6 +439,7 @@ $bOkToEdit = ($_SESSION['bEditRecords'] ||
 
                 <div class="timeline-item">
                   <span class="time"><i class="fa fa-clock-o"></i> <?= $item['datetime'] ?></span>
+                  
                   <h3 class="timeline-header">
                     <?php if (in_array('headerlink', $item)) {
             ?>
@@ -851,6 +852,7 @@ $bOkToEdit = ($_SESSION['bEditRecords'] ||
 
                 <div class="timeline-item">
                   <span class="time"><i class="fa fa-clock-o"></i> <?= $item['datetime'] ?></span>
+                  
                   <h3 class="timeline-header">
                     <?php if (in_array('headerlink', $item)) {
                                             ?>
