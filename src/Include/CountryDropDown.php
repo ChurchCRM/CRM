c14dc1842148c8d0c9b7bbd80a5ca88193487170
George Dawoud <george@dawouds.com>
2017-12-18 14:44:22 +0000

    Apply fixes from StyleCI

--- src/Include/CountryDropDown.php
+++ src/Include/CountryDropDown.php
@@ -3,25 +3,25 @@ use ChurchCRM\data\Countries;
 
 class CountryDropDown extends Countries
 {
-    public static function getDropDown($selected_country="",$countryname= "Country")
+    public static function getDropDown($selected_country="", $countryname= "Country")
     {
-      $country = $countryname;
-      $id_input = strtolower($country)."-input";
+        $country = $countryname;
+        $id_input = strtolower($country)."-input";
       
-      $res = "";
+        $res = "";
       
-      $res .= '<select name="'.$country.'" class="form-control select2" id="'.$id_input.'" style="width:100%">';      
-      $res .= '<option value="">'.gettext('Unassigned').'</option>';
-      $res .= '<option value="" disabled>--------------------</option>';
+        $res .= '<select name="'.$country.'" class="form-control select2" id="'.$id_input.'" style="width:100%">';
+        $res .= '<option value="">'.gettext('Unassigned').'</option>';
+        $res .= '<option value="" disabled>--------------------</option>';
         foreach (self::getNames() as $county) {
-          $res .= '<option value="'.$county.'"';
-          if ($selected_country == $county) {
-            $res .=  'selected';
-          } 
-          $res .=  '>'.gettext($county);
+            $res .= '<option value="'.$county.'"';
+            if ($selected_country == $county) {
+                $res .=  'selected';
+            }
+            $res .=  '>'.gettext($county);
         }
-      $res .= '</select>';
+        $res .= '</select>';
       
-      return $res;
+        return $res;
     }
 }
