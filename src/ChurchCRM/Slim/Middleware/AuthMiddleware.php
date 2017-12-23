16ea935f5cc03a3e2d245c46a18129644678facc
George Dawoud <george@dawouds.com>
2017-12-23 13:09:51 -0800

    added AuthMiddleware

---
+++ src/ChurchCRM/Slim/Middleware/AuthMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ChurchCRM\Slim\Middleware;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use ChurchCRM\Service\SystemService;
+
+class AuthMiddleware {
+
+    public function __invoke( Request $request, Response $response, callable $next )
+    {
+        $user = $_SESSION['user'];
+        if (empty($user)) {
+            return $response->withStatus( 401, 'No logged in user' );
+        }
+        return $next( $request, $response )->withHeader( "CRM_USER_ID", $user->getId());
+    }
+}
