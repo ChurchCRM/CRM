b6a878d247a13e37a9255cf3e26e9b5be1c68c26
George Dawoud <george@dawouds.com>
2017-12-23 21:03:46 +0000

    401 is returned and not Login.php

--- src/api/dependencies.php
+++ src/api/dependencies.php
@@ -11,9 +11,11 @@ use ChurchCRM\Service\SystemService;
 // DIC configuration
 
 $container['PersonService'] = new PersonService();
+
 $container['GroupService'] = new GroupService();
 
 $container['FinancialService'] = new FinancialService();
+
 $container['ReportingService'] = new ReportingService();
 
 $container['SystemService'] = new SystemService();
