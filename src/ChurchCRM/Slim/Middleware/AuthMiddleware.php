9260ea5ae3487598935f5789230be9d99233e753
George Dawoud <george@dawouds.com>
2017-12-23 16:01:03 -0800

    refactor for later use (API Key)

--- src/ChurchCRM/Slim/Middleware/AuthMiddleware.php
+++ src/ChurchCRM/Slim/Middleware/AuthMiddleware.php
@@ -8,12 +8,14 @@ use ChurchCRM\Service\SystemService;
 
 class AuthMiddleware {
 
+    private $user;
+
     public function __invoke( Request $request, Response $response, callable $next )
     {
-        $user = $_SESSION['user'];
-        if (empty($user)) {
+        $this->user = $_SESSION['user'];
+        if (empty($this->user)) {
             return $response->withStatus( 401)->withJson( ["message" => gettext('No logged in user')]);
         }
-        return $next( $request, $response )->withHeader( "CRM_USER_ID", $user->getId());
+        return $next( $request, $response )->withHeader( "CRM_USER_ID", $this->user->getId());
     }
 }
