019281eaee7c3db6e19b0387ff33118ce287d8f4
George Dawoud <george@dawouds.com>
2017-12-27 18:51:30 +0000

    Apply fixes from StyleCI

--- src/Include/SimpleConfig.php
+++ src/Include/SimpleConfig.php
@@ -4,7 +4,6 @@ use ChurchCRM\dto\LocaleInfo;
 use ChurchCRM\dto\SystemConfig;
 use ChurchCRM\dto\SystemURLs;
 
-
 $sMetaRefresh = '';  // Initialize to empty
 
 if (SystemConfig::getValue('sTimeZone')) {
