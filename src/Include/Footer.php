c653ba3d0bac0aa0343c7e04271c2246421dd022
George Dawoud <george@dawouds.com>
2017-12-27 18:30:51 -0800

    Update Footer.php (#3797)

--- src/Include/Footer.php
+++ src/Include/Footer.php
@@ -294,7 +294,6 @@ $isAdmin = $_SESSION['user']->isAdmin();
 <script src="<?= SystemURLs::getRootPath() ?>/skin/js/Tooltips.js"></script>
 <script src="<?= SystemURLs::getRootPath() ?>/skin/js/Events.js"></script>
 <script src="<?= SystemURLs::getRootPath() ?>/skin/js/Footer.js"></script>
-<script src="<?= SystemURLs::getRootPath() ?>/skin/external/bootstrap-validator/validator.min.js"></script>
 
 <?php if (isset($sGlobalMessage)) {
         ?>
