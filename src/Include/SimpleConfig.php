f9cfc77cfeeaa2c6e918f045612862bad75e1cc8
George Dawoud <george@dawouds.com>
2017-12-29 08:34:28 -0800

    removed useless Header_head_metatag

--- src/Include/SimpleConfig.php
+++ src/Include/SimpleConfig.php
@@ -4,8 +4,6 @@ use ChurchCRM\dto\LocaleInfo;
 use ChurchCRM\dto\SystemConfig;
 use ChurchCRM\dto\SystemURLs;
 
-$sMetaRefresh = '';  // Initialize to empty
-
 if (SystemConfig::getValue('sTimeZone')) {
     date_default_timezone_set(SystemConfig::getValue('sTimeZone'));
 }
