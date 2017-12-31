cac90856947c3f1f3d83b9453401b6064672cd77
Louis Bridgman <ljbinmtl@gmail.com>
2017-12-31 01:16:31 -0500

    Initial icon size refactoring (#3810)
    * Initial icon size refactoring
    
    * Inline styling changes
    
    * Fixed GroupView
    
    * Standardized variable naming between JS and PHP usage
    
    * Additional changes
    
    * Updated Google Map icon usage
    
    * Added MapUsingGoogle

--- src/Menu.php
+++ src/Menu.php
@@ -374,7 +374,7 @@ if ($depositData) { // If the user has Finance permissions, then let's display t
                                 <a class="users-list" href="PersonView.php?PersonID=<?= $person->getId() ?>">
                                     <img src="<?= SystemURLs::getRootPath(); ?>/api/persons/<?= $person->getId() ?>/thumbnail"
                                          alt="<?= $person->getFullName() ?>" class="user-image initials-image"
-                                         width="85" height="85"/><br/>
+                                         width="<?= SystemConfig::getValue('iProfilePictureListSize') ?>px" height="<?= SystemConfig::getValue('iProfilePictureListSize') ?>px"/><br/>
                                     <?= $person->getFullName() ?></a>
                                 <span class="users-list-date"><?= date_format($person->getDateEntered(), SystemConfig::getValue('sDateFormatLong')); ?>&nbsp;</span>
                             </li>
@@ -408,7 +408,7 @@ if ($depositData) { // If the user has Finance permissions, then let's display t
                                 <a class="users-list" href="PersonView.php?PersonID=<?= $person->getId() ?>">
                                     <img src="<?= SystemURLs::getRootPath(); ?>/api/persons/<?= $person->getId() ?>/thumbnail"
                                          alt="<?= $person->getFullName() ?>" class="user-image initials-image"
-                                         width="85" height="85"/><br/>
+                                         width="<?= SystemConfig::getValue('iProfilePictureListSize') ?>px" height="<?= SystemConfig::getValue('iProfilePictureListSize') ?>px"/><br/>
                                     <?= $person->getFullName() ?></a>
                                 <span
                                     class="users-list-date"><?= date_format($person->getDateLastEdited(), SystemConfig::getValue('sDateFormatLong')); ?>&nbsp;</span>
