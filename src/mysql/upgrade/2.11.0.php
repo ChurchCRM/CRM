622937d08486cbc5347d972c6410c1b690df09d4
Charles <crossan007@gmail.com>
2018-01-13 22:41:31 -0500

    begin upgrade script for 2.11.0 public events

---
+++ src/mysql/upgrade/2.11.0.php
@@ -0,0 +1,32 @@
+<?php
+
+use ChurchCRM\Event;
+use ChurchCRM\Calendar;
+use ChurchCRM\dto\SystemConfig;
+
+$publicEventsQuery = "SELECT * FROM events_event where event_publicly_visible = TRUE";
+
+$statement = $connection->prepare($publicEventsQuery);
+$statement->execute();
+$PublicEvents = $statement->fetchAll();
+
+if (count($PublicEvents) > 0) {
+  $PublicCalendar = new Calendar();
+  $PublicCalendar->setName(gettext("Public Calendar"));
+  $PublicCalendar->save();
+
+  foreach ($PublicEvents as $PublicEvent) {
+    $w = new Event();
+    $w->setType($PublicEvent['event_type']);
+    $w->setTitle($PublicEvent['event_title']);
+    $w->setDesc($PublicEvent['event_desc']);
+    $w->setText($PublicEvent['event_text']);
+    $w->setStart($PublicEvent['event_start']);
+    $w->setEnd($PublicEvent['event_end']);
+    $w->setInActive($PublicEvent['inactive']);
+    $w->setTypeName($PublicEvent['event_typename']);
+    $w->addCalendar($PublicCalendar);
+    $w->save();
+  }
+  
+}
