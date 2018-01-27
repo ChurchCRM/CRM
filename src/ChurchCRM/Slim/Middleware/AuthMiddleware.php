78610e927fc126dd0c5c15474d16f7d21bf1ec98
George Dawoud <george@dawouds.com>
2018-01-27 10:54:15 -0800

    added support for auth via x-api-key header

--- src/ChurchCRM/Slim/Middleware/AuthMiddleware.php
+++ src/ChurchCRM/Slim/Middleware/AuthMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace ChurchCRM\Slim\Middleware;
 
+use ChurchCRM\UserQuery;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use ChurchCRM\Service\SystemService;
@@ -9,19 +10,32 @@ use ChurchCRM\Service\SystemService;
 class AuthMiddleware {
 
     private $user;
+    private $apiKey;
 
     public function __invoke( Request $request, Response $response, callable $next )
     {
         if (!$this->isPublic( $request->getUri()->getPath())) {
-            $this->user = $_SESSION['user'];
-            if (empty($this->user)) {
-                return $response->withHeader("uri", $uri)->withStatus( 401)->withJson( ["message" => gettext('No logged in user')]);
+            $this->apiKey = $request->getHeader("x-api-key");
+            if (!empty($this->apiKey)) {
+                $this->user = UserQuery::create()->findOneByApiKey($this->apiKey);
+
             }
+            if (empty($this->user)) {
+                $this->user = $_SESSION['user'];
+            } else {
+                $_SESSION['user'] = $this->user;
+            }
+
+            if (empty($this->user)) {
+                return $response->withStatus(401)->withJson(["message" => gettext('No logged in user')]);
+            }
+
+
             return $next( $request, $response )->withHeader( "CRM_USER_ID", $this->user->getId())->withHeader("uri", $uri);
-        }  
+        }
         return $next( $request, $response );
     }
-    
+
     private function isPublic($path) {
         $pathAry = explode("/", $path);
         if (!empty($path) && $pathAry[0] === "public") {
