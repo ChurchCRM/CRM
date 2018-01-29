1cb302d80c4d8578870a69e029b52c0e3eb6ad13
Charles <crossan007@gmail.com>
2018-01-28 21:08:41 -0500

    use "userland" CSP nonce generation for PHP5 compatibility

--- src/ChurchCRM/dto/SystemURLs.php
+++ src/ChurchCRM/dto/SystemURLs.php
@@ -25,7 +25,14 @@ class SystemURLs
         self::$rootPath = $rootPath;
         self::$urls = $urls;
         self::$documentRoot = $documentRoot;
-        self::$CSPNonce = base64_encode(random_bytes(16));
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array(); //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < 16; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        self::$CSPNonce = base64_encode( implode($pass));
     }
 
     public static function getRootPath()
