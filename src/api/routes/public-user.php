936f3323407990a267fc6bcbee9b4a6465c624b9
George Dawoud <george@dawouds.com>
2018-02-03 14:13:06 -0800

    login now works and retuns api key

--- src/api/routes/public-user.php
+++ src/api/routes/public-user.php
@@ -1,21 +1,22 @@
 <?php
 
+use ChurchCRM\UserQuery;
 use Slim\Http\Request;
 use Slim\Http\Response;
-use ChurchCRM\UserQuery;
 
 $app->group('/public/user', function () {
-    $this->get('{userName}/login', 'userLogin');
-    $this->get('{userName}/login/', 'userLogin');
+    $this->post('/{userName}/login', 'userLogin');
+    $this->post('/{userName}/login/', 'userLogin');
 });
 
 function userLogin(Request $request, Response $response, array $args)
 {
     $userName = $args['userName'];
-    $user = UserQuery::create()->findOneBuyUserName();
+    $user = UserQuery::create()->findOneByUserName("$userName");
+    $body = json_decode($request->getBody());
     if (!empty($user)) {
-        $password = $request->getBody()->password;
-        if ($user->isPassord($password)) {
+        $password = $body->password;
+        if ($user->isPasswordValid($password)) {
             return $response->withJson(["apiKey" => $user->getApiKey()]);
         } else {
             return $response->withStatus(401);
