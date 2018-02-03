d0ba3d796ae063ed55221b3ea992b9c4e762c743
George Dawoud <george@dawouds.com>
2018-02-03 14:57:30 -0800

    working register person

--- src/api/routes/public-register.php
+++ src/api/routes/public-register.php
@@ -4,6 +4,7 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use ChurchCRM\Person;
 use ChurchCRM\Family;
+use ChurchCRM\Utils\LoggerUtils;
 
 $app->group('/public/register', function () {
     $this->post('/family', 'registerFamily');
@@ -22,20 +23,29 @@ function registerFamily(Request $request, Response $response, array $args)
 function registerPerson(Request $request, Response $response, array $args)
 {
     $body = json_decode($request->getBody());
+    if (empty($body->firstName) || empty($body->lastName) || empty($body->email)) {
+        return $response->withStatus(401)->withJson(["error" => "FirstName, LastName and email are required"]);
+    }
+
     $person = new Person();
     $person->setFirstName($body->firstName);
+    $person->setMiddleName($body->middleName);
     $person->setLastName($body->lastName);
+    $person->setTitle($body->title);
     $person->setEmail($body->email);
     $person->setCellPhone($body->cellPhone);
     $person->setWorkEmail($body->workEmail);
     $person->setWorkPhone($body->workPhone);
+    $person->setHomePhone($body->homePhone);
     $person->setAddress1($body->address1);
     $person->setAddress2($body->address2);
     $person->setCity($body->city);
     $person->setZip($body->zip);
     $person->setState($body->state);
     $person->setCountry($body->countryCode);
-    $person->setGender($body->gender);
+    $person->setGender($body->genderCode);
+    $person->setEnteredBy(Person::SELF_REGISTER);
+    $person->setDateEntered(new \DateTime());
     $person->save();
-    return $response->withStatus(401);
+    return $response->write($person->toJSON());
 }
