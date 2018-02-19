a9559936ab264b38eec695041b9e15e27938724d
George Dawoud <george@dawouds.com>
2018-02-19 13:42:48 -0800

    name cleanup curUserId from iUserID

--- src/ConvertIndividualToFamily.php
+++ src/ConvertIndividualToFamily.php
@@ -40,7 +40,7 @@ $sPageTitle = gettext('Convert Individuals to Families');
 
 require 'Include/Header.php';
 
-$iUserID = $_SESSION['user']->getId();
+$curUserId = $_SESSION['user']->getId();
 
 // find the family ID so we can associate to person record
 $sSQL = 'SELECT MAX(fam_ID) AS iFamilyID FROM family_fam';
@@ -86,7 +86,7 @@ while ($aRow = mysqli_fetch_array($rsList)) {
                 $per_Country."','".
                 $per_HomePhone."',".
                 'NOW()'.",'".
-                $iUserID."')";
+                $curUserId."')";
 
     echo '<br>'.$sSQL;
     // RunQuery to add family record
@@ -117,7 +117,7 @@ while ($aRow = mysqli_fetch_array($rsList)) {
             '    per_Country=NULL,'.
             '    per_HomePhone=NULL,'.
             '    per_DateLastEdited=NOW(),'.
-            "    per_EditedBy='$iUserID' ".
+            "    per_EditedBy='$curUserId' ".
             "WHERE per_ID='$per_ID'";
 
     echo '<br>'.$sSQL;
