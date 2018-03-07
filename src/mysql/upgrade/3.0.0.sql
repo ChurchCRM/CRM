98960fc94fcffa046d32a164d3fce5b6802d60c3
Charles <crossan007@gmail.com>
2018-03-07 10:48:18 -0500

    Bugfix/misc 3.0.0 bugs (#4085)
    * closes #4078
    
    * closes #4077
    
    * closes #4079
    
    * closes #4082
    
    * closes #4081
    
    * Apply fixes from StyleCI (#4084)
    
    * closes #4085
    
    * Type-check upgrade script extension && minor self-code-review
    
    * Handle invalid upgrade file steps

--- src/mysql/upgrade/3.0.0.sql
+++ src/mysql/upgrade/3.0.0.sql
@@ -40,3 +40,12 @@ ALTER TABLE user_usr
 	ADD UNIQUE INDEX `usr_apiKey_unique` (`usr_apiKey` ASC);
 
 DROP TABLE IF EXISTS menuconfig_mcf;
+
+DROP TABLE IF EXISTS `menu_links`;
+CREATE TABLE `menu_links` (
+  `linkId` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `linkName` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `linkUri` text COLLATE utf8_unicode_ci NOT NULL,
+  `linkOrder` INT NOT NULL,
+  PRIMARY KEY (`linkId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
