4132b7a2c949ab45e7a377be32043208ed374d64
George Dawoud <george@dawouds.com>
2018-03-10 14:17:02 -0800

    API stub for testing notification

--- src/api/routes/system/system.php
+++ src/api/routes/system/system.php
@@ -1,16 +1,24 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+use ChurchCRM\Utils\LoggerUtils;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 $app->group('/system', function () {
-    $this->post('/csp-report', function ($request, $response, $args) {
-        $input = json_decode($request->getBody());
-        $log = json_encode($input, JSON_PRETTY_PRINT);
-        $this->Logger->warn($log);
-    });
+    $this->post('/csp-report', 'logCSPReportAPI');
+    $this->get('/notification', 'getUiNotificationAPI');
 });
+
+function logCSPReportAPI(Request $request, Response $response, array $args)
+{
+    $input = json_decode($request->getBody());
+    $log = json_encode($input, JSON_PRETTY_PRINT);
+    LoggerUtils::getAppLogger()->warn($log);
+}
+
+function getUiNotificationAPI(Request $request, Response $response, array $args)
+{
+    $notifications = [];
+    array_push($notifications, new \ChurchCRM\dto\Notification\UiNotification("my title", "info"));
+    return $response->withJson($notifications);
+}
