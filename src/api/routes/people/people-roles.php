bb2c3edd928ccbcad59a5161b37ee7d2cee9a431
George Dawoud <george@dawouds.com>
2018-03-31 11:20:57 -0700

    method name updates

--- src/api/routes/people/people-roles.php
+++ src/api/routes/people/people-roles.php
@@ -6,11 +6,11 @@ use Slim\Http\Response;
 
 
 $app->group('/persons', function () {
-    $this->get('/roles', 'getAllRoles');
-    $this->get('/roles/', 'getAllRoles');
+    $this->get('/roles', 'getAllRolesAPI');
+    $this->get('/roles/', 'getAllRolesAPI');
 });
 
-function getAllRoles(Request $request, Response $response, array $p_args)
+function getAllRolesAPI(Request $request, Response $response, array $p_args)
 {
     $roles = ListOptionQuery::create()->getFamilyRoles();
     return $response->withJson($roles->toArray());
