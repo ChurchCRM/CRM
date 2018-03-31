b3aeb083c4c3f0a398aa2ce3819557f6aa728125
George Dawoud <george@dawouds.com>
2018-03-31 11:53:08 -0700

    moved person APIs to use new route / middleware

--- src/api/routes/people/people-person.php
+++ src/api/routes/people/people-person.php
@@ -5,12 +5,58 @@
 use ChurchCRM\ListOptionQuery;
 use ChurchCRM\Slim\Middleware\PersonAPIMiddleware;
 use ChurchCRM\Slim\Middleware\Role\EditRecordsRoleAuthMiddleware;
+use ChurchCRM\Slim\Middleware\Role\DeleteRecordRoleAuthMiddleware;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use ChurchCRM\SessionUser;
 
 
 $app->group('/person/{personId:[0-9]+}', function () {
     $this->post('/role/{roleId:[0-9]+}', 'setPersonRoleAPI')->add(new EditRecordsRoleAuthMiddleware());
+
+    $this->get('/photo', function ($request, $response, $args) {
+        $res = $this->cache->withExpires($response, MiscUtils::getPhotoCacheExpirationTimestamp());
+        $photo = new Photo("Person", $args['personId']);
+        return $res->write($photo->getPhotoBytes())->withHeader('Content-type', $photo->getPhotoContentType());
+
+    });
+
+    $this->post('/photo', function ($request, $response, $args) {
+        $input = (object)$request->getParsedBody();
+        $person = PersonQuery::create()->findPk($args['personId']);
+        $person->setImageFromBase64($input->imgBase64);
+        $response->withJSON(array("status" => "success"));
+    });
+
+    $this->delete('/photo', function ($request, $response, $args) {
+        $person = PersonQuery::create()->findPk($args['personId']);
+        return json_encode(array("status" => $person->deletePhoto()));
+    });
+
+    $this->get('/thumbnail', function ($request, $response, $args) {
+        $res = $this->cache->withExpires($response, MiscUtils::getPhotoCacheExpirationTimestamp());
+        $photo = new Photo("Person", $args['personId']);
+        return $res->write($photo->getThumbnailBytes())->withHeader('Content-type', $photo->getThumbnailContentType());
+    });
+
+    $this->post('/addToCart', function ($request, $response, $args) {
+        Cart::AddPerson($args['personId']);
+    });
+
+    $this->delete('', function ($request, $response, $args) {
+
+        $person = $request->getAttribute("person");
+        if (SessionUser::getId() == $person->getId()) {
+            return $response->withStatus(403, gettext("Can't delete yourself"));
+        }
+
+        $person->delete();
+
+        return $response->withJSON(["status" => gettext("success")]);
+
+    })->add(new DeleteRecordRoleAuthMiddleware());
+
+
 })->add(new PersonAPIMiddleware());
 
 
--- src/api/routes/people/people-persons.php
+++ src/api/routes/people/people-persons.php
@@ -51,61 +51,6 @@ $app->group('/persons', function () {
         return $response->withJson(MenuEventsCount::getNumberBirthDates());
     });
 
-    $this->get('/{personId:[0-9]+}/photo', function ($request, $response, $args) {
-        $res = $this->cache->withExpires($response, MiscUtils::getPhotoCacheExpirationTimestamp());
-        $photo = new Photo("Person", $args['personId']);
-        return $res->write($photo->getPhotoBytes())->withHeader('Content-type', $photo->getPhotoContentType());
-
-    });
-
-    $this->get('/{personId:[0-9]+}/thumbnail', function ($request, $response, $args) {
-        $res = $this->cache->withExpires($response, MiscUtils::getPhotoCacheExpirationTimestamp());
-        $photo = new Photo("Person", $args['personId']);
-        return $res->write($photo->getThumbnailBytes())->withHeader('Content-type', $photo->getThumbnailContentType());
-    });
-
-    $this->post('/{personId:[0-9]+}/photo', function ($request, $response, $args) {
-        $input = (object)$request->getParsedBody();
-        $person = PersonQuery::create()->findPk($args['personId']);
-        $person->setImageFromBase64($input->imgBase64);
-        $response->withJSON(array("status" => "success"));
-    });
-
-    $this->delete('/{personId:[0-9]+}/photo', function ($request, $response, $args) {
-        $person = PersonQuery::create()->findPk($args['personId']);
-        return json_encode(array("status" => $person->deletePhoto()));
-    });
-
-    $this->post('/{personId:[0-9]+}/addToCart', function ($request, $response, $args) {
-        Cart::AddPerson($args['personId']);
-    });
-
-    /**
-     * @var $response \Psr\Http\Message\ResponseInterface
-     */
-    $this->delete('/{personId:[0-9]+}', function ($request, $response, $args) {
-        /**
-         * @var \ChurchCRM\User $sessionUser
-         */
-        $sessionUser = $_SESSION['user'];
-        if (!$sessionUser->isDeleteRecordsEnabled()) {
-            return $response->withStatus(403);
-        }
-        $personId = $args['personId'];
-        if ($sessionUser->getId() == $personId) {
-            return $response->withStatus(403);
-        }
-        $person = PersonQuery::create()->findPk($personId);
-        if (is_null($person)) {
-            return $response->withStatus(404);
-        }
-
-        $person->delete();
-
-        return $response->withJSON(array("status" => "success"));
-
-    });
-
     $this->get('/self-register', function ($request, $response, $args) {
         $people = PersonQuery::create()
             ->filterByEnteredBy(Person::SELF_REGISTER)
@@ -125,11 +70,6 @@ function getAllRolesAPI(Request $request, Response $response, array $p_args)
 
 /**
  * A method that review dup emails in the db and returns families and people where that email is used.
- *
- * @param \Slim\Http\Request $p_request The request.
- * @param \Slim\Http\Response $p_response The response.
- * @param array $p_args Arguments
- * @return \Slim\Http\Response The augmented response.
  */
 function getEmailDupesAPI(Request $request, Response $response, array $args)
 {
