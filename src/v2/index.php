63e97b4ba366b819c9160108b872b75a63aee8f0
George Dawoud <george@dawouds.com>
2018-04-21 10:08:10 -0700

    added renderPage helper method

--- src/v2/index.php
+++ src/v2/index.php
@@ -24,9 +24,11 @@ $app->add(new AuthMiddleware());
 // Set up
 require __DIR__.'/../Include/slim/error-handler.php';
 
+require __DIR__.'/routes/common/mvc-helper.php';
+
 // admin routes
-require __DIR__ . '/routes/admin/admin.php';
-require __DIR__ . '/routes/user.php';
+require __DIR__.'/routes/admin/admin.php';
+require __DIR__.'/routes/user.php';
 
 // people routes
 require __DIR__.'/routes/family.php';
