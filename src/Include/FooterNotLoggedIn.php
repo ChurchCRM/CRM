e70313ea723cf85a7e990f2a621ec126e795ed66
boluak <79247+boluak@users.noreply.github.com>
2018-05-26 18:21:29 +0100

    Footer Copyright can auto update
    Edited Footer Copyright to auto update yearly #4316

--- src/Include/FooterNotLoggedIn.php
+++ src/Include/FooterNotLoggedIn.php
@@ -3,7 +3,15 @@ use ChurchCRM\dto\SystemURLs;
 
 ?>
     <div style="background-color: white; padding-top: 5px; padding-bottom: 5px; text-align: center; position: fixed; bottom: 0; width: 100%">
-      <strong><?= gettext('Copyright') ?> &copy; 2015-2018 <a href="http://www.churchcrm.io" target="_blank"><b>Church</b>CRM</a>.</strong> <?= gettext('All rights reserved')?>.
+      <strong><?= gettext('Copyright') ?>
+			<script type="text/javascript">
+				<!-- Begin
+					copyright=new Date();
+					update=copyright.getFullYear();
+					document.write("2005-"+ update);
+				//  End -->
+			</script> &copy;  <a href="http://www.churchcrm.io" target="_blank" TITLE="www.churchcrm.io"><b>Church</b>CRM</a>.</strong> <?= gettext('All rights reserved') ?>.
+    | <a href="https://twitter.com/church_crm" target="_blank"><i class="fa fa-twitter"></i> <?= gettext("Follow us on Twitter") ?></a>.
     </div>
 
 
