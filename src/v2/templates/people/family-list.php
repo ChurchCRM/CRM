a7472f6a9da2193abdea319fd93e2cb0c08dc8e4
Charles <crossan007@gmail.com>
2018-07-08 15:38:23 -0400

    adjust table columns to close #4331 move families to routed view in slim.

--- src/v2/templates/people/family-list.php
+++ src/v2/templates/people/family-list.php
@@ -1,6 +1,5 @@
 <?php
-require 'Include/Config.php';
-require 'Include/Functions.php';
+
 
 use ChurchCRM\dto\SystemConfig;
 use ChurchCRM\FamilyQuery;
@@ -8,31 +7,18 @@ use Propel\Runtime\ActiveQuery\Criteria;
 use ChurchCRM\Utils\InputUtils;
 use ChurchCRM\dto\SystemURLs;
 
-$sMode = 'Active';
-// Filter received user input as needed
-if (isset($_GET['mode'])) {
-    $sMode = InputUtils::LegacyFilterInput($_GET['mode']);
-}
-if (strtolower($sMode) == 'inactive') {
-    $families = FamilyQuery::create()
-        ->filterByDateDeactivated(null, Criteria::ISNOTNULL)
-            ->orderByName()
-            ->find();
-} else {
-    $sMode = 'Active';
-    $families = FamilyQuery::create()
-        ->filterByDateDeactivated(null)
-            ->orderByName()
-            ->find();
-}
 
-// Set the page title and include HTML header
+require SystemURLs::getDocumentRoot() . '/Include/SimpleConfig.php';
+
+//Set the page title
 $sPageTitle = gettext(ucfirst($sMode)) . ' ' . gettext('Family List');
-require 'Include/Header.php'; ?>
+include SystemURLs::getDocumentRoot() . '/Include/Header.php';
+/* @var $families ObjectCollection */
+?>
 
 <div class="pull-right">
-  <a class="btn btn-success" role="button" href="FamilyEditor.php"> <span class="fa fa-plus"
-                                                                          aria-hidden="true"></span><?= gettext('Add Family') ?>
+  <a class="btn btn-success" role="button" href="<?= SystemURLs::getRootPath()?>/FamilyEditor.php">
+    <span class="fa fa-plus" aria-hidden="true"></span><?= gettext('Add Family') ?>
   </a>
 </div>
 <p><br/><br/></p>
@@ -41,6 +27,7 @@ require 'Include/Header.php'; ?>
         <table id="families" class="table table-striped table-bordered data-table" cellspacing="0" width="100%">
             <thead>
             <tr>
+                <th><?= gettext('Actions') ?></th>
                 <th><?= gettext('Name') ?></th>
                 <th><?= gettext('Address') ?></th>
                 <th><?= gettext('Home Phone') ?></th>
@@ -54,20 +41,22 @@ require 'Include/Header.php'; ?>
 
             <!--Populate the table with family details -->
             <?php foreach ($families as $family) {
+              /* @var $family ChurchCRM\Family */
     ?>
             <tr>
-                <td><a href='FamilyView.php?FamilyID=<?= $family->getId() ?>'>
+              <td><a href='<?= SystemURLs::getRootPath()?>/FamilyView.php?FamilyID=<?= $family->getId() ?>'>
                         <span class="fa-stack">
                             <i class="fa fa-square fa-stack-2x"></i>
                             <i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>
                         </span>
                     </a>
-                    <a href='FamilyEditor.php?FamilyID=<?= $family->getId() ?>'>
+                    <a href='<?= SystemURLs::getRootPath()?>/FamilyEditor.php?FamilyID=<?= $family->getId() ?>'>
                         <span class="fa-stack">
                             <i class="fa fa-square fa-stack-2x"></i>
                             <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
                         </span>
-                    </a><?= $family->getName() ?></td>
+                    </a></td>
+              <td><?= $family->getName() ?></td>
                 <td> <?= $family->getAddress() ?></td>
                 <td><?= $family->getHomePhone() ?></td>
                 <td><?= $family->getCellPhone() ?></td>
@@ -90,5 +79,5 @@ require 'Include/Header.php'; ?>
 </script>
 
 <?php
-require 'Include/Footer.php';
+require SystemURLs::getDocumentRoot() .  '/Include/Footer.php';
 ?>
