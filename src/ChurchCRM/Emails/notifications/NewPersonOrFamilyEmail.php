49934e150bb06b5f001c77d21ad2484ac398bae0
Charles <crossan007@gmail.com>
2018-07-08 16:31:14 -0400

    move to tokens in template

--- src/ChurchCRM/Emails/notifications/NewPersonOrFamilyEmail.php
+++ src/ChurchCRM/Emails/notifications/NewPersonOrFamilyEmail.php
@@ -54,17 +54,31 @@ class NewPersonOrFamilyEmail extends BaseEmail
         ];
         if (get_class($this->relatedObject) == "ChurchCRM\Family")
         {
-          $myTokens['body'] = gettext("New Family Added")."\r\n".
-                  gettext("Family Name").": ".$this->relatedObject->getName()."\r\n". gettext("EMAIL").": ". $this->relatedObject->getEmail() ."\r\n". gettext("MOBILE PHONE").": ". $this->relatedObject->getCellPhone() ."\r\n". gettext("ADDRESS").": ". $this->relatedObject->getAddress()."\r\n". SystemConfig::getValue("sGreeterCustomMsg1")."\r\n". SystemConfig::getValue("sGreeterCustomMsg2");
+          /* @var $family ChurchCRM\Family */
+          $family = $this->relatedObject;
+          $myTokens['body'] = gettext("New Family Added")."\r\n". 
+            gettext("Family Name").": ". $family->getName();
+          $myTokens['FamilyEmail'] =  $family->getEmail();
+          $myTokens['FamilyPhone'] = $family->getCellPhone();
+          $myTokens['FamilyAddress'] =  $family->getAddress();
           $myTokens["familyLink"] = SystemURLs::getURL()."/FamilyView.php?FamilyID=".$this->relatedObject->getId();
         }
         elseif (get_class($this->relatedObject) == "ChurchCRM\Person")
         {
-          $myTokens['body'] = gettext("New Person Added,")."\r\n".
-					gettext("NAME").": ". $this->relatedObject->getFullName() ."\r\n". gettext("EMAIL").": ". $this->relatedObject->getEmail() ."\r\n". gettext("MOBILE PHONE").": ". $this->relatedObject->getCellPhone() ."\r\n". gettext("ADDRESS").": ". $this->relatedObject->getAddress() ."\r\n". gettext("AGE").": ". $this->relatedObject->getAge()."\r\n".SystemConfig::getValue("sGreeterCustomMsg1")."\r\n". SystemConfig::getValue("sGreeterCustomMsg2");
-          $myTokens['personLink'] = SystemURLs::getURL()."/PersonView.php?PersonID=".$this->relatedObject->getId();
+          /* @var $person ChurchCRM\Person */
+          $person = $this->relatedObject;
+          $myTokens['body'] = gettext("New Person Added")."\r\n".
+            gettext("Person Name").": ". $person->getFullName();
+          $myTokens['PersonEmail'] = $person->getEmail();
+          $myTokens['PersonPhone'] = $person->getCellPhone();
+          $myTokens['PersonAddress'] = $person->getAddress();
+          $myTokens['PersonAge'] = $person->getAge();
+          $myTokens['personLink'] = SystemURLs::getURL()."/PersonView.php?PersonID=".$person->getId();
         }
-        
+        $myTokens['IncludeDataInNewPersonNotifications'] = SystemConfig::getBooleanValue("IncludeDataInNewPersonNotifications");
+        $myTokens['sGreeterCustomMsg1'] = SystemConfig::getValue("sGreeterCustomMsg1");
+        $myTokens['sGreeterCustomMsg2'] = SystemConfig::getValue("sGreeterCustomMsg1");
+          
         return array_merge($this->getCommonTokens(), $myTokens);
     }
 }
