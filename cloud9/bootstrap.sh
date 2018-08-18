df55b22bae1661b63522c4f4228580e0e09671e0
Charles <crossan007@gmail.com>
2018-08-17 20:53:32 -0400

    ensure node 8 on c9

--- cloud9/bootstrap.sh
+++ cloud9/bootstrap.sh
@@ -28,6 +28,7 @@ echo "=========================================================="
 echo "===============   NPM                    ================="
 echo "=========================================================="
 
+nvm install 8
 npm install -g i18next-extract-gettext
 npm install -g grunt-cli
 npm install --unsafe-perm --no-bin-links
