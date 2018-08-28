ChurchCRM/CRM

commit e15e34774f7774709d189a4a6ec189d3149323b9

Issue 4055 - Add support for MySQL on non-standard ports (#4071)
* Issue 4055

Added support for MySQL running on non-standard ports. This maintains backward compatibility with previous configurations that do NOT have '$dbPort' defined in the Config.php file.

* Additional function calls

Found some more function calls that were not including "dbPort" in the parameter list. Fixed the function calls to include it.

* Remove unnecessary logic

Removed redundant "if" block and instead used PHP's default-style in the function arguments list.
http://php.net/manual/en/functions.arguments.php#functions.arguments.default

* Apply fixes from StyleCI (#4074)

src/Include/LoadConfigs.php
@@ -43,7 +43,7 @@ function system_failure($message, $header = 'Setup failure')
     exit();
 }
 
-function buildConnectionManagerConfig($sSERVERNAME, $sDATABASE, $sUSER, $sPASSWORD, $dbClassName, $dbPort = '3306')
+function buildConnectionManagerConfig($sSERVERNAME, $sDATABASE, $sUSER, $sPASSWORD, $dbClassName, $dbPort = "3306")
 {
     return [
         'dsn' => 'mysql:host=' . $sSERVERNAME . ';port='.$dbPort.';dbname=' . $sDATABASE,
@@ -91,7 +91,7 @@ $serviceContainer = Propel::getServiceContainer();
 $serviceContainer->checkVersion('2.0.0-dev');
 $serviceContainer->setAdapterClass('default', 'mysql');
 $manager = new ConnectionManagerSingle();
-$manager->setConfiguration(buildConnectionManagerConfig($sSERVERNAME, $sDATABASE, $sUSER, $sPASSWORD, $dbClassName));
+$manager->setConfiguration(buildConnectionManagerConfig($sSERVERNAME, $sDATABASE, $sUSER, $sPASSWORD, $dbClassName, $dbPort));
 $manager->setName('default');
 $serviceContainer->setConnectionManager('default', $manager);
 $serviceContainer->setDefaultDatasource('default');
@@ -130,7 +130,7 @@ $logger = LoggerUtils::getAppLogger();
 // ORM Logs
 $ormLogger = new Logger('ormLogger');
 $dbClassName = "\\Propel\\Runtime\\Connection\\DebugPDO";
-$manager->setConfiguration(buildConnectionManagerConfig($sSERVERNAME, $sDATABASE, $sUSER, $sPASSWORD, $dbClassName));
+$manager->setConfiguration(buildConnectionManagerConfig($sSERVERNAME, $sDATABASE, $sUSER, $sPASSWORD, $dbClassName, $dbPort));
 $ormLogger->pushHandler(new StreamHandler(LoggerUtils::buildLogFilePath("orm"), LoggerUtils::getLogLevel()));
 $serviceContainer->setLogger('defaultLogger', $ormLogger);
 

src/Include/Config.php.example
@@ -9,6 +9,7 @@
 
 // Database connection constants
 $sSERVERNAME = '||DB_SERVER_NAME||';
+$dbPort = '||DB_SERVER_PORT||';
 $sUSER = '||DB_USER||';
 $sPASSWORD = '||DB_PASSWORD||';
 $sDATABASE = '||DB_NAME||';

src/setup/routes/setup.php
@@ -25,6 +25,7 @@ $app->group('/', function () {
         $template = file_get_contents(SystemURLs::getDocumentRoot().'/Include/Config.php.example');
 
         $template = str_replace('||DB_SERVER_NAME||', $setupDate['DB_SERVER_NAME'], $template);
+        $template = str_replace('||DB_SERVER_PORT||', $setupDate['DB_SERVER_PORT'], $template);
         $template = str_replace('||DB_NAME||', $setupDate['DB_NAME'], $template);
         $template = str_replace('||DB_USER||', $setupDate['DB_USER'], $template);
         $template = str_replace('||DB_PASSWORD||', $setupDate['DB_PASSWORD'], $template);

src/setup/templates/setup-steps.php
@@ -100,6 +100,12 @@ require '../Include/HeaderNotLoggedIn.php';
                 <small id="DB_SERVER_NAME_HELP" class="form-text text-muted"></small>
             </div>
             <div class="form-group">
+                <label for="DB_SERVER_PORT">Database Server Port</label>
+                <input type="text" name="DB_SERVER_PORT" id="DB_SERVER_PORT" class="form-control"
+                       aria-describedby="DB_SERVER_PORT_HELP" required>
+                <small id="DB_SERVER_PORT_HELP" class="form-text text-muted"></small>
+            </div>
+            <div class="form-group">
                 <label for="DB_NAME">Database Name</label>
                 <input type="text" name="DB_NAME" id="DB_NAME" placeholder="churchcrm" class="form-control"
                        aria-describedby="DB_NAME_HELP" required>

commit 6d9c5b953a5bdb31159e4c947110f2c9290690fd

Fixed database connection to correctly implement port number (fixes regression bug introduced in #4071)

src/Include/LoadConfigs.php
@@ -70,7 +70,7 @@ try {
 SystemURLs::checkAllowedURL($bLockURL, $URL);
 
 // Due to mysqli handling connections on 'localhost' via socket only, we need to tease out this case and handle
-// TCP/IP connections separately defaulting $dbPort to 3306 for the general case when$dbPort is not set.
+// TCP/IP connections separately defaulting $dbPort to 3306 for the general case when $dbPort is not set.
 if ($sSERVERNAME == "localhost") {
     $cnInfoCentral = mysqli_connect($sSERVERNAME, $sUSER, $sPASSWORD)
     or system_failure('Could not connect to MySQL on <strong>'.$sSERVERNAME.'</strong> as <strong>'.$sUSER.'</strong>. Please check the settings in <strong>Include/Config.php</strong>.<br/>MySQL Error: '.mysqli_error($cnInfoCentral));
@@ -78,7 +78,9 @@ if ($sSERVERNAME == "localhost") {
     if (!isset($dbPort)) {
         $dbPort=3306;
     }
-    $cnInfoCentral = mysqli_connect($sSERVERNAME.':'.$dbPort, $sUSER, $sPASSWORD)
+    // Connect via TCP to specified port and pass a 'null' for database name.
+    // We specify the database name in a different call, ie 'mysqli_select_db()' just below here
+    $cnInfoCentral = mysqli_connect($sSERVERNAME, $sUSER, $sPASSWORD, null, $dbPort)
         or system_failure('Could not connect to MySQL on <strong>'.$sSERVERNAME.'</strong> on port <strong>'.$dbPort.'</strong> as <strong>'.$sUSER.'</strong>. Please check the settings in <strong>Include/Config.php</strong>.<br/>MySQL Error: '.mysqli_error($cnInfoCentral));
 }
 