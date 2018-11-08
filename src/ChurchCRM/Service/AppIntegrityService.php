15d8aee0ab5e8fda602535b077e0166b7ade496c
chrishsieh <902006+chrishsieh@users.noreply.github.com>
2018-11-08 17:40:45 +0800

    Add rewrite check for https, localhost with proxy or fastcgi. Add curl rewrite fail log.

--- src/ChurchCRM/Service/AppIntegrityService.php
+++ src/ChurchCRM/Service/AppIntegrityService.php
@@ -3,7 +3,7 @@
 namespace ChurchCRM\Service;
 
 use ChurchCRM\dto\SystemURLs;
-use ChurchCRM\dto\Prerequisite;
+use ChurchCRM\Utils\LoggerUtils;
 
 class AppIntegrityService
 {
@@ -79,10 +79,10 @@ class AppIntegrityService
       return ['status' => 'success'];
     }
   }
-  
+
   private static function testImagesWriteable()
   {
-    return is_writable(SystemURLs::getDocumentRoot().'/Images/') && 
+    return is_writable(SystemURLs::getDocumentRoot().'/Images/') &&
             is_writable(SystemURLs::getDocumentRoot().'/Images/Family') &&
             is_writable(SystemURLs::getDocumentRoot().'/Images/Person');
 
@@ -143,21 +143,43 @@ class AppIntegrityService
     // Third, finally try calling a known invalid URL on this installation
     //   and check for a header that would only be present if .htaccess was processed.
     //   This header comes from index.php (which is the target of .htaccess for invalid URLs)
-    
+
     $check = AppIntegrityService::hasApacheModule('mod_rewrite');
 
     if (!$check && function_exists('shell_exec')) {
-        $check = strpos(shell_exec('/usr/local/apache/bin/apachectl -l'), 'mod_rewrite') !== false;
+        if (strpos($_SERVER['SERVER_SOFTWARE'], 'Apache') !== false) {
+            $check = strpos(shell_exec('/usr/local/apache/bin/apachectl -l'), 'mod_rewrite') !== false;
+        }
     }
 
     if ( function_exists('curl_version')) {
-        $ch = curl_init(); 
-        curl_setopt($ch, CURLOPT_URL, $_SERVER['HTTP_HOST'] . SystemURLs::getRootPath()."/INVALID"); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        $verbose = fopen('php://temp', 'w+');
+        $ch = curl_init();
+        $request_url_parser = parse_url($_SERVER['HTTP_REFERER']);
+        $request_scheme = isset($request_url_parser['scheme']) ? $request_url_parser['scheme'] : 'http';
+        $request_host = isset($request_url_parser['host']) ? $request_url_parser['host'] : 'localhost';
+        $request_port = isset($request_url_parser['port']) ? $request_url_parser['port'] : (($request_scheme == 'https')? '443' : '80');
+        curl_setopt($ch, CURLOPT_URL, $request_scheme ."://". $request_host . SystemURLs::getRootPath()."/INVALID");
+
+        if ( gethostbyname($request_host) == '127.0.0.1') {
+            $sapi_type = php_sapi_name();
+
+            if ( $sapi_type == 'fpm-fcgi') {
+                curl_setopt($ch, CURLOPT_RESOLVE, array(sprintf("%s:%d:%s", $request_host, $request_port, $_SERVER['SERVER_ADDR'])));
+            } else if (isset($_SERVER['HTTP_X_REAL_IP'])) {
+                curl_setopt($ch, CURLOPT_RESOLVE, array(sprintf("%s:%d:%s", $request_host, $request_port, $_SERVER['REMOTE_ADDR'])));
+            }
+        }
+
+        curl_setopt($ch, CURLOPT_STDERR, $verbose);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_NOBODY, 1);
-        $output = curl_exec($ch); 
-        curl_close($ch);      
+        $output = curl_exec($ch);
+        curl_close($ch);
         $headers=array();
         $data=explode("\n",$output);
         $headers['status']=$data[0];
@@ -169,7 +191,16 @@ class AppIntegrityService
               $headers[trim($middle[0])] = trim($middle[1]);
             }
         }
-        $check =  $headers['CRM'] == "would redirect";      
+        $check =  $headers['CRM'] == "would redirect";
+
+        if ( $check == false) {
+            rewind($verbose);
+            $verboseLog = stream_get_contents($verbose);
+            $logger = LoggerUtils::getAppLogger();
+            $logger->warn("Curl information: ".htmlspecialchars($verboseLog));
+        }
+
+        fclose($verbose);
       }
 
       return $check;
