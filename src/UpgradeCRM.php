a062cbe8a83a175a28b0f8802b6316524c575c1d
Charles Crossan <crossan007@gmail.com>
2018-11-27 13:21:44 -0500

    add step in upgrade wizard if there are files failing validation (#4532)
    * add step in upgrade wizard if there are files failing validation
    
    * Apply fixes from StyleCI (#4531)

--- src/UpgradeCRM.php
+++ src/UpgradeCRM.php
@@ -8,6 +8,7 @@ require_once 'Include/Header-function.php';
 
 use ChurchCRM\dto\SystemURLs;
 use ChurchCRM\Utils\RedirectUtils;
+use ChurchCRM\Service\AppIntegrityService;
 
 // Set the page title and include HTML header
 $sPageTitle = gettext('Upgrade ChurchCRM');
@@ -29,11 +30,61 @@ Header_body_scripts();
             <?= gettext('Upgrade ChurchCRM') ?>
         </span>
     </li>
+    <?php
+      if (AppIntegrityService::getIntegrityCheckStatus() == gettext("Failed")) {
+          ?>
+    <li>
+      <i class="fa fa-bomb bg-red"></i>
+      <div class="timeline-item" >
+        <h3 class="timeline-header"><?= gettext('Warning: Signature mismatch') ?> <span id="status1"></span></h3>
+        <div class="timeline-body" id="integrityCheckWarning">
+          <p><?= gettext("Some ChurchCRM system files may have been modified since the last installation.")?><b><?= gettext("This upgrade will completely destroy any customizations made to the following files by reverting the files to the official version.")?></b></p>
+          <p><?= gettext("If you wish to maintain your changes to these files, please take a manual backup of these files before proceeding with this upgrade, and then manually restore the files after the upgrade is complete.")?></p>
+          <div>
+              <p><?= gettext('Integrity Check Details:')?> <?=  AppIntegrityService::getIntegrityCheckMessage() ?></p>
+                <?php
+                  if (count(AppIntegrityService::getFilesFailingIntegrityCheck()) > 0) {
+                      ?>
+                    <p><?= gettext('Files failing integrity check') ?>:
+                    <table class="display responsive no-wrap" width="100%" id="fileIntegrityCheckResultsTable">
+                      <thead>
+                      <td>FileName</td>
+                      <td>Expected Hash</td>
+                      <td>Actual Hash</td>
+                    </thead>
+                      <?php
+                      foreach (AppIntegrityService::getFilesFailingIntegrityCheck() as $file) {
+                          ?>
+                    <tr>
+                      <td><?= $file->filename ?></td>
+                      <td><?= $file->expectedhash ?></td>
+                      <td>
+                          <?php
+                          if ($file->status == 'File Missing') {
+                              echo gettext('File Missing');
+                          } else {
+                              echo $file->actualhash;
+                          } ?>
+                      </td>
+                    </tr>
+                        <?php
+                      } ?>
+                    </table>
+                    <?php
+                  } ?>
+            </div>
+          <input type="button" class="btn btn-primary" id="acceptIntegrityCheckWarking" <?= 'value="'.gettext('I Understand').'"' ?>>
+        </div>
+      </div>
+    </li>
+    <?php
+      }
+    ?>
     <li>
       <i class="fa fa-database bg-blue"></i>
       <div class="timeline-item" >
         <h3 class="timeline-header"><?= gettext('Step 1: Backup Database') ?> <span id="status1"></span></h3>
-        <div class="timeline-body" id="backupPhase">
+        <div class="timeline-body" id="backupPhase" <?= AppIntegrityService::getIntegrityCheckStatus() == gettext("Failed") ? 'style="display:none"' : '' ?>>
           <p><?= gettext('Please create a database backup before beginning the upgrade process.')?></p>
           <input type="button" class="btn btn-primary" id="doBackup" <?= 'value="'.gettext('Generate Database Backup').'"' ?>>
           <span id="backupStatus"></span>
@@ -82,6 +133,19 @@ Header_body_scripts();
   </ul>
 </div>
 <script nonce="<?= SystemURLs::getCSPNonce() ?>">
+  $(document).ready(function() { 
+    $("#fileIntegrityCheckResultsTable").DataTable({
+      responsive: true,
+      paging:false,
+      searching: false
+    });
+
+    $("#acceptIntegrityCheckWarking").click(function() {
+      $("#integrityCheckWarning").slideUp();
+      $("#backupPhase").show("slow");
+    });
+  });
+
  $("#doBackup").click(function(){
    $("#status1").html('<i class="fa fa-circle-o-notch fa-spin"></i>');
    window.CRM.APIRequest({
@@ -150,6 +214,13 @@ function downloadbutton(filename) {
     $("#downloadbutton").attr("disabled","true");
 }
 </script>
+
+<script src="<?= SystemURLs::getRootPath() ?>/skin/adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= SystemURLs::getRootPath() ?>/skin/adminlte/plugins/datatables/dataTables.bootstrap.js"></script>
+<script src="<?= SystemURLs::getRootPath() ?>/skin/adminlte/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js" ></script>
+<script src="<?= SystemURLs::getRootPath() ?>/skin/adminlte/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js"></script>
+<script src="<?= SystemURLs::getRootPath() ?>/skin/adminlte/plugins/datatables/extensions/Select/dataTables.select.min.js"></script>
+
 
 <?php
 // Add the page footer
