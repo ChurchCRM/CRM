786d4fbbc1865726dae52aeb264dfdc4ea0da158
Charles Crossan <crossan007@gmail.com>
2018-12-03 21:19:08 -0500

    First attempt at a unified bootstrapper with vastly improved logging

--- src/ChurchCRM/Service/SystemService.php
+++ src/ChurchCRM/Service/SystemService.php
@@ -190,11 +190,7 @@ class SystemService
       }
     }
 
-    static public function isDBCurrent()
-    {
-        return SystemService::getDBVersion() == SystemService::getInstalledVersion();
-    }
-    
+  
     static public function getDBTableExists($tableName) {
       if (!isset($_SESSION['CRM_DB_TABLES']))
       {
