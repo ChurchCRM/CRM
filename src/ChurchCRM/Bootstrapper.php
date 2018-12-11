f60b765ecea0809372b08b4f5deb90a138ff6518
George Dawoud <george@dawouds.com>
2018-12-10 17:45:05 -0800

    Apply fixes from StyleCI (#4598)

--- src/ChurchCRM/Bootstrapper.php
+++ src/ChurchCRM/Bootstrapper.php
@@ -83,31 +83,32 @@ namespace ChurchCRM
           LoggerUtils::ResetAppLoggerLevel();
       }
       
-      private static function ConfigureLocale() {
-        global $aLocaleInfo,$localeInfo;
-        if (SystemConfig::getValue('sTimeZone')) {
-          self::$bootStrapLogger->debug("Setting TimeZone to: " . SystemConfig::getValue('sTimeZone'));
-          date_default_timezone_set(SystemConfig::getValue('sTimeZone'));
-        }
+      private static function ConfigureLocale()
+      {
+          global $aLocaleInfo,$localeInfo;
+          if (SystemConfig::getValue('sTimeZone')) {
+              self::$bootStrapLogger->debug("Setting TimeZone to: " . SystemConfig::getValue('sTimeZone'));
+              date_default_timezone_set(SystemConfig::getValue('sTimeZone'));
+          }
 
-        $localeInfo = new LocaleInfo(SystemConfig::getValue('sLanguage'));
-        self::$bootStrapLogger->debug("Setting locale to: " . SystemConfig::getValue('sLanguage'));
-        setlocale(LC_ALL, $localeInfo->getLocale());
+          $localeInfo = new LocaleInfo(SystemConfig::getValue('sLanguage'));
+          self::$bootStrapLogger->debug("Setting locale to: " . SystemConfig::getValue('sLanguage'));
+          setlocale(LC_ALL, $localeInfo->getLocale());
 
-        // Get numeric and monetary locale settings.
-        $aLocaleInfo = $localeInfo->getLocaleInfo();
+          // Get numeric and monetary locale settings.
+          $aLocaleInfo = $localeInfo->getLocaleInfo();
 
-        // This is needed to avoid some bugs in various libraries like fpdf.
-        // http://www.velanhotels.com/fpdf/FAQ.htm#6
-        setlocale(LC_NUMERIC, 'C');
+          // This is needed to avoid some bugs in various libraries like fpdf.
+          // http://www.velanhotels.com/fpdf/FAQ.htm#6
+          setlocale(LC_NUMERIC, 'C');
 
-        $domain = 'messages';
-        $sLocaleDir = SystemURLs::getDocumentRoot() . '/locale/textdomain';
-        self::$bootStrapLogger->debug("Setting local text domain bind to: " . $sLocaleDir);
-        bind_textdomain_codeset($domain, 'UTF-8');
-        bindtextdomain($domain, $sLocaleDir);
-        textdomain($domain);
-        self::$bootStrapLogger->debug("Locale configuration complete");
+          $domain = 'messages';
+          $sLocaleDir = SystemURLs::getDocumentRoot() . '/locale/textdomain';
+          self::$bootStrapLogger->debug("Setting local text domain bind to: " . $sLocaleDir);
+          bind_textdomain_codeset($domain, 'UTF-8');
+          bindtextdomain($domain, $sLocaleDir);
+          textdomain($domain);
+          self::$bootStrapLogger->debug("Locale configuration complete");
       }
       
       private static function initMySQLI()
