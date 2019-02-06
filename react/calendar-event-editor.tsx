02b24b6d149bae0ac69ae062d47c8ce428bf25ef
Charles Crossan <crossan007@gmail.com>
2019-02-05 19:35:15 -0500

    add interface definitions for the javascript window object's i18next for TypeScript

--- react/calendar-event-editor.tsx
+++ react/calendar-event-editor.tsx
@@ -4,11 +4,17 @@ import CRMEvent from './interfaces/CRMEvent';
 import ExistingEvent from './components/ExistingEvent';
 declare global {
     interface Window { 
+      // Since TypeScript requires a definition for all methods, let's tell it how to handle the javascript objects already in the page 
       showEventForm(object): void,
       showNewEventForm(start: string,end: string): void,
       CRM: {
+        // we need to access this method of CRMJSOM, so let's tell TypeScript how to use it
         refreshAllFullCalendarSources(): void 
-      } 
+      },
+      // React does have it's own i18next implementation, but for now, lets use the one that's already being loaded
+      i18next: { 
+        t(string): string
+      }
     }
 }
 
