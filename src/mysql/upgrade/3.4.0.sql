49212404ddf450ddbd702a08b0cd636a82bb7d97
troysmith31 <50779339+troysmith31@users.noreply.github.com>
2019-06-16 12:12:26 -0230

    Update 3.4.0.sql
    Update Newfoundland and Labrador Provincial code from NF to NL on 3.4.0 update.

--- src/mysql/upgrade/3.4.0.sql
+++ src/mysql/upgrade/3.4.0.sql
@@ -3,3 +3,9 @@
     Issue #4794 refers
 */
 DELETE FROM query_qry WHERE qry_ID=1;
+
+/*
+    Newfoundland and Labrador Provincial code changed from NF to NL
+*/
+update family_fam set fam_State = "NL" where fam_State = "NF";
+update person_per set per_State = "NL" where per_State = "NF";
