b48f74717b83ae9ca4207ce0a7a6c6997c7d64b7
Charles Crossan <crossan007@gmail.com>
2019-08-17 19:01:44 -0400

    use values from Config.php to reset database in NPM command

--- scripts/restore-demo-db.php
+++ scripts/restore-demo-db.php
@@ -2,9 +2,49 @@
 
 include "src\ChurchCRM\SQLUtils.php";
 Use ChurchCRM\SQLUtils;
-$mysqli = new mysqli("127.0.0.1", "churchcrm", "churchcrm", "churchcrm");
+
+$sSERVERNAME = "";
+$sUSER = "";
+$sPASSWORD = "";
+$sDATABASE = "";
+
+function extract_config_values($value){
+
+  global $sSERVERNAME,$sUSER,$sPASSWORD,$sDATABASE;
+
+  if (preg_match('/\\$sSERVERNAME\\s+=\\s+[\'"](.*?)[\'"];/',$value,$matches)) {
+        $sSERVERNAME = $matches[1];
+  }
+
+  if (preg_match('/\\$sUSER\\s+=\\s+[\'"](.*?)[\'"];/',$value,$matches)) {
+    print_r($matches);
+    $sUSER = $matches[1];
+  }
+
+  if (preg_match('/\\$sPASSWORD\\s+=\\s+[\'"](.*?)[\'"];/',$value,$matches)) {
+    print_r($matches);
+    $sPASSWORD = $matches[1];
+  }
+
+  if (preg_match('/\\$sDATABASE\\s+=\\s+[\'"](.*?)[\'"];/',$value,$matches)) {
+    print_r($matches);
+    $sDATABASE = $matches[1];
+  }
+}
+
+$config = explode("\n",file_get_contents ("src\Include\Config.php"));
+array_map("extract_config_values",$config);
+
+echo "$sSERVERNAME\n";
+echo "$sUSER\n";
+echo "$sPASSWORD\n";
+echo "$sDATABASE\n";
+
+$mysqli = new mysqli($sSERVERNAME, $sUSER, $sPASSWORD, $sDATABASE);
+$mysqli->select_db($sDATABASE);
 echo "Connected to database\n";
 echo "Deleting all tables\n";
+
 if ($result = $mysqli->query("SHOW TABLES"))
 {
     while($row = $result->fetch_array(MYSQLI_NUM))
