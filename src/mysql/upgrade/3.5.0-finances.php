94d41975ac657fbc316c0e310d022ae45954017c
Charles Crossan <crossan007@gmail.com>
2019-08-17 14:32:27 -0400

    adjust upgrade script to work with new schema

--- src/mysql/upgrade/3.5.0-finances.php
+++ src/mysql/upgrade/3.5.0-finances.php
@@ -1,5 +1,7 @@
 <?php
 
+include "../../vendor/autoload.php";
+
 use ChurchCRM\dto\SystemConfig;
 use ChurchCRM\Utils\RedirectUtils;
 use ChurchCRM\Base\FamilyQuery;
@@ -9,60 +11,64 @@ use Propel\Runtime\Propel;
 use Propel\Runtime\Exception\PropelException;
 use ChurchCRM\Base\Family as BaseFamily;
 use ChurchCRM\Base\PersonQuery;
-use ChurchCRM\Contrib;
-use ChurchCRM\ContribSplit;
+use ChurchCRM\DonationFundQuery;
+use ChurchCRM\Transaction;
+use ChurchCRM\TransactionSplit;
+use ChurchCRM\Account;
+use ChurchCRM\AccountQuery;
 
 // // create typeofmbr table
-$sSQL = "CREATE TABLE IF NOT EXISTS `typeofmbr` (
-    `typeid` tinyint(3) NOT NULL,
-    `Name` tinytext NOT NULL,
-    PRIMARY KEY (`typeid`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+$sSQL = "CREATE TABLE `account_acct` (
+    `acct_id` SMALLINT(9) NOT NULL AUTO_INCREMENT,
+    `acct_name` TEXT NOT NULL,
+    `acct_person_id` SMALLINT NOT NULL,
+    `acct_family_id` SMALLINT NOT NULL,
+    PRIMARY KEY (`acct_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
   
- $bval = RunQuery($sSQL);
-
- //echo "<script>alert('Create typeofmbr " . $bval . "');</script>";
-
- // 
-$sSQL = "IF NOT EXISTS (SELECT * FROM typeofmbr LIMIT 1)
-        THEN
-            INSERT INTO `typeofmbr` (`typeid`, `Name`) VALUES (1, 'Business'), (2, 'Family'), (3, 'Person');
-        END IF";
-
 $bval = RunQuery($sSQL);
 
 // create table to hold contributions
-$sSQL = "CREATE TABLE IF NOT EXISTS `contrib_con` (
-  `con_ID` mediumint(9) unsigned NOT NULL AUTO_INCREMENT,
-  `con_ContribID` mediumint(9) unsigned NOT NULL,
-  `con_TypeOfMbr` enum('0','1','2','3') COLLATE utf8_unicode_ci DEFAULT NULL,
-  `con_DepID` mediumint(9) unsigned DEFAULT NULL,
-  `con_Date` date DEFAULT NULL,
-  -- `con_Amount` decimal(8,2) unsigned NOT NULL,
-  `con_Method` enum('CREDITCARD','CHECK','CASH','BANKDRAFT','EGIVE'),
-  `con_CheckNo` bigint(16) unsigned,
-  `con_Comment` text COLLATE utf8_unicode_ci,
-  `con_DateEntered` datetime DEFAULT NULL,
-  `con_EnteredBy` mediumint(9) unsigned DEFAULT NULL,
-  `con_DateLastEdited` datetime DEFAULT NULL,
-  `con_EditedBy` mediumint(9) unsigned DEFAULT NULL,
-  PRIMARY KEY (`con_ID`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
+$sSQL = "CREATE TABLE `transaction_tran` (
+    `tran_ID` SMALLINT(9) NOT NULL AUTO_INCREMENT,
+    `tran_DepID` SMALLINT(9),
+    `tran_method` enum('CREDITCARD','CHECK','CASH','BANKDRAFT','EGIVE'),
+    `tran_Comment` TEXT,
+    `tran_DateEntered` DATETIME,
+    `tran_EnteredBy` SMALLINT(9) DEFAULT 0,
+    `tran_DateLastEdited` DATETIME,
+    `tran_EditedBy` SMALLINT(9) DEFAULT 0,
+    `tran_CheckNo` bigint(16) unsigned,
+    PRIMARY KEY (`tran_ID`)    
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 
  $bval = RunQuery($sSQL);
 
 // create table to hold splits
-$sSQL = "CREATE TABLE IF NOT EXISTS `contrib_split` (
-  `spl_ID` mediumint(9) unsigned NOT NULL AUTO_INCREMENT,
-  `spl_ConID` mediumint(9) unsigned NOT NULL,
-  `spl_FundID` tinyint(3) unsigned NOT NULL,
-  `spl_Amount` decimal(8,2) unsigned NOT NULL,
-  `spl_Comment` text COLLATE utf8_unicode_ci,
-  `spl_NonDeductible` tinyint(1) unsigned NOT NULL DEFAULT 0,
-  PRIMARY KEY (`spl_ID`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8";
+$sSQL = "CREATE TABLE `tran_split`
+(
+    `tran_split_ID` SMALLINT(9) NOT NULL AUTO_INCREMENT,
+    `tran_split_trans_ID` SMALLINT(9) NOT NULL,
+    `tran_split_Acct_ID` SMALLINT(9),
+    `tran_split_Fund_ID` TINYINT(3) NOT NULL,
+    `tran_split_Amount` DECIMAL(8,2),
+    `tran_split_Comment` TEXT,
+    `tran_split_NonDeductible` DECIMAL(8,2) NOT NULL,
+    PRIMARY KEY (`tran_split_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
   
  $bval = RunQuery($sSQL);
+
+
+// create accounts for funds
+
+$funds = DonationFundQuery::create()->find();
+
+foreach ($funds as $fund) {
+  $account = new Account();
+  $account->setName($fund->getName());
+  $account->save();
+}
 
 // move pledge from family to person
 $plgGroup = PledgeQuery::create()->filterByPledgeOrPayment('Payment')->groupByGroupkey()->find();
@@ -76,52 +82,65 @@ $plgGroup = PledgeQuery::create()->filterByPledgeOrPayment('Payment')->groupByGr
     $PerID = $people->getId();
 
     // create contribution info
-    $contributions = new Contrib();
-    $contributions->setConID($PerID);
-    $contributions->setTypeOfMbr(3);
-    $contributions->setDepId($grp->getDepId());
-    $contributions->setDate($grp->getDate());
-    $contributions->setMethod($grp->getMethod());
-    $contributions->setCheckNo($grp->getCheckNo());
-    // $contributions->setComment();
-    // $contributions->setDateentered();
-    // $contributions->setEnteredby();
-    $contributions->setDatelastedited($grp->getDatelastedited());
-    $contributions->setEditedby($grp->getEditedby());
-    $contributions->save();
-    $ConID = $contributions->getId();
+    $transaction = new Transaction();
+    $transaction->setDepId($grp->getDepId());
+    $transaction->setDateEntered($grp->getDate());
+    $transaction->setMethod($grp->getMethod());
+    $transaction->setCheckNo($grp->getCheckNo());
+    // $transaction->setComment();
+    // $transaction->setDateentered();
+    // $transaction->setEnteredby();
+    $transaction->setDatelastedited($grp->getDatelastedited());
+    $transaction->setEditedby($grp->getEditedby());
+    
+   
 
 
   $pledges = PledgeQuery::create()->filterByGroupkey($grp->getGroupkey())->find();
+  $totalAmount = 0;
   foreach ($pledges as $plg) {
     // create associated splits
     $Amt = $plg->getAmount();
     $DedAmt = $plg->getNondeductible();
+
+    $totalAmount += $Amt;
+    $FundAccount = AccountQuery::create()->findOneByName($plg->getDonationFund()->getName());
     // echo "Amt: $Amt, DedAmt: $DedAmt";
     // add taxable amount
     if ($Amt != 0) {
       // echo "Reg Split Added";
-      $split = new ContribSplit();
-      $split->setConId($ConID);
-      $split->setFundId($plg->getFundid());
+      $split = new TransactionSplit();
+      $split->setAccount($FundAccount);
       $split->setAmount($plg->getAmount());
       $split->setNondeductible(false);
       // $split->setDatelastedited($plg->getDatelastedited());
       // $split->setEditedby($plg->getEditedby());
-      $split->save();
+      $transaction->addTransactionSplit($split);
 
     }
     // add non taxable amount
     if ($DedAmt != 0) {
       // echo "Non-Ded Split Added";
-      $split = new ContribSplit();
-      $split->setConId($ConID);
-      $split->setFundId($plg->getFundid());
+      $split = new TransactionSplit();
+      $split->setAccount($FundAccount);
       $split->setAmount($plg->getNondeductible());
       $split->setNondeductible(true);
       // $split->setDatelastedited($plg->getDatelastedited());
       // $split->setEditedby($plg->getEditedby());
-      $split->save();
+      $transaction->addTransactionSplit($split);
     }
   }
+
+  $PersonAccount = AccountQuery::create()->filterByOwnerPersonId($PerID)->findOneOrCreate();
+  $split = new TransactionSplit();
+  $split->setAccount($PersonAccount);
+  $split->setAmount($totalAmount * -1);
+  $split->setNondeductible(false);
+  // $split->setDatelastedited($plg->getDatelastedited());
+  // $split->setEditedby($plg->getEditedby());
+  $transaction->addTransactionSplit($split);
+
+  $transaction->save();
 }
+
+?>
