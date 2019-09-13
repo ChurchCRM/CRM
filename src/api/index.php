e1955d15065f76fd5272b0c77598fb3b739bd3ce
George Dawoud <george@dawouds.com>
2019-09-13 21:31:31 +0530

    refactor user dir

--- src/api/index.php
+++ src/api/index.php
@@ -69,7 +69,7 @@ require __DIR__ . '/routes/background.php';
 require __DIR__ . '/routes/geocoder.php';
 require __DIR__ . '/routes/kiosks.php';
 require __DIR__ . '/routes/search.php';
-require __DIR__ . '/routes/users.php';
+require __DIR__ . '/routes/users/users-admin.php';
 
 // Run app
 $app->run();
