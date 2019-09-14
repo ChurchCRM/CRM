a5c75f5c433da1e3d5de7bba8d2063e1b8819fd7
George Dawoud <george@dawouds.com>
2019-09-14 09:43:16 +0530

    added new API to get pluge ORM info

--- src/api/routes/finance/finance-payments.php
+++ src/api/routes/finance/finance-payments.php
@@ -1,9 +1,12 @@
 <?php
 
 use ChurchCRM\Slim\Middleware\Request\Auth\FinanceRoleAuthMiddleware;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use ChurchCRM\PledgeQuery;
 
 $app->group('/payments', function () {
-    $this->get('/', function ($request, $response, $args) {
+    $this->get('/', function (Request $request, Response $response, array $args) {
         $this->FinancialService->getPaymentJSON($this->FinancialService->getPayments());
     });
 
@@ -12,7 +15,14 @@ $app->group('/payments', function () {
         echo json_encode(['payment' => $this->FinancialService->submitPledgeOrPayment($payment)]);
     });
 
-    $this->delete('/{groupKey}', function ($request, $response, $args) {
+    $this->get('/family/{familyId:[0-9]+}/list', function (Request $request, Response $response, array $args) {
+        $familyId = $request->getAttribute("route")->getArgument("familyId");
+        $data = PledgeQuery::create()->findByFamId($familyId);
+        return $response->withHeader('Content-Type: application/json')->write($data->exportTo("JSON"));
+
+    });
+
+    $this->delete('/{groupKey}', function (Request $request, Response $response, array $args) {
         $groupKey = $args['groupKey'];
         $this->FinancialService->deletePayment($groupKey);
         echo json_encode(['status' => 'ok']);
