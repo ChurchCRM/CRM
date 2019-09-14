269f829f11c37e07e79edb3a9aa3903265585fd4
George Dawoud <george@dawouds.com>
2019-09-14 10:02:31 +0530

    added API to get payment/pledges by family using user filters

--- src/api/routes/finance/finance-payments.php
+++ src/api/routes/finance/finance-payments.php
@@ -1,9 +1,14 @@
 <?php
 
 use ChurchCRM\Slim\Middleware\Request\Auth\FinanceRoleAuthMiddleware;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use ChurchCRM\PledgeQuery;
+use ChurchCRM\SessionUser;
+use Propel\Runtime\ActiveQuery\Criteria;
 
 $app->group('/payments', function () {
-    $this->get('/', function ($request, $response, $args) {
+    $this->get('/', function (Request $request, Response $response, array $args) {
         $this->FinancialService->getPaymentJSON($this->FinancialService->getPayments());
     });
 
@@ -12,7 +17,24 @@ $app->group('/payments', function () {
         echo json_encode(['payment' => $this->FinancialService->submitPledgeOrPayment($payment)]);
     });
 
-    $this->delete('/{groupKey}', function ($request, $response, $args) {
+    $this->get('/family/{familyId:[0-9]+}/list', function (Request $request, Response $response, array $args) {
+        $familyId = $request->getAttribute("route")->getArgument("familyId");
+        $query = PledgeQuery::create()->filterByFamId($familyId);
+        if (!empty(SessionUser::getUser()->getShowSince())) {
+            $query->filterByDate(SessionUser::getUser()->getShowSince(), Criteria::GREATER_EQUAL);
+        }
+        if (!SessionUser::getUser()->isShowPayments()) {
+            $query->filterByPledgeorpayment("Payment", Criteria::NOT_EQUAL);
+        }
+        if (!SessionUser::getUser()->isShowPledges()) {
+            $query->filterByPledgeorpayment("Pledge", Criteria::NOT_EQUAL);
+        }
+        $data = $query->find();
+        return $response->withHeader('Content-Type: application/json')->write($data->exportTo("JSON"));
+
+    });
+
+    $this->delete('/{groupKey}', function (Request $request, Response $response, array $args) {
         $groupKey = $args['groupKey'];
         $this->FinancialService->deletePayment($groupKey);
         echo json_encode(['status' => 'ok']);
