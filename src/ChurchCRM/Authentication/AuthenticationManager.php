27443bd7133de6f3b1362abfbac118e768ade3ac
Charles Crossan <crossan007@gmail.com>
2019-09-29 20:49:37 -0400

    add logging to auth manager

--- src/ChurchCRM/Authentication/AuthenticationManager.php
+++ src/ChurchCRM/Authentication/AuthenticationManager.php
@@ -5,17 +5,23 @@ namespace ChurchCRM\Authentication {
   use ChurchCRM\Authentication\AuthenticationProviders\LocalAuthentication;
   use ChurchCRM\Utils\RedirectUtils;
   use ChurchCRM\Service\NotificationService;
+  use ChurchCRM\Utils\LoggerUtils;
+  use ChurchCRM\dto\SystemURLs;
 
 class AuthenticationManager
   {
 
+    // This class exists to abstract the implementations of various authentication providers
+    // Currently, only local auth is implemented; hence the zero-indexed array elements.
+
     private static $authenticationProviders;
 
     private static $correlationId;
+
     public static function EndSession() {
       self::initializeAuthentication();
       $result =self::$authenticationProviders[0]->EndSession();
-      RedirectUtils::Redirect("/session/begin");
+      RedirectUtils::Redirect(self::GetSessionBeginURL());
     }
 
     private static function initializeAuthentication() {
@@ -30,24 +36,37 @@ class AuthenticationManager
       self::initializeAuthentication();
       $result =self::$authenticationProviders[0]->Authenticate($AuthenticationRequest);
       if (null !== $result->nextStepURL){
+        LoggerUtils::getAuthLogger()->addDebug("Authentication requires additional step: " . $result->nextStepURL);
         RedirectUtils::Redirect($result->nextStepURL);
       }
-      NotificationService::updateNotifications();
+      
+      if ($result->isAuthenticated) {
+        $redirectLocation = array_key_exists("location", $_SESSION) ? $_SESSION['location'] : 'Menu.php';
+        NotificationService::updateNotifications();
+        LoggerUtils::getAuthLogger()->addDebug("Authentication Successful; redirecting to: " . $redirectLocation);
+        RedirectUtils::Redirect($redirectLocation);
+       
+      }
+      return $result;
     }
-    
+
     public static function GetCorrelationId() {
       self::initializeAuthentication();
       return self::$correlationId;
     }
+    
     public static function GetAuthenticationStatus()
     {
       self::initializeAuthentication();
       $result = self::$authenticationProviders[0]->GetAuthenticationStatus();
-      $result = self::$authenticationProviders[0]->isAuthenticated();
-      if (null !== $result->nextStepURL){
+      LoggerUtils::getAuthLogger()->addDebug("Session authentication status");
       return $result;
     }
 
+    public static function GetCurrentAuthenticationProvider() {
+      self::initializeAuthentication();
+      return self::$authenticationProviders[0];
+    }
 
     public static function EnsureAuthentication() {
       // This function differs from the sematinc `GetAuthenticationStatus` in that it will
@@ -59,10 +78,13 @@ class AuthenticationManager
       // Sometimes other actions may require a `nextStepURL` that should be enforced with 
       // an autentication request (2FA, Expired Password, etc).
       if (!$result->isAuthenticated){
+        LoggerUtils::getAuthLogger()->addDebug("Session not authenticated.  Redirecting to login page");
         RedirectUtils::Redirect(self::GetSessionBeginURL());
       }elseif(null !== $result->nextStepURL){
+        LoggerUtils::getAuthLogger()->addDebug("Session authenticated, but redirect requested by authentication provider.");
         RedirectUtils::Redirect($result->nextStepURL);
-      } 
+      }
+      LoggerUtils::getAuthLogger()->addDebug("Session valid");
     }
 
     public static function GetSessionBeginURL() {
