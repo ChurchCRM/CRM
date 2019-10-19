227cadc4bf43fb778af9cd09c73c8f68ec68ba05
George Dawoud <george@dawouds.com>
2019-10-19 01:08:04 -0700

    moved to jquery steps for the family registraion

--- src/external/routes/register.php
+++ src/external/routes/register.php
@@ -14,30 +14,9 @@ $app->group('/register', function () {
     if ($enableSelfReg) {
         $this->get('/', function ($request, $response, $args) {
             $renderer = new PhpRenderer('templates/registration/');
-
-            return $renderer->render($response, 'family-register.php', ['sRootPath' => SystemURLs::getRootPath()]);
-        });
-
-        $this->post('/', function ($request, $response, $args) {
-            $renderer = new PhpRenderer('templates/registration/');
-
-            $body = $request->getParsedBody();
-
-            $family = [];
-            $family["Name"] = $body['familyName'];
-            $family["Address1"] = $body['familyAddress1'];
-            $family["City"] = $body['familyCity'];
-            $family["State"] = $body['familyState'];
-            $family["Country"] = $body['familyCountry'];
-            $family["Zip"] = $body['familyZip'];
-            $family["HomePhone"] = $body['familyHomePhone'];
-
             $familyRoles = ListOptionQuery::create()->filterById(2)->orderByOptionSequence()->find();
 
-            $pageObjects = ['sRootPath' => SystemURLs::getRootPath(), 'family' => $family, 'familyCount' => $body['familyCount'], 'familyRoles' => $familyRoles];
-
-            return $renderer->render($response, 'family-register-members.php', $pageObjects);
+            return $renderer->render($response, 'family-register.php', ['sRootPath' => SystemURLs::getRootPath(), 'familyRoles' => $familyRoles]);
         });
-
     }
 });
