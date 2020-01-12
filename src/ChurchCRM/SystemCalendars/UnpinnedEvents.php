dd04ad264b918f5b60b51110b21f9c2c99721f56
Charles Crossan <crossan007@gmail.com>
2020-01-12 10:58:13 -0500

    Create a SystemCalendar to display events which are not pinned to any calendar.
    Modify client-side logic to provie a mechanism by which to edit eligible SystemCalendar events (i.e. the new Unpinned Events)
    
    This is a mitigation for #4830 in which events created by the legacy events creation form are not pinned to the calendar, and are therefor invisible.
    
    The old editor is deplrecated, and will be removed in a future release, so I'm not adding the "pin" capability to the old editor.
    
    Closes #4830

---
+++ src/ChurchCRM/SystemCalendars/UnpinnedEvents.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ChurchCRM\SystemCalendars;
+
+use ChurchCRM\Interfaces\SystemCalendar;
+use ChurchCRM\PersonQuery;
+use ChurchCRM\EventQuery;
+use Propel\Runtime\ActiveQuery\Criteria;
+
+class UnpinnedEvents implements SystemCalendar {
+ 
+  public static function isAvailable() {
+    return true;
+  }
+  
+  public function getAccessToken() {
+    return false;
+  }
+
+  public function getBackgroundColor() {
+    return "FF0000";
+  }
+  
+  public function getForegroundColor() {
+    return "FFFFFF";
+  }
+
+  public function getId() {
+    return 3;
+  }
+
+  public function getName() {
+    return gettext("Unpinned Events");
+  }
+    
+  public function getEvents($start,$end) {
+    $Events = EventQuery::create()
+        ->filterByStart(array("min" => $start))
+        ->filterByEnd(array("max" => $end))
+        ->useCalendarEventQuery(null,\Propel\Runtime\ActiveQuery\Criteria::LEFT_JOIN)
+          ->filterByCalendarId(null)
+        ->endUse()
+        ->find();
+    return $Events;
+  }
+  
+  public function getEventById($Id) {
+    $people = PersonQuery::create()
+            ->filterByBirthDay('', Criteria::NOT_EQUAL)
+            ->filterById($Id)
+            ->find();
+    return $this->peopleCollectionToEvents($people);  
+  }
+  
+}
