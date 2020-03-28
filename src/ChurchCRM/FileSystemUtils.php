99d3dd8c1a0c0b76a28a23faeeab488537cc0e98
George Dawoud <george@dawouds.com>
2020-03-28 01:50:35 +0000

    Apply fixes from StyleCI

--- src/ChurchCRM/FileSystemUtils.php
+++ src/ChurchCRM/FileSystemUtils.php
@@ -73,10 +73,11 @@ namespace ChurchCRM
             }
         }
 
-        static function human_filesize($bytes, $decimals = 2) {
+        public static function human_filesize($bytes, $decimals = 2)
+        {
             $size = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
             $factor = floor((strlen($bytes) - 1) / 3);
             return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
-          }
+        }
     }
 }
