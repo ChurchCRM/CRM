b8febe0d541b178a7f7ce3861d2959a6a071b2df
Charles Crossan <crossan007@gmail.com>
2020-03-27 22:11:45 -0400

    add family association schema

--- src/mysql/install/Install.sql
+++ src/mysql/install/Install.sql
@@ -1282,15 +1282,20 @@ DROP TABLE IF EXISTS `file_associations`;
 CREATE TABLE `file_associations`
 (
     `file_id` mediumint(8) unsigned NOT NULL,
-    `person_id` mediumint(8) unsigned NOT NULL,
-    PRIMARY KEY (`file_id`,`person_id`),
+    `person_id` mediumint(8) unsigned,
+    `family_id` mediumint(8) unsigned,
+    PRIMARY KEY (`file_id`),
     INDEX `file_associations_fi_bd0b42` (`person_id`),
+    INDEX `file_associations_fi_15a2d6` (`family_id`),
     CONSTRAINT `file_associations_fk_f03653`
         FOREIGN KEY (`file_id`)
         REFERENCES `files` (`file_id`),
     CONSTRAINT `file_associations_fk_bd0b42`
         FOREIGN KEY (`person_id`)
-        REFERENCES `person_per` (`per_ID`)
+        REFERENCES `person_per` (`per_ID`),
+    CONSTRAINT `file_associations_fk_15a2d6`
+        FOREIGN KEY (`family_id`)
+        REFERENCES `family_fam` (`fam_ID`)
 ) ENGINE=InnoDB COMMENT='This is a join-table to link files with other types';
 
 update version_ver set ver_update_end = now();
