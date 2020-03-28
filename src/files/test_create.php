ec58a75e1801ac31321f73aeea97d86fe796260d
Charles Crossan <crossan007@gmail.com>
2020-03-27 21:49:51 -0400

    update test_create script

--- src/files/test_create.php
+++ src/files/test_create.php
@@ -7,12 +7,21 @@ require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 
 use ChurchCRM\File as File;
+use ChurchCRM\FileAssociation;
+use ChurchCRM\PersonQuery;
 use ChurchCRM\Utils\MiscUtils;
 
 $file = new File();
 
 $filename = MiscUtils::random_word();
 $contents = MiscUtils::random_word().MiscUtils::random_word().MiscUtils::random_word();
-$file->setFileName($filename);
+$file->setFileName($filename.".txt");
 $file->setContent($contents);
 $file->save();
+
+$Person = PersonQuery::create()->findOneById(1);
+
+$fileAssociation = new FileAssociation();
+$fileAssociation->setFile($file);
+$fileAssociation->setPerson($Person);
+$fileAssociation->save();
