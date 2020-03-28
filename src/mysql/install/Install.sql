d05b2d0cbd0349ac12cc5bc78fa6f8301a737d30
Charles Crossan <crossan007@gmail.com>
2020-03-27 21:16:22 -0400

    add file associations

--- src/mysql/install/Install.sql
+++ src/mysql/install/Install.sql
@@ -1272,8 +1272,24 @@ CREATE TABLE `files`
     PRIMARY KEY (`file_id`)
 ) ENGINE=InnoDB COMMENT='Table containing metadata for ChurchCRM File attachments';
 
-# This restores the fkey checks, after having unset them earlier
-SET FOREIGN_KEY_CHECKS = 1;
+-- ---------------------------------------------------------------------
+-- file_associations
+-- ---------------------------------------------------------------------
 
+DROP TABLE IF EXISTS `file_associations`;
+
+CREATE TABLE `file_associations`
+(
+    `file_id` mediumint(8) unsigned NOT NULL,
+    `person_id` mediumint(8) unsigned NOT NULL,
+    PRIMARY KEY (`file_id`,`person_id`),
+    INDEX `file_associations_fi_bd0b42` (`person_id`),
+    CONSTRAINT `file_associations_fk_f03653`
+        FOREIGN KEY (`file_id`)
+        REFERENCES `files` (`file_id`),
+    CONSTRAINT `file_associations_fk_bd0b42`
+        FOREIGN KEY (`person_id`)
+        REFERENCES `person_per` (`per_ID`)
+) ENGINE=InnoDB COMMENT='This is a join-table to link files with other types';
 
 update version_ver set ver_update_end = now();
