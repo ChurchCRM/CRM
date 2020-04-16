b50ab033f4590a9175600f397053cf90d6a7e272
Charles Crossan <crossan007@gmail.com>
2020-04-15 21:10:07 -0400

    fix directory permissions

--- src/ChurchCRM/FileSystemUtils.php
+++ src/ChurchCRM/FileSystemUtils.php
@@ -69,7 +69,7 @@ namespace ChurchCRM
         public static function ensureDir($path)
         {
             if (! is_dir($path)) {
-                mkdir($path, 0600, true);
+                mkdir($path, 0755, true);
             }
         }
 
