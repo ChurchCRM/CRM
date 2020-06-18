7bb40da6014d5c2ef5996a944177f4b2e39ac386
George Dawoud <george@dawouds.com>
2020-06-17 23:28:12 -0700

    fixed auth / js error on use of userId

--- src/v2/templates/user/user.php
+++ src/v2/templates/user/user.php
@@ -36,23 +36,21 @@ include SystemURLs::getDocumentRoot() . '/Include/Header.php';
             </div>
         </div>
     </div>
-    <div class="col-lg-3">
+    <div class="col-lg-4">
         <div class="box">
             <div class="box-header">
                 <h4><?= _("Permissions") ?></h4>
             </div>
             <div class="box-body">
-                <li><b><?= gettext("Admin") ?>:</b> <?= $user->isAdmin() ? _("Yes") : _("No")  ?></li>
-                <li><b><?= gettext("Add Records") ?>:</b> <?= $user->isAddRecords() ? _("Yes") : _("No")  ?></li>
-                <li><b><?= gettext("Edit Records") ?>:</b> <?= $user->isEditRecords() ? _("Yes") : _("No")  ?></li>
-                <li><b><?= gettext("Delete Records") ?>:</b> <?= $user->isDeleteRecords() ? _("Yes") : _("No")  ?></li>
-                <li><b><?= gettext("Manage Properties and Classifications") ?>:</b> <?= $user->isMenuOptions() ? _("Yes") : _("No")  ?></li>
-                <li><b><?= gettext("Manage Groups and Roles") ?>:</b> <?= $user->isManageGroups() ? _("Yes") : _("No")  ?></li>
-                <li><b><?= gettext("Manage Donations and Finance") ?>:</b> <?= $user->isFinance() ? _("Yes") : _("No")  ?></li>
-                <li><b><?= gettext("Manage Notes") ?>:</b> <?= $user->isNotes() ? _("Yes") : _("No")  ?></li>
-                <li><b><?= gettext("Canvasser") ?>:</b> <?= $user->isCanvasser() ? _("Yes") : _("No")  ?></li>
-
-                <!--<li><b><?/*= gettext("Self Edit") */?>:</b> <?/*= $user->isDeleteRecords() ? _("Yes") : _("No")  */?></li>-->
+                <li><b><?= gettext("Admin") ?>:</b> <?= $user->isAdmin() ? _("Yes") : _("No") ?></li>
+                <li><b><?= gettext("Add Records") ?>:</b> <?= $user->isAddRecords() ? _("Yes") : _("No") ?></li>
+                <li><b><?= gettext("Edit Records") ?>:</b> <?= $user->isEditRecords() ? _("Yes") : _("No") ?></li>
+                <li><b><?= gettext("Delete Records") ?>:</b> <?= $user->isDeleteRecords() ? _("Yes") : _("No") ?></li>
+                <li><b><?= gettext("Manage Properties and Classifications") ?>:</b> <?= $user->isMenuOptions() ? _("Yes") : _("No") ?></li>
+                <li><b><?= gettext("Manage Groups and Roles") ?>:</b> <?= $user->isManageGroups() ? _("Yes") : _("No") ?></li>
+                <li><b><?= gettext("Manage Donations and Finance") ?>:</b> <?= $user->isFinance() ? _("Yes") : _("No") ?></li>
+                <li><b><?= gettext("Manage Notes") ?>:</b> <?= $user->isNotes() ? _("Yes") : _("No") ?></li>
+                <li><b><?= gettext("Canvasser") ?>:</b> <?= $user->isCanvasser() ? _("Yes") : _("No") ?></li>
             </div>
         </div>
     </div>
@@ -118,7 +116,7 @@ include SystemURLs::getDocumentRoot() . '/Include/Header.php';
                         <p class="text-center no-margin">Green</p>
                     </li>
                     <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-red" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)"  class="clearfix full-opacity-hover">
+                        <a href="javascript:void(0)" data-skin="skin-red" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
                             <div>
                                 <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-red-active"></span>
                                 <span class="bg-red" style="display:block; width: 80%; float: left; height: 7px;"></span>
@@ -223,8 +221,9 @@ include SystemURLs::getDocumentRoot() . '/Include/Header.php';
         </div>
     </div>
 </div>
-</div>
-
+<script>
+    window.CRM.viewUserId = <?= $user->getId() ?>;
+</script>
 <script src="<?= SystemURLs::getRootPath() ?>/skin/js/user.js"></script>
 
 <?php include SystemURLs::getDocumentRoot() . '/Include/Footer.php'; ?>
