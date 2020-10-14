30a80acfc8cfb1b99693ea6f44730f6fe27532a4
George Dawoud <george@dawouds.com>
2020-10-13 22:15:50 -0700

    added Amin/Standard login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,13 +10,21 @@
 
 
 // -- This is a login command --
- Cypress.Commands.add("login", (username, password, assertLoggedIn = true) => {
-     cy.visit("/");
-     cy.get("#UserBox").type(username);
-     cy.get("#PasswordBox").type(password);
-     cy.get("form").submit();
-     if (assertLoggedIn) {
-         cy.location('pathname').should('include', "/Menu.php");
-     }
- })
+Cypress.Commands.add("loginAdmin", (username, password, assertLoggedIn = true) => {
+    cy.login("admin", "changeme");
+});
+
+Cypress.Commands.add("loginStandard", (username, password, assertLoggedIn = true) => {
+    cy.login("tony.wade@example.com", "abc123");
+});
+
+Cypress.Commands.add("login", (username, password, assertLoggedIn = true) => {
+    cy.visit("/");
+    cy.get("#UserBox").type(username);
+    cy.get("#PasswordBox").type(password);
+    cy.get("form").submit();
+    if (assertLoggedIn) {
+        cy.location('pathname').should('include', "/Menu.php");
+    }
+});
 
