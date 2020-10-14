ccaff77c77b2d4c2ac5b4f137627586efca25d10
George Dawoud <george@dawouds.com>
2020-10-13 19:38:16 -0700

    added login command

---
+++ cypress/support/commands.js
@@ -0,0 +1,22 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+
+// -- This is a login command --
+ Cypress.Commands.add("login", (username, password, assertLoggedIn = true) => {
+     cy.visit("/");
+     cy.get("#UserBox").type(username);
+     cy.get("#PasswordBox").type(password);
+     cy.get("form").submit();
+     if (assertLoggedIn) {
+         cy.location('pathname').should('eq', Cypress.env('base_path') + "/Menu.php");
+     }
+ })
+
