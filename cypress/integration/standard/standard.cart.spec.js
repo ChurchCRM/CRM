bfe1a314176299bf4fc485c0e91e879a546394a1
George Dawoud <george@dawouds.com>
2020-10-14 08:18:17 -0700

    moved to Cypress from Behat

---
+++ cypress/integration/standard/standard.cart.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+context('Cart', () => {
+
+    it('Cart Add and Remove', () => {
+        cy.loginStandard();
+        cy.visit("/v2/cart");
+        cy.contains('You have no items in your cart');
+        cy.visit("/PersonView.php?PersonID=1")
+        cy.get("#AddPersonToCart").click();
+        cy.visit("/v2/cart");
+        cy.contains('Cart Functions');
+        cy.contains('Church Admin');
+        cy.get("#emptyCart").click();
+        cy.contains('You have no items in your cart');
+    });
+
+});
