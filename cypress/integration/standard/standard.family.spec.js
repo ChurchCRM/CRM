cd5cf4725130b31b9b709261752c8c97684175ea
George Dawoud <george@dawouds.com>
2020-10-14 09:55:27 -0700

    fixed inactive family tests

--- cypress/integration/standard/standard.family.spec.js
+++ cypress/integration/standard/standard.family.spec.js
@@ -18,12 +18,14 @@ context('Family', () => {
         cy.contains('This Family is Deactivated').should('not.be.visible');
         cy.get("#activateDeactivate").click();
         cy.get("body > div.bootbox.modal.fade.bootbox-confirm.in > div > div > div.modal-footer > button.btn.btn-primary.bootbox-accept").click();
-        cy.wait(300);
+        cy.wait(2000);
         cy.visit("v2/family?mode=inactive");
         cy.contains('Lewis');
         cy.visit("v2/family/3");
         cy.contains('This Family is Deactivated').should('be.visible');
         cy.get("#activateDeactivate").click();
+        cy.get("body > div.bootbox.modal.fade.bootbox-confirm.in > div > div > div.modal-footer > button.btn.btn-primary.bootbox-accept").click();
+        cy.wait(2000);
         cy.visit("v2/family?mode=inactive");
         cy.contains('Lewis').should('not.exist');
     });
