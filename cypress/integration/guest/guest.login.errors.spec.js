bc22e03513bea7243a8eac72b34a3d257d6c4ab9
George Dawoud <george@dawouds.com>
2020-11-01 17:10:29 -0800

    matching new login api

--- cypress/integration/guest/guest.login.errors.spec.js
+++ cypress/integration/guest/guest.login.errors.spec.js
@@ -3,12 +3,12 @@
 context('Login', () => {
 
     it('Bad password', () => {
-        cy.login("admin", "badpassword", false);
+        cy.login("admin", "badpassword");
         cy.location('pathname').should('include', "session/begin");
     });
 
     it('Bad username', () => {
-        cy.login("idonknowyou", "badpassword", false);
+        cy.login("idonknowyou", "badpassword");
         cy.location('pathname').should('include', "session/begin");
     });
 
