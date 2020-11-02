1516defc160cd030cb549e70bdbdcfc0c5ca3eb9
George Dawoud <george@dawouds.com>
2020-11-01 17:39:39 -0800

    skipping dashboard page

--- cypress/integration/standard/standard.sundayschool.spec.js
+++ cypress/integration/standard/standard.sundayschool.spec.js
@@ -1,10 +1,9 @@
 /// <reference types="cypress" />
 
-context('Sunday School', () => {
+context('Standard Sunday School', () => {
 
     it('View Sunday School dashboard', () => {
-        cy.loginStandard();
-        cy.visit("sundayschool/SundaySchoolDashboard.php");
+        cy.loginStandard("sundayschool/SundaySchoolDashboard.php");
         cy.contains('Sunday School Dashboard');
         cy.contains('Sunday School Classes');
         cy.contains('Students not in a Sunday School Class');
