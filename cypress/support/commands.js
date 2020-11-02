7cd97f44b1c735bd4d19965d701f29a5c0c9f874
George Dawoud <george@dawouds.com>
2020-11-01 17:06:39 -0800

    support rediect to the needed page

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,22 +10,22 @@
 
 
 // -- This is a login command --
-Cypress.Commands.add("loginAdmin", (username, password, assertLoggedIn = true) => {
-    cy.login("admin", "changeme");
+Cypress.Commands.add("loginAdmin", (location) => {
+    cy.login("admin", "changeme", location);
 });
 
-Cypress.Commands.add("loginStandard", (username, password, assertLoggedIn = true) => {
-    cy.login("tony.wade@example.com", "abc123");
+Cypress.Commands.add("loginStandard", (location) => {
+    cy.login("tony.wade@example.com", "abc123", location);
 });
 
-Cypress.Commands.add("login", (username, password, assertLoggedIn = true) => {
-    cy.visit("/");
+Cypress.Commands.add("login", (username, password, location) => {
+    cy.visit("/?location=/" + location);
     cy.wait(150);
     cy.get("#UserBox").type(username);
     cy.get("#PasswordBox").type(password);
     cy.get("form").submit();
-    if (assertLoggedIn) {
-        cy.location('pathname').should('include', "/v2/dashboard");
+    if (location) {
+        cy.location('pathname').should('include', location);
     }
 });
 
