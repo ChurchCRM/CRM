7d0e11ce6c28180c04751325ad57ba61a0612cce
George Dawoud <george@dawouds.com>
2020-11-21 10:35:44 -0800

    split split commands into UI and APIs

---
+++ cypress/support/api-commands.js
@@ -0,0 +1,23 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+
+// -- This is a login command --
+Cypress.Commands.add("makePrivateAPICall", (method, url, body, expectedStatus = 200) => {
+    cy.request({
+        method: method,
+        url: url,
+        headers: {'content-type': 'application/json', "x-api-key": Cypress.env('admin.api.key')},
+        body: body
+    }).then((resp) => {
+        expect(resp.status).to.eq(expectedStatus);
+        return JSON.parse(JSON.stringify(resp.body));
+    })
+});
