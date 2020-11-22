a2046b5ab2f1ac299d855f9e084700974749a24d
George Dawoud <george@dawouds.com>
2020-11-22 12:23:56 -0800

    new model for UserSettings

---
+++ src/ChurchCRM/model/ChurchCRM/UserSetting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ChurchCRM;
+
+use ChurchCRM\Base\UserSetting as BaseUserSetting;
+
+/**
+ * Skeleton subclass for representing a row from the 'user_settings' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class UserSetting extends BaseUserSetting
+{
+
+}
