b0d700386d24fd838cc326142d02e8a8ce38629d
George Dawoud <george@dawouds.com>
2020-11-23 11:29:10 -0800

    No longer doing finance as a bulk update

--- src/api/routes/users/user-settings.php
+++ src/api/routes/users/user-settings.php
@@ -9,7 +9,6 @@ use ChurchCRM\Slim\Middleware\Request\UserAPIMiddleware;
 $app->group('/user/{userId:[0-9]+}/setting', function () {
     $this->get("/{settingName}", "getUserSetting");
     $this->post("/{settingName}", "updateUserSetting");
-    $this->post("/finance", "updateSessionFinance");
 })->add(new UserAPIMiddleware());
 
 function getUserSetting(Request $request, Response $response, array $args)
@@ -32,35 +31,4 @@ function updateUserSetting(Request $request, Response $response, array $args)
     $input = (object)$request->getParsedBody();
     $user->setSetting($settingName, $input->value);
     return $response->withJson(["value" => $user->getSetting($settingName)->getValue()]);
-}
-
-function updateSessionFinance(Request $request, Response $response, array $args)
-{
-    $user = $request->getAttribute("user");
-
-   if ($request->getContentLength() > 0) {
-        $setting = (object)$request->getParsedBody();
-
-        $user->setSetting( "finance.show.pledges", $setting->pledges);
-        $user->setSetting( "finance.show.payments", $setting->payments);
-        $user->setSetting( "finance.show.since", $setting->since);
-    }
-
-   $tempSetting = $user->getSetting("finance.show.pledges");
-   $pledges = $tempSetting? $tempSetting->getValue() : "";
-
-   $tempSetting = $user->getSetting("finance.show.payments");
-    $payments = $tempSetting? $tempSetting->getValue() : "";
-
-    $tempSetting = $user->getSetting("finance.show.since");
-    $since = $tempSetting? $tempSetting->getValue() : "";
-
-    return $response->withJson([
-        "user" => $user->getName(),
-        "userId" => $user->getId(),
-        "showPledges" => $pledges,
-        "showPayments" => $payments,
-        "showSince" => $since
-    ]);
-
 }
