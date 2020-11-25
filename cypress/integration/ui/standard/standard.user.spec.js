9da74b669a93931d8065876bea3baade1a68d1d4
George Dawoud <george@dawouds.com>
2020-11-25 13:51:25 -0800

    added logout test case & username prefil

---
+++ cypress/integration/ui/standard/standard.user.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+context('Standard User Session', () => {
+
+    it('Login and Logout', () => {
+        cy.loginStandard('v2/dashboard');
+        cy.visit('/session/end');
+    });
+
+    it('UserName prefilled', () => {
+        cy.loginStandard('session/begin?username=test@user.com');
+        cy.get('input[id="UserBox"]').should('have.value', "test@user.com");
+    });
+
+});
