c1980d74f0aa18ee1524ff63ea43227d94b6f519
George Dawoud <george@dawouds.com>
2021-04-25 14:19:00 -0700

    updated the order incase we are too fast

--- cypress/integration/ui/finance/finance.family.spec.js
+++ cypress/integration/ui/finance/finance.family.spec.js
@@ -4,15 +4,16 @@ context('Finance Family', () => {
 
     it('View a Family', () => {
         cy.loginAdmin("v2/family/1");
+        cy.intercept({ method: "GET", url: "/api/payments/family/1/**"}).as("getFamilyPayments1");
         cy.contains('Campbell - Family');
         cy.contains('Darren Campbell');
-        cy.intercept({ method: "GET", url: "/api/payments/family/1/**"}).as("getFamilyPayments1");
+
         cy.wait("@getFamilyPayments1");
         cy.contains('Music Ministry');
 
         cy.visit("v2/family/20");
-        cy.contains('Black - Family');
         cy.intercept({ method: "GET", url: "/api/payments/family/20/**"}).as("getFamilyPayments20");
+        cy.contains('Black - Family');
         cy.wait("@getFamilyPayments20");
         cy.contains('New Building Fund');
     });
