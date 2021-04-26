c4e24dfb90de64d715622cadf8d0f39f2b7a5e5f
George Dawoud <george@dawouds.com>
2021-04-25 19:02:24 -0700

    trying to fix the flaky tests

--- cypress/integration/ui/finance/finance.family.spec.js
+++ cypress/integration/ui/finance/finance.family.spec.js
@@ -3,8 +3,10 @@
 context('Finance Family', () => {
 
     it('View a Family', () => {
-        cy.loginAdmin("v2/family/1");
         cy.intercept({ method: "GET", url: "/api/payments/family/1/**"}).as("getFamilyPayments1");
+        cy.intercept({ method: "GET", url: "/api/payments/family/20/**"}).as("getFamilyPayments20");
+
+        cy.loginAdmin("v2/family/1");
         cy.contains('Campbell - Family');
         cy.contains('Darren Campbell');
 
@@ -12,7 +14,6 @@ context('Finance Family', () => {
         cy.contains('Music Ministry');
 
         cy.visit("v2/family/20");
-        cy.intercept({ method: "GET", url: "/api/payments/family/20/**"}).as("getFamilyPayments20");
         cy.contains('Black - Family');
         cy.wait("@getFamilyPayments20");
         cy.contains('New Building Fund');
