20d83b705559d2d5cb7c9023c7a59a969ceee6ba
George Dawoud <george@dawouds.com>
2021-05-15 09:48:35 -0700

    added new tables that were commented out for Pre defined reports

---
+++ src/ChurchCRM/model/ChurchCRM/PredefinedReports.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ChurchCRM;
+
+use ChurchCRM\Base\PredefinedReports as BasePredefinedReports;
+
+/**
+ * Skeleton subclass for representing a row from the 'query_qry' table.
+ *
+ * This contains all the predefined queries that appear in the queries page
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class PredefinedReports extends BasePredefinedReports
+{
+
+}
