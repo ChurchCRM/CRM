3364919ee5add20c5e89520e6429e8d862cbc41d
George Dawoud <DawoudIO@users.noreply.github.com>
2021-09-05 16:19:30 +0000

    Apply fixes from StyleCI

--- src/Include/Footer.php
+++ src/Include/Footer.php
@@ -95,14 +95,14 @@ $isAdmin = AuthenticationManager::GetCurrentUser()->isAdmin();
 <script src="<?= SystemURLs::getRootPath() ?>/skin/js/Footer.js"></script>
 
 <?php if (isset($sGlobalMessage)) {
-        ?>
+    ?>
     <script nonce="<?= SystemURLs::getCSPNonce() ?>">
         $("document").ready(function () {
             showGlobalMessage("<?= $sGlobalMessage ?>", "<?=$sGlobalMessageClass?>");
         });
     </script>
     <?php
-    } ?>
+} ?>
 
 <?php  include_once('analyticstracking.php'); ?>
 </body>
