a9a78c4752bbe61d1b9286e339772b07b7ebd581
Craig Hiebert <craig.hiebert@gmail.com>
2022-10-06 00:12:23 -0700

    Add new MakeFirstNameSalutationUtility function

--- src/Include/ReportFunctions.php
+++ src/Include/ReportFunctions.php
@@ -66,3 +66,54 @@ function MakeSalutationUtility($famID)
         return $fam_Name.' Family';
     }
 }
+
+// MakeFirstNameSalutation: this utility figures out how address a family in the salutation of a
+// piece of correspondence (Dear NAME,)
+function MakeFirstNameSalutationUtility(famID)
+{
+    // If there is only one person in the family, return the first name of that person
+    // If there are two persons, return the first names concatenated by ' & '
+    // If there are three or more persons, return the first names concatenated by ', ' except for
+    // the last person, which is preceded by ' & '
+    $sSQL = 'SELECT * FROM family_fam WHERE fam_ID='.$famID;
+    $rsFamInfo = RunQuery($sSQL);
+
+    if (mysqli_num_rows($rsFamInfo) == 0) {
+        return 'Invalid Family'.$famID;
+    }
+
+    $aFam = mysqli_fetch_array($rsFamInfo);
+    extract($aFam);
+
+    $sSQL = 'SELECT * FROM person_per WHERE per_fam_ID='.$famID.' ORDER BY per_fmr_ID';
+    $rsMembers = RunQuery($sSQL);
+    $numMembers = mysqli_num_rows($rsMembers);
+
+    $aChildren = [];
+    $aNotChildren = [];
+    for ($ind = 0; $ind < $numMembers; $ind++) {
+        $member = mysqli_fetch_array($rsMembers);
+        extract($member);
+        if ($per_fmr_ID == 3) {
+            extract($member);
+            $aChildren[] = $per_FirstName;
+        } else {
+            extract($member);
+            $aNotChildren[] = $per_FirstName;
+        }
+    }
+    $allNames = $aNotChildren + $aChildren;
+    if ($numMembers == 1) {
+        return $allNames[0];
+    } elseif ($numMembers == 2) {
+        return $allNames[0].' & '.$allNames[1];
+    } else {
+        $sNames = $allNames[0];
+        for ($ind = 1; $ind < ($numMembers - 1); $ind++) {
+            // 
+            $sNames .= ', '.$allNames[$ind];
+        }
+        $sNames .= ' & '.$allNames[$numMembers - 1];
+        return $sNames;
+    }
+}
