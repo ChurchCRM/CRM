4e6dd5fdc5878b24fe297ce8d15c5fb2df865cff
George Dawoud <george@dawouds.com>
2023-10-28 11:44:20 -0700

    removed skin style change

--- src/v2/templates/user/user.php
+++ src/v2/templates/user/user.php
@@ -58,170 +58,6 @@ include SystemURLs::getDocumentRoot() . '/Include/Header.php';
     <div class="col-lg-6">
         <div class="card">
             <div class="card-header">
-                <div class="card-title"><?= _("Skins") ?></div>
-            </div>
-            <div class="card-body">
-                <ul class="list-unstyled clearfix">
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-blue" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)"
-                           class="clearfix full-opacity-hover">
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 7px; background: #367fa9"></span>
-                                <span class="bg-light-blue" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin"><?= _("Blue")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-black" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div style="box-shadow: 0 0 2px rgba(0,0,0,0.1)" class="clearfix">
-                                <span style="display:block; width: 20%; float: left; height: 7px; background: #fefefe"></span>
-                                <span style="display:block; width: 80%; float: left; height: 7px; background: #fefefe"></span>
-                            </div>
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 20px; background: #222"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin"><?= _("Black")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-purple" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-purple-active"></span>
-                                <span class="bg-purple" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin"><?= _("Purple")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-green" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-green-active"></span>
-                                <span class="bg-green" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin"><?= _("Green")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-red" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-red-active"></span>
-                                <span class="bg-red" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin"><?= _("Red")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-yellow" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div><span style="display:block; width: 20%; float: left; height: 7px;" class="bg-yellow-active"></span>
-                                <span class="bg-yellow" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div><span style="display:block; width: 20%; float: left; height: 20px; background: #222d32"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin"><?= _("Yellow")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-blue-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 7px; background: #367fa9"></span>
-                                <span class="bg-light-blue" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div><span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
-                                <span
-                                    style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin" style="font-size: 12px"><?= _("Blue Light")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-black-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)"
-                           class="clearfix full-opacity-hover">
-                            <div style="box-shadow: 0 0 2px rgba(0,0,0,0.1)" class="clearfix"><span style="display:block; width: 20%; float: left; height: 7px; background: #fefefe"></span>
-                                <span style="display:block; width: 80%; float: left; height: 7px; background: #fefefe"></span>
-                            </div>
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin" style="font-size: 12px"><?= _("Black Light")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-purple-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-purple-active"></span>
-                                <span class="bg-purple" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin" style="font-size: 12px"><?= _("Purple Light")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-green-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-green-active"></span>
-                                <span class="bg-green" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin" style="font-size: 12px"><?= _("Green Light")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-red-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-red-active"></span>
-                                <span class="bg-red" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin" style="font-size: 12px"><?= _("Red Light")?></p>
-                    </li>
-                    <li style="float:left; width: 33.33333%; padding: 5px;">
-                        <a href="javascript:void(0)" data-skin="skin-yellow-light" style="display: block; box-shadow: 0 0 3px rgba(0,0,0,0.4)" class="clearfix full-opacity-hover">
-                            <div>
-                                <span style="display:block; width: 20%; float: left; height: 7px;" class="bg-yellow-active"></span>
-                                <span class="bg-yellow" style="display:block; width: 80%; float: left; height: 7px;"></span>
-                            </div>
-                            <div><span style="display:block; width: 20%; float: left; height: 20px; background: #f9fafc"></span>
-                                <span style="display:block; width: 80%; float: left; height: 20px; background: #f4f5f7"></span>
-                            </div>
-                        </a>
-                        <p class="text-center no-margin" style="font-size: 12px"><?= _("Yellow Light")?></p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-6">
-        <div class="card">
-            <div class="card-header">
                 <div class="card-title"><?= _("User Interface") ?></div>
             </div>
             <div class="card-body">
@@ -235,16 +71,14 @@ include SystemURLs::getDocumentRoot() . '/Include/Header.php';
                 </div>
                 <div class="form-group">
                     <label class="control-sidebar-subheading">
+                        <b><?= _("Locale")?></b></label>
                         <select id="user-locale-setting" class="pull-right user-setting-select" data-setting-name="ui.locale">
                         </select>
-                        <b><?= _("Locale")?></b></label>
-                    <p><?= _("Override system locale")?>: <?= Bootstrapper::GetCurrentLocale()->getSystemLocale() ?></p>
+                    <p><?= _("Override system locale")?>: <?= Bootstrapper::GetCurrentLocale()->getName() ?></p>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="row">
     <div class="col-lg-6">
         <div class="card">
             <div class="card-header">
