55917630fa34d5e65dd501109dd27919e97c7a96
George Dawoud <george@dawouds.com>
2023-10-28 15:46:40 -0700

    Email Dashboard and tools tested

---
+++ cypress/e2e/ui/email/standard.email.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+context('Email Pages', () => {
+
+    it('Email Dashboard', () => {
+        cy.loginStandard('v2/email/dashboard');
+        cy.contains("eMail Dashboard");
+    });
+
+
+    it('Duplicate Emails', () => {
+        cy.loginStandard('v2/email/duplicate');
+        cy.contains("Duplicate Emails");
+        cy.contains("lady@nower.com");
+    });
+
+
+    it('Families Without Emails', () => {
+        cy.loginStandard('v2/email/missing');
+        cy.contains("Families Without Emails");
+        cy.contains("Troy543267");
+    });
+
+
+});
