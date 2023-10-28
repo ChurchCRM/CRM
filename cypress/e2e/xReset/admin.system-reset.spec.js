4514bf4777cae4af395197b06cac606a4abf5524
George Dawoud <george@dawouds.com>
2023-10-28 09:05:42 -0700

    temp stop of reseting the system

--- cypress/e2e/xReset/admin.system-reset.spec.js
+++ cypress/e2e/xReset/admin.system-reset.spec.js
@@ -10,8 +10,8 @@ context('Admin System Reset', () => {
         cy.contains('Please type I AGREE to access the database reset functions page.').should('not.be.visible');
         cy.get('#confirm-people').click();
         cy.contains('This will remove all the member data, people, and families and can\'t be undone.').should("be.visible");
-        cy.get('.bootbox-accept').should('be.visible').click();
-        cy.url().should('contains', 'v2/dashboard');
+        // cy.get('.bootbox-accept').should('be.visible').click();
+        // cy.url().should('contains', 'v2/dashboard');
 
     });
 
@@ -23,8 +23,8 @@ context('Admin System Reset', () => {
         cy.contains('Please type I AGREE to access the database reset functions page.').should('not.be.visible');
         cy.get('#confirm-db').click();
         cy.contains('This will reset the system data and will restart the system as a new install.').should("be.visible");
-        cy.get('.bootbox-accept').should('be.visible').click();
-        cy.url().should('contains', 'session/begin');
+        // cy.get('.bootbox-accept').should('be.visible').click();
+        // cy.url().should('contains', 'session/begin');
 
     });
 
