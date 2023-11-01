6989d45478f8e254bf44d36aabd5345842dbd0a2
George Dawoud <george@dawouds.com>
2023-10-31 21:04:50 -0700

    no longer have support for software registration

--- src/mysql/upgrade/5.1.0.sql
+++ src/mysql/upgrade/5.1.0.sql
@@ -2,7 +2,6 @@ delete from permissions where permission_name ='usAddressVerification';
 
 delete from config_cfg where cfg_id ='54';
 delete from config_cfg where cfg_id ='55';
+delete from config_cfg where cfg_id ='999';
 
 delete from userconfig_ucfg where ucfg_name = "bUSAddressVerification";
-
-Update `config_cfg` set `cfg_value` = 0 where `cfg_name` = "bRegistered";
