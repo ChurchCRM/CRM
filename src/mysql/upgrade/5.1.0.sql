962750639de89cda0b4f31a1795625e38993d1e0
George Dawoud <george@dawouds.com>
2023-10-31 20:57:01 -0700

    reset the system to unregistered

--- src/mysql/upgrade/5.1.0.sql
+++ src/mysql/upgrade/5.1.0.sql
@@ -4,3 +4,5 @@ delete from config_cfg where cfg_id ='54';
 delete from config_cfg where cfg_id ='55';
 
 delete from userconfig_ucfg where ucfg_name = "bUSAddressVerification";
+
+Update `config_cfg` set `cfg_value` = 0 where `cfg_name` = "bRegistered";
