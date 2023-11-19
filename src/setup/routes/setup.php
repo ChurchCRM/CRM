22a9cacf2bb031621685e6bc84e5a64e45dfe099
George Dawoud <george@dawouds.com>
2023-11-18 17:11:20 -0800

    working setup MVC

--- src/setup/routes/setup.php
+++ src/setup/routes/setup.php
@@ -3,6 +3,8 @@
 use ChurchCRM\dto\SystemURLs;
 use ChurchCRM\Service\AppIntegrityService;
 use Slim\Views\PhpRenderer;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->group('/', function () use ($app) {
     $app->get('', function ($request, $response, $args) {
@@ -17,26 +19,28 @@ $app->group('/', function () use ($app) {
 
     $app->get('SystemIntegrityCheck', function ($request, $response, $args) {
         $AppIntegrity = AppIntegrityService::verifyApplicationIntegrity();
-        echo $AppIntegrity['status'];
+        $response->getBody()->write(json_encode($AppIntegrity['status']));
+        return $response->withHeader('Content-Type', 'application/json');
     });
 
     $app->get('SystemPrerequisiteCheck', function ($request, $response, $args) {
         $required = AppIntegrityService::getApplicationPrerequisites();
-
-        return $response->withJson($required);
+        $response->getBody()->write(json_encode($required));
+        return $response->withHeader('Content-Type', 'application/json');
     });
 
-    $app->post('', function ($request, $response, $args) {
-        $setupDate = $request->getParsedBody();
+    $app->post('', function (Request $request, Response $response, array $args) {
+        $setupData = $request->getParsedBody();
+
         $template = file_get_contents(SystemURLs::getDocumentRoot().'/Include/Config.php.example');
 
-        $template = str_replace('||DB_SERVER_NAME||', $setupDate['DB_SERVER_NAME'], $template);
-        $template = str_replace('||DB_SERVER_PORT||', $setupDate['DB_SERVER_PORT'], $template);
-        $template = str_replace('||DB_NAME||', $setupDate['DB_NAME'], $template);
-        $template = str_replace('||DB_USER||', $setupDate['DB_USER'], $template);
-        $template = str_replace('||DB_PASSWORD||', $setupDate['DB_PASSWORD'], $template);
-        $template = str_replace('||ROOT_PATH||', $setupDate['ROOT_PATH'], $template);
-        $template = str_replace('||URL||', $setupDate['URL'], $template);
+        $template = str_replace('||DB_SERVER_NAME||', $setupData['DB_SERVER_NAME'], $template);
+        $template = str_replace('||DB_SERVER_PORT||', $setupData['DB_SERVER_PORT'], $template);
+        $template = str_replace('||DB_NAME||', $setupData['DB_NAME'], $template);
+        $template = str_replace('||DB_USER||', $setupData['DB_USER'], $template);
+        $template = str_replace('||DB_PASSWORD||', $setupData['DB_PASSWORD'], $template);
+        $template = str_replace('||ROOT_PATH||', $setupData['ROOT_PATH'], $template);
+        $template = str_replace('||URL||', $setupData['URL'], $template);
 
         file_put_contents(SystemURLs::getDocumentRoot().'/Include/Config.php', $template);
 
