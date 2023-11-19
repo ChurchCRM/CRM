663b272118d0afd5ea19788079d048be05d104f4
George Dawoud <george@dawouds.com>
2023-11-18 22:18:31 -0800

    helper methods

---
+++ src/ChurchCRM/Slim/Renderer/JsonResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ChurchCRM\Slim\Request;
+
+use Psr\Http\Message\ResponseInterface as Response;
+
+class JsonResponse
+{
+    public static function render(Response $response, array $obj) {
+        $response->getBody()->write(json_encode($obj));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    public static function renderSuccess(Response $response) {
+        return self::render(['status' => 'success']);
+    }
+}
