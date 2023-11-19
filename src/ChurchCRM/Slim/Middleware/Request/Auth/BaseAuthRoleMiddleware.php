3000f35cdec453d30122e2864213eb03dd0e3fac
George Dawoud <george@dawouds.com>
2023-11-18 22:17:40 -0800

    fixed middleware

--- src/ChurchCRM/Slim/Middleware/Request/Auth/BaseAuthRoleMiddleware.php
+++ src/ChurchCRM/Slim/Middleware/Request/Auth/BaseAuthRoleMiddleware.php
@@ -6,6 +6,7 @@ use ChurchCRM\Authentication\AuthenticationManager;
 use ChurchCRM\model\ChurchCRM\User;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 abstract class BaseAuthRoleMiddleware
 {
@@ -14,18 +15,19 @@ abstract class BaseAuthRoleMiddleware
      */
     protected $user;
 
-    public function __invoke(Request $request, Response $response, callable $next)
+    public function  __invoke(Request $request, RequestHandler $handler) : Response
     {
+        $response = $handler->handle($request);
         $this->user = AuthenticationManager::getCurrentUser();
         if (empty($this->user)) {
+            $response = $handler->handle($request);
             return $response->withStatus(401, gettext('No logged in user'));
         }
 
         if (!$this->hasRole()) {
             return $response->withStatus(403, $this->noRoleMessage());
         }
-
-        return $next($request, $response);
+        return $handler->handle($request);
     }
 
     abstract protected function hasRole();
