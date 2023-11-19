a3d6878a1a6ce0875596407707dbbb872b5190af
George Dawoud <george@dawouds.com>
2023-11-18 18:22:49 -0800

    exit if we have Config.php

--- src/setup/index.php
+++ src/setup/index.php
@@ -3,22 +3,24 @@
 use ChurchCRM\dto\SystemConfig;
 use ChurchCRM\dto\SystemURLs;
 use ChurchCRM\Slim\Middleware\VersionMiddleware;
-use Monolog\Logger;
 use Selective\BasePath\BasePathMiddleware;
 use Slim\Container;
 use Slim\Factory\AppFactory;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+if (file_exists('../Include/Config.php')) {
+    header('Location: ../');
+    exit;
+}
 
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $rootPath = str_replace('/setup/index.php', '', $_SERVER['SCRIPT_NAME']);
 SystemURLs::init($rootPath, '', __DIR__ . "/../");
 SystemConfig::init();
 
 $app = AppFactory::create();
-$app->setBasePath('/setup/');
+$app->setBasePath('/setup');
 
-SystemConfig::getValue("sLogLevel", Logger::DEBUG);
 require __DIR__ . '/../Include/slim/error-handler.php';
 
 $app->addRoutingMiddleware();
