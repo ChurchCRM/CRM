076a5842c1de803071f511f973b7dc08ba3ea0d3
George Dawoud <george@dawouds.com>
2023-11-23 12:26:03 -0800

    fixed bad merge

--- src/v2/index.php
+++ src/v2/index.php
@@ -28,22 +28,6 @@ require __DIR__ . '/routes/people.php';
 require __DIR__ . '/routes/family.php';
 require __DIR__ . '/routes/person.php';
 
-// Set up
-require __DIR__ . '/../Include/slim/error-handler.php';
-
-require __DIR__ . '/routes/common/mvc-helper.php';
-
-// admin routes
-require __DIR__ . '/routes/admin/admin.php';
-require __DIR__ . '/routes/user.php';
-
-// people routes
-require __DIR__ . '/routes/root.php';
-
-require __DIR__ . '/routes/people.php';
-require __DIR__ . '/routes/family.php';
-require __DIR__ . '/routes/person.php';
-
 require __DIR__ . '/routes/email.php';
 require __DIR__ . '/routes/calendar.php';
 require __DIR__ . '/routes/cart.php';
