8c4d9062ab3394fc34def4b2cf197ab10b820358
George Dawoud <george@dawouds.com>
2023-11-24 09:55:41 -0800

    matching Slim4 geURI

--- src/ChurchCRM/Slim/Middleware/AuthMiddleware.php
+++ src/ChurchCRM/Slim/Middleware/AuthMiddleware.php
@@ -12,7 +12,7 @@ class AuthMiddleware
 {
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
-        if (!$this->isPath($request, 'public')) {
+        if (!str_contains($request->getUri(),'api/public')) {
             $apiKey = $request->getHeader('x-api-key');
             if (!empty($apiKey)) {
                 $authenticationResult = AuthenticationManager::authenticate(new APITokenAuthenticationRequest($apiKey[0]));
