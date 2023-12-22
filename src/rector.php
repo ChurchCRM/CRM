b5ac36fa831010a18008b1d218c475966001028b
Arun Philip <dacodedbeat@gmail.com>
2023-12-22 13:22:38 -0500

    rename pdf class in AdvancedDeposit to avoid name collision

--- src/rector.php
+++ src/rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -33,5 +34,6 @@ return static function (RectorConfig $rectorConfig): void {
     // define sets of rules
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_74,
+        SetList::GMAGICK_TO_IMAGICK,
     ]);
 };
