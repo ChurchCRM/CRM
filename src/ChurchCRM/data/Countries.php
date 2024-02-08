5c49ac7f9554881b45e6615317cee4e021d454f4
Bob Robison <bob@robisonranch.net>
2024-02-08 15:57:34 -0600

    Run phpcbf also on Countries.php

--- src/ChurchCRM/data/Countries.php
+++ src/ChurchCRM/data/Countries.php
@@ -302,8 +302,8 @@ class Countries
 
         self::initializeCountries();
         $result = array_filter(self::$countries, fn ($e): bool => $e->getCountryName() === $CountryName);
-	if (count($result) === 1) {
-	    // Note that array_values() is needed because array_filter does not return  continuous array
+        if (count($result) === 1) {
+            // Note that array_values() is needed because array_filter does not return  continuous array
             return array_values($result)[0];
         }
 
