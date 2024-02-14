e417be3ade073518841212db9f341fd8ed6e0411
Arun Philip <dacodedbeat@gmail.com>
2024-02-13 21:36:49 -0500

    remove unused imports from react code

--- react/calendar-event-editor.tsx
+++ react/calendar-event-editor.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import CRMEvent from "./interfaces/CRMEvent";
 import ExistingEvent from "./components/Events/ExistingEvent";
 declare global {
   interface Window {
