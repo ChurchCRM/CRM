d06e5217ee22da887b89a03f59eab0da2984355c
Tiago Rodrigues <tiagomartins_25@hotmail.com>
2024-02-24 22:42:45 +0000

    changing the test to in the end leave everything uncheckd to not destroy other possible tests

--- cypress/e2e/ui/people/standard.people.filterbyclassification.spec.js
+++ cypress/e2e/ui/people/standard.people.filterbyclassification.spec.js
@@ -65,6 +65,16 @@ describe('template spec', () => {
       cy.get("#members_filter input").type("edwin.adams@example.com");
       cy.contains("No matching records found");
 
+      cy.visit("OptionManager.php?mode=classes");
+      cy.get("#inactive4").uncheck();
+      cy.get("#inactive5").uncheck();
+      cy.reload();
+      cy.get("tr:nth-child(5) .TextColumn~ .TextColumn+ td input").should('not.be.checked');
+      cy.get("tr:nth-child(1) .TextColumn~ .TextColumn+ td input").should('not.be.checked');
+      cy.get("tr:nth-child(2) .TextColumn~ .TextColumn+ td input").should('not.be.checked');
+      cy.get("tr:nth-child(3) .TextColumn~ .TextColumn+ td input").should('not.be.checked');
+      cy.get("tr:nth-child(4) .TextColumn~ .TextColumn+ td input").should('not.be.checked');
+
   })
 
 })
