2ca6d89e6edb81870f0f6f5454c9f2fb8bb2b9f7
Arun Philip <dacodedbeat@gmail.com>
2024-04-03 03:59:22 -0400

    run prettier on cypress directory

--- cypress/e2e/ui/events/standard.calendar.spec.js
+++ cypress/e2e/ui/events/standard.calendar.spec.js
@@ -4,7 +4,7 @@ context("Standard Calendar", () => {
     it("Create New Event", () => {
         let title = "My New Event - " + Cypress._.random(0, 1e6);
         cy.loginStandard("v2/calendar");
-        cy.get('tr:nth-child(1) > .fc-day-thu > .fc-daygrid-day-frame').click();
+        cy.get("tr:nth-child(1) > .fc-day-thu > .fc-daygrid-day-frame").click();
         cy.get(".modal-header > input").click();
         cy.get(".modal-header > input").type(title);
         cy.get("tr:nth-child(2) textarea").type("New adult Service");
