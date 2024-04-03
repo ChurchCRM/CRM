e6c48afcb50de146475dd7f76c33c35a72641bed
Arun Philip <dacodedbeat@gmail.com>
2024-04-03 16:07:03 -0400

    fix family editor to save wedding date

--- cypress/e2e/ui/people/standard.family.spec.js
+++ cypress/e2e/ui/people/standard.family.spec.js
@@ -50,7 +50,9 @@ context("Standard Family", () => {
 
     it("Entering a new Family", () => {
         cy.loginStandard("FamilyEditor.php");
+
         cy.contains("Family Info");
+        // Fill in Family Info section
         cy.get("#FamilyName").type("Troy" + Cypress._.random(0, 1e6));
         cy.get('input[name="Address1"').type("4222 Clinton Way");
         cy.get('input[name="City"]').clear().type("Los Angelas");
@@ -58,6 +60,14 @@ context("Standard Family", () => {
         // Add clearing of Lat/Long to verify these can be null, instead of default 0
         cy.get('input[name="Latitude"]').clear();
         cy.get('input[name="Longitude"]').clear();
+
+        // Fill in Other Info section
+        const weddingYear = '2024';
+        const weddingMonth = '04';
+        const weddingDay = '03';
+        cy.get('#WeddingDate').type(`${weddingYear}-${weddingMonth}-${weddingDay}`);
+
+        // Fill in Family Members
         cy.get('input[name="FirstName1"]').type("Mike");
         cy.get('input[name="FirstName2"]').type("Carol");
         cy.get('input[name="FirstName3"]').type("Alice");
@@ -70,7 +80,9 @@ context("Standard Family", () => {
         cy.get('select[name="Classification4"]').select("2", { force: true });
         cy.get('select[name="Classification5"]').select("1", { force: true });
         cy.get('select[name="Classification6"]').select("2", { force: true });
+
         cy.get("input[id='FamilySubmitBottom']").click();
+
         cy.location("pathname").should("include", "/v2/family/");
         cy.contains("Mike Troy");
         cy.contains("Carol Troy");
@@ -79,5 +91,6 @@ context("Standard Family", () => {
         cy.contains("Marcia Troy");
         cy.contains("Peter Troy");
         cy.contains("4222 Clinton Way Los Angelas, CA");
+        cy.contains(`${weddingMonth}/${weddingDay}/${weddingYear}`);
     });
 });
