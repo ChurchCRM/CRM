9f47dd40e5a5eb911312c9ca1dd7f728854fed64
romdricks <156947887+romdricks@users.noreply.github.com>
2024-09-30 17:34:13 -0500

    Update ResetPasswordEmail.php

--- src/ChurchCRM/Emails/users/ResetPasswordEmail.php
+++ src/ChurchCRM/Emails/users/ResetPasswordEmail.php
@@ -21,7 +21,7 @@ class ResetPasswordEmail extends BaseUserEmail
 
     protected function buildMessageBody(): string
     {
-        return gettext('Your ChurchCRM updated password has been changed') . ':';
+        return gettext('Your ChurchCRM password has been changed') . ':';
     }
 
     public function getTokens(): array
