756ae8cd4200a930360d026ee5b0ad8ee5b85bd9
George Dawoud <DawoudIO@users.noreply.github.com>
2025-02-22 19:53:32 +0000

    Apply fixes from StyleCI

--- src/propel/config.php
+++ src/propel/config.php
@@ -1,22 +1,23 @@
 <?php
+
 $serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
 $serviceContainer->checkVersion(2);
 $serviceContainer->setAdapterClass('default', 'mysql');
 $manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();
-$manager->setConfiguration(array (
+$manager->setConfiguration(array(
   'dsn' => 'mysql:host=localhost;port=3306;dbname=churchcrm',
   'user' => 'churchcrm',
   'password' => 'churchcrm',
   'settings' =>
-  array (
+  array(
     'charset' => 'utf8',
     'queries' =>
-    array (
+    array(
     ),
   ),
   'classname' => '\\Propel\\Runtime\\Connection\\ConnectionWrapper',
   'model_paths' =>
-  array (
+  array(
     0 => 'src',
     1 => 'vendor',
   ),
