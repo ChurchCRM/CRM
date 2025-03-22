6502b9a690cf6036a3e90d23cfb3786981b68449
George Dawoud <george@dawouds.com>
2025-03-22 14:46:31 -0700

    added missing drop cfg_default

--- src/mysql/upgrade/2.6.0.sql
+++ src/mysql/upgrade/2.6.0.sql
@@ -5,3 +5,4 @@ alter table config_cfg drop column `cfg_tooltip`;
 alter table config_cfg drop column `cfg_section`;
 alter table config_cfg drop column `cfg_category`;
 alter table config_cfg drop column `cfg_data`;
+alter table config_cfg drop column `cfg_default`;
