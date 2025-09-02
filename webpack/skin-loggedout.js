17d74057bb8b4119f52900ed9a1d6ed86f5e432c
George Dawoud <george@dawouds.com>
2025-09-01 17:08:01 -0700

    created a few new files for use by webpack

---
+++ webpack/skin-loggedout.js
@@ -0,0 +1,20 @@
+// Main entry for all common JS and CSS includes
+
+// Import main SCSS (which includes all CSS dependencies)
+// import '../src/skin/churchcrm.scss';
+
+
+import 'bootstrap';
+import 'admin-lte';
+import '@fortawesome/fontawesome-free/js/all';
+
+// Import plugins that depend on jQuery and Bootstrap
+import 'inputmask';
+import 'bootbox';
+import 'moment';
+import 'pace-js';
+import 'flag-icons';
+import 'select2';
+
+import 'jquery-steps/build/jquery.steps.js';
+import 'jquery-steps/demo/css/jquery.steps.css'; // if you want the CSS
