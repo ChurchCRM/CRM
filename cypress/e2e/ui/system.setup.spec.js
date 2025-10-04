9fd426df1370af9718bfac1e9486c01b7151751f
George Dawoud <george@dawouds.com>
2025-10-04 11:12:07 -0700

    added extra validation rules

---
+++ cypress/e2e/ui/system.setup.spec.js
@@ -0,0 +1,7 @@
+context("Test Post Setup block", () => {
+    
+    it("Redirects to session/begin", () => {
+        cy.visit("/setup");
+        cy.location("pathname").should("eq", "/session/begin");
+    });
+});
--- src/setup/routes/setup.php
+++ src/setup/routes/setup.php
@@ -8,6 +8,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 use Slim\Views\PhpRenderer;
 
+
 $app->group('/', function (RouteCollectorProxy $group): void {
     $group->get('', function (Request $request, Response $response, array $args): Response {
         $renderer = new PhpRenderer('templates/');
@@ -32,20 +33,65 @@ $app->group('/', function (RouteCollectorProxy $group): void {
     });
 
     $group->post('', function (Request $request, Response $response, array $args): Response {
+        $configFile = SystemURLs::getDocumentRoot() . '/Include/Config.php';
+        if (file_exists($configFile)) {
+            return $response->withStatus(403, 'Setup is already complete.');
+        }
+
         $setupData = $request->getParsedBody();
 
+        // Validate each field
+        if (
+            !isset($setupData['DB_SERVER_NAME'], $setupData['DB_SERVER_PORT'], $setupData['DB_NAME'], $setupData['DB_USER'], $setupData['DB_PASSWORD'], $setupData['ROOT_PATH'], $setupData['URL']) ||
+            !is_valid_db_field($setupData['DB_SERVER_NAME']) ||
+            !is_valid_port($setupData['DB_SERVER_PORT']) ||
+            !is_valid_db_field($setupData['DB_NAME']) ||
+            !is_valid_db_field($setupData['DB_USER']) ||
+            !is_valid_db_field($setupData['DB_PASSWORD']) ||
+            !is_valid_root_path($setupData['ROOT_PATH']) ||
+            !filter_var($setupData['URL'], FILTER_VALIDATE_URL)
+        ) {
+            return $response->withStatus(400, 'Invalid setup data.');
+        }
+
+        // Use sanitized values
+        $dbServerName = sanitize_db_field($setupData['DB_SERVER_NAME']);
+        $dbServerPort = preg_replace('/[^0-9]/', '', $setupData['DB_SERVER_PORT']);
+        $dbName      = sanitize_db_field($setupData['DB_NAME']);
+        $dbUser      = sanitize_db_field($setupData['DB_USER']);
+        $dbPassword  = sanitize_db_field($setupData['DB_PASSWORD']);
+        $rootPath    = $setupData['ROOT_PATH'];
+        $url         = $setupData['URL'];
+
         $template = file_get_contents(SystemURLs::getDocumentRoot() . '/Include/Config.php.example');
+        $template = str_replace('||DB_SERVER_NAME||', $dbServerName, $template);
+        $template = str_replace('||DB_SERVER_PORT||', $dbServerPort, $template);
+        $template = str_replace('||DB_NAME||', $dbName, $template);
+        $template = str_replace('||DB_USER||', $dbUser, $template);
+        $template = str_replace('||DB_PASSWORD||', $dbPassword, $template);
+        $template = str_replace('||ROOT_PATH||', $rootPath, $template);
+        $template = str_replace('||URL||', $url, $template);
 
-        $template = str_replace('||DB_SERVER_NAME||', $setupData['DB_SERVER_NAME'], $template);
-        $template = str_replace('||DB_SERVER_PORT||', $setupData['DB_SERVER_PORT'], $template);
-        $template = str_replace('||DB_NAME||', $setupData['DB_NAME'], $template);
-        $template = str_replace('||DB_USER||', $setupData['DB_USER'], $template);
-        $template = str_replace('||DB_PASSWORD||', $setupData['DB_PASSWORD'], $template);
-        $template = str_replace('||ROOT_PATH||', $setupData['ROOT_PATH'], $template);
-        $template = str_replace('||URL||', $setupData['URL'], $template);
-
-        file_put_contents(SystemURLs::getDocumentRoot() . '/Include/Config.php', $template);
+        file_put_contents($configFile, $template);
 
         return $response->withStatus(200);
     });
 });
+
+
+function sanitize_db_field($value) {
+    // Allow only letters, numbers, underscore, dash, dot, colon, and @
+    return preg_replace('/[^a-zA-Z0-9_\-\.:\@]/', '', $value);
+}
+
+function is_valid_db_field($value) {
+    return preg_match('/^[a-zA-Z0-9_\-\.:\@]+$/', $value);
+}
+
+function is_valid_port($value) {
+    return preg_match('/^[0-9]{1,5}$/', $value) && (int)$value > 0 && (int)$value < 65536;
+}
+
+function is_valid_root_path($value) {
+    return preg_match('#^\/[a-zA-Z0-9_\-\.\/]*$#', $value);
+}
