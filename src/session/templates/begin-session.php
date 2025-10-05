5bf9c4c7c979b1fe23c9cadfc97ea118cdb7cfa2
George Dawoud <george@dawouds.com>
2025-10-05 08:49:06 -0700

    re-added lock for password

--- src/session/templates/begin-session.php
+++ src/session/templates/begin-session.php
@@ -45,6 +45,11 @@ require SystemURLs::getDocumentRoot() . '/Include/HeaderNotLoggedIn.php';
                     </div>
                     <div class="input-group mb-3">
                         <input type="password" id="PasswordBox" name="Password" class="form-control" placeholder="<?= gettext('Password') ?>" required autofocus>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
                     </div>
                     <div class="row">
                         <div class="col-8">
