40dc4a7596e29c90dde74bf8bf25aee403c3e4ab
George Dawoud <george@dawouds.com>
2025-10-18 09:30:53 -0700

    PSR-15 compliant

--- src/ChurchCRM/Slim/Middleware/VersionMiddleware.php
+++ src/ChurchCRM/Slim/Middleware/VersionMiddleware.php
@@ -3,13 +3,14 @@
 namespace ChurchCRM\Slim\Middleware;
 
 use ChurchCRM\Utils\VersionUtils;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
 
-class VersionMiddleware
+class VersionMiddleware implements MiddlewareInterface
 {
-    public function __invoke(Request $request, RequestHandler $handler): Response
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $response = $handler->handle($request);
         return $response->withAddedHeader('CRM_VERSION', VersionUtils::getInstalledVersion());
