53239925de65e26a47ce4154cfd5b677c667ec83
George Dawoud <george@dawouds.com>
2025-10-22 08:00:40 -0700

    Import flag-icons CSS - flags are automatically bundled by webpack

--- webpack/skin-main.js
+++ webpack/skin-main.js
@@ -12,6 +12,9 @@
 // Import FontAwesome CSS - webfonts are automatically bundled by webpack
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+// Import flag-icons CSS - flags are automatically bundled by webpack
+import 'flag-icons/css/flag-icons.min.css';
+
 import '../src/skin/churchcrm.scss';
 
 // No additional exports needed - this bundle is for core CSS and JS assets
