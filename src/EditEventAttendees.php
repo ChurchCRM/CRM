a1237823c8eb5457e499b8a9718409f8b84bf881
George Dawoud <george@dawouds.com>
2025-10-24 21:01:06 -0700

    moving to ORM and fixing the UI

--- src/EditEventAttendees.php
+++ src/EditEventAttendees.php
@@ -5,10 +5,15 @@ require_once 'Include/Functions.php';
 
 use ChurchCRM\Utils\RedirectUtils;
 use ChurchCRM\Utils\InputUtils;
+use ChurchCRM\dto\SystemConfig;
 use ChurchCRM\model\ChurchCRM\EventQuery;
+use ChurchCRM\model\ChurchCRM\EventAttendQuery;
+use ChurchCRM\model\ChurchCRM\PersonQuery;
 
 $sPageTitle = gettext('Church Event Editor');
 require_once 'Include/Header.php';
+
+$nameFormat = (int)SystemConfig::getValue('iPersonNameStyle');
 
 $sAction = $_POST['Action'];
 $EventID = InputUtils::legacyFilterInput($_POST['EID'], 'int'); // from ListEvents button=Attendees
@@ -20,8 +25,13 @@ $EvtDate = $_POST['EDate'];
 if ($sAction == 'Delete') {
     $dpeEventID = InputUtils::legacyFilterInput($_POST['DelPerEventID'], 'int');
     $dpePerID = InputUtils::legacyFilterInput($_POST['DelPerID'], 'int');
-    $dpeSQL = "DELETE FROM event_attend WHERE event_id=$dpeEventID AND person_id=$dpePerID LIMIT 1";
-    RunQuery($dpeSQL);
+    $attendance = EventAttendQuery::create()
+        ->filterByEventId($dpeEventID)
+        ->filterByPersonId($dpePerID)
+        ->findOne();
+    if ($attendance !== null) {
+        $attendance->delete();
+    }
     $ShowAttendees = 1;
 }
 
@@ -57,31 +67,38 @@ if (empty($event)) {
       <td width="15%" nowrap><strong><?= gettext('Action') ?></strong></td>
   </tr>
 <?php
-$sSQL = 'SELECT person_id, per_LastName FROM event_attend JOIN person_per ON person_per.per_id = event_attend.person_id WHERE event_id = ' . $EventID . ' ORDER by per_LastName, per_FirstName';
-$rsOpps = RunQuery($sSQL);
-$numAttRows = mysqli_num_rows($rsOpps);
-if ($numAttRows != 0) {
+$attendees = EventAttendQuery::create()
+    ->filterByEventId($EventID)
+    ->innerJoinWithPerson()
+    ->orderByPersonId()
+    ->find();
+
+if ($attendees->count() > 0) {
     $sRowClass = 'RowColorA';
-    for ($na = 0; $na < $numAttRows; $na++) {
-        $attRow = mysqli_fetch_array($rsOpps, MYSQLI_BOTH);
-        extract($attRow);
-        $sSQL = 'SELECT per_Title, per_ID, per_FirstName, per_MiddleName, per_LastName, per_Suffix, per_Email, per_HomePhone, per_Country, fam_HomePhone, fam_Email, fam_Country FROM person_per LEFT JOIN family_fam ON per_fam_id=fam_id WHERE per_ID = ' . $person_id;
-        $perOpps = RunQuery($sSQL);
-        $perRow = mysqli_fetch_array($perOpps, MYSQLI_BOTH);
-        extract($perRow);
+    foreach ($attendees as $attendance) {
+        $person = $attendance->getPerson();
         $sRowClass = AlternateRowStyle($sRowClass);
+
+        $family = $person->getFamily();
+        $per_Country = $person->getCountry();
+        $fam_Country = $family ? $family->getCountry() : '';
 
         $sPhoneCountry = SelectWhichInfo($per_Country, $fam_Country, false);
 
+        $per_HomePhone = $person->getHomePhone();
+        $fam_HomePhone = $family ? $family->getHomePhone() : '';
         $sHomePhone = SelectWhichInfo(ExpandPhoneNumber($per_HomePhone, $sPhoneCountry, $dummy), ExpandPhoneNumber($fam_HomePhone, $fam_Country, $dummy), true);
+        
+        $per_Email = $person->getEmail();
+        $fam_Email = $family ? $family->getEmail() : '';
         $sEmail = SelectWhichInfo($per_Email, $fam_Email, false); ?>
     <tr class="<?= $sRowClass ?>">
-        <td class="TextColumn"><?= FormatFullName($per_Title, $per_FirstName, $per_MiddleName, $per_LastName, $per_Suffix, 3) ?></td>
+        <td class="TextColumn"><?= $person->getFormattedName($nameFormat) ?></td>
         <td class="TextColumn"><?= $sEmail ? '<a href="mailto:' . $sEmail . '" title="Send Email">' . $sEmail . '</a>' : 'Not Available' ?></td>
         <td class="TextColumn"><?= $sHomePhone ? $sHomePhone : 'Not Available' ?></td>
     <td  class="TextColumn" colspan="1" align="center">
       <form method="POST" action="EditEventAttendees.php" name="DeletePersonFromEvent">
-          <input type="hidden" name="DelPerID" value="<?= $per_ID ?>">
+          <input type="hidden" name="DelPerID" value="<?= $person->getId() ?>">
           <input type="hidden" name="DelPerEventID" value="<?= $EventID ?>">
           <input type="hidden" name="EID" value="<?= $EventID ?>">
           <input type="hidden" name="EName" value="<?= $EvtName ?>">
