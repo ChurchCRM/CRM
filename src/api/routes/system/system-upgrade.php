37064ba20405d2b5b48cc7b63eee3d586cf2c36b
George Dawoud <george@dawouds.com>
2025-11-10 19:41:27 -0800

    Update system-upgrade.php

--- src/api/routes/system/system-upgrade.php
+++ src/api/routes/system/system-upgrade.php
@@ -9,15 +9,25 @@ use Slim\Routing\RouteCollectorProxy;
 
 $app->group('/systemupgrade', function (RouteCollectorProxy $group): void {
     $group->get('/downloadlatestrelease', function (Request $request, Response $response, array $args): Response {
-        $upgradeFile = ChurchCRMReleaseManager::downloadLatestRelease();
-
-        return SlimUtils::renderJSON($response, $upgradeFile);
+        try {
+            $upgradeFile = ChurchCRMReleaseManager::downloadLatestRelease();
+            return SlimUtils::renderJSON($response, $upgradeFile);
+        } catch (\Exception $e) {
+            return SlimUtils::renderJSON($response, [
+                'message' => $e->getMessage()
+            ], 400);
+        }
     });
 
     $group->post('/doupgrade', function (Request $request, Response $response, array $args): Response {
-        $input = $request->getParsedBody();
-        ChurchCRMReleaseManager::doUpgrade($input['fullPath'], $input['sha1']);
-
-        return SlimUtils::renderSuccessJSON($response);
+        try {
+            $input = $request->getParsedBody();
+            ChurchCRMReleaseManager::doUpgrade($input['fullPath'], $input['sha1']);
+            return SlimUtils::renderSuccessJSON($response);
+        } catch (\Exception $e) {
+            return SlimUtils::renderJSON($response, [
+                'message' => $e->getMessage()
+            ], 500);
+        }
     });
 })->add(AdminRoleAuthMiddleware::class);
