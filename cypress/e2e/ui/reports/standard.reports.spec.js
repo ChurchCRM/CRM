f5b82ac0b09e4eeefa1a1ea3c8e0e1ee8958655d
George Dawoud <george@dawouds.com>
2025-11-15 11:01:54 -0800

    cleanup

--- cypress/e2e/ui/reports/standard.reports.spec.js
+++ cypress/e2e/ui/reports/standard.reports.spec.js
@@ -7,13 +7,11 @@ describe("Standard Reports", () => {
         cy.visit("LettersAndLabels.php");
         cy.contains("Letters and Mailing Labels");
         cy.contains("Newsletter labels");
-        //  TODO    cy.get('.btn-default:nth-child(1)').click();
     });
 
     it("Gen data labels", () => {
         cy.visit("LettersAndLabels.php");
         cy.contains("Letters and Mailing Labels");
         cy.contains("Confirm data labels");
-        //   TODO     cy.get('.btn-default:nth-child(2)').click();
     });
 });
