d219c649001e891fd4ec28f15b642a2d75dc8935
George Dawoud <george@dawouds.com>
2025-11-16 09:03:10 -0800

    Update standard.family.spec.js

--- cypress/e2e/ui/people/standard.family.spec.js
+++ cypress/e2e/ui/people/standard.family.spec.js
@@ -70,48 +70,4 @@ describe("Standard Family", () => {
         cy.should('not.have.text', 'mike@example.com');
         cy.should('not.have.text', `${weddingMonth}/${weddingDay}/${weddingYear}`);
     });
-
-
-    it("View Family Lists", () => {
-
-        cy.visit("v2/family");
-        cy.contains("Active Family List");
-
-        cy.makePrivateUserAPICall(
-            "POST",
-            "/api/families/3/activate/true",
-            "",
-            200,
-        );
-
-        cy.intercept("POST", "/api/families/3/activate/true").as(
-            "updateToActive",
-        );
-        cy.intercept("POST", "/api/families/3/activate/false").as(
-            "updateToInActive",
-        );
-
-        cy.visit("v2/family?mode=inactive");
-        cy.contains("Inactive Family List").should("be.visible");
-        cy.contains("Lewis").should("not.exist");
-
-        cy.visit("v2/family/3");
-        cy.contains("This Family is Deactivated").should("not.be.visible");
-        cy.get("#activateDeactivate").click();
-        cy.get(".bootbox-accept").should("be.visible").click();
-        cy.wait("@updateToInActive");
-
-        cy.visit("v2/family?mode=inactive");
-        cy.contains("Lewis");
-
-        cy.visit("v2/family/3");
-        cy.contains("This Family is Deactivated").should("be.visible");
-        cy.get("#activateDeactivate").click();
-        cy.get(".bootbox-accept").should("be.visible").click();
-        cy.wait("@updateToActive");
-
-        cy.visit("v2/family?mode=inactive");
-        cy.contains("Lewis").should("not.exist");
-    });
-
 });
