e762f4516ed6b182bac0eda0205b97efc5b48aad
George Dawoud <george@dawouds.com>
2025-11-23 22:16:21 -0800

    better timeout for APIs

--- cypress/e2e/xReset/admin.system-demo.spec.js
+++ cypress/e2e/xReset/admin.system-demo.spec.js
@@ -5,14 +5,11 @@ describe('API Private Admin Demo', () => {
     const body = { includeFinancial: false, includeEvents: false, force: true };
 
     cy.makePrivateAdminAPICall('POST', '/admin/api/demo/load', body, 200).then((resp) => {
-
-      cy.wait(3000); // wait for demo data to be processed
-
       // Expect success flag and import metadata in body
       expect(resp.body).to.have.property('success');
       // success may be true or false depending on internal errors; at minimum assert shape
       expect(resp.body).to.have.property('imported');
       expect(resp.body).to.have.property('warnings');
-    });
+    }, 120000);
   });
 });
