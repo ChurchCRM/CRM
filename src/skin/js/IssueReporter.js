cb6aaeeeca8d1dd8c299029fc2b075359763aaf3
George Dawoud <george@dawouds.com>
2025-11-28 11:51:57 -0800

    Update in-app reporter: Use bug_report.md template with multiple query params

--- src/skin/js/IssueReporter.js
+++ src/skin/js/IssueReporter.js
@@ -23,8 +23,14 @@ $("#submitIssue").click(function () {
     }).done(function (data) {
         var bugMsg = "**Describe the issue** \n\n\n\n";
         var systemInfo = encodeURIComponent(bugMsg + data["issueBody"]);
+        // Use issue template selection to mark this as a bug (new GitHub issue type).
+        // Use multiple params to encourage GitHub to select the correct template.
+        // include `template=` and `issue_template=` variants and prefill the title.
+        var title = encodeURIComponent('Bug: ');
         var gitHubURL =
-            "https://github.com/ChurchCRM/CRM/issues/new?assignees=&labels=Web%20Report&body=" +
+            "https://github.com/ChurchCRM/CRM/issues/new?assignees=&template=bug_report.md&issue_template=bug_report.md&title=" +
+            title +
+            "&body=" +
             systemInfo;
         window.open(gitHubURL, `github`);
         $("#IssueReportModal").modal("toggle");
