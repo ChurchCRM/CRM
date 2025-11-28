d96cea278819f9567f5bbf9e9e403fd96a4c9ef8
George Dawoud <george@dawouds.com>
2025-11-28 13:24:35 -0800

    Fix: Include body parameter with system diagnostics in GitHub issue URL

--- src/skin/js/IssueReporter.js
+++ src/skin/js/IssueReporter.js
@@ -23,16 +23,11 @@ $("#submitIssue").click(function () {
     }).done(function (data) {
         var bugMsg = "**Describe the issue** \n\n\n\n";
         var systemInfo = encodeURIComponent(bugMsg + data["issueBody"]);
-        // Use issue template selection to mark this as a bug (new GitHub issue type).
-        // Use multiple params to encourage GitHub to select the correct template.
-        // include `template=` and `issue_template=` variants and prefill the title.
-        var title = encodeURIComponent('Bug: ');
-        var gitHubURL =
-            "https://github.com/ChurchCRM/CRM/issues/new?assignees=&template=bug_report.md&issue_template=bug_report.md&title=" +
-            title +
-            "&body=" +
-            systemInfo;
-        window.open(gitHubURL, `github`);
+        // Include body parameter to show system info in GitHub issue
+        // Template parameter still works alongside body parameter
+        var gitHubTemplateURL =
+            "https://github.com/ChurchCRM/CRM/issues/new?assignees=&template=bug_report&body=" + systemInfo;
+        window.open(gitHubTemplateURL, `github`);
         $("#IssueReportModal").modal("toggle");
     });
 });
