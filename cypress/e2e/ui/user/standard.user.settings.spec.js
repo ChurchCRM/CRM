583a070abf22233cd3ab7f73c79b6e8c55539456
George Dawoud <george@dawouds.com>
2025-11-30 20:23:06 -0800

    filterSanitizeString removed

--- cypress/e2e/ui/user/standard.user.settings.spec.js
+++ cypress/e2e/ui/user/standard.user.settings.spec.js
@@ -4,7 +4,7 @@ describe("Standard User Setting", () => {
     beforeEach(() => cy.setupStandardSession());
     
     it("View User Setting and Edit Page", () => {
-        cy.visit("v2/user/3");
+        cy.visit("/v2/user/3");
         cy.contains("User -");
         cy.get("#editSettings").click();
         cy.url().should("contain", "SettingsIndividual.php");
