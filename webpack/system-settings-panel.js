0f4a629ddb32491920924160ae00e90cfb9fdacd
George Dawoud <george@dawouds.com>
2025-12-01 17:20:34 -0800

    settingsHtml

--- webpack/system-settings-panel.js
+++ webpack/system-settings-panel.js
@@ -315,6 +315,7 @@ import '../src/skin/scss/system-settings-panel.scss';
 
         // Render the panel HTML
         render() {
+            let settingsHtml = '';
 
             this.options.settings.forEach(setting => {
                 const settingName = typeof setting === 'string' ? setting : setting.name;
