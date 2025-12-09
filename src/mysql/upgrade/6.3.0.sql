e1879db91a937380fdb6aea505f0ef701f0cc768
George Dawoud <george@dawouds.com>
2025-12-08 19:12:34 -0800

    Revert "Remove eGive feature (deprecated, never tested)"
    This reverts commit 61972d3f8491bcdc31cfe4c6ed742776bee43843.

--- src/mysql/upgrade/6.3.0.sql
+++ src/mysql/upgrade/6.3.0.sql
@@ -9,19 +9,3 @@ WHERE cfg_name = 'bEnabledCalendar'
 
 -- Note: Deleting by both name and id makes this safe whether the cfg_name was renamed
 -- or the numeric id was previously used in older dumps.
-
--- Remove eGive feature (deprecated, never tested)
--- 
--- Changes:
--- 1. Remove 'eGive' enum from deposit type field
--- 2. Remove 'EGIVE' enum from pledge method field
--- 3. Drop egive_egv lookup table
-
--- Update deposit_dep table: remove 'eGive' from dep_Type enum
-ALTER TABLE `deposit_dep` CHANGE COLUMN `dep_Type` `dep_Type` enum('Bank','CreditCard','BankDraft') NOT NULL default 'Bank';
-
--- Update pledge_plg table: remove 'EGIVE' from plg_method enum
-ALTER TABLE `pledge_plg` CHANGE COLUMN `plg_method` `plg_method` enum('CREDITCARD','CHECK','CASH','BANKDRAFT') DEFAULT NULL;
-
--- Drop egive_egv table (no longer used)
-DROP TABLE IF EXISTS `egive_egv`;
