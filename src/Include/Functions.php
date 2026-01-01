f730654108f1c53ec2b90d608abbbf4d2b30c26c
George Dawoud <george@dawouds.com>
2026-01-01 11:45:52 -0800

    removed MySQLquote

--- src/Include/Functions.php
+++ src/Include/Functions.php
@@ -1082,33 +1082,6 @@ function FindMemberClassID()
     return 1; // Should not get here, but if we do get here use the default value.
 }
 
-// Prepare data for entry into MySQL database.
-// This function solves the problem of inserting a NULL value into MySQL since
-// MySQL will not accept 'NULL'.  One drawback is that it is not possible
-// to insert the character string "NULL" because it will be inserted as a MySQL NULL!
-// This will produce a database error if NULL's are not allowed!  Do not use this
-// function if you intend to insert the character string "NULL" into a field.
-function MySQLquote($sfield): string
-{
-    $sfield = trim($sfield);
-
-    if ($sfield == 'NULL') {
-        return 'NULL';
-    } elseif ($sfield == "'NULL'") {
-        return 'NULL';
-    } elseif ($sfield == '') {
-        return 'NULL';
-    } elseif ($sfield == "''") {
-        return 'NULL';
-    } else {
-        if ((mb_substr($sfield, 0, 1) == "'") && (mb_substr($sfield, strlen($sfield) - 1, 1)) == "'") {
-            return $sfield;
-        } else {
-            return "'" . $sfield . "'";
-        }
-    }
-}
-
 //Function to check email
 //From http://www.tienhuis.nl/php-email-address-validation-with-verify-probe
 //Functions checkndsrr and getmxrr are not enabled on windows platforms & therefore are disabled
