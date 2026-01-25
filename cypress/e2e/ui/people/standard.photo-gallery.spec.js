1aa49fff100cd0f3a21908353379f218c43a3e6d
George Dawoud <george@dawouds.com>
2026-01-24 18:03:47 -0800

    Update standard.photo-gallery.spec.js

--- cypress/e2e/ui/people/standard.photo-gallery.spec.js
+++ cypress/e2e/ui/people/standard.photo-gallery.spec.js
@@ -3,39 +3,19 @@
 describe("Photo Gallery Page", () => {
     beforeEach(() => cy.setupStandardSession());
 
-    it("Photo Directory page loads successfully", () => {
+    it("Photo Directory page loads and filters work", () => {
+        // Test page load
         cy.visit("v2/people/photos");
-        cy.contains("Photo Directory").should("exist");
-        cy.get("#photo-grid").should("exist");
-    });
+        cy.contains("Photo Directory");
+        cy.get("#photo-grid");
 
-    it("Filter by classification works", () => {
-        cy.visit("v2/people/photos");
-        cy.get("#classification").select("Member");
-        cy.url().should("include", "classification=");
-        cy.contains("Photo Directory").should("exist");
-    });
-
-    it("Show only people with photos filter works", () => {
+        // Test photosOnly filter with All Classifications (the bug fix)
         cy.visit("v2/people/photos?photosOnly=1");
         cy.get("#photosOnly").should("be.checked");
-        cy.contains("Photo Directory").should("exist");
-    });
+        cy.get("#photo-grid");
 
-    it("Both filters combined work with All Classifications", () => {
-        // This tests the bug fix - photosOnly should work with All Classifications
-        cy.visit("v2/people/photos?photosOnly=1");
-        cy.get("#classification").should("have.value", "");
-        cy.get("#photosOnly").should("be.checked");
-        // Should not show "No people found" if there are people with photos
-        cy.get("#photo-grid").should("exist");
-    });
-
-    it("Reset Filters clears all filters", () => {
-        cy.visit("v2/people/photos?classification=1&photosOnly=1");
+        // Test reset filters
         cy.contains("Reset Filters").click();
-        cy.url().should("include", "/v2/people/photos");
-        cy.url().should("not.include", "classification=");
         cy.url().should("not.include", "photosOnly=");
     });
 });
