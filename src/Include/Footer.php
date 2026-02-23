319aecc19ad2c6a7181a4d41b31df90735855d6c
George Dawoud <george@dawouds.com>
2026-02-22 22:17:40 -0800

    footer cleanup

--- src/Include/Footer.php
+++ src/Include/Footer.php
@@ -19,15 +19,15 @@ $isAdmin = AuthenticationManager::getCurrentUser()->isAdmin();
     </div>
     <strong><?= gettext('Copyright') ?> &copy; <?= SystemService::getCopyrightDate() ?> <a href="https://churchcrm.io" target="_blank" rel="noopener noreferrer"><b>Church</b>CRM</a>.</strong> <?= gettext('All rights reserved') ?>.
     <span class="ml-2">
-        <a href="https://www.facebook.com/getChurchCRM" target="_blank" rel="noopener noreferrer"><?= gettext("Facebook") ?> <i class="fa-brands fa-facebook"></i></a>
+        <a href="https://www.facebook.com/getChurchCRM" target="_blank" rel="noopener noreferrer">Facebook <i class="fa-brands fa-facebook"></i></a>
         &nbsp;|&nbsp;
-        <a href="https://www.instagram.com/getchurchcrm/" target="_blank" rel="noopener noreferrer"><?= gettext("Instagram") ?> <i class="fa-brands fa-instagram"></i></a>
+        <a href="https://www.instagram.com/getchurchcrm/" target="_blank" rel="noopener noreferrer">Instagram <i class="fa-brands fa-instagram"></i></a>
         &nbsp;|&nbsp;
-        <a href="https://x.com/getChurchCRM" target="_blank" rel="noopener noreferrer"><?= gettext("X") ?> <i class="fa-brands fa-x-twitter"></i></a>
+        <a href="https://x.com/getChurchCRM" target="_blank" rel="noopener noreferrer">X <i class="fa-brands fa-x-twitter"></i></a>
         &nbsp;|&nbsp;
-        <a href="https://www.linkedin.com/company/getchurchcrm/" target="_blank" rel="noopener noreferrer"><?= gettext("LinkedIn") ?> <i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://www.linkedin.com/company/getchurchcrm/" target="_blank" rel="noopener noreferrer">LinkedIn <i class="fa-brands fa-linkedin"></i></a>
         &nbsp;|&nbsp;
-        <a href="https://www.youtube.com/@getChurchCRM" target="_blank" rel="noopener noreferrer"><?= gettext("YouTube") ?> <i class="fa-brands fa-youtube"></i></a>
+        <a href="https://www.youtube.com/@getChurchCRM" target="_blank" rel="noopener noreferrer">YouTube <i class="fa-brands fa-youtube"></i></a>
     </span>
 </footer>
 
