1289b6be606fd6c4f88105abac209a6217a84ec7
George Dawoud <george@dawouds.com>
2026-03-23 21:39:40 -0700

    Update standard.members.del.spec.js

--- cypress/e2e/ui/people/standard.members.del.spec.js
+++ cypress/e2e/ui/people/standard.members.del.spec.js
@@ -2,23 +2,46 @@
 
 describe("Standard Family", () => {
     beforeEach(() => cy.setupStandardSession());
-    
+
     it("Delete Person", () => {
-        cy.visit("PersonView.php?PersonID=69");
-        cy.get("#deletePersonBtn").click();
-        cy.get(".bootbox-accept").should("be.visible").click();
-        cy.url().should("contain", "v2/dashboard");
-        cy.visit("PersonView.php?PersonID=69");
-        cy.contains("Not Found: Person");
+        // Create a temporary person with minimum required fields
+        cy.visit("PersonEditor.php");
+        cy.get("#FirstName").type("TempDel");
+        cy.get("#LastName").type("TestPerson");
+        cy.get("#Gender").select("1");
+        cy.get("#Classification").select("1");
+        cy.get(".fab-save").click();
+
+        cy.url().should("contain", "PersonView.php").then((url) => {
+            const personId = new URL(url).searchParams.get("PersonID");
+
+            cy.get("#deletePersonBtn").click();
+            cy.get(".bootbox-accept").should("be.visible").click();
+            cy.url().should("contain", "v2/dashboard");
+
+            cy.visit(`PersonView.php?PersonID=${personId}`);
+            cy.contains("Person not found");
+        });
     });
 
     it("Delete Family", () => {
-        cy.visit("v2/family/7");
-        cy.get("#deleteFamilyBtn").click();
-        cy.url().should("contain", "SelectDelete.php");
-        cy.get("#deleteFamilyAndMembersBtn").click();
-        cy.url().should("contain", "v2/family");
-        cy.visit("v2/family/7");
-        cy.contains("Not Found: Family");
+        // Create a temporary family with minimum required fields
+        cy.visit("FamilyEditor.php");
+        cy.get("#FamilyName").type("TempDelFamily");
+        cy.get('input[name="FirstName1"]').type("TempDel");
+        cy.get('select[name="Classification1"]').select("1", { force: true });
+        cy.get(".fab-save").click();
+
+        cy.location("pathname").should("include", "/v2/family/").then((pathname) => {
+            const familyId = pathname.split("/").pop();
+
+            cy.get("#deleteFamilyBtn").click();
+            cy.url().should("contain", "SelectDelete.php");
+            cy.get("#deleteFamilyAndMembersBtn").click();
+            cy.url().should("contain", "v2/family");
+
+            cy.visit(`v2/family/${familyId}`);
+            cy.contains("Family not found");
+        });
     });
 });
