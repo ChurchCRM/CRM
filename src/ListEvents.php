e4b6698f65271cdfe8387f2b54b3c73f8a5ab4d8
George Dawoud <george@dawouds.com>
2026-04-01 19:43:06 -0700

    RC1 Fixes: Email copying and debug cleanup (#8403)

--- src/ListEvents.php
+++ src/ListEvents.php
@@ -335,7 +335,7 @@ foreach ($allMonths as $mVal) {
     <span class="badge bg-blue-lt ms-auto"><?= (int) $EventYear ?></span>
   </div>
   <div class="card-body p-0" style="overflow: visible;">
-    <div class="table-responsive" style="overflow: visible;">
+    <div style="overflow: visible;">
       <table class="table table-hover table-vcenter mb-0">
         <thead>
           <tr>
