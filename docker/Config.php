7e09f212daa849b5f61ff50094a452cc7b74fc3c
George Dawoud <george@dawouds.com>
2026-05-01 14:35:55 -0700

    cleanup(config): remove outdated bLockURL from docker/Config.php template
    Remove legacy $bLockURL = false; definition from docker Config template.
    This variable is now admin-configurable via SystemConfig.
    
    Also simplify and clarify URL configuration comments to match the improved
    Config.php.example documentation.

--- docker/Config.php
+++ docker/Config.php
@@ -26,32 +26,16 @@ $sDATABASE = 'churchcrm';
 // - the is case sensitive.
 $sRootPath = '';
 
-// Set $bLockURL=TRUE to enforce https access by specifying exactly
-// which URL's your users may use to log into ChurchCRM.
-$bLockURL = false;
-
-// URL[0] is the URL that you prefer most users use when they
-// log in.  These are case sensitive.
+// Primary URL for user access (must include https://, domain, and trailing slash)
 $URL[0] = 'http://localhost/';
-// List as many other URL's as may be needed. Number them sequentially.
-//$URL[1] = 'https://www.mychurch.org/churchcrm/';
-//$URL[2] = 'https://www.mychurch.org:8080/churchcrm/';
-//$URL[3] = 'https://www.mychurch.org/churchcrm/';
-//$URL[4] = 'https://ssl.sharedsslserver.com/mychurch.org/churchcrm/';
-//$URL[5] = 'https://crm.mychurch.org/';
 
-// If you are using a non-standard port number be sure to include the
-// port number in the URL. See example $URL[2]
+// Optional: Alternate URLs (uncomment if using multiple domains/ports)
+// Alternate URLs are only enforced when URL locking is enabled in System Config admin settings
+// Format: https://domain/path/ (with trailing slash)
+//$URL[1] = 'https://www.mychurch.org:8080/churchcrm/';
+//$URL[2] = 'https://crm.mychurch.org/';
 
-// To enforce https access make sure that "https" is specified in all of the
-// the allowable URLs.  Safe exceptions are when you are at the local machine
-// and using "localhost" or "127.0.0.1"
-
-// When using a shared SSL certificate provided by your webhost for https access
-// you may need to add the shared SSL server name as well as your host name to
-// the URL.  See example $URL[4]
-
-// Sets which PHP errors are reported see http://php.net/manual/en/errorfunc.constants.php
+// PHP error reporting level
 error_reporting(E_ERROR);
 
 //
